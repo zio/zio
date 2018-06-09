ed652d680fcd86061f97bd278186e0900b6d42b5
Gaston Tonietti <gaston.tonietti@gmail.com>
2018-06-10 05:14:37 +1000

    Port changes from SZ8

--- core/shared/src/main/scala/scalaz/effect/package.scala
+++ core/shared/src/main/scala/scalaz/effect/package.scala
@@ -2,15 +2,23 @@
 
 package scalaz
 
-package object effect {
+import scala.AnyRef
 
-  implicit class IONothingSyntax[A](val io: IO[Nothing, A]) extends AnyRef {
+package object effect {
+  type Error            = java.lang.Error
+  type Exception        = java.lang.Exception
+  type InternalError    = java.lang.InternalError
+  type OutOfMemoryError = java.lang.OutOfMemoryError
+  type RuntimeException = java.lang.RuntimeException
+  type Throwable        = java.lang.Throwable
+
+  implicit class IOVoidSyntax[A](val io: IO[Void, A]) extends AnyRef {
     def apply[E]: IO[E, A] = io.asInstanceOf[IO[E, A]]
   }
 
   type Task[A] = IO[Throwable, A]
 
-  type Unexceptional[A] = IO[Nothing, A]
+  type Unexceptional[A] = IO[Void, A]
 
   type Canceler     = Throwable => Unit
   type PureCanceler = Throwable => IO[Void, Unit]
