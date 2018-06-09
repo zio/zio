86f9cbf6594c38e7dcef237750cd1d5cb5080862
Gaston Tonietti <gaston.tonietti@gmail.com>
2018-06-10 01:33:12 +1000

    Use `Nothing` instead of (the wrong) `Void`

--- core/jvm/src/main/scala/scalaz/effect/SafeApp.scala
+++ core/jvm/src/main/scala/scalaz/effect/SafeApp.scala
@@ -13,7 +13,7 @@ import scala.concurrent.duration.Duration
  *
  * object MyApp extends SafeApp {
  *
- *   def run(args: List[String]): IO[Void, ExitStatus] =
+ *   def run(args: List[String]): IO[Nothing, ExitStatus] =
  *     myAppLogic.attempt.map(_.fold(_ => 1)(_ => 0)).map(ExitStatus.ExitNow(_))
  *
  *   def myAppLogic: IO[IOException, Unit] =
