bc84ec1659c2daa49d106bf7d939cd7632d5cb04
Gaston Tonietti <gaston.tonietti@gmail.com>
2018-06-11 01:35:50 +1000

    Remove alias for java exceptions

--- core/shared/src/main/scala/scalaz/effect/package.scala
+++ core/shared/src/main/scala/scalaz/effect/package.scala
@@ -5,12 +5,6 @@ package scalaz
 import scala.AnyRef
 
 package object effect {
-  type Error            = java.lang.Error
-  type Exception        = java.lang.Exception
-  type InternalError    = java.lang.InternalError
-  type OutOfMemoryError = java.lang.OutOfMemoryError
-  type RuntimeException = java.lang.RuntimeException
-  type Throwable        = java.lang.Throwable
 
   implicit class IONothingSyntax[A](val io: IO[Nothing, A]) extends AnyRef {
     def apply[E]: IO[E, A] = io.asInstanceOf[IO[E, A]]
