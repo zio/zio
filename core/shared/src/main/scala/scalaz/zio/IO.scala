c57069ceacfb3e99812039fdaaabd89f0744051d
Fabian Page <faebu3007@gmail.com>
2018-06-12 09:47:00 +0200

    replaced lambda with tupled

--- core/shared/src/main/scala/scalaz/zio/IO.scala
+++ core/shared/src/main/scala/scalaz/zio/IO.scala
@@ -818,13 +818,15 @@ object IO {
    * Reduces a list of IO to a single IO, works in parallel.
    */
   def reduceAll[E, A](a: IO[E, A], as: TraversableOnce[IO[E, A]])(f: (A, A) => A): IO[E, A] =
-    as.foldLeft(a) { case (l, r) => l.par(r).map(o => f(o._1, o._2)) }
+    as.foldLeft(a) { (l, r) =>
+      l.par(r).map(f.tupled)
+    }
 
   /**
    * Merges a list of IO to a single IO, works in parallel.
    */
   def mergeAll[E, A, B](in: TraversableOnce[IO[E, A]])(zero: B, f: (B, A) => B): IO[E, B] =
-    in.foldLeft(IO.point[E, B](zero))((acc, a) => acc.par(a).map(o => f(o._1, o._2)))
+    in.foldLeft(IO.point[E, B](zero))((acc, a) => acc.par(a).map(f.tupled))
 
   private final val Never: IO[Nothing, Any] =
     IO.async[Nothing, Any] { (k: (ExitResult[Nothing, Any]) => Unit) =>
