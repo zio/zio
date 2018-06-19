5ec409736c9e7953f2312627aae68f0bd3af726b
Gaston Tonietti <gaston.tonietti@gmail.com>
2018-06-19 23:10:05 +1000

    Initial draft for future conversions API

--- interop/shared/src/main/scala/scalaz/zio/interop/future.scala
+++ interop/shared/src/main/scala/scalaz/zio/interop/future.scala
@@ -4,5 +4,40 @@ package interop
 import scala.concurrent.Future
 
 object future {
-  val dummy = Future.unit
+
+  implicit class IOObjOps(private val ioObj: IO.type) extends AnyVal {
+    def fromFuture[A](ftr: () => Future[A]): IO[Throwable, A] = {
+      val _ = ftr() // avoid warning
+      ???
+    }
+  }
+
+  implicit class IOThrowableOps[A](private val io: IO[Throwable, A]) extends AnyVal {
+    def toFuture: IO[Void, Future[A]] = ???
+  }
+
+  implicit class IOOps[E, A](private val io: IO[E, A]) extends AnyVal {
+    def toFuture(f: E => Throwable): IO[Void, Future[A]] = io.leftMap(f).toFuture
+  }
+
+}
+
+object SampleUsage {
+  import future._
+
+  val ec = scala.concurrent.ExecutionContext.Implicits.global
+
+  def unsafePerformIO[E, A](io: IO[E, A]): A = {
+    val _ = io // avoid warning
+    ???
+  }
+
+  def du: Future[Int] = Future(42)(ec)
+
+  val duHast: IO[Throwable, Int] = IO.fromFuture(du _)
+
+  val duHastMich: IO[Void, Future[Int]] = duHast.toFuture
+
+  val duHastMichGefragt: Future[Int] = unsafePerformIO(duHastMich)
+
 }
