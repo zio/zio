2008be93bfc24dc55db45f4c578935fbb809c374
Gaston Tonietti <gaston.tonietti@gmail.com>
2018-06-20 02:01:41 +1000

    Address PR feedback and implement conversions + tests

--- interop/shared/src/main/scala/scalaz/zio/interop/future.scala
+++ interop/shared/src/main/scala/scalaz/zio/interop/future.scala
@@ -1,43 +1,28 @@
 package scalaz.zio
 package interop
 
-import scala.concurrent.Future
+import scala.concurrent.{ ExecutionContext, Future }
+import scala.util.{ Failure, Success }
 
 object future {
 
   implicit class IOObjOps(private val ioObj: IO.type) extends AnyVal {
-    def fromFuture[A](ftr: () => Future[A]): IO[Throwable, A] = {
-      val _ = ftr() // avoid warning
-      ???
-    }
+    def fromFuture[A](ftr: () => Future[A])(implicit ec: ExecutionContext): IO[Throwable, A] =
+      IO.async { (cb: ExitResult[Throwable, A] => Unit) =>
+        ftr().onComplete {
+          case Success(a) => cb(ExitResult.Completed(a))
+          case Failure(t) => cb(ExitResult.Failed(t))
+        }
+      }
   }
 
   implicit class IOThrowableOps[A](private val io: IO[Throwable, A]) extends AnyVal {
-    def toFuture: IO[Void, Future[A]] = ???
+    def toFuture[E]: IO[E, Future[A]] =
+      io.redeem[E, Future[A]](t => IO.now(Future.failed(t)))(a => IO.now(Future.successful(a)))
   }
 
   implicit class IOOps[E, A](private val io: IO[E, A]) extends AnyVal {
-    def toFuture(f: E => Throwable): IO[Void, Future[A]] = io.leftMap(f).toFuture
+    def toFutureE[E2](f: E => Throwable): IO[E2, Future[A]] = io.leftMap(f).toFuture
   }
-
-}
-
-object SampleUsage {
-  import future._
-
-  val ec = scala.concurrent.ExecutionContext.Implicits.global
-
-  def unsafePerformIO[E, A](io: IO[E, A]): A = {
-    val _ = io // avoid warning
-    ???
-  }
-
-  def du: Future[Int] = Future(42)(ec)
-
-  val duHast: IO[Throwable, Int] = IO.fromFuture(du _)
-
-  val duHastMich: IO[Void, Future[Int]] = duHast.toFuture
-
-  val duHastMichGefragt: Future[Int] = unsafePerformIO(duHastMich)
 
 }
