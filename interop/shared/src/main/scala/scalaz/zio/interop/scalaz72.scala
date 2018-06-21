9c45364fc82844689f536528616d0d9548377328
Gaston Tonietti <gaston.tonietti@gmail.com>
2018-06-22 00:27:17 +1000

    Port moar instances from work done by Sam in `ioeffect`

--- interop/shared/src/main/scala/scalaz/zio/interop/scalaz72.scala
+++ interop/shared/src/main/scala/scalaz/zio/interop/scalaz72.scala
@@ -2,14 +2,27 @@ package scalaz
 package zio
 package interop
 
-object scalaz72 extends IOInstances
+import Tags.Parallel
+import scalaz72.ParIO
+
+object scalaz72 extends IOInstances {
+  type ParIO[E, A] = IO[E, A] @@ Parallel
+}
 
 abstract class IOInstances {
-  // cached for efficiency
-  implicit val taskInstances: MonadError[Task, Throwable] with BindRec[Task] = new IOMonadError[Throwable]
 
-  implicit def ioInstances[E]: MonadError[IO[E, ?], E] with BindRec[IO[E, ?]] with Bifunctor[IO] =
-    new IOMonadError[E] with IOBifunctor
+  // cached for efficiency
+  implicit val taskInstances: MonadError[Task, Throwable] with BindRec[Task] with Plus[Task] =
+    new IOMonadError[Throwable] with IOPlus[Throwable]
+
+  implicit val taskParAp: Applicative[ParIO[Throwable, ?]] = new IOParApplicative[Throwable]
+
+  implicit def ioInstances[E]: MonadError[IO[E, ?], E] with BindRec[IO[E, ?]] with Bifunctor[IO] with Plus[IO[E, ?]] =
+    new IOMonadError[E] with IOPlus[E] with IOBifunctor
+
+  implicit def ioParAp[E]: Applicative[ParIO[E, ?]] = new IOParApplicative[E]
+
+  implicit def ioMonadPlus[E: Monoid]: MonadPlus[IO[E, ?]] with BindRec[IO[E, ?]] = new IOMonadPlus[E]
 }
 
 private class IOMonad[E] extends Monad[IO[E, ?]] with BindRec[IO[E, ?]] {
@@ -25,7 +38,34 @@ private class IOMonadError[E] extends IOMonad[E] with MonadError[IO[E, ?], E] {
   override def raiseError[A](e: E): IO[E, A]                            = IO.fail(e)
 }
 
+// lossy, throws away errors using the "first success" interpretation of Plus
+private trait IOPlus[E] extends Plus[IO[E, ?]] {
+  override def plus[A](a: IO[E, A], b: => IO[E, A]): IO[E, A] = a.catchAll(_ => b)
+}
+
+// annoyingly, we just need M.zero
+private class IOMonadPlus[E](implicit M: Monoid[E]) extends IOMonadError[E] with IOPlus[E] with MonadPlus[IO[E, ?]] {
+  override def empty[A]: IO[E, A] = raiseError(M.zero)
+}
+
 private trait IOBifunctor extends Bifunctor[IO] {
-  def bimap[A, B, C, D](fab: IO[A, B])(f: A => C, g: B => D): IO[C, D] =
+  override def bimap[A, B, C, D](fab: IO[A, B])(f: A => C, g: B => D): IO[C, D] =
     fab.leftMap(f).map(g)
+}
+
+private class IOParApplicative[E] extends Applicative[ParIO[E, ?]] {
+  override def point[A](a: => A): ParIO[E, A] = Tag(IO.point(a))
+  override def ap[A, B](fa: => ParIO[E, A])(f: => ParIO[E, A => B]): ParIO[E, B] = {
+    lazy val fa0: IO[E, A] = Tag.unwrap(fa)
+    Tag(Tag.unwrap(f).flatMap(x => fa0.map(x)))
+  }
+
+  override def map[A, B](fa: ParIO[E, A])(f: A => B): ParIO[E, B] =
+    Tag(Tag.unwrap(fa).map(f))
+
+  override def apply2[A, B, C](
+    fa: => ParIO[E, A],
+    fb: => ParIO[E, B]
+  )(f: (A, B) => C): ParIO[E, C] =
+    Tag(Tag.unwrap(fa).par(Tag.unwrap(fb)).map(f.tupled))
 }
