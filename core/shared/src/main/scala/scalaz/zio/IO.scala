8d6be6d0e4f677ff186ad56351c592543b6c740e
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2018-06-25 20:09:24 +0100

    Meaningful message in spurious exception

--- core/shared/src/main/scala/scalaz/zio/IO.scala
+++ core/shared/src/main/scala/scalaz/zio/IO.scala
@@ -800,7 +800,9 @@ object IO {
   )(release: (A, ExitResult[E, B]) => Infallible[Unit])(use: A => IO[E, B]): IO[E, B] =
     IORef[E, Option[(A, ExitResult[E, B])]](None).flatMap { m =>
       (for {
-        a <- acquire.flatMap(a => m.write[E](Some((a, ExitResult.Terminated(new Throwable)))).const(a)).uninterruptibly
+        a <- acquire
+              .flatMap(a => m.write[E](Some((a, ExitResult.Terminated(new Throwable("Interrupted fiber"))))).const(a))
+              .uninterruptibly
         b <- use(a).run.flatMap(
               r =>
                 m.write[E](Some((a, r))) *> (r match {
