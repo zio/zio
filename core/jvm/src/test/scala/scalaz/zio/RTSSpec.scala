bedf2967646aae3ab7e9b4b5bd8acbf32c7aeba2
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2018-07-20 07:56:45 +0100

    Formatting

--- core/jvm/src/test/scala/scalaz/zio/RTSSpec.scala
+++ core/jvm/src/test/scala/scalaz/zio/RTSSpec.scala
@@ -183,15 +183,14 @@ class RTSSpec(implicit ee: ExecutionEnv) extends Specification with AroundTimeou
       _  <- f1.join
     } yield ()).run) must_=== ExitResult.Terminated(List(InterruptCause1, InterruptCause2))
 
-  def testEvalOfMultipleFailingFinalizers = {
+  def testEvalOfMultipleFailingFinalizers =
     unsafeRun(
       IO.fail[Throwable, Unit](ExampleError)
-            .ensuring(IO.sync(throw InterruptCause1))
-            .ensuring(IO.sync(throw InterruptCause2))
-            .ensuring(IO.sync(throw InterruptCause3))
-            .run
-      ) must_=== ExitResult.Failed(ExampleError, List(InterruptCause1, InterruptCause2, InterruptCause3))
-  }
+        .ensuring(IO.sync(throw InterruptCause1))
+        .ensuring(IO.sync(throw InterruptCause2))
+        .ensuring(IO.sync(throw InterruptCause3))
+        .run
+    ) must_=== ExitResult.Failed(ExampleError, List(InterruptCause1, InterruptCause2, InterruptCause3))
 
   def testEvalOfFailEnsuring = {
     var finalized = false
