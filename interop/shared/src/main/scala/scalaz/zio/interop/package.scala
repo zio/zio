5fc88675c7a34616f9e78ac4af818a739a62524d
Ondra Pelech <ondra.pelech@gmail.com>
2018-08-14 00:04:33 +0200

    Fixes #166 Add interoperability with Java's `AutoCloseable`

--- interop/shared/src/main/scala/scalaz/zio/interop/package.scala
+++ interop/shared/src/main/scala/scalaz/zio/interop/package.scala
@@ -4,4 +4,21 @@ package object interop {
 
   type Task[A] = IO[Throwable, A]
 
+  implicit final class AutoCloseableOps(private val a: AutoCloseable) extends AnyVal {
+
+    /**
+     * Returns an `IO` action which closes this `AutoCloseable` resource.
+     */
+    def closeIO(): IO[Nothing, Unit] = IO.sync(a.close())
+  }
+
+  implicit final class IOAutocloseableOps[E, A <: AutoCloseable](private val io: IO[E, A]) extends AnyVal {
+
+    /**
+     * Like `bracket`, safely wraps a use and release of a resource.
+     * This resource will get automatically closed, because it implements `AutoCloseable`.
+     */
+    def bracketAuto[E1 >: E, B](use: A => IO[E1, B]): IO[E1, B] =
+      io.bracket[E1, B](_.closeIO())(use)
+  }
 }
