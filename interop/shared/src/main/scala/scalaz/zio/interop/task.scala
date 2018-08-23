c17dbf978389e19e8c28fa1c64d1b82f0397f591
Tim Steinbach <tim@nequissimus.com>
2018-08-22 22:14:30 -0400

    Port ioeffect.Task into interop

---
+++ interop/shared/src/main/scala/scalaz/zio/interop/task.scala
@@ -0,0 +1,38 @@
+package scalaz.zio
+package interop
+
+import scala.concurrent.{ ExecutionContext, Future }
+import scala.concurrent.duration.Duration
+import scala.util.{ Failure, Success }
+
+import scalaz.@@
+import scalaz.Tags.Parallel
+
+object Task {
+  type Par[A] = Task[A] @@ Parallel
+
+  final def apply[A](effect: => A): Task[A] = IO.syncThrowable(effect)
+
+  final def now[A](effect: A): Task[A]                                              = IO.now(effect)
+  final def point[A](effect: => A): Task[A]                                         = IO.point(effect)
+  final def sync[A](effect: => A): Task[A]                                          = IO.sync(effect)
+  final def async[A](register: (ExitResult[Throwable, A] => Unit) => Unit): Task[A] = IO.async(register)
+
+  final def fail[A](error: Throwable): Task[A] = IO.fail(error)
+
+  final def unit: Task[Unit]                      = IO.unit
+  final def sleep(duration: Duration): Task[Unit] = IO.sleep(duration)
+
+  final def fromFuture[E, A](io: Task[Future[A]])(ec: ExecutionContext): Task[A] =
+    io.attempt.flatMap { f =>
+      IO.async { (cb: ExitResult[Throwable, A] => Unit) =>
+        f.fold(
+          t => cb(ExitResult.Failed(t)),
+          _.onComplete {
+            case Success(a) => cb(ExitResult.Completed(a))
+            case Failure(t) => cb(ExitResult.Failed(t))
+          }(ec)
+        )
+      }
+    }
+}
