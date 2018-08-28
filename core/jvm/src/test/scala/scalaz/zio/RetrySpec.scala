094a953557ffe5b6dab35eec2e352f3604321f01
nicodelpiano <ndel314@gmail.com>
2018-08-27 21:38:09 -0300

    Added tests for jittered functionality.

--- core/jvm/src/test/scala/scalaz/zio/RetrySpec.scala
+++ core/jvm/src/test/scala/scalaz/zio/RetrySpec.scala
@@ -2,14 +2,15 @@ package scalaz.zio
 
 import scala.concurrent.duration._
 import org.specs2.ScalaCheck
-import org.specs2.specification.core.SpecStructure
 
 class RetrySpec extends AbstractRTSSpec with GenIO with ScalaCheck {
-  def is: SpecStructure = "RetrySpec".title ^ s2"""
+  def is = "RetrySpec".title ^ s2"""
    Retry on failure according to a provided strategy
-       for a given number of times $retryN
-       fixed delay with error predicate $fixedWithErrorPredicate
-      """
+      for a given number of times $retryN
+      for a given number of times with random jitter $retryNJittered
+      fixed delay with error predicate $fixedWithErrorPredicate
+      fixed delay with error predicate and random jitter $fixedWithErrorPredicateJittered
+    """
 
   def retryCollect[E, A, E1 >: E, S](io: IO[E, A],
                                      retry: Schedule[E1, S]): IO[Nothing, (Either[E1, A], List[(Duration, S)])] = {
@@ -24,7 +25,7 @@ class RetrySpec extends AbstractRTSSpec with GenIO with ScalaCheck {
               step =>
                 if (!step.cont) IO.now((Left(err), (step.delay, step.finish()) :: ss))
                 else loop(step.state, (step.delay, step.finish()) :: ss)
-            ),
+          ),
         suc => IO.now((Right(suc), ss))
       )
 
@@ -37,6 +38,12 @@ class RetrySpec extends AbstractRTSSpec with GenIO with ScalaCheck {
     retried must_=== expected
   }
 
+  def retryNJittered = {
+    val retried  = unsafeRun(retryCollect(IO.fail("Error"), Schedule.recurs(5).jittered))
+    val expected = (Left("Error"), List(1, 2, 3, 4, 5).map((Duration.Zero, _)))
+    retried must_=== expected
+  }
+
   def fixedWithErrorPredicate = {
     var i = 0
     val io = IO.sync[Unit](i += 1).flatMap { _ =>
@@ -45,6 +52,19 @@ class RetrySpec extends AbstractRTSSpec with GenIO with ScalaCheck {
     val strategy = Schedule.spaced(200.millis).whileInput[String](_ == "KeepTryingError")
     val retried  = unsafeRun(retryCollect(io, strategy))
     val expected = (Left("GiveUpError"), List(1, 2, 3, 4, 5).map((200.millis, _)))
+    retried must_=== expected
+  }
+
+  def fixedWithErrorPredicateJittered = {
+    var i                  = 0
+    val duration: Duration = 200.millis
+    val io = IO.sync[Unit](i += 1).flatMap { _ =>
+      if (i < 5) IO.fail("KeepTryingError") else IO.fail("GiveUpError")
+    }
+    val strategy         = Schedule.spaced(duration).jittered.whileInput[String](_ == "KeepTryingError")
+    val (error, results) = unsafeRun(retryCollect(io, strategy))
+    val retried          = (error, results.collect { case (dur, count) if dur <= duration => (duration, count) })
+    val expected         = (Left("GiveUpError"), List(1, 2, 3, 4, 5).map((duration, _)))
     retried must_=== expected
   }
 }
