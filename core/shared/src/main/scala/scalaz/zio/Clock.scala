80b05328ca8d2f1e4d9b9c7c002ac4b21afff8c6
Oleksandra Holubitska <aleksandrag@ukr.net>
2018-09-08 12:50:16 +0200

    add clock spec

--- core/shared/src/main/scala/scalaz/zio/Clock.scala
+++ core/shared/src/main/scala/scalaz/zio/Clock.scala
@@ -5,17 +5,17 @@ import java.util.concurrent.TimeUnit
 import scala.concurrent.duration._
 
 trait Clock {
-  def currentTimeMillis(unit: TimeUnit): IO[Nothing, Long]
-  def nanoTime: IO[Nothing, Long]
+  def currentTime(unit: TimeUnit): IO[Nothing, Long]
+  val nanoTime: IO[Nothing, Long]
   def sleep(length: Long, unit: TimeUnit): IO[Nothing, Unit]
 }
 
 object Clock {
   object Live extends Clock {
-    final def currentTimeMillis(unit: TimeUnit): IO[Nothing, Long] =
+    final def currentTime(unit: TimeUnit): IO[Nothing, Long] =
       system.currentTimeMillis.map(l => unit.convert(l, MILLISECONDS))
 
-    final def nanoTime: IO[Nothing, Long] = system.nanoTime
+    final val nanoTime: IO[Nothing, Long] = system.nanoTime
 
     final def sleep(length: Long, unit: TimeUnit): IO[Nothing, Unit] =
       IO.sleep(FiniteDuration(length, unit))
