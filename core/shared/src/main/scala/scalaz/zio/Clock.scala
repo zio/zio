05c370644f8297a6b540f08c1292b8b0300684f7
oleksandra <oleksandra@oleksandra.local>
2018-09-08 12:02:37 +0200

    save changes to clock

--- core/shared/src/main/scala/scalaz/zio/Clock.scala
+++ core/shared/src/main/scala/scalaz/zio/Clock.scala
@@ -4,22 +4,20 @@ import java.util.concurrent.TimeUnit
 import scala.concurrent.duration._
 import scalaz.zio._
 
-
-
 trait Clock {
-  def currentTime(unit: TimeUnit): IO[Nothing, Long]
+  def currentTimeMillis(unit: TimeUnit): IO[Nothing, Long]
   def nanoTime: IO[Nothing, Long]
   def sleep(length: Long, unit: TimeUnit): IO[Nothing, Unit]
 }
 
 object Clock {
-
   object Live extends Clock {
-    def currentTime(unit: TimeUnit): IO[Nothing, Long] = IO.sync(unit.convert(System.currentTimeMillis(), MILLISECONDS))
+    def currentTimeMillis(unit: TimeUnit): IO[Nothing, Long] =
+      IO.sync(unit.convert(System.currentTimeMillis(), MILLISECONDS))
 
     def nanoTime: IO[Nothing, Long] = IO.sync(System.nanoTime())
 
-    def sleep(length: Long, unit: TimeUnit): IO[Nothing, Unit] = IO.sleep(FiniteDuration(length, unit))
+    def sleep(length: Long, unit: TimeUnit): IO[Nothing, Unit] =
+      IO.sleep(FiniteDuration(length, unit))
   }
-
 }
