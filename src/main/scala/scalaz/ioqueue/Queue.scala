b3b9438718b4a91045821def96a1b64fd78e9670
Pierre Ricadat <ghostdogpr@gmail.com>
2018-09-23 11:26:49 +0900

    fmt

--- src/main/scala/scalaz/ioqueue/Queue.scala
+++ src/main/scala/scalaz/ioqueue/Queue.scala
@@ -254,8 +254,8 @@ object Queue {
 
     final case class Surplus[A](
       queue: IQueue[A],
-      putters: IQueue[(Iterable[A], Promise[Nothing, Unit])])
-        extends State[A] {
+      putters: IQueue[(Iterable[A], Promise[Nothing, Unit])]
+    ) extends State[A] {
 
       def size: IO[Nothing, Int] = IO.point {
         queue.size + putters.foldLeft(0) {
