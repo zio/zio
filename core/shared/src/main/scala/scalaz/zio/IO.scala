049c514a096e2c79ba810b2307f47eead173335f
Kai <kaidaxofficial@gmail.com>
2018-09-25 16:31:02 +0100

    Add .uninterruptibly on Ref creation in bracket

--- core/shared/src/main/scala/scalaz/zio/IO.scala
+++ core/shared/src/main/scala/scalaz/zio/IO.scala
@@ -1013,7 +1013,7 @@ object IO {
   final def bracket0[E, A, B](
     acquire: IO[E, A]
   )(release: (A, ExitResult[E, B]) => IO[Nothing, Unit])(use: A => IO[E, B]): IO[E, B] =
-    Ref[Option[(A, ExitResult[E, B])]](None).flatMap { m =>
+    Ref[Option[(A, ExitResult[E, B])]](None).uninterruptibly.flatMap { m =>
       (for {
         r <- (for {
               a <- acquire
@@ -1033,7 +1033,7 @@ object IO {
   final def bracket[E, A, B](
     acquire: IO[E, A]
   )(release: A => IO[Nothing, Unit])(use: A => IO[E, B]): IO[E, B] =
-    Ref[Option[A]](None).flatMap { m =>
+    Ref[Option[A]](None).uninterruptibly.flatMap { m =>
       (for {
         a <- acquire.flatMap(a => m.set(Some(a)).const(a)).uninterruptibly
         b <- use(a)
