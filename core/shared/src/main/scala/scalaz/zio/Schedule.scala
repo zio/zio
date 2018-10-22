47d1cdaaacc60005a33520cdcbc8486d129c3506
John <lgastako@gmail.com>
2018-10-22 13:45:51 -0600

    Fix typo in documentation for `fixed`.

--- core/shared/src/main/scala/scalaz/zio/Schedule.scala
+++ core/shared/src/main/scala/scalaz/zio/Schedule.scala
@@ -657,7 +657,7 @@ object Schedule {
    * A schedule that recurs on a fixed interval. Returns the number of
    * repetitions of the schedule so far.
    *
-   * If the action takes run between updates longer than the interval, then the
+   * If the action run between updates takes longer than the interval, then the
    * action will be run immediately, but re-runs will not "pile up".
    *
    * <pre>
