1e5804830b9d259627d036d01bd256a1f93556e5
Olivier Schultz <schultz.olivier83@gmail.com>
2018-10-31 14:15:23 +0100

    Add timeoutFail

--- core/shared/src/main/scala/scalaz/zio/IO.scala
+++ core/shared/src/main/scala/scalaz/zio/IO.scala
@@ -543,10 +543,16 @@ sealed abstract class IO[+E, +A] extends Serializable { self =>
       )
 
   /**
-   * Times out an action by the specified duration
+   * Times out an action by the specified duration.
    */
   final def timeout(d: Duration): IO[E, Option[A]] = timeout0[Option[A]](None)(Some(_))(d)
 
+  /**
+   * Flattens a nested action with a specified duration.
+   */
+  final def timeoutFail[E1 >: E](e: E1)(d: Duration): IO[E1, A] =
+    IO.flatten(timeout0[IO[E1, A]](IO.fail(e))(IO.now(_))(d))
+  
   /**
    * Returns a new action that executes this one and times the execution.
    */
