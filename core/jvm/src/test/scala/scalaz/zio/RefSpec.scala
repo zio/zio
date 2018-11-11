e807da0b8d23fc6480773cdb52f32f3419f89d6b
wi101 <wiemzin@gmail.com>
2018-11-11 13:52:07 +0100

    implemented updateSome for RefM and added test case in RefMSpec

--- core/jvm/src/test/scala/scalaz/zio/RefSpec.scala
+++ core/jvm/src/test/scala/scalaz/zio/RefSpec.scala
@@ -7,8 +7,8 @@ class RefSpec extends AbstractRTSSpec {
       `read` returns the current value.                                                        $e1
       `write` puts the new value correctly.                                                    $e2
       `update` changes the value and returns the updated value.                                $e3
-      `modify` changes the value and returns another value computed from the modification.     $e4
-      `updateSome` changes a given type State in some cases and returns the updated value.     $e5
+      `updateSome` changes a given type State in some cases and returns the updated value.     $e4
+      `modify` changes the value and returns another value computed from the modification.     $e5
     """
 
   val (current, update) = ("value", "new value")
@@ -38,16 +38,7 @@ class RefSpec extends AbstractRTSSpec {
       } yield value must beTheSameAs(update)
     )
 
-  def e4 =
-    unsafeRun(
-      for {
-        ref   <- Ref(current)
-        r     <- ref.modify[String](_ => ("hello", update))
-        value <- ref.get
-      } yield (r must beTheSameAs("hello")) and (value must beTheSameAs(update))
-    )
-
-  def e5 = {
+  def e4 = {
     sealed trait State
     case object Active  extends State
     case object Changed extends State
@@ -66,4 +57,14 @@ class RefSpec extends AbstractRTSSpec {
         (value1 must beTheSameAs(Changed)) and (value2 must beTheSameAs(Changed)) and (value3 must beTheSameAs(Closed))
     )
   }
+
+  def e5 =
+    unsafeRun(
+      for {
+        ref   <- Ref(current)
+        r     <- ref.modify[String](_ => ("hello", update))
+        value <- ref.get
+      } yield (r must beTheSameAs("hello")) and (value must beTheSameAs(update))
+    )
+
 }
