cd17fd2d44ff52b6692efdd4cdb3e133b7381b59
J. Eduardo V. Morango <edvmorango@gmail.com>
2018-11-10 23:40:05 -0200

    Changed implementation of `releaseN` by #357 version and minor changes

--- core/shared/src/main/scala/scalaz/zio/Semaphore.scala
+++ core/shared/src/main/scala/scalaz/zio/Semaphore.scala
@@ -21,8 +21,7 @@ final class Semaphore private (private val state: Ref[State]) extends Serializab
 
   final def release: IO[Nothing, Unit] = releaseN(1)
 
-  final def withPermit[E, A](task: IO[E, A]): IO[E, A] =
-    IO.bracket0[E, AcquireTasks, A](prepare(1L))(cleanup) { _._1 *> task }
+  final def withPermit[E, A](task: IO[E, A]): IO[E, A] = prepare(1L).bracket(_._2)(_._1 *> task)
 
   /**
    * Ported from @mpilquist work in cats-effects (https://github.com/typelevel/cats-effect/pull/403)
@@ -55,30 +54,29 @@ final class Semaphore private (private val state: Ref[State]) extends Serializab
 
   final private def cleanup[E, A](ops: AcquireTasks, res: ExitResult[E, A]): IO[Nothing, Unit] =
     res match {
-      case ExitResult.Failed(_) => ops._2
-      case _                    => IO.unit
+      case ExitResult.Failed(c) if c.isInterrupted => ops._2
+      case _                                       => IO.unit
     }
 
   final def releaseN(toRelease: Long): IO[Nothing, Unit] = {
-    @tailrec def loop(
-      n: Long,
-      io: IO[Nothing, Boolean],
-      st: Option[(Entry, IQueue[Entry])]
-    ): (IO[Nothing, Boolean], State) = (n, st) match {
-      case (_, None)                          => io -> Right(n)
-      case (_, Some(((p2, n2), q))) if n < n2 => io -> Left(q :+ (p2 -> (n2 - n)))
-      case (_, Some(((p2, n2), q)))           => loop(n - n2, io *> p2.complete(()), q.dequeueOption)
+
+    @tailrec def loop(n: Long, state: State, acc: IO[Nothing, Unit]): (IO[Nothing, Unit], State) = state match {
+      case Right(m) => acc -> Right(n + m)
+      case Left(q) =>
+        q.dequeueOption match {
+          case None => acc -> Right(n)
+          case Some(((p, m), q)) =>
+            if (n > m)
+              loop(n - m, Left(q), acc <* p.complete(()))
+            else if (n == m)
+              (acc <* p.complete(())) -> Left(q)
+            else
+              acc -> Left((p -> (m - n)) +: q)
+        }
     }
 
-    val acquire: (Promise[Nothing, Unit], State) => (IO[Nothing, Boolean], State) = {
-      case (p, Right(n))             => p.complete(()) -> Right(n + toRelease)
-      case (p, Left(q)) if q.isEmpty => p.complete(()) -> Right(toRelease)
-      case (p, Left(q))              => loop(toRelease, p.complete(()), q.dequeueOption)
-    }
+    IO.flatten(assertNonNegative(toRelease) *> state.modify(loop(toRelease, _, IO.unit))).uninterruptibly
 
-    val release: (Boolean, Promise[Nothing, Unit]) => IO[Nothing, Unit] = (_, _) => IO.unit
-
-    assertNonNegative(toRelease) *> Promise.bracket[Nothing, State, Unit, Boolean](state)(acquire)(release)
   }
 
   private final def count_(state: State): Long = state match {
