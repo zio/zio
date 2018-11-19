344d02ff908388ea333079e9908308ba7aea014d
John A. De Goes <john@degoes.net>
2018-11-19 08:48:26 -0800

    turn timeouts into failures

--- core/jvm/src/test/scala/scalaz/zio/AbstractRTSSpec.scala
+++ core/jvm/src/test/scala/scalaz/zio/AbstractRTSSpec.scala
@@ -2,11 +2,19 @@ package scalaz.zio
 
 import scala.concurrent.duration._
 import org.specs2.Specification
+import org.specs2.specification.AroundEach
+import org.specs2.execute.{ AsResult, Failure, Result, Skipped }
 import org.specs2.specification.core.Fragments
 import scalaz.zio.ExitResult.Cause
 
-trait AbstractRTSSpec extends Specification with RTS {
+trait AbstractRTSSpec extends Specification with RTS with AroundEach {
   override def defaultHandler: Cause[Any] => IO[Nothing, Unit] = _ => IO.unit
+
+  def around[R: AsResult](r: => R): Result =
+    AsResult.safely(r) match {
+      case Skipped(m, e) if m contains "TIMEOUT" => Failure(m, e)
+      case other                                 => other
+    }
 
   lazy val ShutdownRTS =
     step {
