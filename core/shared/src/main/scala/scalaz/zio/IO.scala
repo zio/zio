3b28fae801b99e557022034102ff2fa770ceb98b
John A. De Goes <john@degoes.net>
2018-11-19 21:30:59 -0800

    fmt

--- core/shared/src/main/scala/scalaz/zio/IO.scala
+++ core/shared/src/main/scala/scalaz/zio/IO.scala
@@ -175,8 +175,7 @@ sealed abstract class IO[+E, +A] extends Serializable { self =>
       done: Promise[E2, C]
     )(res: ExitResult[E0, A]): IO[Nothing, _] =
       race
-        .modify((b: Boolean) =>
-          (if (b) IO.unit else f(res, loser).to(done).void) -> true)
+        .modify((b: Boolean) => (if (b) IO.unit else f(res, loser).to(done).void) -> true)
         .flatMap(identity(_))
 
     for {
