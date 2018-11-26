9a51633194db7722805730dcfaa1185c6572a8c6
Francois @fanf42 Armand <francois.armand@normation.com>
2018-11-26 16:11:59 +0100

    Correct comments

--- core/jvm/src/test/scala/scalaz/zio/RepeatSpec.scala
+++ core/jvm/src/test/scala/scalaz/zio/RepeatSpec.scala
@@ -20,7 +20,7 @@ class RepeatSpec(implicit ee: org.specs2.concurrent.ExecutionEnv) extends Abstra
     } yield s
 
   /*
-   * A repeat with a negative number of times should not execute the action at all
+   * A repeat with a negative number of times should not repeat the action at all
    */
   def repeatNeg = {
     val repeated = unsafeRun(repeat(-5))
@@ -28,7 +28,7 @@ class RepeatSpec(implicit ee: org.specs2.concurrent.ExecutionEnv) extends Abstra
   }
 
   /*
-   * A repeat with 0 number of times should not execute the action at all
+   * A repeat with 0 number of times should not repeat the action at all
    */
   def repeat0 = {
     val n        = 0
