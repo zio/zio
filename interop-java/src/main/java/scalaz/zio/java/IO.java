1ae076c11342759af3a71496dbc923fc11b340cb
xplosunn <gi.ciberon@gmail.com>
2018-11-28 00:18:17 +0000

    Add more IO functions; Converted Nothing to Void

--- interop-java/src/main/java/scalaz/zio/java/IO.java
+++ interop-java/src/main/java/scalaz/zio/java/IO.java
@@ -1,5 +1,6 @@
 package scalaz.zio.java;
 
+import scala.runtime.BoxedUnit;
 import scalaz.zio.ExitResult;
 import scalaz.zio.Fiber;
 
@@ -27,9 +28,21 @@ public class IO<E, A> {
         return new IO<>(delegate.flatMap(f.andThen(io -> io.delegate)::apply));
     }
 
-    // TODO Fiber
-    // final def fork: IO[Nothing, Fiber[E, A]]
-    // final def fork0(handler: Cause[Any] => IO[Nothing, Unit]): IO[Nothing, Fiber[E, A]]
+    // TODO do we want a separate java Fiber?
+    public IO<Void, Fiber<E, A>> fork() {
+        return new IO<>(delegate.fork()).leftMap(nothing -> null);
+    }
+
+    public IO<Void, Fiber<E, A>> fork0(Function<ExitResult.Cause<Object>, IO<Void, Void>> f) {
+        return new IO<>(
+                delegate.fork0(f.andThen(io ->
+                        io.delegate.<Object, BoxedUnit>bimap(
+                                _void -> _void,
+                                _void -> BoxedUnit.UNIT
+                        ))::apply
+                )
+        ).bimap(nothing -> null, nothing -> null);
+    }
 
     public <E1 extends E, B, C> IO<E1, C> parWith(IO<E1, B> that, BiFunction<A, B, C> f) {
         return new IO<>(delegate.parWith(that.delegate, f::apply));
@@ -43,6 +56,7 @@ public class IO<E, A> {
         return new IO<>(delegate.race(that.delegate));
     }
 
+    // TODO should we use the Scala Either? Maybe create an Either for Java?
     public <B> IO<E, scala.util.Either<A, B>> raceBoth(IO<E, B> that) {
         return new IO<>(delegate.raceBoth(that.delegate));
     }
@@ -78,14 +92,47 @@ public class IO<E, A> {
         return new IO<>(delegate.redeem(err.andThen(io -> io.delegate)::apply, succ.andThen(io -> io.delegate)::apply));
     }
 
-    // TODO other transformation/composition methods...
-
-    public static <E, A> IO<E, A> now(A a) {
-        return new IO(scalaz.zio.IO$.MODULE$.now(a));
+    public <E2, B> IO<E2, B> redeem0(Function<ExitResult.Cause<E>, IO<E2, B>> err, Function<A, IO<E2, B>> succ) {
+      return new IO<>(delegate.redeem0(err.andThen(io -> io.delegate)::apply, succ.andThen(io -> io.delegate)::apply));
     }
 
-    public static <E, A> IO<E, A> point(Supplier<A> a) {
-        return new IO(scalaz.zio.IO$.MODULE$.point(a::get));
+    public <E2, B> IO<E2, B> redeemPure(Function<E, B> err, Function<A, B> succ) {
+        return new IO<>(delegate.redeemPure(err::apply, succ::apply));
+    }
+
+    public IO<Void, scala.util.Either<E, A>> attempt() {
+        return new IO<>(delegate.attempt()).leftMap(nothing -> null);
+    }
+
+    public <B> IO<E, B> bracket(Function<A, scalaz.zio.IO<Object, BoxedUnit>> release, Function<A, scalaz.zio.IO<E, B>> use) {
+        return new IO<>(delegate.bracket(release::apply, use::apply));
+    }
+
+    // TODO find a better name for all method names ending in "0" and "_"
+    public <B> IO<E, B> bracket0(BiFunction<A, ExitResult<E, B>, IO<Void, BoxedUnit>> release,
+                                 Function<A, IO<E, B>> use) {
+        return new IO<>(delegate.bracket0(
+                release.andThen(io -> io.leftMap(nothing -> null).delegate)::apply,
+                use.andThen(io -> io.delegate)::apply
+        ));
+    }
+
+    public <B> IO<E, B> bracket_(IO<Void, BoxedUnit> release, IO<E, B> use) {
+        return new IO<>(delegate.bracket_(release.leftMap(nothing -> null).delegate, use.delegate));
+    }
+
+    public IO<E, A> ensuring(IO<Void, BoxedUnit> finalizer) {
+        return new IO<>(delegate.ensuring(finalizer.leftMap(nothing -> null).delegate));
+    }
+
+    // TODO other transformation/composition methods...
+
+    public static <A> IO<Void, A> now(A a) {
+        return new IO<>(scalaz.zio.IO$.MODULE$.now(a)).leftMap(nothing -> null);
+    }
+
+    public static <A> IO<Void, A> point(Supplier<A> a) {
+        return new IO<>(scalaz.zio.IO$.MODULE$.point(a::get)).leftMap(nothing -> null);
     }
 
     public static <E, A> IO<E, A> fail(E error) {
