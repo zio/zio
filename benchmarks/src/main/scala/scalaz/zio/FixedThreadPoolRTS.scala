4975178dc084c19d565831a7c0e0607cbd87a8f1
Pierre Ricadat <ghostdogpr@gmail.com>
2018-12-06 22:36:55 +0900

    Renaming

--- benchmarks/src/main/scala/scalaz/zio/FixedThreadPoolRTS.scala
+++ benchmarks/src/main/scala/scalaz/zio/FixedThreadPoolRTS.scala
@@ -2,18 +2,17 @@ package scalaz.zio
 import java.util.concurrent.{ ExecutorService, LinkedBlockingQueue, ThreadPoolExecutor, TimeUnit }
 import scalaz.zio.RTS.NamedThreadFactory
 
-class CustomRTS(threadPoolSize: Int) extends RTS {
+class FixedThreadPoolRTS(threadPoolSize: Int) extends RTS {
 
   override val threadPool: ExecutorService = {
-    val corePoolSize  = threadPoolSize
     val keepAliveTime = 1000L
     val timeUnit      = TimeUnit.MILLISECONDS
     val workQueue     = new LinkedBlockingQueue[Runnable]()
     val threadFactory = new NamedThreadFactory("zio", true)
 
     val threadPool = new ThreadPoolExecutor(
-      corePoolSize,
-      corePoolSize,
+      threadPoolSize,
+      threadPoolSize,
       keepAliveTime,
       timeUnit,
       workQueue,
