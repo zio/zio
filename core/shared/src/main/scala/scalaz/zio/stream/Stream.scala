8c46dfd32ae55565d1bcbb7f5b5ab495d56a94c9
Pierre Ricadat <ghostdogpr@gmail.com>
2018-12-08 20:17:02 +0900

    Stream.fromQueue should end when the Queue is shutdown

--- core/shared/src/main/scala/scalaz/zio/stream/Stream.scala
+++ core/shared/src/main/scala/scalaz/zio/stream/Stream.scala
@@ -653,7 +653,11 @@ object Stream {
    * Constructs an infinite stream from a `Queue`.
    */
   final def fromQueue[A](queue: Queue[A]): Stream[Nothing, A] =
-    unfoldM(())(_ => queue.take.map(a => Some((a, ()))))
+    unfoldM(())(
+      _ =>
+        queue.take
+          .redeem0(cause => if (cause.interrupted) IO.now(None) else IO.fail0(cause), a => IO.now(Some((a, ()))))
+    )
 
   /**
    * Constructs a stream from effectful state. This method should not be used
