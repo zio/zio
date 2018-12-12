7cf5eea48a30f3b65581dcb200a143ab7ae7c3ea
Artem Pyanykh <artem.pyanykh@gmail.com>
2018-12-12 17:47:56 +0300

    RingBuffer.build returns pow2 or arb size RingBuffer dep. on capacity

--- core/jvm/src/main/scala/scalaz/zio/internal/impls/RingBuffer.scala
+++ core/jvm/src/main/scala/scalaz/zio/internal/impls/RingBuffer.scala
@@ -3,5 +3,18 @@ package scalaz.zio.internal.impls
 import scalaz.zio.internal.MutableConcurrentQueue
 
 object RingBuffer {
-  def build[A](requiredCapacity: Int): MutableConcurrentQueue[A] = new RingBufferPow2(requiredCapacity)
+  def build[A](capacity: Int): MutableConcurrentQueue[A] = {
+    val pow2Cap = nextPow2(capacity)
+    if (capacity == pow2Cap) new RingBufferPow2(capacity)
+    else new RingBufferArb(capacity)
+  }
+
+  /*
+   * Used only once during queue creation. Doesn't need to be
+   * performant or anything.
+   */
+  def nextPow2(n: Int): Int = {
+    val nextPow = (Math.log(n.toDouble) / Math.log(2.0)).ceil.toInt
+    Math.pow(2.0, nextPow.toDouble).toInt.max(2)
+  }
 }
--- core/jvm/src/main/scala/scalaz/zio/internal/impls/RingBufferPow2.scala
+++ core/jvm/src/main/scala/scalaz/zio/internal/impls/RingBufferPow2.scala
@@ -1,17 +1,6 @@
 package scalaz.zio.internal.impls
 
-class RingBufferPow2[A](val desiredCapacity: Int) extends RingBufferBase[A](RingBufferPow2.nextPow2(desiredCapacity)) {
+class RingBufferPow2[A](val desiredCapacity: Int) extends RingBufferBase[A](RingBuffer.nextPow2(desiredCapacity)) {
   protected final def posToIdx(pos: Long, capacity: Int): Int =
     (pos & (capacity - 1).toLong).toInt //(pos % capacity.toLong).toInt //(pos & mask).toInt
-}
-
-object RingBufferPow2 {
-  /*
-   * Used only once during queue creation. Doesn't need to be
-   * performant or anything.
-   */
-  def nextPow2(n: Int): Int = {
-    val nextPow = (Math.log(n.toDouble) / Math.log(2.0)).ceil.toInt
-    Math.pow(2.0, nextPow.toDouble).toInt.max(2)
-  }
 }
