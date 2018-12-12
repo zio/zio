5d0fd295f8f8b37b1a26edb679d938cc458d76d6
Artem Pyanykh <artem.pyanykh@gmail.com>
2018-12-12 17:59:22 +0300

    Cleanup: RingBufferBase -> RingBuffer, RingBuffer.build return RiBu

--- core/jvm/src/main/scala/scalaz/zio/internal/impls/RingBufferPow2.scala
+++ core/jvm/src/main/scala/scalaz/zio/internal/impls/RingBufferPow2.scala
@@ -1,6 +1,6 @@
 package scalaz.zio.internal.impls
 
-class RingBufferPow2[A](val desiredCapacity: Int) extends RingBufferBase[A](RingBuffer.nextPow2(desiredCapacity)) {
+class RingBufferPow2[A](val desiredCapacity: Int) extends RingBuffer[A](RingBuffer.nextPow2(desiredCapacity)) {
   protected final def posToIdx(pos: Long, capacity: Int): Int =
-    (pos & (capacity - 1).toLong).toInt //(pos % capacity.toLong).toInt //(pos & mask).toInt
+    (pos & (capacity - 1).toLong).toInt
 }
