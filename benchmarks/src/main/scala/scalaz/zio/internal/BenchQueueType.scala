07d0592995e882f472f0b9a27809d837bd286e16
Artem Pyanykh <artem.pyanykh@gmail.com>
2018-12-14 21:20:26 +0300

    Add sequential benchmark for OneElQueue

--- benchmarks/src/main/scala/scalaz/zio/internal/BenchQueueType.scala
+++ benchmarks/src/main/scala/scalaz/zio/internal/BenchQueueType.scala
@@ -4,19 +4,21 @@ sealed abstract class BenchQueueType(val name: String) extends Product with Seri
 
 object BenchQueueType {
   def lookup(tpe: String): Option[BenchQueueType] = tpe match {
-    case RingBufferPow2Type.name => Some(RingBufferPow2Type)
-    case RingBufferArbType.name  => Some(RingBufferArbType)
-    case LinkedQueueType.name    => Some(LinkedQueueType)
-    case JucBlockingType.name    => Some(JucBlockingType)
-    case JCToolsType.name        => Some(JCToolsType)
-    case NotThreadSafeType.name  => Some(NotThreadSafeType)
-    case _                       => None
+    case RingBufferPow2Type.name  => Some(RingBufferPow2Type)
+    case RingBufferArbType.name   => Some(RingBufferArbType)
+    case OneElementQueueType.name => Some(OneElementQueueType)
+    case LinkedQueueType.name     => Some(LinkedQueueType)
+    case JucBlockingType.name     => Some(JucBlockingType)
+    case JCToolsType.name         => Some(JCToolsType)
+    case NotThreadSafeType.name   => Some(NotThreadSafeType)
+    case _                        => None
   }
 }
 
-case object RingBufferPow2Type extends BenchQueueType("RingBufferPow2")
-case object RingBufferArbType  extends BenchQueueType("RingBufferArb")
-case object LinkedQueueType    extends BenchQueueType("LinkedQueue")
-case object JucBlockingType    extends BenchQueueType("JucBlocking")
-case object JCToolsType        extends BenchQueueType("JCTools")
-case object NotThreadSafeType  extends BenchQueueType("NotThreadSafe")
+case object RingBufferPow2Type  extends BenchQueueType("RingBufferPow2")
+case object RingBufferArbType   extends BenchQueueType("RingBufferArb")
+case object OneElementQueueType extends BenchQueueType("OneElementQueue")
+case object LinkedQueueType     extends BenchQueueType("LinkedQueue")
+case object JucBlockingType     extends BenchQueueType("JucBlocking")
+case object JCToolsType         extends BenchQueueType("JCTools")
+case object NotThreadSafeType   extends BenchQueueType("NotThreadSafe")
