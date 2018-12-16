7b664aa040dc2452931ea154e29fa54963071b4e
Artem Pyanykh <artem.pyanykh@gmail.com>
2018-12-16 18:21:23 +0300

    Add an assert re: capacity to RingBufferArb

--- core/jvm/src/main/scala/scalaz/zio/internal/impls/RingBufferPow2.scala
+++ core/jvm/src/main/scala/scalaz/zio/internal/impls/RingBufferPow2.scala
@@ -8,6 +8,7 @@ class RingBufferPow2[A](val requestedCapacity: Int) extends RingBuffer[A](RingBu
 object RingBufferPow2 {
   def apply[A](requestedCapacity: Int): RingBufferPow2[A] = {
     assert(requestedCapacity > 0)
+
     new RingBufferPow2(requestedCapacity)
   }
 }
