26c774d6aa65d469c4322ce06e501f0d68c303b8
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2018-12-16 08:41:52 +0000

    Do not lose information

--- core/shared/src/main/scala/scalaz/zio/IO.scala
+++ core/shared/src/main/scala/scalaz/zio/IO.scala
@@ -753,7 +753,7 @@ sealed abstract class IO[+E, +A] extends Serializable { self =>
    * Keep or break a promise based on the result of this action.
    */
   final def to[E1 >: E, A1 >: A](p: Promise[E1, A1]): IO[Nothing, Boolean] =
-    self.redeem(p.error, p.complete).onTermination(_ => p.interrupt)
+    self.redeem(p.error, p.complete).onTermination(cause => p.done(IO.fail0(cause)))
 
   /**
    * An integer that identifies the term in the `IO` sum type to which this
