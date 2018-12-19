3cdd4fd870b885dc6d3f973b93a0157bb884bb16
Ondra Pelech <ondra.pelech@gmail.com>
2018-12-19 22:22:45 +0100

    javaconcurrentSpec

--- interop-javaconcurrent/shared/src/main/scala/scalaz/zio/interop/javaconcurrent.scala
+++ interop-javaconcurrent/shared/src/main/scala/scalaz/zio/interop/javaconcurrent.scala
@@ -1,8 +1,8 @@
 package scalaz.zio.interop
 
-import java.util.concurrent.{ CompletionStage, Future }
+import java.util.concurrent.{ CompletableFuture, CompletionStage, Future }
 
-import scalaz.zio.IO
+import scalaz.zio.{ ExitResult, Fiber, IO }
 
 object javaconcurrent {
 
@@ -28,9 +28,13 @@ object javaconcurrent {
       }
 
     private def unsafeFutureJavaToIO[A](future: Future[A]): IO[Exception, A] =
-      IO.syncException(future.get())
+      if (future.isDone) {
+        IO.syncException(future.get())
+      } else {
+        IO.unyielding(IO.syncException(future.get()))
+      }
 
-    def fromFutureJava[A, E >: Exception](futureIo: IO[E, Future[A]]): IO[E, A] =
+    def fromFutureJavaIO[A, E >: Exception](futureIo: IO[E, Future[A]]): IO[E, A] =
       futureIo.flatMap(unsafeFutureJavaToIO)
 
     def fromFutureJava[A](future: () => Future[A]): IO[Exception, A] =
@@ -38,4 +42,54 @@ object javaconcurrent {
         unsafeFutureJavaToIO(future())
       }
   }
+
+  implicit class FiberObjOps(private val fiberObj: Fiber.type) extends AnyVal {
+
+    def fromFutureJava[A](_ftr: () => Future[A]): Fiber[Throwable, A] = {
+
+      lazy val ftr = _ftr()
+
+      new Fiber[Throwable, A] {
+
+        def observe: IO[Nothing, ExitResult[Throwable, A]] =
+          IO.fromFutureJava(() => ftr).redeemPure(ExitResult.checked, ExitResult.succeeded)
+
+        def poll: IO[Nothing, Option[ExitResult[Throwable, A]]] =
+          IO.suspend {
+            if (ftr.isDone) {
+              IO.syncException(ftr.get())
+                .redeemPure(ExitResult.checked, ExitResult.succeeded)
+                .map(Some(_))
+            } else {
+              IO.now(None)
+            }
+          }
+
+        def interrupt: IO[Nothing, ExitResult[Throwable, A]] =
+          join.redeemPure(ExitResult.checked, ExitResult.succeeded)
+      }
+    }
+  }
+
+  /**
+   * CompletableFuture#failedFuture(Throwable) available only since Java 9
+   */
+  object CompletableFuture_ {
+    def failedFuture[A](e: Throwable): CompletableFuture[A] = {
+      val f = new CompletableFuture[A]
+      f.completeExceptionally(e)
+      f
+    }
+  }
+
+  implicit class IOThrowableOps[A](private val io: IO[Throwable, A]) extends AnyVal {
+    def toCompletableFuture: IO[Nothing, CompletableFuture[A]] =
+      io.redeemPure(CompletableFuture_.failedFuture, CompletableFuture.completedFuture[A])
+  }
+
+  implicit class IOOps[E, A](private val io: IO[E, A]) extends AnyVal {
+    def toCompletableFutureE(f: E => Throwable): IO[Nothing, CompletableFuture[A]] =
+      io.leftMap(f).toCompletableFuture
+  }
+
 }
