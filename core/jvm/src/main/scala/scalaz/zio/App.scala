60857fe55bc3fbaaba0bed42fa3e91f8c01a612e
Oleksandra Holubitska <aleksandrag@ukr.net>
2018-12-22 20:24:22 +0200

    Ensure all methods that can be final are final

--- core/jvm/src/main/scala/scalaz/zio/App.scala
+++ core/jvm/src/main/scala/scalaz/zio/App.scala
@@ -13,7 +13,7 @@ import scalaz.zio.duration.Duration
  *
  * object MyApp extends App {
  *
- *   def run(args: List[String]): IO[Nothing, ExitStatus] =
+ *  final def run(args: List[String]): IO[Nothing, ExitStatus] =
  *     myAppLogic.attempt.map(_.fold(_ => 1, _ => 0)).map(ExitStatus.ExitNow(_))
  *
  *   def myAppLogic: IO[IOException, Unit] =
