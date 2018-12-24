9303b1ca773042e7598f0f587d55b85ef7b3b00d
Oleksandra Holubitska <aleksandrag@ukr.net>
2018-12-24 15:46:41 +0200

    Ensure all methods that can be final are final v2

--- core/js/src/main/scala/scalaz/zio/App.scala
+++ core/js/src/main/scala/scalaz/zio/App.scala
@@ -6,5 +6,5 @@ trait App {
    * The main function of the application, which will be passed the command-line
    * arguments to the program.
    */
-  final def run(args: List[String]): IO[Nothing, Unit]
+  def run(args: List[String]): IO[Nothing, Unit]
 }
