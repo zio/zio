b5a5f5bc6df03775ab392ded73e3ed42739e5912
Dejan Mijic <dmijic@acm.org>
2018-12-26 00:04:04 +0100

    Add IO/Coeval conversions

--- interop-monix/jvm/src/test/scala/scalaz/zio/interop/MonixSpec.scala
+++ interop-monix/jvm/src/test/scala/scalaz/zio/interop/MonixSpec.scala
@@ -17,8 +17,17 @@ class MonixSpec(implicit ee: ExecutionEnv) extends AbstractRTSSpec {
 
   `IO.toTask` must
     produce a successful `IO` of `Task`. $toTaskAlwaysSucceeds
-    returns a `Task` that fails if `IO` fails. $propagateIOFailure
-    returns a `Task` that procudes the value from `IO`. $propagateIOResult
+    returns a `Task` that fails if `IO` fails. $propagateFailureToTask
+    returns a `Task` that procudes the value from `IO`. $propagateResultToTask
+
+  `IO.fromCoeval` must
+    return an `IO` that fails if `Coeval` failed. $propagateCoevalFailure
+    return an `IO` that produces the value from `Coeval`. $propagateCoevalResult
+
+  `IO.toCoeval` must
+    produce a successful `IO` of `Coeval`. $toTaskAlwaysSucceeds
+    returns a `Coeval` that fails if `IO` fails. $propagateFailureToCoeval
+    returns a `Coeval` that produces the value from `IO`. $propagateResultToCoeval
   """
 
   implicit val scheduler = Scheduler(ee.executionContext)
@@ -44,17 +53,52 @@ class MonixSpec(implicit ee: ExecutionEnv) extends AbstractRTSSpec {
     unsafeRun(task) must beAnInstanceOf[eval.Task[Unit]]
   }
 
-  def propagateIOFailure = {
+  def propagateFailureToTask = {
     val ex   = new Exception
     val task = IO.fail(ex).toTask
 
     unsafeRun(task).runSyncStep must throwAn(ex)
   }
 
-  def propagateIOResult = {
+  def propagateResultToTask = {
     val value = 10
     val task  = IO.now(value).toTask
 
     unsafeRun(task).runSyncStep must beRight(10)
+  }
+
+  def propagateCoevalFailure = {
+    val error  = new Exception
+    val coeval = eval.Coeval.raiseError[Int](error)
+    val io     = IO.fromCoeval(coeval)
+
+    unsafeRun(io) must throwAn(FiberFailure(Checked(error)))
+  }
+
+  def propagateCoevalResult = {
+    val value  = 10
+    val coeval = eval.Coeval(value)
+    val io     = IO.fromCoeval(coeval)
+
+    unsafeRun(io) === value
+  }
+
+  def toCoevalAlwaysSucceeds = {
+    val coeval = IO.fail(new Exception).toCoeval
+    unsafeRun(coeval) must beAnInstanceOf[eval.Coeval[Unit]]
+  }
+
+  def propagateFailureToCoeval = {
+    val ex     = new Exception
+    val coeval = IO.fail(ex).toCoeval
+
+    unsafeRun(coeval).runTry must beFailedTry(ex)
+  }
+
+  def propagateResultToCoeval = {
+    val value  = 10
+    val coeval = IO.now(value).toCoeval
+
+    unsafeRun(coeval).runTry must beSuccessfulTry(value)
   }
 }
