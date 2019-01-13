e342c247cc7744259edc40f903fd1e46973840d9
John A. De Goes <john@degoes.net>
2019-01-13 15:17:31 -0700

    add some type bounds to where they were previously missing

--- core/shared/src/main/scala/scalaz/zio/Promise.scala
+++ core/shared/src/main/scala/scalaz/zio/Promise.scala
@@ -29,7 +29,7 @@ class Promise[E, A] private (private val state: AtomicReference[State[E, A]]) ex
    * until the result is available.
    */
   final def await: IO[E, A] =
-    IO.asyncInterrupt[E, A](k => {
+    IO.asyncInterrupt[Any, E, A](k => {
       var result = null.asInstanceOf[Either[Canceler, IO[E, A]]]
       var retry  = true
 
