045e643a1709e935581fd95126274dd309c11c9c
Scala steward <me@scala-steward.org>
2019-01-14 03:14:47 +0100

    Update kind-projector to 0.9.9

--- project/ScalazBuild.scala
+++ project/ScalazBuild.scala
@@ -54,7 +54,7 @@ object Scalaz {
     scalaVersion in ThisBuild := crossScalaVersions.value.head,
     scalacOptions := stdOptions ++ extraOptions(scalaVersion.value),
     libraryDependencies ++= compileOnlyDeps ++ testDeps ++ Seq(
-      compilerPlugin("org.spire-math"         %% "kind-projector"  % "0.9.7"),
+      compilerPlugin("org.spire-math"         %% "kind-projector"  % "0.9.9"),
       compilerPlugin("com.github.tomasmikula" %% "pascal"          % "0.2.1"),
       compilerPlugin("com.github.ghik"        %% "silencer-plugin" % "1.0")
     ),
