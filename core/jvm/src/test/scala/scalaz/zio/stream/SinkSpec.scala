78d40f0c4359dc33877f831c9edda79cb57c6f29
Itamar Ravid <iravid@iravid.com>
2019-01-26 22:48:12 +0200

    Add tests for Sink.foldLeft, Sink.fold, Sink.foldM and Sink.readWhile

--- core/jvm/src/test/scala/scalaz/zio/stream/SinkSpec.scala
+++ core/jvm/src/test/scala/scalaz/zio/stream/SinkSpec.scala
@@ -1,16 +1,116 @@
 package scalaz.zio.stream
 
+import org.scalacheck.Arbitrary
 import org.specs2.ScalaCheck
 import scala.{ Stream => _ }
 import scalaz.zio.{ AbstractRTSSpec, Exit, GenIO, IO }
 
-class SinkSpec(implicit ee: org.specs2.concurrent.ExecutionEnv) extends AbstractRTSSpec with GenIO with ScalaCheck {
+class SinkSpec(implicit ee: org.specs2.concurrent.ExecutionEnv)
+    extends AbstractRTSSpec
+    with StreamTestUtils
+    with GenIO
+    with ScalaCheck {
+  import ArbitraryStream._, Sink.Step
+
   def is = "SinkSpec".title ^ s2"""
-  A sink written with Sink.foldM works properly. $jsonNumArrayParsingSinkFoldM
-  A sink written with combinators works properly. $jsonNumArrayParsingSinkWithCombinators
+  Constructors
+    Sink.foldLeft               $foldLeft
+    Sink.fold                   $fold
+    Sink.fold short circuits    $foldShortCircuits
+    Sink.foldM                  $foldM
+    Sink.foldM short circuits   $foldMShortCircuits
+    Sink.readWhile              $readWhile
+
+  Usecases
+    Number array parsing with Sink.foldM  $jsonNumArrayParsingSinkFoldM
+    Number array parsing with combinators $jsonNumArrayParsingSinkWithCombinators
   """
 
-  def jsonNumArrayParsingSinkFoldM = {
+  private def foldLeft =
+    prop { (s: Stream[String, Int], f: (String, Int) => String, z: String) =>
+      unsafeRunSync(s.run(Sink.foldLeft(z)(f))) must_=== slurp(s).map(_.foldLeft(z)(f))
+    }
+
+  private def fold =
+    prop { (s: Stream[String, Int], f: (String, Int) => String, z: String) =>
+      val ff = (acc: String, el: Int) => Step.more(f(acc, el))
+
+      unsafeRunSync(s.run(Sink.fold(z)(ff))) must_=== slurp(s).map(_.foldLeft(z)(f))
+    }
+
+  private def foldShortCircuits = {
+    val empty: Stream[Nothing, Int]     = Stream.empty
+    val single: Stream[Nothing, Int]    = Stream.succeed(1)
+    val double: Stream[Nothing, Int]    = Stream(1, 2)
+    val failed: Stream[String, Nothing] = Stream.fail("Ouch")
+
+    def run[E](stream: Stream[E, Int]) = {
+      var effects: List[Int] = Nil
+      val sink = Sink.fold(0) { (_, (a: Int)) =>
+        effects ::= a
+        Step.done(30, Chunk.empty)
+      }
+
+      val exit = unsafeRunSync(stream.run(sink))
+
+      (exit, effects)
+    }
+
+    run(empty) must_=== ((Exit.succeed(0), Nil))
+    run(single) must_=== ((Exit.succeed(30), List(1)))
+    run(double) must_=== ((Exit.succeed(30), List(1)))
+    run(failed) must_=== ((Exit.checked("Ouch"), Nil))
+  }
+
+  private def foldM = {
+    implicit val ioArb = Arbitrary(genSuccess[String, String])
+
+    prop { (s: Stream[String, Int], f: (String, Int) => IO[String, String], z: IO[String, String]) =>
+      val ff         = (acc: String, el: Int) => f(acc, el).map(Step.more)
+      val sinkResult = unsafeRunSync(s.run(Sink.foldM(z)(ff)))
+      val foldResult = unsafeRunSync {
+        s.foldLeft(List[Int]())((acc, el) => el :: acc)
+          .map(_.reverse)
+          .flatMap(_.foldLeft(z)((acc, el) => acc.flatMap(f(_, el))))
+      }
+
+      foldResult.succeeded ==> (sinkResult must_=== foldResult)
+    }
+  }
+
+  private def foldMShortCircuits = {
+    val empty: Stream[Nothing, Int]     = Stream.empty
+    val single: Stream[Nothing, Int]    = Stream.succeed(1)
+    val double: Stream[Nothing, Int]    = Stream(1, 2)
+    val failed: Stream[String, Nothing] = Stream.fail("Ouch")
+
+    def run[E](stream: Stream[E, Int]) = {
+      var effects: List[Int] = Nil
+      val sink = Sink.foldM(IO.succeed(0)) { (_, (a: Int)) =>
+        effects ::= a
+        IO.succeed(Step.done(30, Chunk.empty))
+      }
+
+      val exit = unsafeRunSync(stream.run(sink))
+
+      (exit, effects)
+    }
+
+    run(empty) must_=== ((Exit.succeed(0), Nil))
+    run(single) must_=== ((Exit.succeed(30), List(1)))
+    run(double) must_=== ((Exit.succeed(30), List(1)))
+    run(failed) must_=== ((Exit.checked("Ouch"), Nil))
+  }
+
+  private def readWhile =
+    prop { (s: Stream[String, String], f: String => Boolean) =>
+      val sinkResult = unsafeRunSync(s.run(Sink.readWhile(f)))
+      val listResult = slurp(s).map(_.takeWhile(f))
+
+      listResult.succeeded ==> (sinkResult must_=== listResult)
+    }
+
+  private def jsonNumArrayParsingSinkFoldM = {
     sealed trait ParserState
     object ParserState {
       case object Start               extends ParserState
@@ -53,7 +153,7 @@ class SinkSpec(implicit ee: org.specs2.concurrent.ExecutionEnv) extends Abstract
       (fullParse must_=== (Exit.Success(List(123, 4))))
   }
 
-  def jsonNumArrayParsingSinkWithCombinators = {
+  private def jsonNumArrayParsingSinkWithCombinators = {
     val comma = Sink.readWhile[Char](_ == ',')
     val brace = Sink.read1[String, Char](a => s"Expected closing brace; instead: ${a}")((_: Char) == ']')
     val number: Sink[String, Char, Char, Int] =
