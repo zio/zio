f34f7ff8a4c6dc2f0f4b9bfe7c3973065265ef49
John A. De Goes <john@degoes.net>
2019-01-27 21:08:37 +0100

    fix big mess caused by merge

--- core/jvm/src/test/scala/scalaz/zio/stream/StreamTestUtils.scala
+++ core/jvm/src/test/scala/scalaz/zio/stream/StreamTestUtils.scala
@@ -3,15 +3,15 @@ package scalaz.zio.stream
 import scalaz.zio.{ AbstractRTSSpec, Exit, IO }
 
 trait StreamTestUtils { self: AbstractRTSSpec =>
-  def slurp[E, A](s: Stream[E, A]): Exit[E, List[A]] =
+  def slurp[E, A](s: Stream[Any, E, A]): Exit[E, List[A]] =
     slurp0(s)(_ => true)
 
-  def slurp0[E, A](s: Stream[E, A])(cont: List[A] => Boolean): Exit[E, List[A]] = s match {
-    case s: StreamPure[A] =>
+  def slurp0[E, A](s: Stream[Any, E, A])(cont: List[A] => Boolean): Exit[E, List[A]] = s match {
+    case s: StreamPure[Any, A] =>
       Exit.succeed(s.foldPureLazy(List[A]())(cont)((acc, el) => el :: acc).reverse)
     case s =>
       unsafeRunSync {
-        s.fold[E, A, List[A]].flatMap(f0 => f0(List[A](), cont, (acc, el) => IO.succeed(el :: acc)).map(_.reverse))
+        s.fold[Any, E, A, List[A]].flatMap(f0 => f0(List[A](), cont, (acc, el) => IO.succeed(el :: acc)).map(_.reverse))
       }
   }
 }
