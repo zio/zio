cb172e2f6815eac4f2c943101b4761da0a46af53
John A. De Goes <john@degoes.net>
2019-01-28 23:20:09 +0100

    convert system module

--- core/shared/src/main/scala/scalaz/zio/system/System.scala
+++ core/shared/src/main/scala/scalaz/zio/system/System.scala
@@ -1,3 +1,29 @@
 package scalaz.zio.system
 
-trait System {}
+import scalaz.zio.ZIO
+
+trait System {
+  val system: System.Interface[Any]
+}
+object System {
+  trait Interface[R] {
+    def env(variable: String): ZIO[R, Nothing, Option[String]]
+
+    def property(prop: String): ZIO[R, Throwable, Option[String]]
+
+    val lineSeparator: ZIO[R, Nothing, String]
+  }
+  trait Live extends System {
+    object system extends Interface[Any] {
+      import java.lang.{ System => JSystem }
+
+      def env(variable: String): ZIO[Any, Nothing, Option[String]] = ZIO.sync(Option(JSystem.getenv(variable)))
+
+      def property(prop: String): ZIO[Any, Throwable, Option[String]] =
+        ZIO.syncThrowable(Option(JSystem.getProperty(prop)))
+
+      val lineSeparator: ZIO[Any, Nothing, String] = ZIO.sync(JSystem.lineSeparator)
+    }
+  }
+  object Live extends Live
+}
--- core/shared/src/main/scala/scalaz/zio/system/package.scala
+++ core/shared/src/main/scala/scalaz/zio/system/package.scala
@@ -1,26 +1,16 @@
 package scalaz.zio
 
-import java.time.Instant
-
-package object system {
-
-  /** Determine the current system time **/
-  val currentTime: UIO[Instant] = IO.sync(Instant.now)
-
-  /** Determine the current system time in milliseconds **/
-  val currentTimeMillis: UIO[Long] = IO.sync(System.currentTimeMillis)
-
-  /** Returns the current value of the running Java Virtual Machine's high-resolution time source, in nanoseconds.
-   * This method can only be used to measure elapsed time and is not related to any other notion of system or wall-clock time
-   */
-  val nanoTime: UIO[Long] = IO.sync(System.nanoTime)
+package object system extends System.Interface[System] {
 
   /** Retrieve the value of an environment variable **/
-  def env(variable: String): UIO[Option[String]] = IO.sync(Option(System.getenv(variable)))
+  def env(variable: String): ZIO[System, Nothing, Option[String]] =
+    ZIO.readM(_.system.env(variable))
 
   /** Retrieve the value of a system property **/
-  def property(prop: String): Task[Option[String]] = Task.syncThrowable(Option(System.getProperty(prop)))
+  def property(prop: String): ZIO[System, Throwable, Option[String]] =
+    ZIO.readM(_.system.property(prop))
 
   /** System-specific line separator **/
-  val lineSeparator: UIO[String] = IO.sync(System.lineSeparator)
+  val lineSeparator: ZIO[System, Nothing, String] =
+    ZIO.readM(_.system.lineSeparator)
 }
