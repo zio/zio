0e07bc23a1c9ac122f04eb911f7263e7d4c6f38f
Scala steward <me@scala-steward.org>
2019-02-02 03:47:35 +0100

    Update pascal to 0.3.1

--- project/ScalazBuild.scala
+++ project/ScalazBuild.scala
@@ -55,7 +55,7 @@ object Scalaz {
     scalacOptions := stdOptions ++ extraOptions(scalaVersion.value),
     libraryDependencies ++= compileOnlyDeps ++ testDeps ++ Seq(
       compilerPlugin("org.spire-math"         %% "kind-projector"  % "0.9.9"),
-      compilerPlugin("com.github.tomasmikula" %% "pascal"          % "0.3"),
+      compilerPlugin("com.github.tomasmikula" %% "pascal"          % "0.3.1"),
       compilerPlugin("com.github.ghik"        %% "silencer-plugin" % "1.3.1")
     ),
     parallelExecution in Test := true,
