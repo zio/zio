e2b507abdaf4077591a38142b078fa1f9a3623bd
John A. De Goes <john@degoes.net>
2019-02-20 14:52:12 -0700

    Fixup merge

--- project/ScalazBuild.scala
+++ project/ScalazBuild.scala
@@ -25,8 +25,7 @@ object Scalaz {
     "-Xlint:_,-type-parameter-shadow",
     "-Xsource:2.13",
     "-Ywarn-numeric-widen",
-    "-Ywarn-value-discard",
-    "-Xfatal-warnings"
+    "-Ywarn-value-discard"
   )
 
   val buildInfoSettings = Seq(
