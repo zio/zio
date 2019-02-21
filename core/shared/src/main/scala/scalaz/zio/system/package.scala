1bdbbec6e067048f9060af88be03c0e45b38e03c
John A. De Goes <john@degoes.net>
2019-02-21 13:23:44 -0700

    prettify

--- core/shared/src/main/scala/scalaz/zio/system/package.scala
+++ core/shared/src/main/scala/scalaz/zio/system/package.scala
@@ -20,11 +20,11 @@ package object system extends System.Service[System] {
 
   /** Retrieve the value of an environment variable **/
   def env(variable: String): ZIO[System, SecurityException, Option[String]] =
-    ZIO.accessM(_.system.env(variable))
+    ZIO.accessM(_.system env variable)
 
   /** Retrieve the value of a system property **/
   def property(prop: String): ZIO[System, Throwable, Option[String]] =
-    ZIO.accessM(_.system.property(prop))
+    ZIO.accessM(_.system property prop)
 
   /** System-specific line separator **/
   val lineSeparator: ZIO[System, Nothing, String] =
