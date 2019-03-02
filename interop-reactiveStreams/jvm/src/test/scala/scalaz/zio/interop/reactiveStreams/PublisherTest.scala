d49cf55106c847c5fc5ed2d1422539cefbd855ea
Simon Schenk <simon.schenk@risk42.com>
2019-03-02 22:38:08 +0100

    PublisherTest

--- interop-reactiveStreams/jvm/src/test/scala/scalaz/zio/interop/reactiveStreams/PublisherTest.scala
+++ interop-reactiveStreams/jvm/src/test/scala/scalaz/zio/interop/reactiveStreams/PublisherTest.scala
@@ -1,5 +1,33 @@
 package scalaz.zio.interop.reactiveStreams
 
-class PublisherTest {
+import org.reactivestreams.Publisher
+import org.reactivestreams.tck.{PublisherVerification, TestEnvironment}
+import org.scalatest.testng.TestNGSuiteLike
+import scalaz.zio.DefaultRuntime
+import scalaz.zio.stream.Stream
+
+class PublisherTest(env: TestEnvironment, publisherShutdownTimeout: Long)
+    extends PublisherVerification[Int](env, publisherShutdownTimeout)
+    with TestNGSuiteLike
+    with DefaultRuntime {
+
+  def this() {
+    this(new TestEnvironment(500), 1000)
+  }
+
+  def createPublisher(elements: Long): Publisher[Int] =
+    unsafeRun(
+      Stream
+        .unfold(elements)(n => if (n > 0) Some((1, n - 1)) else None)
+        .toPublisher()
+    )
+
+  override def createFailedPublisher(): Publisher[Int] =
+    unsafeRun(
+      Stream
+        .fail(new RuntimeException("boom!"))
+        .map(_.asInstanceOf[Int])
+        .toPublisher()
+    )
 
 }
