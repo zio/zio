e1e59083ecd0ceca39da3546e5df0eee44a1095d
Simon Schenk <simon.schenk@risk42.com>
2019-03-03 20:28:50 +0100

    more compact code for Publisher, three fixes for Subscriber

--- interop-reactiveStreams/shared/src/main/scala/scalaz/zio/interop/reactiveStreams.scala
+++ interop-reactiveStreams/shared/src/main/scala/scalaz/zio/interop/reactiveStreams.scala
@@ -1,8 +1,6 @@
 package scalaz.zio.interop
 
 import org.reactivestreams.{ Publisher, Subscriber, Subscription }
-import scalaz.zio.Exit.Cause.{ Die, Interrupt, Fail => TFail }
-import scalaz.zio.Exit.{ Failure, Success }
 import scalaz.zio._
 import scalaz.zio.stream.{ Sink, Stream, Take }
 
@@ -10,63 +8,42 @@ package object reactiveStreams {
 
   final implicit class streamToPublisher[E <: Throwable, A](val src: Stream[Any, E, A]) extends AnyVal {
     def toPublisher(): UIO[Publisher[A]] =
-      ZIO.runtime.map(
-        runtime =>
-          (s: Subscriber[_ >: A]) => {
-            if (s == null) throw new NullPointerException("Subscriber must not be null.")
-            val wiring =
-              for {
-                q <- Queue.unbounded[Long]
-                control = Stream
-                  .fromQueue(q)
-                  .flatMap(n => Stream.unfold(n)(n => if (n > 0) Some(((), n - 1)) else None))
-                _ <- src
-                      .toQueue(1)
-                      .use {
-                        q =>
-                          Stream
-                            .fromQueue(q)
-                            .peel(Sink.readWhile[Take[E, A]](_.isInstanceOf[Take.Fail[E]]))
-                            .use {
-                              case (errors, stream) =>
-                                errors.headOption match {
-                                  case Some(Take.Fail(e)) => Task(s.onError(e))
-                                  case _ =>
-                                    stream
-                                      .zipWith(control) {
-                                        case (Some(Take.Value(a)), Some(_)) =>
-                                          s.onNext(a)
-                                          Some(())
-                                        case (Some(Take.Fail(e)), _) =>
-                                          s.onError(e)
-                                          None
-                                        case (Some(Take.End), _) =>
-                                          s.onComplete()
-                                          None
-                                        case _ => None
-                                      }
-                                      .run(Sink.drain)
-                                }
-                            }
-                      }
-                      .fork
-                subscription = new Subscription {
-                  override def request(n: Long): Unit = {
-                    if (n <= 0) s.onError(new IllegalArgumentException("n must be > 0"))
-                    runtime.unsafeRunAsync_(q.offer(n).void)
-                  }
-                  override def cancel(): Unit = runtime.unsafeRun(q.shutdown)
+      ZIO.runtime.map { runtime => (s: Subscriber[_ >: A]) =>
+        {
+          if (s == null) throw new NullPointerException("Subscriber must not be null.")
+          def takesToCallbacks(q: Queue[Take[E, A]], control: Stream[Any, Nothing, Unit]): Task[Unit] =
+            Stream
+              .fromQueue(q)
+              .peel(Sink.readWhile[Take[E, A]](_.isInstanceOf[Take.Fail[E]]))
+              .use {
+                case (Take.Fail(e) :: _, _) => Task(s.onError(e))
+                case (_, stream) =>
+                  stream
+                    .zip(control)
+                    .foreach {
+                      case (Take.Value(a), _) => Task(s.onNext(a))
+                      case (Take.Fail(e), _)  => Task(s.onError(e))
+                      case (Take.End, _)      => Task(s.onComplete())
+                    }
+              }
+          val wiring =
+            for {
+              q       <- Queue.unbounded[Long]
+              control = Stream.fromQueue(q).flatMap(n => Stream.unfold(n)(n => if (n > 0) Some(((), n - 1)) else None))
+              _       <- src.toQueue(1).use(takesToCallbacks(_, control)).fork
+            } yield {
+              val subscription = new Subscription {
+                override def request(n: Long): Unit = {
+                  if (n <= 0) s.onError(new IllegalArgumentException("n must be > 0"))
+                  runtime.unsafeRunAsync_(q.offer(n).void)
                 }
-              } yield subscription
-            runtime.unsafeRunAsync(wiring) {
-              case Success(subscription) => s.onSubscribe(subscription)
-              case Failure(Die(e))       => s.onError(e)
-              case Failure(TFail(e))     => s.onError(e)
-              case Failure(Interrupt)    => s.onComplete()
-              case _                     =>
+                override def cancel(): Unit = runtime.unsafeRun(q.shutdown)
+              }
+              s.onSubscribe(subscription)
             }
+          runtime.unsafeRunAsync_(wiring)
         }
-      )
+      }
   }
 
   final implicit class sinkToSubscriber[T, A](val sink: Sink[Any, _ <: Throwable, Unit, T, A]) extends AnyVal {
@@ -79,14 +56,26 @@ package object reactiveStreams {
       } yield {
         val subscriber =
           new Subscriber[T] {
+            var subscribed = false
             // todo: more intelligent requesting without blocking and infinite.
-            override def onSubscribe(s: Subscription): Unit = {
-              runtime.unsafeRunAsync(q.awaitShutdown *> Task(s.cancel()))(_ => ())
-              s.request(Long.MaxValue)
+            override def onSubscribe(s: Subscription): Unit =
+              if (subscribed) {
+                s.cancel()
+              } else {
+                subscribed = true
+                runtime.unsafeRunAsync(q.awaitShutdown *> Task(s.cancel()))(_ => ())
+                s.request(Long.MaxValue) // FIXME
+              }
+            override def onNext(t: T): Unit = {
+              if (t == null) throw new NullPointerException("t was null in onNext")
+              runtime.unsafeRun(q.offer(t).void)
             }
-            override def onNext(t: T): Unit          = runtime.unsafeRun(q.offer(t).void)
-            override def onError(t: Throwable): Unit = runtime.unsafeRun(p.fail(t) *> q.shutdown)
-            override def onComplete(): Unit          = runtime.unsafeRun(q.shutdown)
+            override def onError(t: Throwable): Unit = {
+              if (t == null) throw new NullPointerException("t was null in onError")
+              runtime.unsafeRun(p.fail(t) *> q.shutdown)
+            }
+            override def onComplete(): Unit =
+              runtime.unsafeRun(q.shutdown)
           }
         (subscriber, p.await)
       }
