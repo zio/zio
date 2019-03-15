10dcc0353c581c59ffa9a391103d59dd4578581d
Itamar Ravid <iravid@iravid.com>
2019-03-15 17:25:49 +0200

    Don't recover from defects in orElse

---
+++ core/jvm/src/test/scala/scalaz/zio/ArbitraryCause.scala
@@ -0,0 +1,27 @@
+package scalaz.zio
+
+import org.scalacheck.{ Arbitrary, Gen }
+import Exit.Cause
+
+object ArbitraryCause {
+  implicit def arbCause[T](implicit arbT: Arbitrary[T]): Arbitrary[Cause[T]] =
+    Arbitrary {
+      Gen.oneOf(
+        Gen.const(Cause.interrupt),
+        Arbitrary.arbitrary[String].map(s => Cause.die(new RuntimeException(s))),
+        arbT.arbitrary.map(Cause.fail),
+        Gen.lzy {
+          for {
+            left  <- arbCause[T].arbitrary
+            right <- arbCause[T].arbitrary
+          } yield Cause.Then(left, right)
+        },
+        Gen.lzy {
+          for {
+            left  <- arbCause[T].arbitrary
+            right <- arbCause[T].arbitrary
+          } yield Cause.Both(left, right)
+        }
+      )
+    }
+}
