536ab79086058bc34b29ac1f67775440a8b0507d
Cereopsis <cereopsis@me.com>
2019-03-25 23:40:58 +0000

    WIP #651 Initial pass

--- interop-scalaz7x/jvm/src/test/scala/scalaz/zio/interop/scalaz72Spec.scala
+++ interop-scalaz7x/jvm/src/test/scala/scalaz/zio/interop/scalaz72Spec.scala
@@ -12,27 +12,27 @@ class scalaz72Spec(implicit ee: org.specs2.concurrent.ExecutionEnv) extends Test
 
   def is = s2"""
     laws must hold for
-      Bifunctor              ${bifunctor.laws[IO]}
-      BindRec                ${bindRec.laws[IO[Int, ?]]}
-      Plus                   ${plus.laws[IO[Int, ?]]}
-      MonadPlus              ${monadPlus.laws[IO[Int, ?]]}
-      MonadPlus (Monoid)     ${monadPlus.laws[IO[Option[Unit], ?]]}
-      MonadError             ${monadError.laws[IO[Int, ?], Int]}
-      Applicative (Parallel) ${applicative.laws[scalaz72.ParIO[Int, ?]]}
+      Bifunctor              ${bifunctor.laws[ZIO[Any, ?, ?]]}
+      BindRec                ${bindRec.laws[ZIO[Any, Int, ?]]}
+      Plus                   ${plus.laws[ZIO[Any, Int, ?]]}
+      MonadPlus              ${monadPlus.laws[ZIO[Any, Int, ?]]}
+      MonadPlus (Monoid)     ${monadPlus.laws[ZIO[Any, Option[Unit], ?]]}
+      MonadError             ${monadError.laws[ZIO[Any, Int, ?], Int]}
+      Applicative (Parallel) ${applicative.laws[scalaz72.ParIO[Any, Int, ?]]}
   """
 
-  implicit def ioEqual[E: Equal, A: Equal]: Equal[IO[E, A]] =
-    new Equal[IO[E, A]] {
-      override def equal(io1: IO[E, A], io2: IO[E, A]): Boolean =
+  implicit def ioEqual[E: Equal, A: Equal]: Equal[ZIO[Any, E, A]] =
+    new Equal[ZIO[Any, E, A]] {
+      override def equal(io1: ZIO[Any, E, A], io2: ZIO[Any, E, A]): Boolean =
         unsafeRun(io1.either) === unsafeRun(io2.either)
     }
 
   implicit def ioArbitrary[E: Arbitrary: Cogen, A: Arbitrary: Cogen]: Arbitrary[IO[E, A]] =
     Arbitrary(genIO[E, A])
 
-  implicit def ioParEqual[E: Equal, A: Equal]: Equal[scalaz72.ParIO[E, A]] =
+  implicit def ioParEqual[E: Equal, A: Equal]: Equal[scalaz72.ParIO[Any, E, A]] =
     ioEqual[E, A].contramap(Tag.unwrap)
 
-  implicit def ioParArbitrary[E: Arbitrary: Cogen, A: Arbitrary: Cogen]: Arbitrary[scalaz72.ParIO[E, A]] =
+  implicit def ioParArbitrary[E: Arbitrary: Cogen, A: Arbitrary: Cogen]: Arbitrary[scalaz72.ParIO[Any, E, A]] =
     Arbitrary(genIO[E, A].map(Tag.apply))
 }
