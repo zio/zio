0294f35171fd63c851f7e1b1c8ed95ed17e9f38b
Maxim Schuwalow <mschuwalow@agile-im.de>
2019-03-26 15:35:25 +0100

    resolves scalaz/scalaz-zio#689: ClockSpec in Testkit not testing TestClock

--- testkit/jvm/src/test/scala/scalaz/zio/testkit/ClockSpec.scala
+++ testkit/jvm/src/test/scala/scalaz/zio/testkit/ClockSpec.scala
@@ -1,60 +1,94 @@
 package scalaz.zio.testkit
 
-import scalaz.zio._
-import scalaz.zio.duration._
 import java.util.concurrent.TimeUnit
 
-import scalaz.zio.clock.Clock
+import scalaz.zio._
+import scalaz.zio.duration._
 
 class ClockSpec(implicit ee: org.specs2.concurrent.ExecutionEnv) extends TestRuntime {
 
   def is = "ClockSpec".title ^ s2"""
-      Sleep does not sleep forever                      $e1
-      Nano time is monotonically increasing             $e2
-      Current time millis is monotonically increasing   $e3
+      Sleep does sleep instantly                        $e1
+      Sleep passes nanotime correctly                   $e2
+      Sleep passes currentTime correctly                $e3
+      Sleep correctly records sleeps                    $e4
+      Adjust correctly advances nanotime                $e5
+      Adjust correctly advances currentTime             $e6
+      Adjust does not produce sleeps                    $e7
      """
 
   def e1 =
     unsafeRun(
       for {
-        _ <- clock.sleep(1.millis)
-      } yield true must beTrue
+        ref       <- Ref.make(TestClock.Zero)
+        testClock  = TestClock(ref)
+        result    <- testClock.sleep(10.hours).timeout(100.milliseconds)
+      } yield result.nonEmpty must beTrue
     )
 
   def e2 =
     unsafeRun(
       for {
-        time1 <- clock.nanoTime
-        _     <- clock.sleep(1.millis)
-        time2 <- clock.nanoTime
-      } yield (time1 < time2) must beTrue
+        ref       <- Ref.make(TestClock.Zero)
+        testClock  = TestClock(ref)
+        time1     <- testClock.nanoTime
+        _         <- testClock.sleep(1.millis)
+        time2     <- testClock.nanoTime
+      } yield (time2 - time1) must_== 1000000L
     )
 
   def e3 =
     unsafeRun(
       for {
-        time1 <- clock.currentTime(TimeUnit.MILLISECONDS)
-        _     <- clock.sleep(1.millis)
-        time2 <- clock.currentTime(TimeUnit.MILLISECONDS)
-      } yield (time1 < time2) must beTrue
+        ref       <- Ref.make(TestClock.Zero)
+        testClock  = TestClock(ref)
+        time1     <- testClock.currentTime(TimeUnit.MILLISECONDS)
+        _         <- testClock.sleep(1.millis)
+        time2     <- testClock.currentTime(TimeUnit.MILLISECONDS)
+      } yield (time2 - time1) must_== 1L
     )
 
   def e4 =
     unsafeRun(
       for {
         ref       <- Ref.make(TestClock.Zero)
-        testClock <- IO.succeed(TestClock(ref))
-        result <- (for {
-                   time1  <- testClock.currentTime(TimeUnit.MILLISECONDS)
-                   _      <- testClock.sleep(1.millis)
-                   time2  <- testClock.currentTime(TimeUnit.MILLISECONDS)
-                   sleeps <- testClock.sleeps
-                 } yield
-                   (sleeps must_=== List((1.millis))) and
-                     ((time2 - time1) must_=== 1)).provide(new Clock with scheduler.SchedulerLive {
-                   val clock = testClock
-                 })
-
-      } yield result
+        testClock  = TestClock(ref)
+        _         <- testClock.sleep(1.millis)
+        sleeps    <- testClock.sleeps
+      } yield sleeps must_== List(1.milliseconds)
     )
+
+  def e5 =
+    unsafeRun(
+      for {
+        ref       <- Ref.make(TestClock.Zero)
+        testClock  = TestClock(ref)
+        time1     <- testClock.nanoTime
+        _         <- testClock.adjust(1.millis)
+        time2     <- testClock.nanoTime
+      } yield (time2 - time1) must_== 1000000L
+    )
+
+  def e6 =
+    unsafeRun(
+      for {
+        ref       <- Ref.make(TestClock.Zero)
+        testClock  = TestClock(ref)
+        time1     <- testClock.currentTime(TimeUnit.MILLISECONDS)
+        _         <- testClock.adjust(1.millis)
+        time2     <- testClock.currentTime(TimeUnit.MILLISECONDS)
+      } yield (time2 - time1) must_== 1L
+    )
+
+  def e7 =
+    unsafeRun(
+      for {
+        ref       <- Ref.make(TestClock.Zero)
+        testClock  = TestClock(ref)
+        _         <- testClock.adjust(1.millis)
+        sleeps    <- testClock.sleeps
+      } yield sleeps must_== Nil
+    )
+
+
 }
