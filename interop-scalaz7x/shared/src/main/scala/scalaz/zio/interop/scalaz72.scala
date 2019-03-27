35cae3fbc96e396ec6cd89a5de94f05f2b6c44a2
Cereopsis <cereopsis@me.com>
2019-03-27 23:56:52 +0000

    Fixed #651. Final tweaks to interop.scalaz72
    Refactor naming e.g IOInstances1 -> ZIOInstances1
    Update microsite: change IO -> ZIO and minor changes to examples

--- interop-scalaz7x/shared/src/main/scala/scalaz/zio/interop/scalaz72.scala
+++ interop-scalaz7x/shared/src/main/scala/scalaz/zio/interop/scalaz72.scala
@@ -20,34 +20,38 @@ package interop
 
 import scalaz.Tags.Parallel
 
-object scalaz72 extends IOInstances with Scalaz72Platform {
+object scalaz72 extends ZIOInstances with Scalaz72Platform {
   type ParIO[R, E, A] = ZIO[R, E, A] @@ Parallel
 }
 
-abstract class IOInstances extends IOInstances1 {
+abstract class ZIOInstances extends ZIOInstances1 {
+
+  implicit def zioTaskInstances[R]
+    : MonadError[TaskR[R, ?], Throwable] with BindRec[TaskR[R, ?]] with Plus[TaskR[R, ?]] =
+    new ZIOMonadError[R, Throwable] with ZIOPlus[R, Throwable]
 
   // cached for efficiency
   implicit val taskInstances: MonadError[Task, Throwable] with BindRec[Task] with Plus[Task] =
-    new IOMonadError[Any, Throwable] with IOPlus[Any, Throwable]
+    new ZIOMonadError[Any, Throwable] with ZIOPlus[Any, Throwable]
 
-  implicit val taskParAp: Applicative[scalaz72.ParIO[Any, Throwable, ?]] = new IOParApplicative[Any, Throwable]
+  implicit val taskParAp: Applicative[scalaz72.ParIO[Any, Throwable, ?]] = new ZIOParApplicative[Any, Throwable]
 }
 
-sealed trait IOInstances1 extends IOInstances2 {
-  implicit def ioMonoidInstances[R, E: Monoid]
+sealed trait ZIOInstances1 extends ZIOInstances2 {
+  implicit def zioMonoidInstances[R, E: Monoid]
     : MonadError[ZIO[R, E, ?], E] with BindRec[ZIO[R, E, ?]] with Bifunctor[ZIO[R, ?, ?]] with MonadPlus[ZIO[R, E, ?]] =
-    new IOMonadPlus[R, E] with IOBifunctor[R]
+    new ZIOMonadPlus[R, E] with ZIOBifunctor[R]
 
-  implicit def ioParAp[R, E]: Applicative[scalaz72.ParIO[R, E, ?]] = new IOParApplicative[R, E]
+  implicit def zioParAp[R, E]: Applicative[scalaz72.ParIO[R, E, ?]] = new ZIOParApplicative[R, E]
 }
 
-sealed trait IOInstances2 {
-  implicit def ioInstances[R, E]
+sealed trait ZIOInstances2 {
+  implicit def zioInstances[R, E]
     : MonadError[ZIO[R, E, ?], E] with BindRec[ZIO[R, E, ?]] with Bifunctor[ZIO[R, ?, ?]] with Plus[ZIO[R, E, ?]] =
-    new IOMonadError[R, E] with IOPlus[R, E] with IOBifunctor[R]
+    new ZIOMonadError[R, E] with ZIOPlus[R, E] with ZIOBifunctor[R]
 }
 
-private class IOMonad[R, E] extends Monad[ZIO[R, E, ?]] with BindRec[ZIO[R, E, ?]] {
+private class ZIOMonad[R, E] extends Monad[ZIO[R, E, ?]] with BindRec[ZIO[R, E, ?]] {
   override def map[A, B](fa: ZIO[R, E, A])(f: A => B): ZIO[R, E, B]             = fa.map(f)
   override def point[A](a: => A): ZIO[R, E, A]                                  = ZIO.succeedLazy(a)
   override def bind[A, B](fa: ZIO[R, E, A])(f: A => ZIO[R, E, B]): ZIO[R, E, B] = fa.flatMap(f)
@@ -55,17 +59,17 @@ private class IOMonad[R, E] extends Monad[ZIO[R, E, ?]] with BindRec[ZIO[R, E, ?
     f(a).flatMap(_.fold(tailrecM(f), point(_)))
 }
 
-private class IOMonadError[R, E] extends IOMonad[R, E] with MonadError[ZIO[R, E, ?], E] {
+private class ZIOMonadError[R, E] extends ZIOMonad[R, E] with MonadError[ZIO[R, E, ?], E] {
   override def handleError[A](fa: ZIO[R, E, A])(f: E => ZIO[R, E, A]): ZIO[R, E, A] = fa.catchAll(f)
   override def raiseError[A](e: E): ZIO[R, E, A]                                    = ZIO.fail(e)
 }
 
 /** lossy, throws away errors using the "first success" interpretation of Plus */
-private trait IOPlus[R, E] extends Plus[ZIO[R, E, ?]] {
+private trait ZIOPlus[R, E] extends Plus[ZIO[R, E, ?]] {
   override def plus[A](a: ZIO[R, E, A], b: => ZIO[R, E, A]): ZIO[R, E, A] = a.orElse(b)
 }
 
-private class IOMonadPlus[R, E: Monoid] extends IOMonadError[R, E] with MonadPlus[ZIO[R, E, ?]] {
+private class ZIOMonadPlus[R, E: Monoid] extends ZIOMonadError[R, E] with MonadPlus[ZIO[R, E, ?]] {
   override def plus[A](a: ZIO[R, E, A], b: => ZIO[R, E, A]): ZIO[R, E, A] =
     a.catchAll { e1 =>
       b.catchAll { e2 =>
@@ -75,12 +79,12 @@ private class IOMonadPlus[R, E: Monoid] extends IOMonadError[R, E] with MonadPlu
   override def empty[A]: ZIO[R, E, A] = raiseError(Monoid[E].zero)
 }
 
-private trait IOBifunctor[R] extends Bifunctor[ZIO[R, ?, ?]] {
+private trait ZIOBifunctor[R] extends Bifunctor[ZIO[R, ?, ?]] {
   override def bimap[A, B, C, D](fab: ZIO[R, A, B])(f: A => C, g: B => D): ZIO[R, C, D] =
     fab.bimap(f, g)
 }
 
-private class IOParApplicative[R, E] extends Applicative[scalaz72.ParIO[R, E, ?]] {
+private class ZIOParApplicative[R, E] extends Applicative[scalaz72.ParIO[R, E, ?]] {
   override def point[A](a: => A): scalaz72.ParIO[R, E, A] = Tag(ZIO.succeedLazy(a))
   override def ap[A, B](fa: => scalaz72.ParIO[R, E, A])(f: => scalaz72.ParIO[R, E, A => B]): scalaz72.ParIO[R, E, B] = {
     lazy val fa0: ZIO[R, E, A] = Tag.unwrap(fa)
