1c1cbf9e7b4ec2df34d3d6ba3c2d99e042d99985
Maciej Kowalski <maciej.kowalski@allegro.pl>
2019-03-27 11:55:19 +0100

    Revert "crossScalaVersion with dotty 0.13.0-RC1"
    This reverts commit c8735b6732f6e6ff6a838fef3d91cbc177d32e11.

--- project/ScalazBuild.scala
+++ project/ScalazBuild.scala
@@ -102,7 +102,7 @@ object Scalaz {
   def stdSettings(prjName: String) = Seq(
     name := s"scalaz-$prjName",
     scalacOptions := stdOptions,
-    crossScalaVersions := Seq("2.12.8", "2.11.12", "0.13.0-RC1"),
+    crossScalaVersions := Seq("2.12.8", "2.11.12"),
     scalaVersion in ThisBuild := crossScalaVersions.value.head,
     scalacOptions := stdOptions ++ extraOptions(scalaVersion.value),
     libraryDependencies ++= compileOnlyDeps ++ testDeps ++ Seq(
