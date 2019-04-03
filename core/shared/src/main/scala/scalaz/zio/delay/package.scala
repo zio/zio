ede9ea08f3a6224a48dd78726d2f396040fd22ab
ALEXANDRE SOUSA DANTAS <alexandre.dantas@yandeh.com.br>
2019-04-03 10:28:46 -0300

    Adding implicit conversions import

--- core/shared/src/main/scala/scalaz/zio/delay/package.scala
+++ core/shared/src/main/scala/scalaz/zio/delay/package.scala
@@ -2,6 +2,8 @@ package scalaz.zio
 
 import scalaz.zio.duration.Duration
 
+import scala.language.implicitConversions
+
 package object delay {
 
   implicit final def durationDelay(duration: Duration): DelaySyntax = new DelaySyntax(duration)
