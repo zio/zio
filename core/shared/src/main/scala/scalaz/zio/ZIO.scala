4815ba92feb2d750406b9ef9604a1adfa601778b
Cereopsis <Cereopsis@users.noreply.github.com>
2019-04-14 13:46:36 +0100

    Fixed #696 (#710)
    * WIP #651 Initial pass
    
    * WIP #651 Initial pass
    
    * Fixed #651. Final tweaks to interop.scalaz72
    
    Refactor naming e.g IOInstances1 -> ZIOInstances1
    Update microsite: change IO -> ZIO and minor changes to examples
    
    * #696 Initial pass. Still need to visit microsite
    
    * Re: #696 Addressing review issues: edited scaladoc and updated microsite.
    Also Removed 2 weak test cases in favour of one that tests interruption.

--- core/shared/src/main/scala/scalaz/zio/ZIO.scala
+++ core/shared/src/main/scala/scalaz/zio/ZIO.scala
@@ -1599,6 +1599,19 @@ trait ZIOFunctions extends Serializable {
    * current fiber for this operation to return non-empty lists.
    */
   final def children: UIO[IndexedSeq[Fiber[_, _]]] = descriptor.flatMap(_.children)
+
+  /**
+   * Acquires a resource, uses the resource, and then releases the resource.
+   * However, unlike `bracket`, the separation of these phases allows
+   * the acquisition to be interruptible.
+   *
+   * Useful for concurrent data structures and other cases where the
+   * 'deallocator' can tell if the allocation succeeded or not just by
+   * inspecting internal / external state.
+   */
+  def reserve[R, E, A, B](reservation: ZIO[R, E, Reservation[R, E, A]])(use: A => ZIO[R, E, B]): ZIO[R, E, B] =
+    ZManaged(reservation).use(use)
+
 }
 
 trait ZIO_E_Any extends ZIO_E_Throwable {
