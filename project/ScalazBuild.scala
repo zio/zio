f140868779245a36a98a1eec0c0ece22e69e281c
Denis Garcia <denisgarci@gmail.com>
2019-04-15 18:05:53 -0700

    Put 2.13 last in crossScalaVersions

--- project/ScalazBuild.scala
+++ project/ScalazBuild.scala
@@ -102,7 +102,7 @@ object Scalaz {
   def stdSettings(prjName: String) = Seq(
     name := s"scalaz-$prjName",
     scalacOptions := stdOptions,
-    crossScalaVersions := Seq("2.13.0-RC1", "2.12.8", "2.11.12"),
+    crossScalaVersions := Seq("2.12.8", "2.11.12", "2.13.0-RC1"),
     scalaVersion in ThisBuild := crossScalaVersions.value.head,
     scalacOptions := stdOptions ++ extraOptions(scalaVersion.value),
     libraryDependencies ++= compileOnlyDeps ++ testDeps ++ Seq(
