3a7cc077127ea837eeb7fead652305a41242016a
Dejan Mijic <dmijic@acm.org>
2019-05-06 00:04:00 +0200

    Convert twitter's future to zio Task

--- interop-twitter/jvm/src/main/scala/scalaz/zio/interop/twitter.scala
+++ interop-twitter/jvm/src/main/scala/scalaz/zio/interop/twitter.scala
@@ -1,4 +1,18 @@
 package scalaz.zio.interop
 
+import com.twitter.util.{ Future, Return, Throw }
+import scalaz.zio.Task
+
 package object twitter {
+  implicit class TaskObjOps(private val obj: Task.type) extends AnyVal {
+    final def fromFuture[A](future: => Future[A]): Task[A] =
+      Task.effectAsync { cb =>
+        future.respond {
+          case Return(a) => cb(Task.succeed(a))
+          case Throw(e)  => cb(Task.fail(e))
+        }
+
+        ()
+      }
+  }
 }
