621009b56381e4f3585c14b35c2eaaf03c5b652b
Dejan Mijic <dmijic@acm.org>
2019-05-08 15:48:53 +0200

    Replace by-name with Task[_]

--- interop-twitter/jvm/src/main/scala/scalaz/zio/interop/twitter.scala
+++ interop-twitter/jvm/src/main/scala/scalaz/zio/interop/twitter.scala
@@ -21,14 +21,16 @@ import scalaz.zio.{ Task, UIO }
 
 package object twitter {
   implicit class TaskObjOps(private val obj: Task.type) extends AnyVal {
-    final def fromTwitterFuture[A](future: => Future[A]): Task[A] =
-      Task.effectAsyncInterrupt { cb =>
-        future.respond {
-          case Return(a) => cb(Task.succeed(a))
-          case Throw(e)  => cb(Task.fail(e))
-        }
+    final def fromTwitterFuture[A](future: Task[Future[A]]): Task[A] =
+      future.flatMap { f =>
+        Task.effectAsyncInterrupt { cb =>
+          f.respond {
+            case Return(a) => cb(Task.succeed(a))
+            case Throw(e)  => cb(Task.fail(e))
+          }
 
-        Left(UIO(future.raise(new FutureCancelledException)))
+          Left(UIO(f.raise(new FutureCancelledException)))
+        }
       }
   }
 }
