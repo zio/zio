1d4a9846087794554b53e4305233fdddfe718737
Kai <kaidaxofficial@gmail.com>
2019-05-10 12:26:07 +0100

    Always mention Execution trace / stack trace in ZTrace pretty print, even if empty.

--- core/shared/src/main/scala/scalaz/zio/ZTrace.scala
+++ core/shared/src/main/scala/scalaz/zio/ZTrace.scala
@@ -17,22 +17,19 @@ final case class ZTrace(
       if (execTrace)
         s"Fiber:$fiberId ZIO Execution trace:" ::
           executionTrace.reverse.map(loc => "  at " + loc.prettyPrint)
-      else Nil
+      else s"Fiber:$fiberId ZIO Execution trace: <empty trace>" :: Nil
 
     val stackPrint =
       if (stackTrace)
-        (if (execTrace) "" :: (_: List[String]) else ZIO.identityFn[List[String]]) {
-          s"Fiber:$fiberId was supposed to continue to:" ::
-            this.stackTrace.reverse.map(loc => s"  a future continuation at " + loc.prettyPrint)
-        }
-      else Nil
+        s"\nFiber:$fiberId was supposed to continue to:" ::
+          this.stackTrace.reverse.map(loc => s"  a future continuation at " + loc.prettyPrint)
+      else
+        s"\nFiber:$fiberId was supposed to continue to: <empty trace>" ::Nil
 
     val ancestry: List[String] =
-      (if (execTrace || stackTrace) "" :: (_: List[String]) else ZIO.identityFn[List[String]]) {
-        fiberAncestry.parentTrace.map {
-          trace => s"Fiber:$fiberId was spawned by:\n" :: trace.prettyPrint :: Nil
-        }.getOrElse(s"Fiber was Fiber:$fiberId" :: Nil)
-    }
+      fiberAncestry.parentTrace.map {
+        trace => s"\nFiber:$fiberId was spawned by:\n" :: trace.prettyPrint :: Nil
+      }.getOrElse(s"\nFiber:$fiberId was spawned by: <empty trace>" :: Nil)
 
     (execPrint ++ stackPrint ++ ancestry).mkString("\n")
   }
