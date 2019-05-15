6cde94c14c538175d664bc8ccbd3f75e87dae168
Timo Schmid <timo.schmid@gmail.com>
2019-05-15 21:58:39 +0200

    #793 Add Sink, other changes

--- interop-cats/jvm/src/main/scala/scalaz/zio/interop/stream/Sink.scala
+++ interop-cats/jvm/src/main/scala/scalaz/zio/interop/stream/Sink.scala
@@ -16,6 +16,166 @@
 
 package scalaz.zio.interop.stream
 
-trait Sink[F[_], +A0, -A, +B] {
+import cats.effect.Effect
+import scalaz.zio.{Chunk, Runtime}
+import scalaz.zio.stream.ZSink
+import scalaz.zio.stream.ZSink.Step
+
+final class Sink[F[+ _], +A0, -A, +B] private[stream] (private[stream] val underlying: ZSink[Any, Throwable, A0, A, B])
+    extends AnyVal {
+
+  type State = underlying.State
+
+  import ZSink.Step
+  import Stream.{ liftF, liftZIO }
+
+  def initial(implicit R: Runtime[Any], E: Effect[F]): F[Step[State, Nothing]] =
+    liftF[F, Any, Step[State, Nothing]](underlying.initial)
+
+  def step(state: State, a: A)(implicit R: Runtime[Any], E: Effect[F]): F[Step[State, A0]] =
+    liftF(underlying.step(state, a))
+
+  def extract(state: State)(implicit R: Runtime[Any], E: Effect[F]): F[B] =
+    liftF(underlying.extract(state))
+
+  def stepChunk[A1 <: A](state: State, as: Chunk[A1])(implicit R: Runtime[Any], E: Effect[F]): F[Step[State, A0]] =
+    liftF(underlying.stepChunk(state, as))
+
+  def update(state: Step[State, Nothing]): Sink[F, A0, A, B] =
+    new Sink(underlying.update(state))
+
+  def chunked[A1 >: A0, A2 <: A]: Sink[F, A1, Chunk[A2], B] =
+    new Sink(underlying.chunked)
+
+  def mapM[C](f: B => F[C])(implicit E: Effect[F]): Sink[F, A0, A, C] =
+    new Sink(underlying.mapM(f.andThen(liftZIO(_))))
+
+  def map[C](f: B => C): Sink[F, A0, A, C] =
+    new Sink(underlying.map(f))
+
+  def filter[A1 <: A](f: A1 => Boolean): Sink[F, A0, A1, B] =
+    new Sink(underlying.filter(f))
+
+  def filterM[A1 <: A](f: A1 => F[Boolean])(implicit E: Effect[F]): Sink[F, A0, A1, B] =
+    new Sink(underlying.filterM(f.andThen(liftZIO(_))))
+
+  def filterNot[A1 <: A](f: A1 => Boolean): Sink[F, A0, A1, B] =
+    new Sink(underlying.filterNot(f))
+
+  def filterNotM[A1 <: A](f: A1 => F[Boolean])(implicit E: Effect[F]): Sink[F, A0, A1, B] =
+    new Sink(underlying.filterNotM(f.andThen(liftZIO(_))))
+
+  def contramap[C](f: C => A): Sink[F, A0, C, B] =
+    new Sink(underlying.contramap(f))
+
+  def contramapM[C](f: C => F[A])(implicit E: Effect[F]): Sink[F, A0, C, B] =
+    new Sink(underlying.contramapM(f.andThen(liftZIO(_))))
+
+  def dimap[C, D](f: C => A)(g: B => D): Sink[F, A0, C, D] =
+    new Sink(underlying.dimap(f)(g))
+
+  def mapError[E1 <: Throwable](f: Throwable => E1): Sink[F, A0, A, B] =
+    new Sink(underlying.mapError(f))
+
+  def mapRemainder[A1](f: A0 => A1): Sink[F, A1, A, B] =
+    new Sink(underlying.mapRemainder(f))
+
+  // TODO Not sure this is useful for cats interop, probably should be deleted
+  def provideSome(f: Any => Any): Sink[F, A0, A, B] =
+    new Sink(underlying.provideSome(f))
+
+  def const[C](c: => C): Sink[F, A0, A, C] =
+    new Sink(underlying.const(c))
+
+  def unit: Sink[F, A0, A, Unit] =
+    new Sink(underlying.unit)
+
+  def untilOutput(f: B => Boolean): Sink[F, A0, A, B] =
+    new Sink(underlying.untilOutput(f))
+
+  def ? : Sink[F, A0, A, Option[B]] =
+    new Sink(underlying.?)
+
+  def optional: Sink[F, A0, A, Option[B]] = ?
+
+  def race[A2 >: A0, A1 <: A, B1 >: B](
+    that: Sink[F, A2, A1, B1]
+  ): Sink[F, A2, A1, B1] =
+    new Sink(underlying.race(that.underlying))
+
+  def |[A2 >: A0, A1 <: A, B1 >: B](
+    that: Sink[F, A2, A1, B1]
+  ): Sink[F, A2, A1, B1] = race(that)
+
+  def raceBoth[A2 >: A0, A1 <: A, C](
+    that: Sink[F, A2, A1, C]
+  ): Sink[F, A2, A1, Either[B, C]] =
+    new Sink(underlying.raceBoth(that.underlying))
+
+  def takeWhile[A1 <: A](pred: A1 => Boolean): Sink[F, A0, A1, B] =
+    new Sink(underlying.takeWhile(pred))
+
+  def dropWhile[A1 <: A](pred: A1 => Boolean): Sink[F, A0, A1, B] =
+    new Sink(underlying.dropWhile(pred))
+
+}
+
+object Sink {
+
+  import Stream.liftZIO
+
+  final def more[F[+ _], A0, A, B](end: F[B])(input: A => Sink[F, A0, A, B])(implicit E: Effect[F]): Sink[F, A0, A, B] =
+    new Sink(ZSink.more(liftZIO(end))(input.andThen(_.underlying)))
+
+  final def succeedLazy[F[+ _], B](b: => B): Sink[F, Nothing, Any, B] =
+    new Sink(ZSink.succeedLazy(b))
+
+  final def drain[F[+ _]]: Sink[F, Nothing, Any, Unit] =
+    new Sink(ZSink.drain)
+
+  final def collect[F[+ _], A]: Sink[F, Nothing, A, List[A]] =
+    new Sink(ZSink.collect)
+
+  final def fromEffect[F[+ _], B](b: => F[B])(implicit E: Effect[F]): Sink[F, Nothing, Any, B] =
+    new Sink(ZSink.fromEffect(liftZIO(b)))
+
+  // TODO Not sure this is the right thing to do, what does the error () mean?
+  final def fromFunction[F[+ _], A, B](f: A => B): Sink[F, Nothing, A, B] =
+    new Sink(ZSink.fromFunction(f).mapError(_ => new IllegalStateException()))
+
+  // TODO Not sure this is the right thing to do, what does the error () mean?
+  final def identity[F[+ _], A]: Sink[F, A, A, A] =
+    new Sink(ZSink.identity[A].mapError(_ => new IllegalStateException()))
+
+  final def fail[F[+ _], E <: Throwable](e: E): Sink[F, Nothing, Any, Nothing] =
+    new Sink(ZSink.fail[E](e))
+
+  def fold[F[+ _], A0, A, S](z: S)(f: (S, A) => Step[S, A0]): Sink[F, A0, A, S] =
+    new Sink(ZSink.fold(z)(f))
+
+  def foldLeft[F[+ _], A0, A, S](z: S)(f: (S, A) => S): Sink[F, A0, A, S] =
+    new Sink(ZSink.foldLeft(z)(f))
+
+  def foldM[F[+ _], A0, A, S](z: F[S])(f: (S, A) => F[Step[S, A0]])(implicit E: Effect[F]): Sink[F, A0, A, S] =
+    new Sink(ZSink.foldM(liftZIO(z))((s, a) => liftZIO(f(s, a))))
+
+  def readWhileM[F[+ _], A](p: A => F[Boolean])(implicit E: Effect[F]): Sink[F, A, A, List[A]] =
+    new Sink(ZSink.readWhileM(p.andThen(liftZIO(_))))
+
+  def readWhile[F[+ _], A](p: A => Boolean): Sink[F, A, A, List[A]] =
+    new Sink(ZSink.readWhile(p))
+
+  def ignoreWhile[F[+ _], A](p: A => Boolean): Sink[F, A, A, Unit] =
+    new Sink(ZSink.ignoreWhile(p))
+
+  def ignoreWhileM[F[+ _], A](p: A => F[Boolean])(implicit E: Effect[F]): Sink[F, A, A, Unit] =
+    new Sink(ZSink.ignoreWhileM(p.andThen(liftZIO(_))))
+
+  // TODO Not sure this is the right thing to do, what does the error () mean?
+  def await[F[+ _], A]: Sink[F, Nothing, A, A] =
+    new Sink(ZSink.await.mapError(_ => new IllegalStateException()))
+
+  def read1[F[+ _], E <: Throwable, A](e: Option[A] => E)(p: A => Boolean): Sink[F, A, A, A] =
+    new Sink(ZSink.read1(e)(p))
 
 }
--- interop-cats/jvm/src/main/scala/scalaz/zio/interop/stream/Stream.scala
+++ interop-cats/jvm/src/main/scala/scalaz/zio/interop/stream/Stream.scala
@@ -16,142 +16,336 @@
 
 package scalaz.zio.interop.stream
 
-import cats.~>
-import cats.effect.Effect
+// import cats.~>
+import cats.FlatMap
+import cats.effect.{ Effect, Sync }
 import cats.implicits._
-import scalaz.zio.{Chunk, Queue, ZIO, ZManaged, ZSchedule}
-import scalaz.zio.stream.{Take, ZSink, ZStream}
+import scalaz.zio.clock.Clock
+import scalaz.zio.stream.Stream.ConformsR
+import scalaz.zio._
+import scalaz.zio.stream.{ Take, ZStream }
 
-final class Stream[F[+ _], +A] private[stream] (private[stream] val underlying: ZStream[Any, Throwable, A])(
-  implicit liftIO: ZIO[Any, Throwable, ?] ~> F,
-  liftZIO: F ~> ZIO[Any, Throwable, ?],
-  effect: Effect[F]
-) { self =>
+/**
+ * See [[scalaz.zio.stream.ZStream]]
+ */
+final class Stream[F[+ _], +A] private[stream] (private[stream] val underlying: ZStream[Any, Throwable, A])
+    extends AnyVal {
+
+  self =>
 
   import Stream.Fold
+  // import Stream.Env
+  import Stream.liftF
+  import Stream.liftZIO
 
-  def fold[A1 >: A, S]: Fold[F, A1, S] = ???
-
-  def foldLeft[A1 >: A, S](s: S)(f: (S, A1) => F[S]): F[S] = {
-    (fold[A, S]: F[(S, S => Boolean, (S, A) => F[S]) => F[S]]).flatMap(_(s, _ => false, f))
-  }
-
+  /**
+   * See [[scalaz.zio.stream.ZStream#++]]
+   */
   def ++[A1 >: A](that: => Stream[F, A1]): Stream[F, A1] =
     new Stream(underlying ++ that.underlying)
 
-  def drain: Stream[F, Nothing] =
-    new Stream(underlying.drain)
-
-  def filter(pred: A => Boolean): Stream[F, A] =
-    new Stream(underlying.filter(pred))
-
-  def filterM(pred: A => F[Boolean]): Stream[F, A] =
-    new Stream(underlying.filterM(pred.andThen(liftZIO.apply)))
-
-  def filterNot(pred: A => Boolean): Stream[F, A] =
-    filter(a => !pred(a))
-
-  def foreachWhile(f: A => F[Boolean]): F[Unit] =
-    liftIO.apply(underlying.foreachWhile(f.andThen(liftZIO.apply)))
-
+  /**
+   * See [[scalaz.zio.stream.ZStream#collect]]
+   */
   def collect[B](pf: PartialFunction[A, B]): Stream[F, B] =
     new Stream(underlying.collect(pf))
 
-  def drop(n: Int): Stream[F, A] =
-    new Stream(underlying.drop(n))
-
-  def dropWhile(pred: A => Boolean): Stream[F, A] =
-    new Stream(underlying.dropWhile(pred))
-
-  def flatMap[B](f: A => Stream[F, B]): Stream[F, B] =
-    new Stream(underlying.flatMap(f.andThen(_.underlying)))
-
-  def foreach(f: A => F[Unit]): F[Unit] =
-    liftIO(underlying.foreach(f.andThen(liftZIO.apply)))
-
-  def forever: Stream[F, A] =
-    new Stream(underlying.forever)
-
-  def map[B](f: A => B): Stream[F, B] =
-    new Stream(underlying.map(f))
-
-  // TODO Should we expose Chunk to clients?
-  def mapConcat[B](f: A => Chunk[B]): Stream[F, B] =
-    new Stream(underlying.mapConcat(f))
-
-  def mapM[B](f: A => F[B]): Stream[F, B] =
-    new Stream(underlying.mapM(f.andThen(liftZIO.apply)))
-
-  def merge[A1 >: A](that: Stream[F, A1], capacity: Int = 1): Stream[F, A1] =
-    new Stream(underlying.merge(that.underlying, capacity))
-
-  def mergeEither[B](that: Stream[F, B], capacity: Int = 1): Stream[F, Either[A, B]] =
-    new Stream(underlying.mergeEither(that.underlying, capacity))
-
-  def mergeWith[B, C](that: Stream[F, B], capacity: Int = 1)(l: A => C, r: B => C): Stream[F, C] =
-    new Stream(underlying.mergeWith(that.underlying, capacity)(l, r))
-
-  // TODO Sink, Managed
-  def peel[A1 >: A, B](sink: ZSink[Any, Throwable, A1, A1, B]): ZManaged[Any, Throwable, (B, Stream[F, A1])] = ???
-
-  // TODO Schedule #790
-  //  Find out how to deal correctly with "with Clock"
-  def repeat(schedule: ZSchedule[Any, Unit, _]): Stream[F, A] = ???
-    // new Stream(underlying.repeat(schedule))
-
-  // TODO Schedule #790
-  //  Find out how to deal correctly with "with Clock"
-  def repeatElems[B](schedule: ZSchedule[Any, A, B]): Stream[F, A] = ???
-    // new Stream(underlying.repeatElems(schedule))
-
-  // TODO Sink
-  def run[A0, A1 >: A, B](sink: ZSink[Any, Throwable, A0, A1, B]): F[B] =
-    liftIO(underlying.run(sink))
-
-  def mapAccum[S1, B](s1: S1)(f1: (S1, A) => (S1, B)): Stream[F, B] =
-    new Stream(underlying.mapAccum(s1)(f1))
-
-  def mapAccumM[S1, B](s1: S1)(f1: (S1, A) => F[(S1, B)]): Stream[F, B] =
-    new Stream(underlying.mapAccumM(s1) { (s1, a) => liftZIO(f1(s1, a)) })
-
-  def take(n: Int): Stream[F, A] =
-    new Stream(underlying.take(n))
-
-  def takeWhile(pred: A => Boolean): Stream[F, A] =
-    new Stream(underlying.takeWhile(pred))
-
+  /**
+   * See [[scalaz.zio.stream.ZStream#collectWhile]]
+   */
   def collectWhile[B](pred: PartialFunction[A, B]): Stream[F, B] =
     new Stream(underlying.collectWhile(pred))
 
-  // TODO ZManaged -> Managed
+  /**
+   * See [[scalaz.zio.stream.ZStream#drain]]
+   */
+  def drain: Stream[F, Nothing] =
+    new Stream(underlying.drain)
+
+  /**
+   * See [[scalaz.zio.stream.ZStream#drop]]
+   */
+  def drop(n: Int): Stream[F, A] =
+    new Stream(underlying.drop(n))
+
+  /**
+   * See [[scalaz.zio.stream.ZStream#dropWhile]]
+   */
+  def dropWhile(pred: A => Boolean): Stream[F, A] =
+    new Stream(underlying.dropWhile(pred))
+
+  /**
+   * See [[scalaz.zio.stream.ZStream#filter]]
+   */
+  def filter(pred: A => Boolean): Stream[F, A] =
+    new Stream(underlying.filter(pred))
+
+  /**
+   * See [[scalaz.zio.stream.ZStream#filterM]]
+   */
+  def filterM(pred: A => F[Boolean])(implicit E: Effect[F]): Stream[F, A] =
+    new Stream(underlying.filterM(pred.andThen(liftZIO(_))))
+
+  /**
+   * See [[scalaz.zio.stream.ZStream#filterNot]]
+   */
+  def filterNot(pred: A => Boolean): Stream[F, A] =
+    filter(a => !pred(a))
+
+  /**
+   * See [[scalaz.zio.stream.ZStream#fold]]
+   */
+  def fold[A1 >: A, S]: Fold[F, A1, S] = ???
+
+  /**
+   * See [[scalaz.zio.stream.ZStream#foldLeft]]
+   */
+  def foldLeft[A1 >: A, S](s: S)(f: (S, A1) => F[S])(implicit fm: FlatMap[F]): F[S] =
+    (fold[A, S]: F[(S, S => Boolean, (S, A) => F[S]) => F[S]]).flatMap(_(s, _ => false, f))
+
+  /**
+   * See [[scalaz.zio.stream.ZStream#flatMap]]
+   */
+  def flatMap[B](f: A => Stream[F, B]): Stream[F, B] =
+    new Stream(underlying.flatMap(f.andThen(_.underlying)))
+
+  /**
+   * See [[scalaz.zio.stream.ZStream#foreach]]
+   */
+  def foreach(f: A => F[Unit])(implicit R: Runtime[Any], E: Effect[F]): F[Unit] =
+    liftF(underlying.foreach(f.andThen(liftZIO(_))))
+
+  /**
+   * See [[scalaz.zio.stream.ZStream#foreachWhile]]
+   */
+  def foreachWhile(f: A => F[Boolean])(implicit R: Runtime[Any], E: Effect[F]): F[Unit] =
+    liftF(underlying.foreachWhile(f.andThen(liftZIO(_))))
+
+  /**
+   * See [[scalaz.zio.stream.ZStream#forever]]
+   */
+  def forever: Stream[F, A] =
+    new Stream(underlying.forever)
+
+  /**
+   * See [[scalaz.zio.stream.ZStream#map]]
+   */
+  def map[B](f: A => B): Stream[F, B] =
+    new Stream(underlying.map(f))
+
+  /**
+   * See [[scalaz.zio.stream.ZStream#mapConcat]]
+   */
+  def mapConcat[B](f: A => Chunk[B]): Stream[F, B] =
+    new Stream(underlying.mapConcat(f))
+
+  def mapK[G[+ _]]: Stream[G, A] =
+    new Stream(underlying)
+
+  /**
+   * See [[scalaz.zio.stream.ZStream#mapM]]
+   */
+  def mapM[B](f: A => F[B])(implicit E: Effect[F]): Stream[F, B] =
+    new Stream(underlying.mapM(f.andThen(liftZIO(_))))
+
+  /**
+   * See [[scalaz.zio.stream.ZStream#merge]]
+   */
+  def merge[A1 >: A](that: Stream[F, A1], capacity: Int = 1): Stream[F, A1] =
+    new Stream(underlying.merge(that.underlying, capacity))
+
+  /**
+   * See [[scalaz.zio.stream.ZStream#mergeEither]]
+   */
+  def mergeEither[B](that: Stream[F, B], capacity: Int = 1): Stream[F, Either[A, B]] =
+    new Stream(underlying.mergeEither(that.underlying, capacity))
+
+  /**
+   * See [[scalaz.zio.stream.ZStream#mergeWith]]
+   */
+  def mergeWith[B, C](that: Stream[F, B], capacity: Int = 1)(l: A => C, r: B => C): Stream[F, C] =
+    new Stream(underlying.mergeWith(that.underlying, capacity)(l, r))
+
+  /**
+   * TODO
+   *  - Managed
+   * See [[scalaz.zio.stream.ZStream#peel]]
+   */
+  def peel[A1 >: A, B](sink: Sink[F, A1, A1, B]): ZManaged[Any, Throwable, (B, Stream[F, A1])] =
+    ???
+    // underlying.peel(sink.underlying)
+
+  /**
+   * TODO
+   *  - Schedule #790
+   *  - Find out how to deal correctly with "with Clock"
+   * See [[scalaz.zio.stream.ZStream#]]
+   */
+  def repeat(schedule: ZSchedule[Any, Unit, _])(implicit r: Runtime[Clock]): Stream[F, A] =
+    ??? // new Stream(underlying.repeat(schedule))
+
+  /**
+   * TODO
+   *  - Schedule #790
+   *  - Find out how to deal correctly with "with Clock"
+   * See [[scalaz.zio.stream.ZStream#repeatElems]]
+   */
+  def repeatElems[B](schedule: ZSchedule[Any, A, B])(implicit r: Runtime[Clock]): Stream[F, A] =
+    ??? // new Stream(underlying.repeatElems(schedule))
+
+  /**
+   * TODO Sink
+   * See [[scalaz.zio.stream.ZStream#run]]
+   */
+  def run[A0, A1 >: A, B](sink: Sink[F, A0, A1, B])(implicit r: Runtime[Any], sync: Sync[F]): F[B] =
+    sync.delay(r.unsafeRun(underlying.run(sink.underlying)))
+
+  /**
+   * See [[scalaz.zio.stream.ZStream#mapAccum]]
+   */
+  def mapAccum[S1, B](s1: S1)(f1: (S1, A) => (S1, B)): Stream[F, B] =
+    new Stream(underlying.mapAccum(s1)(f1))
+
+  /**
+   * See [[scalaz.zio.stream.ZStream#mapAccumM]]
+   */
+  def mapAccumM[S1, B](s1: S1)(f1: (S1, A) => F[(S1, B)])(implicit E: Effect[F]): Stream[F, B] =
+    new Stream(underlying.mapAccumM(s1)((s1, a) => liftZIO(f1(s1, a))))
+
+  /**
+   * See [[scalaz.zio.stream.ZStream#take]]
+   */
+  def take(n: Int): Stream[F, A] =
+    new Stream(underlying.take(n))
+
+  /**
+   * See [[scalaz.zio.stream.ZStream#takeWhile]]
+   */
+  def takeWhile(pred: A => Boolean): Stream[F, A] =
+    new Stream(underlying.takeWhile(pred))
+
+  /**
+   * TODO
+   *  - ZManaged
+   * See [[scalaz.zio.stream.ZStream#toQueue]]
+   */
   def toQueue[A1 >: A](capacity: Int = 1): ZManaged[Any, Nothing, Queue[Take[Throwable, A1]]] = ???
 
-  // TODO ZSink -> Sink
-  def transduce[A1 >: A, C](sink: ZSink[Any, Throwable, A1, A1, C]): Stream[F, C] =
-    new Stream(underlying.transduce(sink))
+  /**
+   * See [[scalaz.zio.stream.ZStream#transduce]]
+   */
+  def transduce[A1 >: A, C](sink: Sink[F, A1, A1, C]): Stream[F, C] =
+    new Stream(underlying.transduce(sink.underlying))
 
-  def tap(f: A => F[_]): Stream[F, A] =
-    new Stream(underlying.tap { a =>
-      liftZIO(f(a))
-    })
+  /**
+   * See [[scalaz.zio.stream.ZStream#tap]]
+   */
+  def tap(f: A => F[_])(implicit E: Effect[F]): Stream[F, A] =
+    new Stream(underlying.tap(a => liftZIO(f(a))))
 
+  /**
+   * See [[scalaz.zio.stream.ZStream#zip]]
+   */
   def zip[B](that: Stream[F, B], lc: Int = 1, rc: Int = 1): Stream[F, (A, B)] =
     new Stream(underlying.zip(that.underlying, lc, rc))
 
-  def zipWith[B, C](that: Stream[F, B], lc: Int = 1, rc: Int = 1)(f: (Option[A], Option[B]) => Option[C]): Stream[F, C] =
+  /**
+   * See [[scalaz.zio.stream.ZStream#zipWith]]
+   */
+  def zipWith[B, C](that: Stream[F, B], lc: Int = 1, rc: Int = 1)(
+    f: (Option[A], Option[B]) => Option[C]
+  ): Stream[F, C] =
     new Stream(underlying.zipWith(that.underlying, lc, rc)(f))
 
+  /**
+   * See [[scalaz.zio.stream.ZStream#zipWithIndex]]
+   */
   def zipWithIndex: Stream[F, (A, Int)] =
     new Stream(underlying.zipWithIndex)
-
-  // TODO
-  //   def mapK[G[+ _]](f: F ~> G): Stream[G, A] =
-  //     new Stream(underlying)(f compose liftIO, effect)
 
 }
 
 object Stream {
 
   type Fold[F[+ _], +A, S] = F[(S, S => Boolean, (S, A) => F[S]) => F[S]]
+
+  final def empty[F[+ _]]: Stream[F, Nothing] =
+    new Stream[F, Nothing](ZStream.empty)
+
+  final def never[F[+ _]]: Stream[F, Nothing] =
+    new Stream[F, Nothing](ZStream.never)
+
+  final def succeed[F[+ _], A](a: A): Stream[F, A] =
+    new Stream[F, A](ZStream.succeed(a))
+
+  final def succeedLazy[F[+ _], A](a: => A): Stream[F, A] =
+    new Stream[F, A](ZStream.succeedLazy(a))
+
+  final def fail[F[+ _], E <: Throwable](error: E): Stream[F, Nothing] =
+    new Stream[F, Nothing](ZStream.fail(error))
+
+  final def fromEffect[F[+ _], A](fa: F[A])(implicit E: Effect[F]): Stream[F, A] =
+    new Stream(ZStream.fromEffect[Any, Throwable, A](liftZIO(fa)))
+
+  final def flatten[F[+ _], A](fa: Stream[F, Stream[F, A]]): Stream[F, A] =
+    new Stream(ZStream.flatten(fa.map(_.underlying).underlying))
+
+  final def unwrap[F[+ _], A](stream: F[Stream[F, A]])(implicit E: Effect[F]): Stream[F, A] =
+    new Stream(ZStream.unwrap(liftZIO(stream.map(_.underlying))))
+
+  final def bracket[F[+ _], A, B](
+    acquire: F[A]
+  )(release: A => F[Unit])(read: A => F[Option[B]])(implicit E: Effect[F]): Stream[F, B] =
+    new Stream(
+      ZStream.bracket(liftZIO(acquire))(release.andThen(fa => UIO.apply(E.toIO(fa).unsafeRunSync())))(
+        read.andThen(liftZIO(_))
+      )
+    )
+
+  final def managed[F[+ _], R: ConformsR, E, A, B](
+    m: ZManaged[R, E, A]
+  )(read: A => ZIO[R, E, Option[B]]): Stream[F, B] =
+    ???
+
+  final def fromQueue[F[+ _], A](queue: Queue[A]): Stream[F, A] =
+    new Stream(ZStream.fromQueue(queue))
+
+  final def fromQueue[RB: ConformsR, EB, B](queue: ZQueue[_, _, RB, EB, _, B]): ZStream[RB, EB, B] =
+    ???
+
+  final def unfold[F[+ _], S, A](s: S)(f0: S => Option[(A, S)]): Stream[F, A] =
+    new Stream[F, A](ZStream.unfold(s)(f0))
+
+  final def unfoldM[F[+ _], S, A](s: S)(f0: S => ZIO[Any, Throwable, Option[(A, S)]]): Stream[F, A] =
+    new Stream[F, A](ZStream.unfoldM[Any, S, Throwable, A](s)(f0))
+
+  final def range[F[+ _]](min: Int, max: Int): Stream[F, Int] =
+    new Stream[F, Int](ZStream.range(min, max))
+
+  /*
+  private[stream] implicit def liftZIO[F[+ _]](implicit E: Effect[F]): F ~> ZIO[Any, Throwable, ?] =
+    new cats.arrow.FunctionK[F, ZIO[Any, Throwable, ?]] {
+      override def apply[X](fx: F[X]): ZIO[Any, Throwable, X] =
+        ZIO(E.toIO(fx).unsafeRunSync())
+    }
+
+  private[stream] implicit def liftIO[F[+ _]](implicit R: Runtime[Any], E: Effect[F]): ZIO[Any, Throwable, ?] ~> F =
+    new cats.arrow.FunctionK[ZIO[Any, Throwable, ?], F] {
+      override def apply[X](fx: ZIO[Any, Throwable, X]): F[X] =
+        E.delay(R.unsafeRun(fx))
+    }
+   */
+
+  // FIXME - duplicate code in #790 (shamelessly stole it)
+
+  import scalaz.zio.interop.catz.taskEffectInstances
+
+  type Env = Clock
+
+  private[stream] def liftF[F[+ _], R, A](zio: TaskR[R, A])(implicit R: Runtime[R], E: Effect[F]): F[A] =
+    E.liftIO(taskEffectInstances.toIO(zio))
+
+  private[stream] def liftZIO[F[+ _], R, A](eff: F[A])(implicit E: Effect[F]): TaskR[R, A] =
+    ZIO(E.toIO(eff).unsafeRunSync())
 
 }
