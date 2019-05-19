6e74ad73e2f91e2f5607e34943ce36ee327f5b22
Jan Strnad <hanny.strnad@gmail.com>
2019-05-19 21:07:05 +0200

    More FiberRef changes

--- core/shared/src/test/scala/scalaz/zio/FiberRefSpec.scala
+++ core/shared/src/test/scala/scalaz/zio/FiberRefSpec.scala
@@ -12,6 +12,11 @@ class FiberRefSpec extends BaseCrossPlatformSpec {
       `locally` restores undefined value.                      $e7
       its value is inherited on join.                          $e8
       initial value is always available.                       $e9
+      `update` changes value.                                  $e10
+      `updateSome` changes value.                              $e11
+      `updateSome` not changes value.                          $e12
+      `modify` changes value.                                  $e13
+      `modifySome` not changes value.                          $e14
     """
 
   val (initial, update) = ("initial", "update")
@@ -83,4 +88,47 @@ class FiberRefSpec extends BaseCrossPlatformSpec {
       fiberRef <- child.await.flatMap(ZIO.done)
       value    <- fiberRef.get
     } yield value must beTheSameAs(initial)
+
+  def e10 =
+    for {
+      fiberRef <- FiberRef.make(initial)
+      value1   <- fiberRef.update(_ => update)
+      value2   <- fiberRef.get
+    } yield (value1 must beTheSameAs(update)) and (value2 must beTheSameAs(update))
+
+  def e11 =
+    for {
+      fiberRef <- FiberRef.make(initial)
+      value1 <- fiberRef.updateSome {
+                 case _ => update
+               }
+      value2 <- fiberRef.get
+    } yield (value1 must beTheSameAs(update)) and (value2 must beTheSameAs(update))
+
+  def e12 =
+    for {
+      fiberRef <- FiberRef.make(initial)
+      value1 <- fiberRef.updateSome {
+                 case _ if false => update
+               }
+      value2 <- fiberRef.get
+    } yield (value1 must beTheSameAs(initial)) and (value2 must beTheSameAs(initial))
+
+  def e13 =
+    for {
+      fiberRef <- FiberRef.make(initial)
+      value1 <- fiberRef.modify {
+                 case _ => (1, update)
+               }
+      value2 <- fiberRef.get
+    } yield (value1 must beEqualTo(1)) and (value2 must beTheSameAs(update))
+
+  def e14 =
+    for {
+      fiberRef <- FiberRef.make(initial)
+      value1 <- fiberRef.modifySome(2) {
+                 case _ if false => (1, update)
+               }
+      value2 <- fiberRef.get
+    } yield (value1 must beEqualTo(2)) and (value2 must beTheSameAs(initial))
 }
