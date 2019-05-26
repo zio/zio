415adab7be274324412d2a367ca041ff174042d0
Regis Kuckaertz <regis.kuckaertz@guardian.co.uk>
2019-05-26 09:58:49 +0100

    Make sure await is evaluated

--- core/shared/src/test/scala/scalaz/zio/TestUtils.scala
+++ core/shared/src/test/scala/scalaz/zio/TestUtils.scala
@@ -5,5 +5,12 @@ trait TestUtils {
     Promise.make[Nothing, Unit] >>= (latch => f(latch.succeed(()).unit) <* latch.await)
 
   def withLatch[R, E, A](f: (UIO[Unit], UIO[Unit]) => ZIO[R, E, A]): ZIO[R, E, A] =
-    Promise.make[Nothing, Unit] >>= (latch => f(latch.succeed(()).unit, latch.await))
+    for {
+      ref   <- Ref.make(true)
+      latch <- Promise.make[Nothing, Unit]
+      a <- f(latch.succeed(()).unit, ZIO.uninterruptibleMask { restore =>
+            ref.set(false) *> restore(latch.await)
+          })
+      _ <- UIO.whenM(ref.get)(latch.await)
+    } yield a
 }
