1e039198c1f28d721b0cc4bbf86433f7d75ee8f9
Timothy McCarthy <ebh042@gmail.com>
2019-05-31 17:40:48 +1000

    Add test for converting to Java infinity duration, and then back to zio Infinity

--- core/shared/src/test/scala/scalaz/zio/duration/DurationSpec.scala
+++ core/shared/src/test/scala/scalaz/zio/duration/DurationSpec.scala
@@ -58,10 +58,11 @@ class DurationSpec extends Specification {
           A positive s.c.d.Duration converts to a Finite                       $dur3
 
         Make a Java stdlib j.t.Duration and check that:
-          A negative j.t.Duration converts to Zero                             $jdur1
-          The infinite j.t.Duration converts to Infinity                       $jdur2
-          The infinite j.t.Duration with adjustment converts to Infinity       $jdur3
-          A positive j.t.Duration converts to a Finite                         $jdur4
+          A negative j.t.Duration converts to Zero                               $jdur1
+          A Long.MaxValue second j.t.Duration converts to Infinity               $jdur2
+          A nano-adjusted Long.MaxValue second j.t.Duration converts to Infinity $jdur3
+          A j.t.Duration construted from Infinity converts to Infinity           $jdur4
+          A positive j.t.Duration converts to a Finite                           $jdur5
 
         Check multiplication with finite durations:
           Zero multiplied with zero                                            $mul0
@@ -196,6 +197,9 @@ class DurationSpec extends Specification {
     Duration.fromJava(JavaDuration.ofSeconds(Long.MaxValue, 1L)) must_=== Duration.Infinity
 
   def jdur4 =
+    Duration.fromJava(Duration.Infinity.asJava) must_=== Duration.Infinity
+
+  def jdur5 =
     Duration.fromJava(JavaDuration.ofNanos(1L)) must_=== Duration.fromNanos(1L)
 
   def mul0 =
