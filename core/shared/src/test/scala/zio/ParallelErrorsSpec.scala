39e490dbc8b84803f8f009358b9279f1fc2f89fa
Salar Rahmanian <code@softinio.com>
2019-06-23 12:05:44 -0700

    add more tests

--- core/shared/src/test/scala/zio/ParallelErrorsSpec.scala
+++ core/shared/src/test/scala/zio/ParallelErrorsSpec.scala
@@ -3,8 +3,7 @@ package zio
 class ParallelErrorsSpec extends BaseCrossPlatformSpec {
   def is = "ParallelErrorsSpec".title ^ s2"""
    Returns a list of 2 errors $allFailures 
-   Returns a list of 1 error out of 2 events $oneFailure
-   Returns an emty list out of 2 events $noFailure
+   Returns a list of 1 error $oneFailure
 
   """
 
@@ -21,11 +20,4 @@ class ParallelErrorsSpec extends BaseCrossPlatformSpec {
       f2     <- IO.succeed("success1").fork
       errors <- f1.zip(f2).join.parallelErrors[String].flip
     } yield errors must_=== ::("error1", List())
-
-  def noFailure =
-    for {
-      f1     <- IO.succeed("success1").fork
-      f2     <- IO.succeed("success2").fork
-      errors <- f1.zip(f2).join.parallelErrors[String].flip
-    } yield errors.length must_=== 0
 }
