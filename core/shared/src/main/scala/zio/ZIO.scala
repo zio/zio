1e60b967281127052c29c8de494aa24882f96cd1
Vasil Vasilev <vasil.vasilev97@gmail.com>
2019-07-25 21:40:14 +0200

    Make fold function names consistent

--- core/shared/src/main/scala/zio/ZIO.scala
+++ core/shared/src/main/scala/zio/ZIO.scala
@@ -336,8 +336,8 @@ sealed trait ZIO[-R, +E, +A] extends Serializable { self =>
    * does not fail, but succeeds with the value returned by the left or right
    * function passed to `fold`.
    */
-  final def fold[B](err: E => B, succ: A => B): ZIO[R, Nothing, B] =
-    foldM(new ZIO.MapFn(err), new ZIO.MapFn(succ))
+  final def fold[B](failure: E => B, success: A => B): ZIO[R, Nothing, B] =
+    foldM(new ZIO.MapFn(failure), new ZIO.MapFn(success))
 
   /**
    * A more powerful version of `fold` that allows recovering from any kind of failure except interruptions.
