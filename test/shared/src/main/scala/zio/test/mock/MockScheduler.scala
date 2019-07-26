fa868a7928a8803c7a08e1b1efa4b596fc94f434
Mateusz Sokół <mat646@gmail.com>
2019-07-26 16:40:04 +0200

    Refactored tests that didn't auto-rename

--- test/shared/src/main/scala/zio/test/mock/MockScheduler.scala
+++ test/shared/src/main/scala/zio/test/mock/MockScheduler.scala
@@ -28,7 +28,7 @@ final case class MockScheduler(ref: Ref[MockClock.Data], runtime: Runtime[Clock]
                    }.map(_.map(x => (x._2, x._3)))
         _ <- ZIO.foreach(dequeued) { task =>
               task._1
-                .done(ZIO.succeed(()))
+                .completeWith(ZIO.succeed(()))
                 .flatMap { notInterupted =>
                   if (notInterupted) ZIO.effectTotal(task._2.run())
                   else ZIO.unit
@@ -53,7 +53,7 @@ final case class MockScheduler(ref: Ref[MockClock.Data], runtime: Runtime[Clock]
                 promise     <- Promise.make[Nothing, Unit]
                 _           <- tasksRef.update(tasks => (targetTime, promise, task) :: tasks)
               } yield promise)
-              () => runtime.unsafeRun(promise.done(ZIO.succeed(())))
+              () => runtime.unsafeRun(promise.completeWith(ZIO.succeed(())))
           }
 
         override def size: Int =
