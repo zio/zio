54b3acf13fae8c6501587f60131d2ae5074aee46
Adam Fraser <adam.fraser@gmail.com>
2019-07-27 16:50:16 -0400

    WIP: Create TestEnvironment (#1255)
    * create TestEnvironment
    
    * address review comments
    
    * add additional convenience methods
    
    * rename TestEnvironment to MockEnvironment
    
    * refactor to create mock interfaces and services
    
    * add some tests

--- test/shared/src/main/scala/zio/test/mock/MockConsole.scala
+++ test/shared/src/main/scala/zio/test/mock/MockConsole.scala
@@ -21,50 +21,79 @@ import java.io.EOFException
 
 import zio.console._
 import zio._
-import MockConsole.Data
 
-case class MockConsole(consoleState: Ref[MockConsole.Data]) extends Console.Service[Any] {
-
-  override def putStr(line: String): UIO[Unit] =
-    consoleState.update { data =>
-      Data(data.input, data.output :+ line)
-    }.unit
-
-  override def putStrLn(line: String): ZIO[Any, Nothing, Unit] =
-    consoleState.update { data =>
-      Data(data.input, data.output :+ s"$line\n")
-    }.unit
-
-  val getStrLn: ZIO[Any, IOException, String] = {
-    for {
-      input <- consoleState.get.flatMap(
-                d =>
-                  IO.fromOption(d.input.headOption)
-                    .mapError(_ => new EOFException("There is no more input left to read"))
-              )
-      _ <- consoleState.update { data =>
-            Data(data.input.tail, data.output)
-          }
-    } yield input
-  }
-
-  def feedLines(lines: String*): UIO[Unit] =
-    consoleState.update(data => data.copy(input = lines.toList ::: data.input)).unit
-
-  val output: UIO[Vector[String]] =
-    consoleState.get.map(_.output)
-
-  val clearInput: UIO[Unit] =
-    consoleState.update(data => data.copy(input = List.empty)).unit
-
-  val clearOutput: UIO[Unit] =
-    consoleState.update(data => data.copy(output = Vector.empty)).unit
+trait MockConsole extends Console {
+  val console: MockConsole.Service[Any]
 }
 
 object MockConsole {
 
+  trait Service[R] extends Console.Service[R] {
+    def feedLines(lines: String*): UIO[Unit]
+    def output: UIO[Vector[String]]
+    def clearInput: UIO[Unit]
+    def clearOutput: UIO[Unit]
+  }
+
+  case class Mock(consoleState: Ref[MockConsole.Data]) extends MockConsole.Service[Any] {
+
+    override def putStr(line: String): UIO[Unit] =
+      consoleState.update { data =>
+        Data(data.input, data.output :+ line)
+      }.unit
+
+    override def putStrLn(line: String): ZIO[Any, Nothing, Unit] =
+      consoleState.update { data =>
+        Data(data.input, data.output :+ s"$line\n")
+      }.unit
+
+    val getStrLn: ZIO[Any, IOException, String] = {
+      for {
+        input <- consoleState.get.flatMap(
+                  d =>
+                    IO.fromOption(d.input.headOption)
+                      .mapError(_ => new EOFException("There is no more input left to read"))
+                )
+        _ <- consoleState.update { data =>
+              Data(data.input.tail, data.output)
+            }
+      } yield input
+    }
+
+    def feedLines(lines: String*): UIO[Unit] =
+      consoleState.update(data => data.copy(input = lines.toList ::: data.input)).unit
+
+    val output: UIO[Vector[String]] =
+      consoleState.get.map(_.output)
+
+    val clearInput: UIO[Unit] =
+      consoleState.update(data => data.copy(input = List.empty)).unit
+
+    val clearOutput: UIO[Unit] =
+      consoleState.update(data => data.copy(output = Vector.empty)).unit
+  }
+
   def make(data: Data): UIO[MockConsole] =
-    Ref.make(data).map(MockConsole(_))
+    makeMock(data).map { mock =>
+      new MockConsole {
+        val console = mock
+      }
+    }
+
+  def makeMock(data: Data): UIO[Mock] =
+    Ref.make(data).map(Mock(_))
+
+  def feedLines(lines: String*): ZIO[MockConsole, Nothing, Unit] =
+    ZIO.accessM(_.console.feedLines(lines: _*))
+
+  val output: ZIO[MockConsole, Nothing, Vector[String]] =
+    ZIO.accessM(_.console.output)
+
+  val clearInput: ZIO[MockConsole, Nothing, Unit] =
+    ZIO.accessM(_.console.clearInput)
+
+  val clearOutput: ZIO[MockConsole, Nothing, Unit] =
+    ZIO.accessM(_.console.clearOutput)
 
   val DefaultData: Data = Data(Nil, Vector())
 
