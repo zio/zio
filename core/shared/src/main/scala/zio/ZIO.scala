528df2089d85df8e2d04848f7e79944f542f70d5
Haemin Yoo <haemin@zzz.pe.kr>
2019-08-05 13:58:57 +0900

    Fix scaladoc in ZIO.never

--- core/shared/src/main/scala/zio/ZIO.scala
+++ core/shared/src/main/scala/zio/ZIO.scala
@@ -2074,9 +2074,9 @@ private[zio] trait ZIOFunctions extends Serializable {
   final def provide[R, E, A](r: R): ZIO[R, E, A] => IO[E, A] =
     (zio: ZIO[R, E, A]) => new ZIO.Provide(r, zio)
 
-  /**
-   * Evaluate each effect in the structure in parallel, and collect
-   * the results. For a sequential version, see `collectAll`.
+  /**  
+   * Returns a effect that will never produce anything. The moral
+   * equivalent of `while(true) {}`, only without the wasted CPU cycles.
    */
   final val never: UIO[Nothing] = effectAsync[Any, Nothing, Nothing](_ => ())
 
