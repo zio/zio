52b0ad7691729e2802152fb3a611964a55e8c835
J. Eduardo V. Morango <edvmorango@gmail.com>
2019-08-14 00:57:53 -0300

    either tests

--- examples/jvm/src/test/scala/zio/examples/test/EffectsExampleSpec.scala
+++ examples/jvm/src/test/scala/zio/examples/test/EffectsExampleSpec.scala
@@ -1,6 +1,5 @@
 package examples
 
-import examples.EffectSuites.basicSuite
 import zio.ZIO
 import zio.test.Predicate._
 import zio.test.{ assertM, suite, testM, DefaultRunnableSpec, Predicate }
@@ -15,13 +14,19 @@ object EffectsExampleSpec
           testM("Effect failures") {
             assertM(ZIO.fail("Failure").run, fails(Predicate.equals("Failure")))
           },
-          testM("Effect succeed(through Exit)") {
+          testM("Effect succeed (through Exit)") {
             assertM(ZIO.succeed("Success").run, succeeds(Predicate.equals("Success")))
           },
           testM("Environment (through effect-elimination)") {
             case class Config(something: String)
             val env = Config("value")
-            assertM(ZIO.access[Config](_.something).provide(env), Predicate.equals(env.something))
+            assertM(ZIO.access[Config](identity).provide(env), Predicate.equals(env))
+          },
+          testM("Effect failure (throgh effect-elimination") {
+            assertM(ZIO.fail("Failure").either, isLeft(Predicate.equals("Failure")))
+          },
+          testM("Effect success (throgh effect-elimination") {
+            assertM(ZIO.succeed("Success").either, isRight(Predicate.equals("Success")))
           }
         )
       )
