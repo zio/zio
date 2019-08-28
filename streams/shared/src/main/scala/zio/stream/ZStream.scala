cb210d34c0f5f0f74ed49c27af72ddb5f5628976
Jagadesh Adireddi <adireddijagadesh@gmail.com>
2019-08-28 17:22:56 +0530

    Add ZStrem#zip left and right operators and alias for all zip operators

--- streams/shared/src/main/scala/zio/stream/ZStream.scala
+++ streams/shared/src/main/scala/zio/stream/ZStream.scala
@@ -1793,6 +1793,42 @@ class ZStream[-R, +E, +A](val process: ZManaged[R, E, Pull[R, E, A]]) extends Se
    */
   final def zipWithIndex: ZStream[R, E, (A, Int)] =
     mapAccum(0)((index, a) => (index + 1, (a, index)))
+
+  /**
+    * Operator alias for `zip`
+    */
+  final def <*>[R1 <: R, E1 >: E, B](that: ZStream[R1, E1, B]): ZStream[R1, E1, (A, B)] =
+    self zip that
+
+  /**
+    * Operator alias for `zip`
+    */
+  final def &&&[R1 <: R, E1 >: E, B](that: ZStream[R1, E1, B]): ZStream[R1, E1, (A, B)] =
+    self zip that
+
+  /**
+    * Runs this stream with the specified stream and keeps only values of this stream.
+    */
+  final def zipLeft[R1 <: R, E1 >: E, B](that: ZStream[R1, E1, B]): ZStream[R1, E1, A] =
+    self.zipWith(that)((left, right) => left.flatMap(a => right.map(a -> _))).map(_._1)
+
+  /**
+    * Runs this stream with the specified stream and keeps only values of specified stream.
+    */
+  final def zipRight[R1 <: R, E1 >: E, B](that: ZStream[R1, E1, B]): ZStream[R1, E1, B] =
+    self.zipWith(that)((left, right) => left.flatMap(a => right.map(a -> _))).map(_._2)
+
+  /**
+    * Operator alias for `zipLeft`
+    */
+  final def <*[R1 <: R, E1 >: E, B](that: ZStream[R1, E1, B]): ZStream[R1, E1, A] =
+    self zipLeft that
+
+  /**
+    * Operator alias for `zipRight`
+    */
+  final def *>[R1 <: R, E1 >: E, B](that: ZStream[R1, E1, B]): ZStream[R1, E1, B] =
+    self zipRight that
 }
 
 object ZStream extends ZStreamPlatformSpecific {
