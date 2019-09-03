47d74965c6dff55a16f4985cfc72b3fee6b30127
Regis Kuckaertz <regis.kuckaertz@guardian.co.uk>
2019-09-03 10:26:25 +0100

    Use latest changes

--- streams-tests/jvm/src/test/scala/zio/stream/StreamChunkSpec.scala
+++ streams-tests/jvm/src/test/scala/zio/stream/StreamChunkSpec.scala
@@ -1,162 +1,194 @@
 package zio.stream
 
 import scala.{ Stream => _ }
-import zio.{ Chunk, IO }
+import zio.{ Chunk, IO, Ref }
 import zio.test._
+import zio.test.Assertion.equalTo
 import generators._
 
 object StreamChunkSpec
     extends DefaultRunnableSpec(
       suite("StreamChunkSpec")(
-        Spec.test(
-          "StreamChunk.map",
+        testM("StreamChunk.map") {
           check(streamChunkGen(Gen.string(Gen.anyChar)), Gen[String => Int]) { (s, f) =>
-            assertMM(slurp(s.map(f)), slurp(s).map(_.map(f)))
+            for {
+              res1 <- slurp(s.map(f))
+              res2 <- slurp(s).map(_.map(f))
+            } yield assert(res1, equalTo(res2))
           }
-        ),
-        Spec.test(
-          "StreamChunk.filter",
+        },
+        testM("StreamChunk.filter") {
           check(streamChunkGen(Gen.string(Gen.anyChar)), Gen[String => Boolean]) { (s, p) =>
-            assertMM(slurp(s.filter(p)), slurp(s).map(_.filter(p)))
+            for {
+              res1 <- slurp(s.filter(p))
+              res2 <- slurp(s).map(_.filter(p))
+            } yield assert(res1, equalTo(res2))
           }
-        ),
-        Spec.test(
-          "StreamChunk.filterNot",
+        },
+        testM("StreamChunk.filterNot") {
           check(streamChunkGen(Gen.string(Gen.anyChar)), Gen[String => Boolean]) { (s, p) =>
-            assertMM(slurp(s.filterNot(p)), slurp(s).map(_.filterNot(p)))
+            for {
+              res1 <- slurp(s.filterNot(p))
+              res2 <- slurp(s).map(_.filterNot(p))
+            } yield assert(res1, equalTo(res2))
           }
-        ),
-        Spec.test(
-          "StreamChunk.mapConcat",
+        },
+        testM("StreamChunk.mapConcat") {
           check(streamChunkGen(Gen.string(Gen.anyChar)), Gen[String => Chunk[Int]]) { (s, f) =>
-            assertMM(slurp(s.mapConcat(f)), slurp(s).map(_.flatMap(v => f(v).toSeq)))
+            for {
+              res1 <- slurp(s.mapConcat(f))
+              res2 <- slurp(s).map(_.flatMap(v => f(v).toSeq))
+            } yield assert(res1, equalTo(res2))
           }
-        ),
-        Spec.test(
-          "StreamChunk.dropWhile",
+        },
+        testM("StreamChunk.dropWhile") {
           check(streamChunkGen(Gen.string(Gen.anyChar)), Gen[String => Boolean]) { (s, p) =>
-            assertMM(slurp(s.dropWhile(p)), slurp(s).map(_.dropWhile(p)))
+            for {
+              res1 <- slurp(s.dropWhile(p))
+              res2 <- slurp(s).map(_.dropWhile(p))
+            } yield assert(res1, equalTo(res2))
           }
-        ),
-        Spec.test(
-          "StreamChunk.takeWhile",
+        },
+        testM("StreamChunk.takeWhile") {
           check(succeededStreamChunkGen(Gen.string(Gen.anyChar)), Gen[String => Boolean]) { (s, p) =>
-            assertMM(slurp(s.takeWhile(p)), slurp(s).map(_.takeWhile(p)))
+            for {
+              res1 <- slurp(s.takeWhile(p))
+              res2 <- slurp(s).map(_.takeWhile(p))
+            } yield assert(res1, equalTo(res2))
           }
-        ),
-        Spec.test(
-          "StreamChunk.mapAccum",
+        },
+        testM("StreamChunk.mapAccum") {
           check(streamChunkGen(Gen.anyInt)) { s =>
-            val slurped = slurp(s.mapAccum(0)((acc, el) => (acc + el, acc + el)))
-            assertMM(slurped, slurp(s).map(_.scanLeft(0)((acc, el) => acc + el).drop(1)))
+            for {
+              res1 <- slurp(s.mapAccum(0)((acc, el) => (acc + el, acc + el)))
+              res2 <- slurp(s).map(_.scanLeft(0)((acc, el) => acc + el).drop(1))
+            } yield assert(res1, equalTo(res2))
+          }
+        },
+        testM(
+          "StreamChunk.mapM",
+          check(streamChunkGen(Gen.anyInt), Gen[Int => Int]) { (s, f) =>
+            for {
+              res1 <- slurp(s.mapM(a => IO.succeed(f(a))))
+              res2 <- slurp(s).map(_.map(f))
+            } yield assert(res1, equalTo(res2))
           }
         ),
-        Spec.test("StreamChunk.mapM", check(streamChunkGen(Gen.anyInt), Gen[Int => Int]) { (s, f) =>
-          assertMM(slurp(s.mapM(a => IO.succeed(f(a)))), slurp(s).map(_.map(f)))
-        }),
-        Spec.test(
-          "StreamChunk.++",
+        testM("StreamChunk.++") {
           check(streamChunkGen(Gen.string(Gen.anyChar)), streamChunkGen(Gen.string(Gen.anyChar))) { (s1, s2) =>
-            val listConcat = for {
-              left  <- slurp(s1)
-              right <- slurp(s2)
-            } yield left ++ right
-            val streamConcat = slurp(s1 ++ s2)
-
-            assertMM(streamConcat, listConcat)
+            for {
+              res1 <- slurp(s1).zipWith(s2, _ ++ _)
+              res2 <- slurp(s1 ++ s2)
+            } yield assert(res1, equalTo(res2))
+          }
+        },
+        testM(
+          "StreamChunk.zipWithIndex",
+          check(streamChunkGen(Gen.string(Gen.anyChar))) { s =>
+            for {
+              res1 <- slurp(s.zipWithIndex)
+              res2 <- slurp(s).map(_.zipWithIndex)
+            } yield assert(res1, equalTo(res2))
           }
         ),
-        Spec.test("StreamChunk.zipWithIndex", check(streamChunkGen(Gen.string(Gen.anyChar))) { s =>
-          assertMM(slurp(s.zipWithIndex), slurp(s).map(_.zipWithIndex))
-        }),
-        Spec.test(
-          "StreamChunk.foreach0",
+        testM("StreamChunk.foreach0") {
           check(streamChunkGen(Gen.anyInt), Gen[Int => Boolean]) { (s, cont) =>
-            var acc = List[Int]()
-
-            val result = s.foreachWhile { a =>
-              IO.effectTotal {
-                if (cont(a)) {
-                  acc ::= a
-                  true
-                } else false
-              }
-            }
-
-            assertMM(result.map(_ => acc.reverse), slurp(s.takeWhile(cont)).map(_.toList))
+            for {
+              acc <- Ref.make[List[Int]](Nil)
+              res1 <- s.foreachWhile { a =>
+                       if (cont(a))
+                         acc.update(a :: _) *> ZIO.succeed(true)
+                       else
+                         ZIO.succeed(false)
+                     }.flatMap(_ => acc.update(_.reverse))
+              res2 <- slurp(s.takeWhile(cont)).map(_.toList)
+            } yield assert(res1, equalTo(res2))
           }
-        ),
-        Spec.test(
-          "StreamChunk.foreach",
+        },
+        testM("StreamChunk.foreach") {
           check(streamChunkGen(Gen.anyInt)) { s =>
-            var acc = List[Int]()
-
-            val result = s.foreach(a => IO.effectTotal(acc ::= a))
-
-            assertMM(result.map(_ => acc.reverse), slurp(s).map(_.toList))
+            for {
+              acc <- Ref.make[List[Int]](Nil)
+              res1 <- s.foreach(a => acc.update(a :: _)).map(_ => acc.reverse)
+              res2 <- slurp(s).map(_.toList)
+            } yield assert(res1, equalTo(res2))
           }
-        ),
-        Spec.test(
-          "StreamChunk.monadLaw1",
+        },
+        testM("StreamChunk.monadLaw1") {
           check(Gen.anyInt, Gen[Int => StreamChunk[String, Int]]) { (x, f) =>
-            assertMM(slurp(ZStreamChunk.succeed(Chunk(x)).flatMap(f)), slurp(f(x)))
+            for {
+              res1 <- slurp(ZStreamChunk.succeed(Chunk(x)).flatMap(f))
+              res2 <- slurp(f(x))
+            } yield assert(res1, equalTo(res2))
           }
-        ),
-        Spec.test("StreamChunk.monadLaw2", check(streamChunkGen(Gen.anyInt)) { m =>
-          assertMM(slurp(m.flatMap(i => ZStreamChunk.succeed(Chunk(i)))), slurp(m))
-        }),
-        Spec.test(
-          "StreamChunk.monadLaw3",
+        },
+        testM("StreamChunk.monadLaw2") {
+          check(streamChunkGen(Gen.anyInt)) { m =>
+            for {
+              res1 <- slurp(m.flatMap(i => ZStreamChunk.succeed(Chunk(i))))
+              res2 <- slurp(m)
+            } yield assert(res1, equalTo(res2))
+          }
+        },
+        testM("StreamChunk.monadLaw3") {
           check(streamChunkGen(Gen.anyInt), Gen[Int => StreamChunk[String, Int]], Gen[Int => StreamChunk[String, Int]]) {
             (m, f, g) =>
               val leftStream  = m.flatMap(f).flatMap(g)
               val rightStream = m.flatMap(x => f(x).flatMap(g))
-              assertMM(slurp(leftStream), slurp(rightStream))
-          }
-        ),
-        Spec.test(
-          "StreamChunk.tap",
-          check(streamChunkGen(Gen.string(Gen.anyChar))) { s =>
-            val withoutEffect = slurp(s)
-            var acc           = List[String]()
-            val tap           = slurp(s.tap(a => IO.effectTotal(acc ::= a)))
 
-            assertMM(tap, withoutEffect) &&
-              assertMM(withoutEffect.succeeded Assertion.implies (Success(acc.reverse) must_== withoutEffect) when withoutEffect.succeeded)
+              for {
+                res1 <- slurp(leftStream)
+                res2 <- slurp(rightStream)
+              } yield assert(res1, equalTo(res2))
           }
-        ),
-        Spec.test(
-          "StreamChunk.foldLeft",
+        },
+        // testM("StreamChunk.tap") {
+        //   check(streamChunkGen(Gen.string(Gen.anyChar))) { s =>
+        //     val withoutEffect = slurp(s)
+        //     var acc           = List[String]()
+        //     val tap           = slurp(s.tap(a => IO.effectTotal(acc ::= a)))
+
+        //     assertMM(tap, withoutEffect) &&
+        //       assertMM(withoutEffect.succeeded Assertion.implies (Success(acc.reverse) must_== withoutEffect) when withoutEffect.succeeded)
+        //   }
+        // },
+        testM("StreamChunk.foldLeft") {
           check(streamChunkGen(Gen.string(Gen.anyChar)), Gen.anyInt, Gen[(Int, String) => Int]) { (s, zero, f) =>
-            s.foldLeft(zero)(f) must_=== slurp(s).map(_.foldLeft(zero)(f))
+            for {
+              res1 <- s.foldLeft(zero)(f)
+              res2 <- slurp(s).map(_.foldLeft(zero)(f))
+            } yield assert(res1, equalTo(res2))
           }
-        ),
-        Spec.test(
-          "StreamChunk.fold",
+        },
+        testM("StreamChunk.fold") {
           check(
             succeededStreamChunkGen(Gen.string(Gen.anyChar)),
             Gen.anyInt,
             Gen[Int => Boolean],
             Gen[(Int, String) => Int]
           ) { (s, zero, cont, f) =>
-            val streamResult = s.fold[Any, Nothing, String, Int](zero)(cont)((acc, a) => IO.succeed(f(acc, a)))
-            val listResult   = slurp(s).map(l => foldLazyList(l.toList, zero)(cont)(f))
-            streamResult must_=== listResult
+            for {
+              res1 <- s.fold[Any, Nothing, String, Int](zero)(cont)((acc, a) => IO.succeed(f(acc, a)))
+              res2 <- slurp(s).map(l => foldLazyList(l.toList, zero)(cont)(f))
+            } yield assert(res1, equalTo(res2))
           }
-        ),
-        Spec.test(
-          "StreamChunk.flattenChunks",
+        },
+        testM("StreamChunk.flattenChunks") {
           check(streamChunkGen(Gen.string(Gen.anyChar))) { s =>
-            val result = s.flattenChunks.foldLeft[String, List[String]](Nil)((acc, a) => a :: acc).map(_.reverse)
-            result must_== slurp(s)
+            for {
+              res1 <- s.flattenChunks.foldLeft[String, List[String]](Nil)((acc, a) => a :: acc).map(_.reverse)
+              res2 <- slurp(s)
+            } yield assert(res1, equalTo(res2))
           }
-        ),
-        Spec.test(
-          "StreamChunk.collect",
+        },
+        testM("StreamChunk.collect") {
           check(streamChunkGen(Gen.string(Gen.anyChar)), Gen[PartialFunction[String, String]]) { (s, p) =>
-            slurp(s.collect(p)) must_=== slurp(s).map(_.collect(p))
+            for {
+              res1 <- slurp(s.collect(p))
+              res2 <- slurp(s).map(_.collect(p))
+            } yield assert(res1, equalTo(res2))
           }
-        )
+        }
       )
     )
 //   private def foldLazyList[S, T](list: List[T], zero: S)(cont: S => Boolean)(f: (S, T) => S): S = {
