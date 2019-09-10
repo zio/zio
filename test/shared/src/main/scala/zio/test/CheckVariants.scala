6a29b046630d7fcb3fb5920d8fdf31d0a5aa3674
Adam Fraser <adam.fraser@gmail.com>
2019-09-10 09:54:46 -0400

    enable tracing for test execution

--- test/shared/src/main/scala/zio/test/CheckVariants.scala
+++ test/shared/src/main/scala/zio/test/CheckVariants.scala
@@ -212,12 +212,12 @@ trait CheckVariants {
       case (initial, index) =>
         initial.traverse(
           input =>
-            test(input)
+            test(input).traced
               .map(_.map(_.left.map(_.copy(gen = Some(GenFailureDetails(initial.value, input, index))))))
               .either
         )
     }.dropWhile(!_.value.fold(_ => true, _.isFailure)) // Drop until we get to a failure
-      .take(1)                                          // Get the first failure
+      .take(1)                                         // Get the first failure
       .flatMap(_.shrinkSearch(_.fold(_ => true, _.isFailure)).take(maxShrinks))
       .run(ZSink.collectAll[Either[E, TestResult]]) // Collect all the shrunken failures
       .flatMap { failures =>
