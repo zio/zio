c65e137701fec2ac2aada75f115809212f17adab
fagossa <fagossa@gmail.com>
2019-09-13 16:13:35 +0200

    Split SystemSpec into several suites

--- core-tests/jvm/src/test/scala/zio/system/SystemSpec.scala
+++ core-tests/jvm/src/test/scala/zio/system/SystemSpec.scala
@@ -9,30 +9,36 @@ import scala.reflect.io.File
 class SystemSpec
     extends ZIOBaseSpec(
       suite("SystemSpec")(
-        testM("Fetch an environment variable and check that If it exists, return a reasonable value") {
-          for {
-            io <- system.env("PATH")
-          } yield assert(io, isSome(hasSubstring(File.separator + "bin")))
-        },
-        testM("Fetch an environment variable and check that If it does not exist, return None") {
-          for {
-            io <- system.env("QWERTY")
-          } yield assert(io, isNone)
-        },
-        testM("Fetch a VM property and check that If it exists, return a reasonable value") {
-          for {
-            io <- property("java.vm.name")
-          } yield assert(io, isSome(equalTo("VM")))
-        },
-        testM("Fetch a VM property and check that If it does not exist, return None") {
-          for {
-            io <- property("qwerty")
-          } yield assert(io, isNone)
-        },
-        testM("Fetch the system's line separator and check that it is identical to System.lineSeparator") {
-          for {
-            separator <- lineSeparator
-          } yield assert(separator, equalTo(java.lang.System.lineSeparator))
-        }
+        suite("Fetch an environment variable and check that")(
+          testM("If it exists, return a reasonable value") {
+            for {
+              io <- system.env("PATH")
+            } yield assert(io, isSome(hasSubstring(File.separator + "bin")))
+          },
+          testM("Fetch an environment variable and check that If it does not exist, return None") {
+            for {
+              io <- system.env("QWERTY")
+            } yield assert(io, isNone)
+          }
+        ),
+        suite("Fetch a VM property and check that")(
+          testM("If it exists, return a reasonable value") {
+            for {
+              io <- property("java.vm.name")
+            } yield assert(io, isSome(equalTo("VM")))
+          },
+          testM("If it does not exist, return None") {
+            for {
+              io <- property("qwerty")
+            } yield assert(io, isNone)
+          }
+        ),
+        suite("Fetch the system's line separator and check that")(
+          testM("it is identical to System.lineSeparator") {
+            for {
+              separator <- lineSeparator
+            } yield assert(separator, equalTo(java.lang.System.lineSeparator))
+          }
+        )
       )
     )
