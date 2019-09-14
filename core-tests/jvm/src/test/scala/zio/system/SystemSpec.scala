bf8b392380ff667fb84fdbfc1b6a8a91aa02853e
fagossa <fagossa@gmail.com>
2019-09-14 16:34:26 +0200

    Migrates some tests to ZIOTests (#1668)
    * Migrates SystemSpec
    
    * Add 'substring' assertion test
    
    * Split SystemSpec into several suites
    
    * Migrates BlockingSpec
    
    * uses testM in SystemSpec and BlockingSpec
    
    * Fix PR comments

--- core-tests/jvm/src/test/scala/zio/system/SystemSpec.scala
+++ core-tests/jvm/src/test/scala/zio/system/SystemSpec.scala
@@ -1,44 +1,34 @@
 package zio
 package system
 
-import org.specs2.Specification
+import zio.test._
+import zio.test.Assertion._
+
 import scala.reflect.io.File
 
-class SystemSpec extends Specification with DefaultRuntime {
-  def is = s2"""
-    Fetch an environment variable and check that:
-      If it exists, return a reasonable value                         $env1
-      If it does not exist, return None                               $env2
-
-    Fetch a VM property and check that:
-      If it exists, return a reasonable value                         $prop1
-      If it does not exist, return None                               $prop2
-
-    Fetch the system's line separator and check that:
-      It is identical to System.lineSeparator                         $lineSep1
-  """
-
-  def env1 = {
-    val io = unsafeRun(system.env("PATH"))
-    io must beSome
-    io.get must contain(File.separator + "bin")
-  }
-
-  def env2 = {
-    val io = unsafeRun(system.env("QWERTY"))
-    io must beNone
-  }
-
-  def prop1 = {
-    val io = unsafeRun(property("java.vm.name"))
-    io must beSome
-    io.get must contain("VM")
-  }
-
-  def prop2 = {
-    val io = unsafeRun(property("qwerty"))
-    io must beNone
-  }
-
-  def lineSep1 = unsafeRun(lineSeparator) must_=== (java.lang.System.lineSeparator)
-}
+class SystemSpec
+    extends ZIOBaseSpec(
+      suite("SystemSpec")(
+        suite("Fetch an environment variable and check that")(
+          testM("If it exists, return a reasonable value") {
+            assertM(system.env("PATH"), isSome(containsString(File.separator + "bin")))
+          },
+          testM("If it does not exist, return None") {
+            assertM(system.env("QWERTY"), isNone)
+          }
+        ),
+        suite("Fetch a VM property and check that")(
+          testM("If it exists, return a reasonable value") {
+            assertM(property("java.vm.name"), isSome(equalTo("VM")))
+          },
+          testM("If it does not exist, return None") {
+            assertM(property("qwerty"), isNone)
+          }
+        ),
+        suite("Fetch the system's line separator and check that")(
+          testM("it is identical to System.lineSeparator") {
+            assertM(lineSeparator, equalTo(java.lang.System.lineSeparator))
+          }
+        )
+      )
+    )
