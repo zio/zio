2c245ef964bb1020548452667629d03bb1e61269
Adam Fraser <adam.fraser@gmail.com>
2019-09-14 21:48:46 -0400

    ZIO Test: Fix Issues in Test Migration (#1680)
    * fix tests
    
    * fix compilation error in benchmarks

--- core-tests/jvm/src/test/scala/zio/system/SystemSpec.scala
+++ core-tests/jvm/src/test/scala/zio/system/SystemSpec.scala
@@ -2,32 +2,33 @@ package zio
 package system
 
 import zio.test._
+import zio.test.mock._
 import zio.test.Assertion._
 
 import scala.reflect.io.File
 
-class SystemSpec
+object SystemSpec
     extends ZIOBaseSpec(
       suite("SystemSpec")(
         suite("Fetch an environment variable and check that")(
           testM("If it exists, return a reasonable value") {
-            assertM(system.env("PATH"), isSome(containsString(File.separator + "bin")))
+            assertM(live(system.env("PATH")), isSome(containsString(File.separator + "bin")))
           },
           testM("If it does not exist, return None") {
-            assertM(system.env("QWERTY"), isNone)
+            assertM(live(system.env("QWERTY")), isNone)
           }
         ),
         suite("Fetch a VM property and check that")(
           testM("If it exists, return a reasonable value") {
-            assertM(property("java.vm.name"), isSome(equalTo("VM")))
+            assertM(live(property("java.vm.name")), isSome(containsString("VM")))
           },
           testM("If it does not exist, return None") {
-            assertM(property("qwerty"), isNone)
+            assertM(live(property("qwerty")), isNone)
           }
         ),
         suite("Fetch the system's line separator and check that")(
           testM("it is identical to System.lineSeparator") {
-            assertM(lineSeparator, equalTo(java.lang.System.lineSeparator))
+            assertM(live(lineSeparator), equalTo(java.lang.System.lineSeparator))
           }
         )
       )
