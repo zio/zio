8c7d1fa9207ac04c5d2e0b51c2193449516548da
Chia-Hung Lin <clin4j@googlemail.com>
2019-09-20 23:18:16 +0200

    Format ZIOSpec with fmt in sbt

--- core-tests/shared/src/test/scala/zio/ZIOSpec.scala
+++ core-tests/shared/src/test/scala/zio/ZIOSpec.scala
@@ -4,21 +4,21 @@ import zio.test._
 import zio.test.Assertion._
 
 object ZIOSpec
-  extends ZIOBaseSpec(
-    suite("ZIOSpec")(
-      testM("oneFailure"){
-        for {
-          f1     <- IO.fail("error1").fork
-          f2     <- IO.succeed("success1").fork
-          errors <- f1.zip(f2).join.parallelErrors[String].flip
-        } yield assert(errors, equalTo(::("error1", List())))
-      },
-      testM("allFailures"){
-        for {
-          f1     <- IO.fail("error1").fork
-          f2     <- IO.fail("error2").fork
-          errors <- f1.zip(f2).join.parallelErrors[String].flip
-        } yield assert(errors, equalTo( ::("error1", List("error2"))))
-      }
+    extends ZIOBaseSpec(
+      suite("ZIOSpec")(
+        testM("oneFailure") {
+          for {
+            f1     <- IO.fail("error1").fork
+            f2     <- IO.succeed("success1").fork
+            errors <- f1.zip(f2).join.parallelErrors[String].flip
+          } yield assert(errors, equalTo(::("error1", List())))
+        },
+        testM("allFailures") {
+          for {
+            f1     <- IO.fail("error1").fork
+            f2     <- IO.fail("error2").fork
+            errors <- f1.zip(f2).join.parallelErrors[String].flip
+          } yield assert(errors, equalTo(::("error1", List("error2"))))
+        }
+      )
     )
-  )
