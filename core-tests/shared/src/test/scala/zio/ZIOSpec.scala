0e82136dcf604e174fa679a3fed1e9ee79bb6930
Lech Głowiak <LGLO@users.noreply.github.com>
2019-09-21 20:16:06 +0200

    Moved raceAll test to ZIOSpec (#1752)

--- core-tests/shared/src/test/scala/zio/ZIOSpec.scala
+++ core-tests/shared/src/test/scala/zio/ZIOSpec.scala
@@ -1,24 +1,39 @@
 package zio
 
+import zio.duration._
 import zio.test._
+import zio.test.mock.live
 import zio.test.Assertion._
 
 object ZIOSpec
     extends ZIOBaseSpec(
-      suite("ZIO#parallelErrors")(
-        testM("oneFailure") {
-          for {
-            f1     <- IO.fail("error1").fork
-            f2     <- IO.succeed("success1").fork
-            errors <- f1.zip(f2).join.parallelErrors[String].flip
-          } yield assert(errors, equalTo(List("error1")))
-        },
-        testM("allFailures") {
-          for {
-            f1     <- IO.fail("error1").fork
-            f2     <- IO.fail("error2").fork
-            errors <- f1.zip(f2).join.parallelErrors[String].flip
-          } yield assert(errors, equalTo(List("error1", "error2")))
-        }
+      suite("ZIO")(
+        suite("parallelErrors")(
+          testM("oneFailure") {
+            for {
+              f1     <- IO.fail("error1").fork
+              f2     <- IO.succeed("success1").fork
+              errors <- f1.zip(f2).join.parallelErrors[String].flip
+            } yield assert(errors, equalTo(List("error1")))
+          },
+          testM("allFailures") {
+            for {
+              f1     <- IO.fail("error1").fork
+              f2     <- IO.fail("error2").fork
+              errors <- f1.zip(f2).join.parallelErrors[String].flip
+            } yield assert(errors, equalTo(List("error1", "error2")))
+          }
+        ),
+        suite("raceAll")(
+          testM("returns first sucess") {
+            assertM(ZIO.fail("Fail").raceAll(List(IO.succeed(24))), equalTo(24))
+          },
+          testM("returns last failure") {
+            assertM(live(ZIO.sleep(100.millis) *> ZIO.fail(24)).raceAll(List(ZIO.fail(25))).flip, equalTo(24))
+          },
+          testM("returns success when it happens after failure") {
+            assertM(ZIO.fail(42).raceAll(List(IO.succeed(24) <* live(ZIO.sleep(100.millis)))), equalTo(24))
+          }
+        )
       )
     )
