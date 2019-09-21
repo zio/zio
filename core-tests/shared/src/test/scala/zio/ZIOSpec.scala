ffca42e619e73bc877413101d89cc003d83a3938
chlin501 <chlin501@users.noreply.github.com>
2019-09-21 17:01:32 +0000

    Migrate ZIOSpec to ZIO Test (#1647) (#1721)
    * Migrate ZIOSpec to ZIO Test (#1647)
    
    * Format ZIOSpec with fmt in sbt
    
    * Rename suite label and use more standard way of adding list element
    
    * Rename suite label to ZIO#parallelErrors
    
    * Add list element in a more standard way
    
    * Make a list via the companion o1bject factory

--- core-tests/shared/src/test/scala/zio/ZIOSpec.scala
+++ core-tests/shared/src/test/scala/zio/ZIOSpec.scala
@@ -1,24 +1,24 @@
 package zio
 
-class ZIOSpec extends BaseCrossPlatformSpec {
-  def is = "ZIOSpec".title ^ s2"""
-    ZIO.parallelErrors
-      Returns a list of 2 errors $allFailures
-      Returns a list of 1 error $oneFailure
+import zio.test._
+import zio.test.Assertion._
 
-  """
-
-  def allFailures =
-    for {
-      f1     <- IO.fail("error1").fork
-      f2     <- IO.fail("error2").fork
-      errors <- f1.zip(f2).join.parallelErrors[String].flip
-    } yield errors must_=== ::("error1", List("error2"))
-
-  def oneFailure =
-    for {
-      f1     <- IO.fail("error1").fork
-      f2     <- IO.succeed("success1").fork
-      errors <- f1.zip(f2).join.parallelErrors[String].flip
-    } yield errors must_=== ::("error1", List())
-}
+object ZIOSpec
+    extends ZIOBaseSpec(
+      suite("ZIO#parallelErrors")(
+        testM("oneFailure") {
+          for {
+            f1     <- IO.fail("error1").fork
+            f2     <- IO.succeed("success1").fork
+            errors <- f1.zip(f2).join.parallelErrors[String].flip
+          } yield assert(errors, equalTo(List("error1")))
+        },
+        testM("allFailures") {
+          for {
+            f1     <- IO.fail("error1").fork
+            f2     <- IO.fail("error2").fork
+            errors <- f1.zip(f2).join.parallelErrors[String].flip
+          } yield assert(errors, equalTo(List("error1", "error2")))
+        }
+      )
+    )
