eb10df92de924c8281f5944ff00347e8bd605dbb
Adam Fraser <adam.fraser@gmail.com>
2019-09-24 16:20:21 -0400

    Upgrade Dotty Version (#1814)
    * upgrade dotty version
    
    * -noindent
    
    * fix build settings

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -48,7 +48,13 @@ object BuildHelper {
 
   val dottySettings = Seq(
     // Keep this consistent with the version in .circleci/config.yml
-    crossScalaVersions += "0.19.1-bin-20190904-beba63a-NIGHTLY",
+    crossScalaVersions += "0.19.0-RC1",
+    scalacOptions ++= {
+      if (isDotty.value)
+        Seq("-noindent")
+      else
+        Seq()
+    },
     libraryDependencies := libraryDependencies.value.map(_.withDottyCompat(scalaVersion.value)),
     sources in (Compile, doc) := {
       val old = (Compile / doc / sources).value
