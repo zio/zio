45a343045d389e6c9143fd7c9b67861d81706bd4
Adam Fraser <adam.fraser@gmail.com>
2019-09-24 15:24:46 -0400

    -noindent

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -49,6 +49,7 @@ object BuildHelper {
   val dottySettings = Seq(
     // Keep this consistent with the version in .circleci/config.yml
     crossScalaVersions += "0.19.0-RC1",
+    scalacOptions ++= Seq("-noindent"),
     libraryDependencies := libraryDependencies.value.map(_.withDottyCompat(scalaVersion.value)),
     sources in (Compile, doc) := {
       val old = (Compile / doc / sources).value
