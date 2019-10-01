c3f21ad309adad310fd27cb26ac24bf296e66771
Lech Głowiak <lech.glowiak@gmail.com>
2019-10-01 21:21:54 +0200

    Moved SemaphoreSpec to shared. Removed duplication in MutableConcurrentQueueSpec by moving to shared.

--- core-tests/jvm/src/test/scala/zio/internal/MutableConcurrentQueueSpecJVM.scala
+++ core-tests/jvm/src/test/scala/zio/internal/MutableConcurrentQueueSpecJVM.scala
@@ -11,50 +11,9 @@ import zio.test._
  *
  * Concurrent tests are run via jcstress and are in [[RingBufferConcurrencyTests]].
  */
-object MutableConcurrentQueueSpec2
+object MutableConcurrentQueueJVM
     extends ZIOBaseSpec(
       suite("MutableConcurrentQueueSpec")(
-        suite("Make a bounded MutableConcurrentQueue")(
-          test("of capacity 1 return a queue of capacity 1") {
-            val q = MutableConcurrentQueue.bounded(1)
-
-            assert(q.capacity, equalTo(1))
-          },
-          test("of capacity 2 return a queue of capacity 2") {
-            val q = MutableConcurrentQueue.bounded(2)
-
-            assert(q.capacity, equalTo(2))
-          },
-          test("of capacity 3 return a queue of capacity 3") {
-            val q = MutableConcurrentQueue.bounded(3)
-
-            assert(q.capacity, equalTo(3))
-          }
-        ),
-        suite("With a RingBuffer of capacity 2")(
-          test("`offer` of 2 items succeeds, further offers fail") {
-            val q = MutableConcurrentQueue.bounded[Int](2)
-
-            (assert(q.offer(1), isTrue)
-            && assert(q.size(), equalTo(1))
-            && assert(q.offer(2), isTrue)
-            && assert(q.size(), equalTo(2))
-            && assert(q.offer(3), isFalse)
-            && assert(q.isFull(), isTrue))
-          },
-          test(
-            "`poll` of 2 items from full queue succeeds, further `poll`s return default value"
-          ) {
-            val q = MutableConcurrentQueue.bounded[Int](2)
-            q.offer(1)
-            q.offer(2)
-
-            (assert(q.poll(-1), equalTo(1))
-            && assert(q.poll(-1), equalTo(2))
-            && assert(q.poll(-1), equalTo(-1))
-            && assert(q.isEmpty(), isTrue))
-          }
-        ),
         suite("Serialization works for")(
           test("a one element queue") {
             val q = MutableConcurrentQueue.bounded[Int](1)
