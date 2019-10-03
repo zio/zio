b966eae87f9e1ca15e5f51bc4b191550db9c7d62
Denis Garcia <dgarcia@kabbage.com>
2019-10-03 12:15:20 -0700

    Actually fix silencer plugin and Dotty

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -9,8 +9,7 @@ import dotty.tools.sbtplugin.DottyPlugin.autoImport._
 import BuildInfoKeys._
 
 object BuildHelper {
-  val testDeps        = Seq("org.scalacheck"  %% "scalacheck"   % "1.14.2" % "test")
-  val compileOnlyDeps = Seq("com.github.ghik" %% "silencer-lib" % "1.4.4"  % "provided" cross CrossVersion.full)
+  val testDeps = Seq("org.scalacheck" %% "scalacheck" % "1.14.2" % "test")
 
   private val stdOptions = Seq(
     "-deprecation",
@@ -155,12 +154,15 @@ object BuildHelper {
     crossScalaVersions := Seq("2.12.10", "2.13.1", "2.11.12"),
     scalaVersion in ThisBuild := crossScalaVersions.value.head,
     scalacOptions := stdOptions ++ extraOptions(scalaVersion.value, optimize = !isSnapshot.value),
-    libraryDependencies ++= compileOnlyDeps ++ testDeps,
+    libraryDependencies ++= testDeps,
     libraryDependencies ++= {
       if (isDotty.value)
         Seq()
       else
-        Seq(compilerPlugin("com.github.ghik" %% "silencer-plugin" % "1.4.4" cross CrossVersion.full))
+        Seq(
+          compilerPlugin("com.github.ghik" %% "silencer-plugin" % "1.4.4" cross CrossVersion.full),
+          "com.github.ghik" %% "silencer-lib" % "1.4.4" % "provided" cross CrossVersion.full
+        )
     },
     parallelExecution in Test := true,
     incOptions ~= (_.withLogRecompileOnMacro(false)),
