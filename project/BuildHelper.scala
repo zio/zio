49e677486a1add3c72d73a95a2b49f5086e1ad3d
Ondra Pelech <ondra.pelech@gmail.com>
2019-10-08 14:47:09 +0200

    fmt

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -9,8 +9,8 @@ import dotty.tools.sbtplugin.DottyPlugin.autoImport._
 import BuildInfoKeys._
 
 object BuildHelper {
-  val testDeps        = Seq("org.scalacheck"  %% "scalacheck"   % "1.14.2" % "test")
-  val compileOnlyDeps = Seq("com.github.ghik" % "silencer-lib" % "1.4.4" % Provided cross CrossVersion.full)
+  val testDeps        = Seq("org.scalacheck"  %% "scalacheck"  % "1.14.2" % "test")
+  val compileOnlyDeps = Seq("com.github.ghik" % "silencer-lib" % "1.4.4"  % Provided cross CrossVersion.full)
 
   private val stdOptions = Seq(
     "-deprecation",
