f13f75dd956f3eebb0c4bfaf8fed1a2241d0c003
Vladislav Dolbilov <darl@yandex-team.ru>
2019-10-09 12:51:04 +0300

    fmt

--- core-tests/shared/src/test/scala/zio/ZIOSpec.scala
+++ core-tests/shared/src/test/scala/zio/ZIOSpec.scala
@@ -553,7 +553,7 @@ object ZIOSpec
           },
           testM("effectAsync should not resume fiber twice after interruption") {
             for {
-              step <- Promise.make[Nothing, Unit]
+              step            <- Promise.make[Nothing, Unit]
               unexpectedPlace <- Ref.make(List.empty[Int])
               runtime         <- ZIO.runtime[Live[Clock]]
               fork <- ZIO
@@ -566,7 +566,7 @@ object ZIOSpec
                          runtime.unsafeRunAsync_ {
                            step.succeed(())
                          }
-                         //never complete
+                       //never complete
                        })
                        .ensuring(unexpectedPlace.update(2 :: _))
                        .fork
@@ -579,27 +579,27 @@ object ZIOSpec
           },
           testM("effectAsyncMaybe should not resume fiber twice after synchronous result") {
             for {
-              step <- Promise.make[Nothing, Unit]
+              step            <- Promise.make[Nothing, Unit]
               unexpectedPlace <- Ref.make(List.empty[Int])
               runtime         <- ZIO.runtime[Live[Clock]]
               fork <- ZIO
-                .effectAsyncMaybe[Any, Nothing, Unit] { k =>
-                  runtime.unsafeRunAsync_ {
-                    step.await *> ZIO.effectTotal(k(unexpectedPlace.update(1 :: _).unit))
-                  }
-                  Some(IO.unit)
-                }
-                .flatMap { _ =>
-                  ZIO.effectAsync[Any, Nothing, Unit] { _ =>
-                    runtime.unsafeRunAsync_ {
-                      step.succeed(())
-                    }
-                    //never complete
-                  }
-                }
-                .ensuring(unexpectedPlace.update(2 :: _))
-                .uninterruptible
-                .fork
+                       .effectAsyncMaybe[Any, Nothing, Unit] { k =>
+                         runtime.unsafeRunAsync_ {
+                           step.await *> ZIO.effectTotal(k(unexpectedPlace.update(1 :: _).unit))
+                         }
+                         Some(IO.unit)
+                       }
+                       .flatMap { _ =>
+                         ZIO.effectAsync[Any, Nothing, Unit] { _ =>
+                           runtime.unsafeRunAsync_ {
+                             step.succeed(())
+                           }
+                         //never complete
+                         }
+                       }
+                       .ensuring(unexpectedPlace.update(2 :: _))
+                       .uninterruptible
+                       .fork
               result     <- withLive(fork.interrupt)(_.timeout(5.seconds))
               unexpected <- unexpectedPlace.get
             } yield {
