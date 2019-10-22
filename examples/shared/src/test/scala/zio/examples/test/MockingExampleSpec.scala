8c78e8052eb643ab72173b7205af09fc52117286
ioleo <ioleo+git@protonmail.com>
2019-10-22 15:33:35 +0200

    Mock improvements (#2000)
    * Rename succeed to empty & add doc
    
    * Rename MockSpec to Expectation, improve type inference, expose better API, drop Service container for method tags
    
    * Refactor ExpectationSpec to ZIO Test
    
    * Variance typos
    
    * Code review
    
    * Add shapeless attribution

--- examples/shared/src/test/scala/zio/examples/test/MockingExampleSpec.scala
+++ examples/shared/src/test/scala/zio/examples/test/MockingExampleSpec.scala
@@ -1,61 +1,48 @@
 package zio.examples.test
 
-import zio.{ clock, console, random, Managed }
+import zio.{ clock, console, random }
 import zio.test.{ assertM, suite, testM, DefaultRunnableSpec }
 import zio.test.Assertion._
-import zio.test.mock.{ MockClock, MockConsole, MockRandom, MockSpec }
+import zio.test.mock.{ MockClock, MockConsole, MockRandom }
+import zio.test.mock.Expectation.{ unit, value, valueF }
 
 object MockingExampleSpec
     extends DefaultRunnableSpec(
       suite("suite with mocks")(
         testM("expect call returning output") {
-          val app = clock.nanoTime
-          val mockEnv: Managed[Nothing, MockClock] =
-            MockSpec.expectOut(MockClock.Service.nanoTime)(1000L)
-
-          val result = app.provideManaged(mockEnv)
+          val app     = clock.nanoTime
+          val mockEnv = MockClock.nanoTime returns value(1000L)
+          val result  = app.provideManaged(mockEnv)
           assertM(result, equalTo(1000L))
         },
         testM("expect call with input satisfying assertion") {
-          val app = console.putStrLn("foo")
-          val mockEnv: Managed[Nothing, MockConsole] =
-            MockSpec.expectIn(MockConsole.Service.putStrLn)(equalTo("foo"))
-
-          val result = app.provideManaged(mockEnv)
+          val app     = console.putStrLn("foo")
+          val mockEnv = MockConsole.putStrLn(equalTo("foo")) returns unit
+          val result  = app.provideManaged(mockEnv)
           assertM(result, isUnit)
         },
         testM("expect call with input satisfying assertion and transforming it into output") {
-          val app = random.nextInt(1)
-          val mockEnv: Managed[Nothing, MockRandom] =
-            MockSpec.expect(MockRandom.Service.nextInt._0)(equalTo(1))(_ + 41)
-
-          val result = app.provideManaged(mockEnv)
+          val app     = random.nextInt(1)
+          val mockEnv = MockRandom.nextInt._0(equalTo(1)) returns valueF(_ + 41)
+          val result  = app.provideManaged(mockEnv)
           assertM(result, equalTo(42))
         },
         testM("expect call with input satisfying assertion and returning output") {
-          val app = random.nextInt(1)
-          val mockEnv: Managed[Nothing, MockRandom] =
-            MockSpec.expect_(MockRandom.Service.nextInt._0)(equalTo(1))(42)
-
-          val result = app.provideManaged(mockEnv)
+          val app     = random.nextInt(1)
+          val mockEnv = MockRandom.nextInt._0(equalTo(1)) returns value(42)
+          val result  = app.provideManaged(mockEnv)
           assertM(result, equalTo(42))
         },
         testM("expect call for overloaded method") {
-          val app = random.nextInt
-          val mockEnv: Managed[Nothing, MockRandom] =
-            MockSpec.expectOut(MockRandom.Service.nextInt._1)(42)
-
-          val result = app.provideManaged(mockEnv)
+          val app     = random.nextInt
+          val mockEnv = MockRandom.nextInt._1 returns value(42)
+          val result  = app.provideManaged(mockEnv)
           assertM(result, equalTo(42))
         },
         testM("expect calls from multiple modules") {
-          val app = random.nextInt.map(_.toString) >>= console.putStrLn
-
-          val randomEnv: Managed[Nothing, MockRandom] =
-            MockSpec.expectOut(MockRandom.Service.nextInt._1)(42)
-
-          val consoleEnv: Managed[Nothing, MockConsole] =
-            MockSpec.expectIn(MockConsole.Service.putStrLn)(equalTo("42"))
+          val app        = random.nextInt.map(_.toString) >>= console.putStrLn
+          val randomEnv  = MockRandom.nextInt._1 returns value(42)
+          val consoleEnv = MockConsole.putStrLn(equalTo("42")) returns unit
 
           val mockEnv = (randomEnv &&& consoleEnv).map { case (r, c) => new MockRandom with MockConsole {
             val random  = r.random
@@ -66,26 +53,22 @@ object MockingExampleSpec
           assertM(result, isUnit)
         },
         testM("failure if invalid method") {
-          val app = random.nextInt
-          val mockEnv: Managed[Nothing, MockRandom] =
-            MockSpec.expectOut(MockRandom.Service.nextLong._0)(42)
-
-          val result = app.provideManaged(mockEnv)
+          val app     = random.nextInt
+          val mockEnv = MockRandom.nextLong._0 returns value(42)
+          val result  = app.provideManaged(mockEnv)
           assertM(result, equalTo(42))
         },
         testM("failure if invalid arguments") {
-          val app = console.putStrLn("foo")
-          val mockEnv: Managed[Nothing, MockConsole] =
-            MockSpec.expectIn(MockConsole.Service.putStrLn)(equalTo("bar"))
-
-          val result = app.provideManaged(mockEnv)
+          val app     = console.putStrLn("foo")
+          val mockEnv = MockConsole.putStrLn(equalTo("bar")) returns unit
+          val result  = app.provideManaged(mockEnv)
           assertM(result, isUnit)
         },
         testM("failure if unmet expectations") {
-          val app = random.nextInt
-          val mockEnv: Managed[Nothing, MockRandom] =
-            MockSpec.expectOut(MockRandom.Service.nextInt._1)(42) *>
-            MockSpec.expectOut(MockRandom.Service.nextInt._1)(42)
+          val app     = random.nextInt
+          val mockEnv =
+            (MockRandom.nextInt._1 returns value(42)) *>
+            (MockRandom.nextInt._1 returns value(42))
 
           val result = app.provideManaged(mockEnv)
           assertM(result, equalTo(42))
