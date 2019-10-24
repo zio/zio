7bcc223135ef909387600c380afdf5957459a650
Dejan Mijic <dmijic@acm.org>
2019-10-24 02:45:28 +0200

    Revert "Express fold in terms of foldM"
    This reverts commit 6124f9a0253265e488576dec7b3a155b19db7665.

--- core/shared/src/main/scala/zio/stm/TMap.scala
+++ core/shared/src/main/scala/zio/stm/TMap.scala
@@ -54,7 +54,10 @@ class TMap[K, V] private (
    * Atomically folds using pure function.
    */
   final def fold[A](zero: A)(op: (A, (K, V)) => A): STM[Nothing, A] =
-    foldM(zero)((a, kv) => STM.succeed(op(a, kv)))
+    for {
+      buckets <- tBuckets.get
+      res     <- buckets.fold(zero)((acc, bucket) => bucket.foldLeft(acc)(op))
+    } yield res
 
   /**
    * Atomically folds using effectful function.
