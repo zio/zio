9b0a2402489354ae469bf62f95d4ad55072cd43e
Dejan Mijic <dmijic@acm.org>
2019-10-24 20:38:09 +0200

    Document the TSet API

--- core/shared/src/main/scala/zio/stm/TSet.scala
+++ core/shared/src/main/scala/zio/stm/TSet.scala
@@ -17,50 +17,96 @@
 package zio.stm
 
 class TSet[A] private (private val tmap: TMap[A, Unit]) extends AnyVal {
+
+  /**
+   * Tests whether or not set contains an element.
+   */
   final def contains(a: A): STM[Nothing, Boolean] =
     tmap.contains(a)
 
+  /**
+   * Removes element from set.
+   */
   final def delete(a: A): STM[Nothing, Unit] =
     tmap.delete(a)
 
+  /**
+   * Atomically transforms the set into the difference of itself and the
+   * provided set.
+   */
   final def diff(other: TSet[A]): STM[Nothing, Unit] =
     for {
       vals <- other.toList.map(_.toSet)
       _    <- removeIf(vals.contains)
     } yield ()
 
+  /**
+   * Atomically folds using pure function.
+   */
   final def fold[B](zero: B)(op: (B, A) => B): STM[Nothing, B] =
     tmap.fold(zero)((acc, kv) => op(acc, kv._1))
 
+  /**
+   * Atomically folds using effectful function.
+   */
   final def foldM[B, E](zero: B)(op: (B, A) => STM[E, B]): STM[E, B] =
     tmap.foldM(zero)((acc, kv) => op(acc, kv._1))
 
+  /**
+   * Atomically performs side-effect for each element in set.
+   */
   final def foreach[E](f: A => STM[E, Unit]): STM[E, Unit] =
     foldM(())((_, a) => f(a))
 
+  /**
+   * Atomically transforms the set into the intersection of itself and the
+   * provided set.
+   */
   final def intersect(other: TSet[A]): STM[Nothing, Unit] =
     for {
       vals <- other.toList.map(_.toSet)
       _    <- retainIf(vals.contains)
     } yield ()
 
+  /**
+   * Stores new element in the set.
+   */
   final def put(a: A): STM[Nothing, Unit] =
     tmap.put(a, ())
 
+  /**
+   * Removes elements matching predicate.
+   */
   final def removeIf(p: A => Boolean): STM[Nothing, Unit] =
     tmap.removeIf((k, _) => p(k))
 
+  /**
+   * Retains elements matching predicate.
+   */
   final def retainIf(p: A => Boolean): STM[Nothing, Unit] =
     tmap.retainIf((k, _) => p(k))
 
+  /**
+   * Collects all elements into a list.
+   */
   final def toList: STM[Nothing, List[A]] = tmap.keys
 
+  /**
+   * Atomically updates all elements using pure function.
+   */
   final def transform(f: A => A): STM[Nothing, Unit] =
     tmap.transform((k, v) => f(k) -> v)
 
+  /**
+   * Atomically updates all elements using effectful function.
+   */
   final def transformM[E](f: A => STM[E, A]): STM[E, Unit] =
     tmap.transformM((k, v) => f(k).map(_ -> v))
 
+  /**
+   * Atomically transforms the set into the union of itself and the provided
+   * set.
+   */
   final def union(other: TSet[A]): STM[Nothing, Unit] =
     for {
       vals <- other.toList
@@ -69,10 +115,20 @@ class TSet[A] private (private val tmap: TMap[A, Unit]) extends AnyVal {
 }
 
 object TSet {
+
+  /**
+   * Makes a new `TSet` that is initialized with specified values.
+   */
   final def apply[A](data: A*): STM[Nothing, TSet[A]] = fromIterable(data)
 
+  /**
+   * Makes an empty `TSet`.
+   */
   final def empty[A]: STM[Nothing, TSet[A]] = fromIterable(Nil)
 
+  /**
+   * Makes a new `TSet` initialized with provided iterable.
+   */
   final def fromIterable[A](data: Iterable[A]): STM[Nothing, TSet[A]] =
     TMap.fromIterable(data.map((_, ()))).map(new TSet(_))
 }
