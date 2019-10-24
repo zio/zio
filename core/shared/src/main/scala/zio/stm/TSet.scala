8afc6745e71fe652cef2c53c92d052ab958bf707
Dejan Mijic <dmijic@acm.org>
2019-10-24 19:36:10 +0200

    Implement factories and conversion to list

---
+++ core/shared/src/main/scala/zio/stm/TSet.scala
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2017-2019 John A. De Goes and the ZIO Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package zio.stm
+
+class TSet[A] private (private val tmap: TMap[A, Unit]) extends AnyVal {
+  final def toList: STM[Nothing, List[A]] = tmap.keys
+}
+
+object TSet {
+  final def apply[A](data: A*): STM[Nothing, TSet[A]] = fromIterable(data)
+
+  final def empty[A]: STM[Nothing, TSet[A]] = fromIterable(Nil)
+
+  final def fromIterable[A](data: Iterable[A]): STM[Nothing, TSet[A]] =
+    TMap.fromIterable(data.map((_, ()))).map(new TSet(_))
+}
