dc4980f25c29e32fa887c794f099466a9c734a5c
Adam Fraser <adam.fraser@gmail.com>
2019-10-27 17:24:30 -0400

    implement decorators for test environment (#2043)

--- test/jvm/src/main/scala/zio/test/environment/TestEnvironment.scala
+++ test/jvm/src/main/scala/zio/test/environment/TestEnvironment.scala
@@ -27,7 +27,6 @@ case class TestEnvironment(
   console: TestConsole.Test,
   live: Live.Service[ZEnv],
   random: TestRandom.Test,
-  scheduler: TestClock.Test,
   sized: Sized.Service[Any],
   system: TestSystem.Test
 ) extends Blocking
@@ -37,7 +36,57 @@ case class TestEnvironment(
     with TestRandom
     with TestSystem
     with Scheduler
-    with Sized
+    with Sized {
+
+  /**
+   * Maps all test implementations in the test environment individually.
+   */
+  final def mapAll(
+    mapTestClock: TestClock.Test => TestClock.Test = identity,
+    mapTestConsole: TestConsole.Test => TestConsole.Test = identity,
+    mapTestRandom: TestRandom.Test => TestRandom.Test = identity,
+    mapTestSystem: TestSystem.Test => TestSystem.Test = identity
+  ): TestEnvironment =
+    TestEnvironment(
+      blocking,
+      mapTestClock(clock),
+      mapTestConsole(console),
+      live,
+      mapTestRandom(random),
+      sized,
+      mapTestSystem(system)
+    )
+
+  /**
+   * Maps the [[TestClock]] implementation in the test environment, leaving
+   * all other test implementations the same.
+   */
+  final def mapTestClock(f: TestClock.Test => TestClock.Test): TestEnvironment =
+    mapAll(mapTestClock = f)
+
+  /**
+   * Maps the [[TestConsole]] implementation in the test environment, leaving
+   * all other test implementations the same.
+   */
+  final def mapTestConsole(f: TestConsole.Test => TestConsole.Test): TestEnvironment =
+    mapAll(mapTestConsole = f)
+
+  /**
+   * Maps the [[TestRandom]] implementation in the test environment, leaving
+   * all other test implementations the same.
+   */
+  final def mapTestRandom(f: TestRandom.Test => TestRandom.Test): TestEnvironment =
+    mapAll(mapTestRandom = f)
+
+  /**
+   * Maps the [[TestSystem]] implementation in the test environment, leaving
+   * all other test implementations the same.
+   */
+  final def mapTestSystem(f: TestSystem.Test => TestSystem.Test): TestEnvironment =
+    mapAll(mapTestSystem = f)
+
+  val scheduler = clock
+}
 
 object TestEnvironment {
 
@@ -53,6 +102,6 @@ object TestEnvironment {
         size     <- Sized.makeService(100)
         system   <- TestSystem.makeTest(TestSystem.DefaultData)
         blocking = Blocking.Live.blocking
-      } yield new TestEnvironment(blocking, clock, console, live, random, clock, size, system)
+      } yield new TestEnvironment(blocking, clock, console, live, random, size, system)
     }
 }
