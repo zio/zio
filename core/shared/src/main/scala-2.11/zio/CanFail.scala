53f26da337c14e906076896b7c9dcf5591b0edcf
Adam Fraser <adam.fraser@gmail.com>
2019-10-30 17:14:01 -0400

    remove extraneous type parameter

--- core/shared/src/main/scala-2.11/zio/CanFail.scala
+++ core/shared/src/main/scala-2.11/zio/CanFail.scala
@@ -27,7 +27,7 @@ sealed trait CanFail[-E]
 
 object CanFail extends CanFail[Any] {
 
-  implicit final def canFail[A >: Nothing, E >: A]: CanFail[E] = CanFail
+  implicit final def canFail[E]: CanFail[E] = CanFail
 
   implicit final val canFailAmbiguous1: CanFail[Nothing] = CanFail
   implicit final val canFailAmbiguous2: CanFail[Nothing] = CanFail
