3e631beee2e7d26db97fb6569eb19fa375713e49
Yuval Itzchakov <yuvalos@gmail.com>
2019-11-05 12:45:18 +0200

    Reintroduce check of successor in zipWithPar

--- core/shared/src/main/scala/zio/ZIO.scala
+++ core/shared/src/main/scala/zio/ZIO.scala
@@ -1502,7 +1502,12 @@ sealed trait ZIO[-R, +E, +A] extends Serializable { self =>
       }
 
     val g = (b: B, a: A) => f(a, b)
-    (self raceWith that)(coordinate(f, true), coordinate(g, false))
+    (self raceWith that)(coordinate(f, true), coordinate(g, false)).fork.flatMap { f =>
+      f.await.flatMap { exit =>
+        if (exit.succeeded) f.inheritFiberRefs *> ZIO.done(exit)
+        else ZIO.done(exit)
+      }
+    }
   }
 
   /**
