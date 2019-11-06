cf031b7e6b57ff75e6c9d955092774e0fbb29320
Dmitry Karlinsky <dmitryka@wix.com>
2019-11-06 11:32:19 +0200

    Moved test to the newly created `SpecSpec`

--- test-tests/shared/src/test/scala/zio/test/SpecSpec.scala
+++ test-tests/shared/src/test/scala/zio/test/SpecSpec.scala
@@ -1,22 +1,69 @@
 package zio.test
 
-import zio.test.Assertion._
-import zio.test.TestUtils.execute
 import zio.ZManaged
+import zio.test.Assertion.{equalTo, isFalse, isTrue}
+import zio.test.TestOnlySpecData._
+import zio.test.TestUtils.{execute, succeeded}
 
 object SpecSpec
     extends ZIOBaseSpec(
       suite("SpecSpec")(
-        testM("provideManagedShared gracefully handles fiber death") {
-          import zio.NeedsEnv.needsEnv
-          val spec = suite("Suite1")(
-            test("Test1") {
-              assert(true, isTrue)
+        suite("SpecSpec")(
+          testM("provideManagedShared gracefully handles fiber death") {
+            import zio.NeedsEnv.needsEnv
+            val spec = suite("Suite1")(
+              test("Test1") {
+                assert(true, isTrue)
+              }
+            ).provideManagedShared(ZManaged.dieMessage("everybody dies"))
+            for {
+              _ <- execute(spec)
+            } yield assertCompletes
+          }
+        ),
+        suite(".only(pattern)")(
+          testM("ignores all tests except one matching the given label") {
+            zio.test.checkM(genSuite) { spec =>
+              for {
+                passed1 <- succeeded(spec.only(passingTest))
+                passed2 <- succeeded(spec.only(failingTest))
+              } yield assert(passed1, isTrue) && assert(passed2, isFalse)
             }
-          ).provideManagedShared(ZManaged.dieMessage("everybody dies"))
-          for {
-            _ <- execute(spec)
-          } yield assertCompletes
-        }
+          },
+          testM("ignores all tests except ones in the suite matching the given label") {
+            zio.test.checkM(genSuite) { spec =>
+              for {
+                passed1 <- succeeded(spec.only(passingSuite))
+                passed2 <- succeeded(spec.only(failingSuite))
+              } yield assert(passed1, isTrue) && assert(passed2, isFalse)
+            }
+          },
+          testM("runs everything if root suite label given") {
+            zio.test.checkM(genSuite) { spec =>
+              for {
+                passed <- succeeded(spec.only(rootSuite))
+              } yield assert(passed, isFalse)
+            }
+          }
+        )
       )
     )
+
+object TestOnlySpecData {
+  val failingTest  = "failing-test"
+  val failingSuite = "failing-suite"
+  val passingTest  = "passing-test"
+  val passingSuite = "passing-suite"
+  val rootSuite    = "root-suite"
+  val genSuite = Gen.anyString zip Gen.anyString map {
+    case (prefix, suffix) => mixedSpec(prefix, suffix)
+  }
+  def mixedSpec(prefix: String, suffix: String) = suite(prefix + rootSuite + suffix)(
+    suite(prefix + failingSuite + suffix)(test(prefix + failingTest + suffix) {
+      assert(1, equalTo(2))
+    }),
+    suite(prefix + passingSuite + suffix)(test(prefix + passingTest + suffix) {
+      assert(1, equalTo(1))
+    })
+  )
+}
