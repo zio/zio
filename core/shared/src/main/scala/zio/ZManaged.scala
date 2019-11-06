a1f550da14c8737b6d5971dccaed89bbbbbdef88
Rafael Saraiva Figueiredo <rafael.saraiva132@gmail.com>
2019-11-06 11:25:25 +0100

    Add eventually to ZManaged (#2161)
    * Add eventually to ZManaged
    
    * Change ZManaged eventually implementation to take advantage of the underlying ZIO eventually implementation

--- core/shared/src/main/scala/zio/ZManaged.scala
+++ core/shared/src/main/scala/zio/ZManaged.scala
@@ -263,6 +263,17 @@ final class ZManaged[-R, +E, +A] private (reservation: ZIO[R, E, Reservation[R, 
     }
 
   /**
+   * Returns a ZManaged that ignores errors raised by the acquire effect and
+   * runs it repeatedly until it eventually succeeds.
+   */
+  final def eventually(implicit ev: CanFail[E]): ZManaged[R, Nothing, A] =
+    ZManaged {
+      reserve.eventually.map { r =>
+        Reservation(r.acquire.eventually, r.release)
+      }
+    }
+
+  /**
    * Zips this effect with its environment
    */
   final def first[R1 <: R, A1 >: A]: ZManaged[R1, E, (A1, R1)] = self &&& ZManaged.identity
