60f2d8f58bc1ed3c8920cbaf1643f410a0618188
Dmitry Karlinsky <dmitryka@wix.com>
2019-11-09 20:12:12 +0200

    t2: IO.foreach` both evaluates effects and returns the list of Ints in the same order

--- core-tests/jvm/src/test/scala/zio/ZIOSpecJvm.scala
+++ core-tests/jvm/src/test/scala/zio/ZIOSpecJvm.scala
@@ -4,15 +4,26 @@ import ZIOSpecJvmUtils._
 import zio.random.Random
 import Assertion._
 
+import scala.collection.mutable
+
 object ZIOSpecJvm extends ZIOBaseSpec (
-  suite("ZIOSpecJvm") {
-    testM("IO.foreach` returns the list of results") {
+  suite("ZIOSpecJvm") (
+    testM("Generate a list of String and a f: String => Task[Int]: `IO.foreach` returns the list of results") {
       checkAllM(functionIOGen, listGen) { (f, list) =>
         val res = IO.foreach(list)(f)
         assertM(res,  isSubtype[List[Int]](anything) && hasSize(equalTo(100)))
       }
+    },
+    testM("Create a list of Strings and pass an f: String => IO[String, Int] " +
+      "`IO.foreach` both evaluates effects and returns the list of Ints in the same order") {
+      val list    = List("1", "2", "3")
+      for {
+        ref <- Ref.make(List.empty[String])
+        res     <- IO.foreach(list) (x => ref.update(_ :+ x) *> IO.effectTotal[Int](x.toInt))
+        effects <- ref.get
+      } yield assert(effects, equalTo(list)) && assert(res, equalTo(List(1, 2, 3)))
     }
-  }
+  )
 )
 object ZIOSpecJvmUtils {
 
