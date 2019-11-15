bcb939ff753b42ba453ae65537dad3b2559cfe7a
Adam Fraser <adam.fraser@gmail.com>
2019-11-14 23:11:08 -0500

    ZIO Test: Support Assertions about Compilation Errors (#2215)
    * support assertions about compilation errors
    
    * fix Scala 2.11 compatibility issue
    
    * rename and clarify the contract

---
+++ test-tests/shared/src/test/scala/zio/test/CompileSpec.scala
@@ -0,0 +1,17 @@
+package zio.test
+
+import zio.test.Assertion._
+
+object CompileSpec
+    extends ZIOBaseSpec(
+      suite("CompileSpec")(
+        testM("typeCheck must return Right if the specified string is valid Scala code") {
+          assertM(typeCheck("1 + 1"), isRight(anything))
+        },
+        testM("typeCheck must return Left with an error message otherwise") {
+          val expected = "value ++ is not a member of Int"
+          if (TestVersion.isScala2) assertM(typeCheck("1 ++ 1"), isLeft(equalTo(expected)))
+          else assertM(typeCheck("1 ++ 1"), isLeft(anything))
+        }
+      )
+    )
