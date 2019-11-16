9d75fb2903d15bb8e2fd2c3d29dcb84ed0a04eeb
pk246 <pawel@kiersznowski.com>
2019-11-16 01:36:55 +0100

    #2129 using collectAll.map(_.headOption) instead of foldUntil

--- streams/shared/src/main/scala/zio/stream/ZSink.scala
+++ streams/shared/src/main/scala/zio/stream/ZSink.scala
@@ -1286,8 +1286,8 @@ object ZSink extends ZSinkPlatformSpecific with Serializable {
   /**
    * Creates a sink containing the first value.
    */
-  final def head[A]: ZSink[Any, Nothing, A, A, Option[A]] =
-    foldUntil[Option[A], A](None, 1) { case (_, a) => Some(a) }
+  final def head[A]: ZSink[Any, Nothing, Nothing, A, Option[A]] =
+    collectAll.map(_.headOption)
 
   /**
    * Creates a sink containing the last value.
