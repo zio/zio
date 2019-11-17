00c0facb2fdbbe5f49ac896e10ee921474ff7117
Tapan Vaishnav <vaishnav.tapan@bizreach.co.jp>
2019-11-17 14:12:12 +0900

    migrate all sub tests for DefaultsTestReporter

--- test-tests/shared/src/test/scala/zio/test/DefaultTestReporterSpec.scala
+++ test-tests/shared/src/test/scala/zio/test/DefaultTestReporterSpec.scala
@@ -3,22 +3,190 @@ package zio.test
 import zio._
 import zio.clock.Clock
 import zio.test.Assertion._
+import zio.test.DefaultTestReporterSpecUtil._
 import zio.test.ReportingTestUtils._
 import zio.test.environment.{ testEnvironmentManaged, TestClock, TestConsole, TestEnvironment }
 
 object DefaultTestReporterSpec
-  extends ZIOBaseSpec(
-    suite("DefaultTestReporterSpec")(
-      testM("correctly reports a successful test") {
-        val x             = zio.test.test("Addition works fine")(assert(1 + 1, equalTo(2)))
-        val test1Expected = expectedSuccess("Addition works fine")
-        assertM(
-          DefaultTestReporterSpecUtil.runLog(x),
-          equalTo(Vector(test1Expected, DefaultTestReporterSpecUtil.reportStats(1, 0, 0)))
-        )
-      }
+    extends ZIOBaseSpec(
+      suite("DefaultTestReporterSpec")(
+        testM("correctly reports a successful test") {
+          val x             = zio.test.test("Addition works fine")(assert(1 + 1, equalTo(2)))
+          val test1Expected = expectedSuccess("Addition works fine")
+          assertM(
+            runLog(x),
+            equalTo(Vector(test1Expected, reportStats(1, 0, 0)))
+          )
+        },
+        testM("correctly reports a failed test") {
+          val x =
+            zio.test.test("Value falls within range")(assert(52, equalTo(42) || (isGreaterThan(5) && isLessThan(10))))
+          val test1Expected = Vector(
+            expectedFailure("Value falls within range"),
+            withOffset(2)(s"${blue("52")} did not satisfy ${cyan("equalTo(42)")}\n"),
+            withOffset(2)(
+              s"${blue("52")} did not satisfy ${cyan("(" + yellowThenCyan("equalTo(42)") + " || (isGreaterThan(5) && isLessThan(10)))")}\n"
+            ),
+            withOffset(2)(s"${blue("52")} did not satisfy ${cyan("isLessThan(10)")}\n"),
+            withOffset(2)(
+              s"${blue("52")} did not satisfy ${cyan("(equalTo(42) || (isGreaterThan(5) && " + yellowThenCyan("isLessThan(10)") + "))")}\n"
+            )
+          )
+          assertM(
+            runLog(x),
+            equalTo(test1Expected :+ reportStats(0, 0, 1))
+          )
+        },
+        testM("correctly reports an error in a test") {
+          val x = Spec.test("Failing test", failed(Cause.fail("Fail")))
+          val test1Expected = Vector(
+            expectedFailure("Failing test"),
+            withOffset(2)("Fiber failed.\n") +
+              withOffset(2)("A checked error was not handled.\n") +
+              withOffset(2)("Fail\n") +
+              withOffset(2)("No ZIO Trace available.\n")
+          )
+          assertM(
+            runLog(x),
+            equalTo(test1Expected :+ reportStats(0, 0, 1))
+          )
+        },
+        testM("correctly reports successful test suite") {
+          val test1  = zio.test.test("Addition works fine")(assert(1 + 1, equalTo(2)))
+          val test2  = zio.test.test("Subtraction works fine")(assert(1 - 1, equalTo(0)))
+          val suite1 = suite("Suite1")(test1, test2)
+
+          val test1Expected = expectedSuccess("Addition works fine")
+          val test2Expected = expectedSuccess("Subtraction works fine")
+          val suite1Expected = Vector(
+            expectedSuccess("Suite1"),
+            withOffset(2)(test1Expected),
+            withOffset(2)(test2Expected)
+          )
+          assertM(
+            runLog(suite1),
+            equalTo(suite1Expected :+ reportStats(2, 0, 0))
+          )
+        },
+        testM("correctly reports failed test suite") {
+          val test1 = zio.test.test("Addition works fine")(assert(1 + 1, equalTo(2)))
+          val test2 = zio.test.test("Subtraction works fine")(assert(1 - 1, equalTo(0)))
+          val test3 =
+            zio.test.test("Value falls within range")(assert(52, equalTo(42) || (isGreaterThan(5) && isLessThan(10))))
+          val suite2 = suite("Suite2")(test1, test2, test3)
+
+          val test1Expected = expectedSuccess("Addition works fine")
+          val test2Expected = expectedSuccess("Subtraction works fine")
+          val test3Expected = Vector(
+            expectedFailure("Value falls within range"),
+            withOffset(2)(s"${blue("52")} did not satisfy ${cyan("equalTo(42)")}\n"),
+            withOffset(2)(
+              s"${blue("52")} did not satisfy ${cyan("(" + yellowThenCyan("equalTo(42)") + " || (isGreaterThan(5) && isLessThan(10)))")}\n"
+            ),
+            withOffset(2)(s"${blue("52")} did not satisfy ${cyan("isLessThan(10)")}\n"),
+            withOffset(2)(
+              s"${blue("52")} did not satisfy ${cyan("(equalTo(42) || (isGreaterThan(5) && " + yellowThenCyan("isLessThan(10)") + "))")}\n"
+            )
+          )
+          val suite2Expected = Vector(
+            expectedFailure("Suite2"),
+            withOffset(2)(test1Expected),
+            withOffset(2)(test2Expected)
+          ) ++ test3Expected.map(withOffset(2)(_))
+          assertM(
+            runLog(suite2),
+            equalTo(suite2Expected :+ reportStats(2, 0, 1))
+          )
+        },
+        testM("correctly reports multiple test suites") {
+          val test1 = zio.test.test("Addition works fine")(assert(1 + 1, equalTo(2)))
+          val test2 = zio.test.test("Subtraction works fine")(assert(1 - 1, equalTo(0)))
+          val test3 =
+            zio.test.test("Value falls within range")(assert(52, equalTo(42) || (isGreaterThan(5) && isLessThan(10))))
+          val suite1 = suite("Suite1")(test1, test2)
+          val suite3 = suite("Suite3")(suite1, test3)
+
+          val test1Expected = expectedSuccess("Addition works fine")
+          val test2Expected = expectedSuccess("Subtraction works fine")
+          val test3Expected = Vector(
+            expectedFailure("Value falls within range"),
+            withOffset(2)(s"${blue("52")} did not satisfy ${cyan("equalTo(42)")}\n"),
+            withOffset(2)(
+              s"${blue("52")} did not satisfy ${cyan("(" + yellowThenCyan("equalTo(42)") + " || (isGreaterThan(5) && isLessThan(10)))")}\n"
+            ),
+            withOffset(2)(s"${blue("52")} did not satisfy ${cyan("isLessThan(10)")}\n"),
+            withOffset(2)(
+              s"${blue("52")} did not satisfy ${cyan("(equalTo(42) || (isGreaterThan(5) && " + yellowThenCyan("isLessThan(10)") + "))")}\n"
+            )
+          )
+          val suite1Expected = Vector(
+            expectedSuccess("Suite1"),
+            withOffset(2)(test1Expected),
+            withOffset(2)(test2Expected)
+          )
+          assertM(
+            runLog(suite3),
+            equalTo(
+              Vector(expectedFailure("Suite3")) ++ suite1Expected.map(withOffset(2)) ++ test3Expected
+                .map(withOffset(2)) :+ reportStats(2, 0, 1)
+            )
+          )
+        },
+        testM("correctly reports failure of simple assertion") {
+          val test5 = zio.test.test("Addition works fine")(assert(1 + 1, equalTo(3)))
+          val test5Expected = Vector(
+            expectedFailure("Addition works fine"),
+            withOffset(2)(s"${blue("2")} did not satisfy ${cyan("equalTo(3)")}\n")
+          )
+          assertM(
+            runLog(test5),
+            equalTo(test5Expected :+ reportStats(0, 0, 1))
+          )
+        },
+        testM("correctly reports multiple nested failures") {
+          val test6 =
+            zio.test.test("Multiple nested failures")(assert(Right(Some(3)), isRight(isSome(isGreaterThan(4)))))
+          val test6Expected = Vector(
+            expectedFailure("Multiple nested failures"),
+            withOffset(2)(s"${blue("3")} did not satisfy ${cyan("isGreaterThan(4)")}\n"),
+            withOffset(2)(
+              s"${blue("Some(3)")} did not satisfy ${cyan("isSome(" + yellowThenCyan("isGreaterThan(4)") + ")")}\n"
+            ),
+            withOffset(2)(
+              s"${blue("Right(Some(3))")} did not satisfy ${cyan("isRight(" + yellowThenCyan("isSome(isGreaterThan(4))") + ")")}\n"
+            )
+          )
+          assertM(
+            runLog(test6),
+            equalTo(test6Expected :+ reportStats(0, 0, 1))
+          )
+        },
+        testM("correctly reports labeled failures") {
+          val test7 = testM("labeled failures") {
+            for {
+              a <- ZIO.effectTotal(Some(1))
+              b <- ZIO.effectTotal(Some(1))
+              c <- ZIO.effectTotal(Some(0))
+              d <- ZIO.effectTotal(Some(1))
+            } yield assert(a, isSome(equalTo(1)).label("first")) &&
+              assert(b, isSome(equalTo(1)).label("second")) &&
+              assert(c, isSome(equalTo(1)).label("third")) &&
+              assert(d, isSome(equalTo(1)).label("fourth"))
+          }
+          val test7Expected = Vector(
+            expectedFailure("labeled failures"),
+            withOffset(2)(s"${blue("0")} did not satisfy ${cyan("equalTo(1)")}\n"),
+            withOffset(2)(
+              s"${blue("Some(0)")} did not satisfy ${cyan("(isSome(" + yellowThenCyan("equalTo(1)") + ") ?? \"third\")")}\n"
+            )
+          )
+          assertM(
+            runLog(test7),
+            equalTo(test7Expected :+ reportStats(0, 0, 1))
+          )
+        }
+      )
     )
-  )
 
 object DefaultTestReporterSpecUtil {
 
@@ -32,21 +200,21 @@ object DefaultTestReporterSpecUtil {
   def runLog[E](spec: ZSpec[TestEnvironment, String, String, Unit]) = {
     val zio = for {
       _ <- TestTestRunner(testEnvironmentManaged)
-        .run(spec)
-        .provideSomeManaged(for {
-          logSvc   <- TestLogger.fromConsoleM.toManaged_
-          clockSvc <- TestClock.make(TestClock.DefaultData)
-        } yield new TestLogger with Clock {
-          override def testLogger: TestLogger.Service = logSvc.testLogger
+            .run(spec)
+            .provideSomeManaged(for {
+              logSvc   <- TestLogger.fromConsoleM.toManaged_
+              clockSvc <- TestClock.make(TestClock.DefaultData)
+            } yield new TestLogger with Clock {
+              override def testLogger: TestLogger.Service = logSvc.testLogger
 
-          override val clock: Clock.Service[Any] = clockSvc.clock
-        })
+              override val clock: Clock.Service[Any] = clockSvc.clock
+            })
       output <- TestConsole.output
     } yield output
     zio
   }
 
-  def TestTestRunner(testEnvironment: Managed[Nothing, TestEnvironment]) =
+  private[this] def TestTestRunner(testEnvironment: Managed[Nothing, TestEnvironment]) =
     TestRunner[TestEnvironment, String, Unit, String, Unit](
       executor = TestExecutor.managed[TestEnvironment, String, String, Unit](testEnvironment),
       reporter = DefaultTestReporter()
