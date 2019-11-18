e1757626e62b80ceb83731f8b55cd95cdf1c7f2e
Adam Fraser <adam.fraser@gmail.com>
2019-11-17 21:20:45 -0500

    remove deprecated code

--- core/shared/src/main/scala/zio/Runtime.scala
+++ core/shared/src/main/scala/zio/Runtime.scala
@@ -121,9 +121,6 @@ trait Runtime[+R] {
    */
   final def as[R1](r1: R1): Runtime[R1] = map(_ => r1)
 
-  @deprecated("use as", "1.0.0")
-  final def const[R1](r1: R1): Runtime[R1] = as(r1)
-
   /**
    * Constructs a new `Runtime` with the specified executor.
    */
