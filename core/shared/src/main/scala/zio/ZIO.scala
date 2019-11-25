8c4a90cce7bec6debc249115b8b9c49151e5117e
reibitto <reibitto@users.noreply.github.com>
2019-11-26 00:34:43 +0900

    Add ZIO.merge (#2322)
    * Add ZIO.merge
    
    * Add CanFail
    
    * Also add for ZManaged

--- core/shared/src/main/scala/zio/ZIO.scala
+++ core/shared/src/main/scala/zio/ZIO.scala
@@ -634,6 +634,13 @@ sealed trait ZIO[-R, +E, +A] extends Serializable { self =>
     } yield l.succeed(()) *> p.await
 
   /**
+   * Returns a new effect where the error channel has been merged into the
+   * success channel to their common combined type.
+   */
+  final def merge[A1 >: A](implicit ev1: E <:< A1, ev2: CanFail[E]): URIO[R, A1] =
+    self.foldM(e => ZIO.succeed(ev1(e)), ZIO.succeed)
+
+  /**
    * Turns off daemon mode for this region, which means that any fibers forked
    * in this region will not be daemon fibers, so they will be visible as
    * children of their parent fiber, and interrupted when their parent fiber is
