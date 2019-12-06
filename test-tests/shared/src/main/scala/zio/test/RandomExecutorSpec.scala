07b849f1414b883c2ba1b04f66b32eef6fdf641c
Jentsch <d@jentsch.berlin>
2019-12-06 13:17:04 +0100

    Make tests dependent of runtime results

--- test-tests/shared/src/main/scala/zio/test/RandomExecutorSpec.scala
+++ test-tests/shared/src/main/scala/zio/test/RandomExecutorSpec.scala
@@ -1,15 +1,12 @@
 package zio.test
 
 import scala.concurrent.Future
-
 import zio.test.TestUtils.label
-import zio.test.RandomExecutor.paths
-import zio.UIO
+import zio.{ Exit, UIO, URIO, ZIO }
 import zio.Exit.Success
 import zio.clock.Clock
 import zio.duration._
 import zio.stream.ZStream
-import zio.random.Random
 
 object RandomExecutorSpec extends AsyncBaseSpec {
   val run: List[Async[(Boolean, String)]] = List(
@@ -19,13 +16,13 @@ object RandomExecutorSpec extends AsyncBaseSpec {
   )
 
   def simpleSuccess: Future[Boolean] = {
-    val gen = paths(UIO.succeed(1))
+    val gen = RandomExecutor.paths(UIO.succeed(1))
 
     expectToSee(gen)(Success(1))
   }
 
   def sleep: Future[Boolean] = {
-    val gen = paths(zio.ZIO.sleep(20.milliseconds))
+    val gen = RandomExecutor.paths(zio.ZIO.sleep(20.milliseconds))
 
     expectToSee(gen)(Success(()))
   }
@@ -33,11 +30,34 @@ object RandomExecutorSpec extends AsyncBaseSpec {
   def simpleRace: Future[Boolean] = {
     val race = UIO.succeed(1) race UIO.succeed(2)
 
-    val gen = paths(race)
+    val gen = RandomExecutor.run(race)
 
-    expectToSee(gen)(Success(1), Success(2))
+    shouldBeASimulation(gen)(race.run)
   }
 
-  private def expectToSee[A](gen: ZStream[Clock with Random, Nothing, A])(expected: A*): Future[Boolean] =
+  private def expectToSee[A](gen: ZStream[Clock, Nothing, A])(expected: A*): Future[Boolean] =
     unsafeRunToFuture(gen.take(100).fold(Set.empty[A])(_ + _).map(_ == expected.toSet))
+
+  private def shouldBeASimulation[E, A](
+    gen: ZIO[Clock, E, A]
+  )(expected: ZIO[Clock, E, A]): Future[Boolean] = {
+    val real = sample(expected)
+
+    unsafeRunToFuture(real.flatMap(collectUntil(gen, _)))
+  }
+
+  private def collectUntil[R, E, A](
+    gen: ZIO[R, E, A],
+    real: Set[Exit[E, A]],
+    max: Int = 1000
+  ): ZIO[R, Nothing, Boolean] =
+    if (real.isEmpty)
+      ZIO.succeed(true)
+    else if (max <= 0)
+      ZIO.succeed(false)
+    else
+      gen.run.flatMap(probe => collectUntil(gen, real - probe, max - 1))
+
+  private def sample[R, E, A](zio: ZIO[R, E, A]): URIO[R, Set[Exit[E, A]]] =
+    ZStream.fromEffect(zio.run).take(10).fold(Set.empty[Exit[E, A]])(_ + _)
 }
