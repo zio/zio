bb36ea306188c96ee49c8acadae21303793f1308
Salar Rahmanian <code@softinio.com>
2019-12-07 18:05:21 -0800

    scala collections compat

--- core/jvm/src/main/scala/zio/zmx/ZMXServer.scala
+++ core/jvm/src/main/scala/zio/zmx/ZMXServer.scala
@@ -21,7 +21,7 @@ import java.nio.ByteBuffer
 import java.nio.channels.{ SelectionKey, Selector, ServerSocketChannel, SocketChannel }
 import java.util.Iterator
 
-import scala.collection.JavaConverters._
+import scala.jdk.CollectionConverters._
 import scala.collection.mutable.Set
 
 object ZMXServer {
