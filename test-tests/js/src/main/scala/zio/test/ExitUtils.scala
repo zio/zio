8367a0643fb3a018ca17616888ecba7b400a0472
lvitaly <lvitalii@gmail.com>
2019-12-16 08:25:37 +0200

    fix for passing testJS

--- test-tests/js/src/main/scala/zio/test/ExitUtils.scala
+++ test-tests/js/src/main/scala/zio/test/ExitUtils.scala
@@ -4,7 +4,7 @@ import scala.concurrent.{ ExecutionContext, Future }
 
 private[test] object ExitUtils {
 
-  def fail(): Unit = System.exit(-1)
+  def fail(): Unit = ()
 
   def await(f: Future[Boolean])(implicit ec: ExecutionContext): Unit =
     f.foreach(_ => ())
