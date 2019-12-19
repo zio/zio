80f75d18e2eb1537b11d1895c5198baeddc201e3
Paweł Kiersznowski <pawel@kiersznowski.com>
2019-12-19 23:25:53 +0100

    #2341 fmt

--- streams/shared/src/main/scala/zio/stream/ZSink.scala
+++ streams/shared/src/main/scala/zio/stream/ZSink.scala
@@ -1062,7 +1062,6 @@ object ZSink extends ZSinkPlatformSpecificConstructors with Serializable {
 
   implicit class InvariantOps[R, E, A0, A, B](val sink: ZSink[R, E, A0, A, B]) extends AnyVal { self =>
 
-
     /**
      * Drops the first `n`` elements from the sink.
      */
@@ -1072,10 +1071,10 @@ object ZSink extends ZSinkPlatformSpecificConstructors with Serializable {
 
         val initial = sink.initial.map((_, 0L)).asInstanceOf[ZIO[R, E, this.State]]
 
-        def step(state: State, a: A) = 
-        if(state._2 != n) UIO.succeed((state._1, state._2 + 1)) 
-        else sink.step(state._1, a).map((_, state._2))
-          
+        def step(state: State, a: A) =
+          if (state._2 != n) UIO.succeed((state._1, state._2 + 1))
+          else sink.step(state._1, a).map((_, state._2))
+
         def extract(state: State) = sink.extract(state._1)
 
         def cont(state: State) = sink.cont(state._1)
