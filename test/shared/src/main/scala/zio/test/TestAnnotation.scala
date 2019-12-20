62a3ec737cc73fe9cbd491a2f527770ea0c6046e
Adam Fraser <adam.fraser@gmail.com>
2019-12-20 16:07:36 -0500

    Implement Ignored, Repeated, and Retried Test Annotations (#2502)
    * implement ignored
    
    * fix failing test
    
    * format
    
    * implement retried
    
    * add type annotation
    
    * implement retried
    
    * update rendering

--- test/shared/src/main/scala/zio/test/TestAnnotation.scala
+++ test/shared/src/main/scala/zio/test/TestAnnotation.scala
@@ -30,10 +30,11 @@ final class TestAnnotation[V] private (
   private val classTag: ClassTag[V]
 ) {
   override final def equals(that: Any): Boolean = that match {
-    case that: TestAnnotation[_] => (identifier, classTag) == ((identifier, that.classTag))
+    case that: TestAnnotation[_] => (identifier, classTag) == ((that.identifier, that.classTag))
   }
 
-  override final lazy val hashCode = (identifier, classTag).hashCode
+  override final lazy val hashCode =
+    (identifier, classTag).hashCode
 }
 object TestAnnotation {
 
@@ -43,8 +44,26 @@ object TestAnnotation {
     new TestAnnotation(identifier, initial, combine, classTag)
 
   /**
+   * An annotation which counts ignored tests.
+   */
+  final val ignored: TestAnnotation[Int] =
+    TestAnnotation("ignored", 0, _ + _)
+
+  /**
+   * An annotation which counts repeated tests.
+   */
+  final val repeated: TestAnnotation[Int] =
+    TestAnnotation("repeated", 0, _ + _)
+
+  /**
+   * An annotation which counts retried tests.
+   */
+  final val retried: TestAnnotation[Int] =
+    TestAnnotation("retried", 0, _ + _)
+
+  /**
    * An annotation for timing.
    */
-  val timing: TestAnnotation[Duration] =
+  final val timing: TestAnnotation[Duration] =
     TestAnnotation("timing", Duration.Zero, _ + _)
 }
