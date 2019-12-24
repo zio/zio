9abdc1e34c76918d03512b642ac0eec509d35072
Dejan Mijic <dmijic@acm.org>
2019-12-24 21:55:30 +0100

    Remove string benchmarks as they bring no value

--- benchmarks/src/main/scala/zio/stm/TMapBenchmarks.scala
+++ benchmarks/src/main/scala/zio/stm/TMapBenchmarks.scala
@@ -1,132 +1,66 @@
 package zio.stm
 
-import java.util.UUID
 import java.util.concurrent.TimeUnit
 
 import org.openjdk.jmh.annotations._
 import zio._
-import scala.util.Random
 
 @State(Scope.Thread)
 @BenchmarkMode(Array(Mode.Throughput))
 @OutputTimeUnit(TimeUnit.SECONDS)
 class TMapBenchmarks {
+  import IOBenchmarks.unsafeRun
 
   @Param(Array("10", "100", "1000", "10000", "100000"))
   private var size: Int = _
 
-  @Benchmark
-  def intInsertion(): List[Int] = insertion(ints)
+  private var map: TMap[Int, Int] = _
+  private var keys: List[Int]     = _
 
-  @Benchmark
-  def intUpdate(): List[Int] = update(ints)(v => v + v)
-
-  @Benchmark
-  def intTransform(): List[Int] = transform(ints)(v => v + v)
-
-  @Benchmark
-  def intTransformM(): List[Int] = transformM(ints)(v => STM.succeed(v + v))
-
-  @Benchmark
-  def intRemoval(): Unit = removal(ints)
-
-  @Benchmark
-  def intLookup(): List[Int] = lookup(ints)
-
-  @Benchmark
-  def stringInsertion(): List[String] = insertion(strings)
-
-  @Benchmark
-  def stringUpdate(): List[String] = update(strings)(v => v + v)
-
-  @Benchmark
-  def stringTransform(): List[String] = transform(strings)(v => v + v)
-
-  @Benchmark
-  def stringTransformM(): List[String] = transformM(strings)(v => STM.succeed(v + v))
-
-  @Benchmark
-  def stringRemoval(): Unit = removal(strings)
-
-  @Benchmark
-  def stringLookup(): List[String] = lookup(strings)
-
-  private def insertion[A](list: Int => List[A]): List[A] = {
-    val tx =
-      for {
-        map   <- TMap.empty[A, A]
-        items = list(size)
-        _     <- STM.foreach(items)(i => map.put(i, i))
-        res   <- map.keys
-      } yield res
-
-    IOBenchmarks.unsafeRun(tx.commit)
+  @Setup(Level.Trial)
+  def setup(): Unit = {
+    keys = (1 to size).toList
+    map = unsafeRun(TMap.fromIterable(keys.zipWithIndex).commit)
   }
 
-  private def update[A](list: Int => List[A])(f: A => A): List[A] = {
-    val items = list(size)
-
+  @Benchmark
+  def insertion(): Unit = {
     val tx =
       for {
-        map  <- TMap.fromIterable(items.map(i => (i, i)))
-        _    <- STM.foreach(items)(i => map.put(i, f(i)))
-        vals <- map.values
-      } yield vals
-
-    IOBenchmarks.unsafeRun(tx.commit)
-  }
-
-  private def transform[A](list: Int => List[A])(f: A => A): List[A] = {
-    val items = list(size)
-
-    val tx =
-      for {
-        map  <- TMap.fromIterable(items.map(i => (i, i)))
-        _    <- map.transform((k, v) => (k, f(v)))
-        vals <- map.values
-      } yield vals
-
-    IOBenchmarks.unsafeRun(tx.commit)
-  }
-
-  private def transformM[A](list: Int => List[A])(f: A => STM[Nothing, A]): List[A] = {
-    val items = list(size)
-
-    val tx =
-      for {
-        map  <- TMap.fromIterable(items.map(i => (i, i)))
-        _    <- map.transformM((k, v) => f(v).map(k -> _))
-        vals <- map.values
-      } yield vals
-
-    IOBenchmarks.unsafeRun(tx.commit)
-  }
-
-  private def removal[A](list: Int => List[A]): Unit = {
-    val items = list(size)
-
-    val tx =
-      for {
-        map <- TMap.fromIterable(items.map(i => (i, i)))
-        _   <- STM.foreach(items)(map.delete)
+        map <- TMap.empty[Int, Int]
+        _   <- STM.foreach(keys)(i => map.put(i, i))
       } yield ()
 
-    IOBenchmarks.unsafeRun(tx.commit)
+    unsafeRun(tx.commit)
   }
 
-  private def lookup[A](list: Int => List[A]): List[A] = {
-    val items = list(size)
-
-    val tx =
-      for {
-        map <- TMap.fromIterable(items.map(i => (i, i)))
-        res <- STM.foreach(items)(map.get)
-      } yield res.flatten
-
-    IOBenchmarks.unsafeRun(tx.commit)
+  @Benchmark
+  def lookup(): List[Int] = {
+    val tx = STM.foreach(keys)(map.get).map(_.flatten)
+    unsafeRun(tx.commit)
   }
 
-  private def ints(n: Int): List[Int] = Random.shuffle((1 to n).toList)
+  @Benchmark
+  def update(): Unit = {
+    val tx = STM.foreach(keys)(i => map.put(i, i * 2)).unit
+    unsafeRun(tx.commit)
+  }
 
-  private def strings(n: Int): List[String] = List.fill(n)(UUID.randomUUID().toString())
+  @Benchmark
+  def transform(): Unit = {
+    val tx = map.transform((k, v) => (k, v * 2))
+    unsafeRun(tx.commit)
+  }
+
+  @Benchmark
+  def transformM(): Unit = {
+    val tx = map.transformM((k, v) => STM.succeed(v * 2).map(k -> _))
+    unsafeRun(tx.commit)
+  }
+
+  @Benchmark
+  def removal(): Unit = {
+    val tx = STM.foreach(keys)(map.delete).unit
+    unsafeRun(tx.commit)
+  }
 }
