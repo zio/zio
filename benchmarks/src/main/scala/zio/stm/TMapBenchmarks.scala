0256b0d909df621c9952678ab579c3fb37916f31
Dejan Mijic <dmijic@acm.org>
2019-12-25 21:08:02 +0100

    Trigger fiber contention

--- benchmarks/src/main/scala/zio/stm/TMapBenchmarks.scala
+++ benchmarks/src/main/scala/zio/stm/TMapBenchmarks.scala
@@ -19,12 +19,32 @@ class TMapBenchmarks {
   private var map: TMap[Int, Int]      = _
   private var ref: TRef[Map[Int, Int]] = _
 
+  private var mapUpdates: List[UIO[Int]] = _
+  private var refUpdates: List[UIO[Int]] = _
+
   @Setup(Level.Trial)
   def setup(): Unit = {
     idx = size / 2
     keys = (1 to size).toList
     map = unsafeRun(TMap.fromIterable(keys.zipWithIndex).commit)
     ref = unsafeRun(TRef.makeCommit(Map.empty[Int, Int]))
+
+    val schedule = Schedule.recurs(1000)
+
+    mapUpdates = keys.map(i => map.put(i, i).commit.repeat(schedule))
+    refUpdates = keys.map(i => ref.update(_.updated(i, i)).commit.repeat(schedule))
+  }
+
+  @Benchmark
+  def contentionMap(): Unit = {
+    val forked = blocking.blocking(UIO.forkAll_(mapUpdates))
+    unsafeRun(forked)
+  }
+
+  @Benchmark
+  def contentionRef(): Unit = {
+    val forked = blocking.blocking(UIO.forkAll_(refUpdates))
+    unsafeRun(forked)
   }
 
   @Benchmark
@@ -55,17 +75,5 @@ class TMapBenchmarks {
   def removal(): Unit = {
     val tx = map.delete(idx)
     unsafeRun(tx.commit)
-  }
-
-  @Benchmark
-  def contentionMap(): Unit = {
-    val txs = keys.map(i => map.put(i, i).commit)
-    unsafeRun(UIO.forkAll_(txs))
-  }
-
-  @Benchmark
-  def contentionRef(): Unit = {
-    val txs = keys.map(i => ref.update(_.updated(i, i)).commit)
-    unsafeRun(UIO.forkAll_(txs))
   }
 }
