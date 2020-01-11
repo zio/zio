1df2690e024df77b4e1f5002a360c65f46f2bf62
Adam Fraser <adam.fraser@gmail.com>
2020-01-11 07:06:08 -0800

    address additional review comment

--- core/native/src/main/scala/zio/PlatformSpecific.scala
+++ core/native/src/main/scala/zio/PlatformSpecific.scala
@@ -25,8 +25,10 @@ import zio.system.System
 private[zio] trait PlatformSpecific {
   type ZEnv = Clock with Console with System with Random with Scheduler
 
-  private[zio] val defaultEnvironment: ZLayer.NoDeps[Nothing, ZEnv] =
-    (Scheduler.live >>> Clock.live) ++ Console.live ++ System.live ++ Random.live ++ Scheduler.live
+  object ZEnv {
+    val live: ZLayer.NoDeps[Nothing, ZEnv] =
+      (Scheduler.live >>> Clock.live) ++ Console.live ++ System.live ++ Random.live ++ Scheduler.live
+  }
 
   type Tagged[A] = scala.reflect.ClassTag[A]
   type TagType   = scala.reflect.ClassTag[_]
