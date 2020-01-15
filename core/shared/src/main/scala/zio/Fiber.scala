329f161c8412ea2d5571961f703ca83f18977ad0
Dmitry Karlinsky <dmitryka@wix.com>
2020-01-16 00:06:51 +0200

    Runtime.unsafeRunTask that applies only for `E <: Throwable` (#2600)
    * adding Runtime.unsafeRunTask that applies only for `E <: Throwable`.
    * on failure throws the result of new method Cause.squashWithTrace to attach fiber traces to the exception.
    * changed ZIO.toFuture (and thus Runtime.unsafeRunToFuture) to use Cause.squashWithTrace as well
    
    * silenced warnings
    
    * attempting to fix dotty compilation failure
    
    * fixing scaladocs
    
    * Update core/jvm/src/main/scala/zio/internal/PlatformSpecific.scala
    
    Co-Authored-By: Ferdinand Svehla <ferdinand@fsvehla.at>
    
    * Update core/shared/src/main/scala/zio/Runtime.scala
    
    Co-Authored-By: Ferdinand Svehla <ferdinand@fsvehla.at>
    
    * Update core/shared/src/main/scala/zio/Cause.scala
    
    Co-Authored-By: Ferdinand Svehla <ferdinand@fsvehla.at>
    
    * Update core/shared/src/main/scala/zio/Runtime.scala
    
    Co-Authored-By: Ferdinand Svehla <ferdinand@fsvehla.at>
    
    * types in scaladocs
    
    * * added `Cause.squashTrace`
    * renamed `Assertion.hasMessage(message: String)` to `hasMessageEqualTo`
    
    * using `squashTrace` in `Runtime.unsafeRunTask`
    * scala fmt
    
    * removed `Assetion.hasMessage(message: String)`
    
    * fixes after rebase
    
    * scalafix and fmt
    
    * fixing native complication
    
    Co-authored-by: Ferdinand Svehla <ferdinand@fsvehla.at>

--- core/shared/src/main/scala/zio/Fiber.scala
+++ core/shared/src/main/scala/zio/Fiber.scala
@@ -285,7 +285,8 @@ trait Fiber[+E, +A] { self =>
 
   /**
    * Converts this fiber into a [[scala.concurrent.Future]], translating
-   * any errors to [[java.lang.Throwable]] with the specified conversion function.
+   * any errors to [[java.lang.Throwable]] with the specified conversion function,
+   * using [[Cause.squashWithTrace]]
    *
    * @param f function to the error into a Throwable
    * @return `UIO[Future[A]]`
@@ -294,7 +295,7 @@ trait Fiber[+E, +A] { self =>
     UIO.effectTotal {
       val p: concurrent.Promise[A] = scala.concurrent.Promise[A]()
 
-      def failure(cause: Cause[E]): UIO[p.type] = UIO(p.failure(cause.squashWith(f)))
+      def failure(cause: Cause[E]): UIO[p.type] = UIO(p.failure(cause.squashWithTrace(f)))
       def success(value: A): UIO[p.type]        = UIO(p.success(value))
 
       ZIO.runtime[Any].map { runtime =>
