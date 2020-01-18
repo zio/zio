581a5df19e7c2a4d69748a91bea67afd32d5dd27
sken <22631338+sken77@users.noreply.github.com>
2020-01-18 07:13:08 -0600

    Expose more methods for Tqueue (#2612)
    * expose more methods for Tqueue
    
    * fix lint issues
    
    * add more combinators
    
    * make requested changes, implement longest
    
    * add TQueueSpec
    
    * add requested changes

--- core/shared/src/main/scala/zio/stm/TQueue.scala
+++ core/shared/src/main/scala/zio/stm/TQueue.scala
@@ -54,9 +54,59 @@ final class TQueue[A] private (val capacity: Int, ref: TRef[ScalaQueue[A]]) {
       .map(_.splitAt(max))
       .flatMap(split => ref.set(split._2) *> STM.succeed(split._1))
       .map(_.toList)
+
+  /**
+   * View the next element in the queue without removing it. retries if the queue is empty.
+   */
+  def peek: STM[Nothing, A] =
+    ref.get.flatMap(
+      _.headOption match {
+        case Some(a) => STM.succeed(a)
+        case None    => STM.retry
+      }
+    )
+
+  /**
+   * View the last element inserted into the queue. retries if the queue is empty
+   */
+  def last: STM[Nothing, A] =
+    ref.get.flatMap(
+      _.lastOption match {
+        case Some(a) => STM.succeed(a)
+        case None    => STM.retry
+      }
+    )
+
+  /**
+   * Checks if the Queue is empty
+   */
+  def isEmpty: STM[Nothing, Boolean] = ref.get.map(_.isEmpty)
+
+  /**
+   * Checks if the Queue is at capacity
+   */
+  def isFull: STM[Nothing, Boolean] = ref.get.map(_.size == capacity)
+
+  /**
+   * Drops elements from the queue while they dont satisfy the predicate until it does and takes it.
+   */
+  def seek(f: A => Boolean): STM[Nothing, A] = {
+    @annotation.tailrec
+    def go(q: ScalaQueue[A]): STM[Nothing, A] =
+      q.dequeueOption match {
+        case Some((a, as)) =>
+          if (f(a)) ref.set(as) *> STM.succeed(a)
+          else go(as)
+        case None => STM.retry
+      }
+
+    ref.get.flatMap(go)
+  }
 }
 
 object TQueue {
   def make[A](capacity: Int): STM[Nothing, TQueue[A]] =
     TRef.make(ScalaQueue.empty[A]).map(ref => new TQueue(capacity, ref))
+
+  def unbounded[A]: STM[Nothing, TQueue[A]] = make(Int.MaxValue)
 }
