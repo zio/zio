47850b564814252b39d15cee3a1bb79876488260
ioleo <ioleo+git@protonmail.com>
2020-01-22 18:37:04 +0100

    ZIO Mock: correctly report unexpected calls (#2675)
    * ZIO Mock: correctly report unexpected calls
    
    * Fix inference issue
    
    * Retrigger CI

--- test-tests/shared/src/test/scala/zio/test/mock/ExpectationSpec.scala
+++ test-tests/shared/src/test/scala/zio/test/mock/ExpectationSpec.scala
@@ -5,7 +5,12 @@ import ExpectationSpecUtils._
 import zio.duration._
 import zio.test.Assertion.{ equalTo, isNone, isUnit, isWithin }
 import zio.test.mock.Expectation.{ failure, failureF, failureM, never, unit, value, valueF, valueM }
-import zio.test.mock.MockException.{ InvalidArgumentsException, InvalidMethodException, UnmetExpectationsException }
+import zio.test.mock.MockException.{
+  InvalidArgumentsException,
+  InvalidMethodException,
+  UnexpectedCallExpection,
+  UnmetExpectationsException
+}
 import zio.test.{ suite, ZIOBaseSpec }
 import zio.{ IO, UIO }
 
@@ -307,6 +312,11 @@ object ExpectationSpec extends ZIOBaseSpec {
             )
           )
         )
+      ),
+      testSpecDied("unexpected call")(
+        Module.singleParam(equalTo(1)) returns value("foo"),
+        Module.>.singleParam(1) *> Module.>.manyParams(2, "3", 4L),
+        equalTo(UnexpectedCallExpection(Module.manyParams, (2, "3", 4L)))
       )
     )
   )
