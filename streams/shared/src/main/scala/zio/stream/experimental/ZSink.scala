6c3ba460d52c6aa260a4d10ac049add01d6fd758
Vasil Vasilev <vasilv@protonmail.com>
2020-01-27 14:44:43 +0100

    Introduce ZStream.Control and ZSink.Control (#2726)
    * Introduce ZStream.Control
    
    * Introduce ZSink.Control
    
    * Remove type aliases
    
    * ZStream and ZSink are value types
    
    * Simplify ZStream.fromEffect

--- streams/shared/src/main/scala/zio/stream/experimental/ZSink.scala
+++ streams/shared/src/main/scala/zio/stream/experimental/ZSink.scala
@@ -2,15 +2,29 @@ package zio.stream.experimental
 
 import zio._
 
-class ZSink[-R, +E, +I, -A, +B](val process: ZManaged[R, E, ZSink.Push[R, E, I, A, B]]) {
-  def map[C](f: B => C): ZSink[R, E, I, A, C] =
-    ZSink(process.map(push => a => push(a).mapError(_.left.map(f))))
+class ZSink[-R, +E, +I, -A, +B](
+  val process: ZManaged[R, E, ZSink.Control[R, E, I, A, B]]
+) extends AnyVal { self =>
+  import ZSink.Control
 
+  def map[C](f: B => C): ZSink[R, E, I, A, C] =
+    ZSink {
+      self.process.map { control =>
+        Control(
+          a => control.push(a).mapError(_.map(f)),
+          control.query
+        )
+      }
+    }
 }
 
 object ZSink {
-  type Push[-R, +E, +I, -A, +B] = A => ZIO[R, Either[B, E], I]
 
-  def apply[R, E, I, A, B](process: ZManaged[R, E, ZSink.Push[R, E, I, A, B]]): ZSink[R, E, I, A, B] =
+  final case class Control[-R, +E, +I, -A, +B](
+    push: A => ZIO[R, Either[E, B], Any],
+    query: ZIO[R, E, I]
+  )
+
+  def apply[R, E, I, A, B](process: ZManaged[R, E, ZSink.Control[R, E, I, A, B]]): ZSink[R, E, I, A, B] =
     new ZSink(process)
 }
