b8061f4666eaf3019a549ab810a610c6204ff4bf
Vasil Vasilev <vasilv@protonmail.com>
2020-01-27 14:44:43 +0100

    Introduce ZStream.Control and ZSink.Control (#2726)
    * Introduce ZStream.Control
    
    * Introduce ZSink.Control
    
    * Remove type aliases
    
    * ZStream and ZSink are value types
    
    * Simplify ZStream.fromEffect

--- streams/shared/src/main/scala/zio/stream/experimental/ZStream.scala
+++ streams/shared/src/main/scala/zio/stream/experimental/ZStream.scala
@@ -2,22 +2,56 @@ package zio.stream.experimental
 
 import zio._
 
-class ZStream[-R, +E, -I, +O, +A](val process: ZManaged[R, Nothing, ZStream.Pull[R, E, I, O, A]]) { self =>
-  def map[B](f: A => B): ZStream[R, E, I, O, B] =
-    ZStream(self.process.map(pull => i => pull(i).map(f)))
+class ZStream[-R, +E, -I, +B, +A](
+  val process: ZManaged[R, Nothing, ZStream.Control[R, E, I, B, A]]
+) extends AnyVal { self =>
+  import ZStream.Control
+
+  def map[C](f: A => C): ZStream[R, E, I, B, C] =
+    ZStream {
+      self.process.map { control =>
+        Control(
+          control.pull.map(f),
+          control.command
+        )
+      }
+    }
 }
 
 object ZStream {
-  type Pull[-R, +E, -I, +O, +A] = I => ZIO[R, Either[O, E], A]
+
+  final case class Control[-R, +E, -I, +B, +A](
+    pull: ZIO[R, Either[E, B], A],
+    command: I => ZIO[R, E, Any]
+  )
+
   object Pull {
-    val end: Pull[Any, Nothing, Any, Unit, Nothing] = _ => IO.fail(Left(()))
+    def end[B](b: => B): IO[Either[Nothing, B], Nothing] = IO.fail(Right(b))
+
+    val endUnit: IO[Either[Nothing, Unit], Nothing] = end(())
   }
 
-  def apply[R, E, I, O, A](process: ZManaged[R, Nothing, Pull[R, E, I, O, A]]): ZStream[R, E, I, O, A] =
+  object Command {
+    val noop: Any => UIO[Unit] = _ => UIO.unit
+  }
+
+  def apply[R, E, I, B, A](process: ZManaged[R, Nothing, Control[R, E, I, B, A]]): ZStream[R, E, I, B, A] =
     new ZStream(process)
 
   def fromEffect[R, E, A](zio: ZIO[R, E, A]): ZStream[R, E, Any, Unit, A] =
-    managed(zio.toManaged_)
+    ZStream {
+      for {
+        done <- Ref.make(false).toManaged_
+        pull = done.get.flatMap {
+          if (_) Pull.endUnit
+          else
+            (for {
+              _ <- done.set(false)
+              a <- zio
+            } yield a).mapError(Left(_))
+        }
+      } yield Control(pull, Command.noop)
+    }
 
   /**
    * Creates a single-valued stream from a managed resource
@@ -27,19 +61,18 @@ object ZStream {
       for {
         doneRef   <- Ref.make(false).toManaged_
         finalizer <- ZManaged.finalizerRef[R](_ => UIO.unit)
-        pull = (_: Any) =>
-          ZIO.uninterruptibleMask { restore =>
-            doneRef.get.flatMap { done =>
-              if (done) IO.fail(Left(()))
-              else
-                (for {
-                  _           <- doneRef.set(true)
-                  reservation <- managed.reserve
-                  _           <- finalizer.set(reservation.release)
-                  a           <- restore(reservation.acquire)
-                } yield a).mapError(Right(_))
-            }
+        pull = ZIO.uninterruptibleMask { restore =>
+          doneRef.get.flatMap { done =>
+            if (done) IO.fail(Right(()))
+            else
+              (for {
+                _           <- doneRef.set(true)
+                reservation <- managed.reserve
+                _           <- finalizer.set(reservation.release)
+                a           <- restore(reservation.acquire)
+              } yield a).mapError(Left(_))
           }
-      } yield pull
+        }
+      } yield Control(pull, Command.noop)
     }
 }
