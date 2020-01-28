e4035035311dea807b8c0b4977082619e1075851
Vasil Vasilev <vasilv@protonmail.com>
2020-01-28 14:16:09 +0100

    Add scaladoc for existing constructors and combinators

--- streams/shared/src/main/scala/zio/stream/experimental/ZStream.scala
+++ streams/shared/src/main/scala/zio/stream/experimental/ZStream.scala
@@ -8,6 +8,13 @@ class ZStream[-R, +E, -I, +B, +A](
     with Serializable { self =>
   import ZStream.Control
 
+  /**
+   * Maps the elements of this stream using a ''pure'' function.
+   *
+   * @tparam C the value type of the new stream
+   * @param f the ''pure'' transformation function
+   * @return a stream of transformed values
+   */
   def map[C](f: A => C): ZStream[R, E, I, B, C] =
     ZStream {
       self.process.map { control =>
@@ -36,10 +43,30 @@ object ZStream extends Serializable {
     val noop: Any => UIO[Unit] = _ => UIO.unit
   }
 
+  /**
+   * Creates a stream from a scoped [[Control]].
+   *
+   * @tparam R the stream environment type
+   * @tparam E the stream error type
+   * @tparam I the stream command input element type
+   * @tparam B the stream exit value type
+   * @tparam A the stream value type
+   * @param process the scoped control
+   * @return a new stream wrapping the scoped control
+   */
   def apply[R, E, I, B, A](process: ZManaged[R, Nothing, Control[R, E, I, B, A]]): ZStream[R, E, I, B, A] =
     new ZStream(process)
 
-  def fromEffect[R, E, A](zio: ZIO[R, E, A]): ZStream[R, E, Any, Unit, A] =
+  /**
+   * Creates a single-valued stream from an effect.
+   *
+   * @tparam R the environment type
+   * @tparam E the error type
+   * @tparam A the effect value type
+   * @param effect the effect
+   * @return a single-valued stream
+   */
+  def fromEffect[R, E, A](effect: ZIO[R, E, A]): ZStream[R, E, Any, Unit, A] =
     ZStream {
       for {
         done <- Ref.make(false).toManaged_
@@ -48,14 +75,20 @@ object ZStream extends Serializable {
           else
             (for {
               _ <- done.set(true)
-              a <- zio
+              a <- effect
             } yield a).mapError(Left(_))
         }
       } yield Control(pull, Command.noop)
     }
 
   /**
-   * Creates a single-valued stream from a managed resource
+   * Creates a single-valued stream from a managed resource.
+   *
+   * @tparam R the environment type
+   * @tparam E the error type
+   * @tparam A the managed resource type
+   * @param managed the managed resource
+   * @return a single-valued stream
    */
   def managed[R, E, A](managed: ZManaged[R, E, A]): ZStream[R, E, Any, Unit, A] =
     ZStream {
