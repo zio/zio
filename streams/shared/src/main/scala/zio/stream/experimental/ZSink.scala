766dd2f6876484cfc01d4ba7140854d96bbdf47e
Vasil Vasilev <vasilv@protonmail.com>
2020-01-28 14:21:48 +0100

    Add scaladoc for existing ZStream constructors and combinators

--- streams/shared/src/main/scala/zio/stream/experimental/ZSink.scala
+++ streams/shared/src/main/scala/zio/stream/experimental/ZSink.scala
@@ -8,6 +8,13 @@ class ZSink[-R, +E, +I, -A, +B](
     with Serializable { self =>
   import ZSink.Control
 
+  /**
+   * Maps the exit value of this sink using a ''pure'' function.
+   *
+   * @tparam C the value type of the new sink
+   * @param f the ''pure'' transformation function
+   * @return a sink that produces a transformed value
+   */
   def map[C](f: B => C): ZSink[R, E, I, A, C] =
     ZSink {
       self.process.map { control =>
@@ -26,6 +33,17 @@ object ZSink extends Serializable {
     query: ZIO[R, E, I]
   )
 
+  /**
+   * Creates a sink from a scoped [[Control]].
+   *
+   * @tparam R the sink environment type
+   * @tparam E the sink error type
+   * @tparam I the sink internal state type
+   * @tparam A the sink input type
+   * @tparam B the sink exit value type
+   * @param process the scoped control
+   * @return a new sink wrapping the scoped control
+   */
   def apply[R, E, I, A, B](process: ZManaged[R, E, ZSink.Control[R, E, I, A, B]]): ZSink[R, E, I, A, B] =
     new ZSink(process)
 }
