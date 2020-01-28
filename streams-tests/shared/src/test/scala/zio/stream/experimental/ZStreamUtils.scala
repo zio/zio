587b9c9eacd342178e9126c3e4758f859b49af3d
Vasil Vasilev <vasilv@protonmail.com>
2020-01-28 13:09:15 +0100

    Remove unnecessary util trait

--- streams-tests/shared/src/test/scala/zio/stream/experimental/ZStreamUtils.scala
+++ streams-tests/shared/src/test/scala/zio/stream/experimental/ZStreamUtils.scala
@@ -2,12 +2,10 @@ package zio.stream.experimental
 
 import zio._
 
-trait ZStreamUtils {
+object ZStreamUtils {
   def nPulls[R, E, B, A](
     control: ZStream.Control[R, E, Nothing, B, A],
     n: Int
   ): ZIO[R, Nothing, List[Either[Either[E, B], A]]] =
     ZIO.foreach(1 to n)(_ => control.pull.either)
 }
-
-object ZStreamUtils extends ZStreamUtils
