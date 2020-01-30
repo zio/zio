89366e6fe2fd75f0c22ddca8c5c40afb558fa6b3
Vasil Vasilev <vasilv@protonmail.com>
2020-01-30 16:41:50 +0100

    Remove string gens because they allocate too many short lived objects

--- streams-tests/jvm/src/test/scala/zio/stream/GenUtils.scala
+++ streams-tests/jvm/src/test/scala/zio/stream/GenUtils.scala
@@ -6,7 +6,5 @@ import zio.test.Gen
 trait GenUtils {
   def toBoolFn[R <: Random, A] = Gen.function[R, A, Boolean](Gen.boolean)
 
-  val stringGen = Gen.small(Gen.stringN(_)(Gen.alphaNumericChar))
-
   val intGen = Gen.int(-10, 10)
 }
