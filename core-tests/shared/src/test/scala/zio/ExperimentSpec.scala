be754aed72dc58cb7008cc130f29221260861b01
Adam Fraser <adam.fraser@gmail.com>
2020-02-06 14:47:10 -0500

    initial work

---
+++ core-tests/shared/src/test/scala/zio/ExperimentSpec.scala
@@ -0,0 +1,130 @@
+import zio.test.Assertion._
+import zio.test._
+import zio.{ ZLayer => _, _ }
+
+object ExperimentSpec extends DefaultRunnableSpec {
+  import Experiment._
+
+  type Module1 = Has[Module1.Service]
+
+  object Module1 {
+    trait Service
+  }
+
+  def makeLayer1(ref: Ref[Vector[String]]): ZLayer[Any, Nothing, Module1] =
+    ZLayer.fromManaged(
+      ZManaged.make(ref.update(_ :+ "Acquiring Module 1").as(Has(new Module1.Service {})))(_ =>
+        ref.update(_ :+ "Releasing Module 1")
+      )
+    )
+
+  type Module2 = Has[Module2.Service]
+
+  object Module2 {
+    trait Service
+  }
+
+  def makeLayer2(ref: Ref[Vector[String]]): ZLayer[Any, Nothing, Module2] =
+    ZLayer.fromManaged(
+      ZManaged.make(ref.update(_ :+ "Acquiring Module 2").as(Has(new Module2.Service {})))(_ =>
+        ref.update(_ :+ "Releasing Module 2")
+      )
+    )
+
+  type Module3 = Has[Module3.Service]
+
+  object Module3 {
+    trait Service
+  }
+
+  def makeLayer3(ref: Ref[Vector[String]]): ZLayer[Any, Nothing, Module3] =
+    ZLayer.fromManaged(
+      ZManaged.make(ref.update(_ :+ "Acquiring Module 3").as(Has(new Module3.Service {})))(_ =>
+        ref.update(_ :+ "Releasing Module 3")
+      )
+    )
+
+  def makeRef: UIO[Ref[Vector[String]]] =
+    Ref.make(Vector.empty)
+
+  def spec = suite("ExperimentSpec")(
+    testM("layers are shared with ++") {
+      val expected = Vector(
+        "Acquiring Module 1",
+        "Releasing Module 1"
+      )
+      for {
+        ref    <- makeRef
+        layer1 = makeLayer1(ref)
+        env    = (layer1 ++ layer1).build
+        _      <- env.use_(ZIO.unit)
+        actual <- ref.get
+      } yield assert(actual)(equalTo(expected))
+    },
+    testM("layers are shared with >>>") {
+      val expected = Vector(
+        "Acquiring Module 1",
+        "Releasing Module 1"
+      )
+      for {
+        ref    <- makeRef
+        layer1 = makeLayer1(ref)
+        env    = (layer1 >>> layer1).build
+        _      <- env.use_(ZIO.unit)
+        actual <- ref.get
+      } yield assert(actual)(equalTo(expected))
+    },
+    testM("multiple layers acquired and released in correct order with ++") {
+      val expected = Vector(
+        "Acquiring Module 1",
+        "Acquiring Module 2",
+        "Releasing Module 2",
+        "Releasing Module 1"
+      )
+      for {
+        ref    <- makeRef
+        layer1 = makeLayer1(ref)
+        layer2 = makeLayer2(ref)
+        env    = (layer1 ++ layer2).build
+        _      <- env.use_(ZIO.unit)
+        actual <- ref.get
+      } yield assert(actual)(equalTo(expected))
+    },
+    testM("multiple layers acquired and released in correct order with >>>") {
+      val expected = Vector(
+        "Acquiring Module 1",
+        "Acquiring Module 2",
+        "Releasing Module 2",
+        "Releasing Module 1"
+      )
+      for {
+        ref    <- makeRef
+        layer1 = makeLayer1(ref)
+        layer2 = makeLayer2(ref)
+        env    = (layer1 >>> layer2).build
+        _      <- env.use_(ZIO.unit)
+        actual <- ref.get
+      } yield assert(actual)(equalTo(expected))
+    },
+    testM("shared dependency at different levels") {
+      val expected = Vector(
+        "Acquiring Module 1",
+        "Acquiring Module 2",
+        "Acquiring Module 3",
+        "Releasing Module 3",
+        "Releasing Module 2",
+        "Releasing Module 1"
+      )
+      for {
+        ref    <- makeRef
+        layer1 = makeLayer1(ref)
+        layer2 = makeLayer2(ref)
+        layer3 = makeLayer3(ref)
+        env    = ((layer1 >>> layer2) ++ (layer1 >>> layer3)).build
+        _      <- env.use_(ZIO.unit)
+        actual <- ref.get
+      } yield assert(actual)(equalTo(expected))
+    }
+  )
+
+}
---
+++ core/shared/src/main/scala/zio/experiment.scala
@@ -0,0 +1,120 @@
+import zio.{ ZLayer => _, _ }
+
+object Experiment {
+
+  final case class ZLayer[-RIn, +E, +ROut <: Has[_]](value: Managed[Nothing, MemoMap => ZManaged[RIn, E, ROut]]) {
+    self =>
+
+    def >>>[E1 >: E, ROut2 <: Has[_]](that: ZLayer[ROut, E1, ROut2]): ZLayer[RIn, E1, ROut2] =
+      ZLayer {
+        Managed {
+          for {
+            ref <- Ref.make[Exit[Any, Any] => ZIO[Any, Nothing, Any]](_ => ZIO.unit)
+            managed = ZIO.succeed { (map: MemoMap) =>
+              for {
+                l <- map.get(self).toManaged_.flatMap {
+                      case None =>
+                        self.value.mapM(_(map).reserve).flatMap {
+                          case Reservation(acquire, release) =>
+                            acquire.flatMap(a => map.memoize(self, a).as(a)).toManaged_ <* ZIO
+                              .environment[RIn]
+                              .flatMap(env => ref.update(finalizer => e => release(e).provide(env) *> finalizer(e)))
+                              .toManaged_
+                        }
+                      case Some(service) => ZManaged.succeed(service)
+                    }
+                r <- map.get(that).toManaged_.flatMap {
+                      case None =>
+                        that.value
+                          .mapM(_(map).reserve)
+                          .flatMap {
+                            case Reservation(acquire, release) =>
+                              acquire.flatMap(a => map.memoize(that, a).as(a)).toManaged_ <* ZIO
+                                .environment[ROut]
+                                .flatMap(env => ref.update(finalizer => e => release(e).provide(env) *> finalizer(e)))
+                                .toManaged_
+                          }
+                          .provide(l)
+                      case Some(service) => ZManaged.succeed(service)
+                    }
+              } yield r
+            }
+          } yield Reservation(managed, e => ref.get.flatMap(_(e)))
+        }
+      }
+
+    def ++[E1 >: E, RIn2, ROut1 >: ROut <: Has[_], ROut2 <: Has[_]](
+      that: ZLayer[RIn2, E1, ROut2]
+    )(implicit tagged: Tagged[ROut2]): ZLayer[RIn with RIn2, E1, ROut1 with ROut2] =
+      ZLayer {
+        ZManaged {
+          for {
+            ref <- Ref.make[Exit[Any, Any] => ZIO[Any, Nothing, Any]](_ => ZIO.unit)
+            managed = ZIO.succeed { (map: MemoMap) =>
+              for {
+                l <- map.get(self).toManaged_.flatMap {
+                      case None =>
+                        self.value.mapM(_(map).reserve).flatMap {
+                          case Reservation(acquire, release) =>
+                            acquire.flatMap(a => map.memoize(self, a).as(a)).toManaged_ <* ZIO
+                              .environment[RIn]
+                              .flatMap(env => ref.update(finalizer => e => release(e).provide(env) *> finalizer(e)))
+                              .toManaged_
+                        }
+                      case Some(service) => ZManaged.succeed(service)
+                    }
+                r <- map.get(that).toManaged_.flatMap {
+                      case None =>
+                        that.value.mapM(_(map).reserve).flatMap {
+                          case Reservation(acquire, release) =>
+                            acquire.flatMap(a => map.memoize(that, a).as(a)).toManaged_ <* ZIO
+                              .environment[RIn2]
+                              .flatMap(env => ref.update(finalizer => e => release(e).provide(env) *> finalizer(e)))
+                              .toManaged_
+                        }
+                      case Some(service) => ZManaged.succeed(service)
+                    }
+              } yield l.union(r)
+            }
+          } yield Reservation(managed, e => ref.get.flatMap(_(e)))
+        }
+      }
+
+    def build: ZManaged[RIn, E, ROut] =
+      for {
+        map   <- MemoMap.make.toManaged_
+        outer <- self.value
+        r     <- outer(map)
+      } yield r
+  }
+
+  object ZLayer {
+
+    def fromManaged[R, E, A <: Has[_]](managed: ZManaged[R, E, A]): ZLayer[R, E, A] =
+      ZLayer(Managed.succeed(_ => managed))
+
+    def succeed[A <: Has[_]](a: => A): ZLayer[Any, Nothing, A] =
+      ZLayer(Managed.succeed(_ => Managed.succeed(a)))
+  }
+
+  trait MemoMap {
+    def get[E, A, B <: Has[_]](layer: ZLayer[A, E, B]): UIO[Option[B]]
+    def memoize[E, A, B <: Has[_]](layer: ZLayer[A, E, B], value: B): UIO[Unit]
+  }
+
+  object MemoMap {
+
+    def make: UIO[MemoMap] =
+      Ref.make[Map[ZLayer[Nothing, Any, Has[_]], Any]](Map.empty).map { ref =>
+        new MemoMap {
+          def get[E, A, B <: Has[_]](layer: ZLayer[A, E, B]): UIO[Option[B]] =
+            ref.get.map { map =>
+              map.get(layer).asInstanceOf[Option[B]]
+            }
+          def memoize[E, A, B <: Has[_]](layer: ZLayer[A, E, B], value: B): UIO[Unit] =
+            ref.update(_ + (layer -> value))
+        }
+      }
+  }
+
+}
