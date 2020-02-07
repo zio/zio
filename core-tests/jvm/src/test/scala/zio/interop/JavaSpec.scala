ce422a7956b831433a96650a94ebacf19d274d91
Regis Kuckaertz <regis.kuckaertz@gmail.com>
2020-02-07 15:12:20 +0000

    Import interop-java

---
+++ core-tests/jvm/src/test/scala/zio/interop/JavaSpec.scala
@@ -0,0 +1,195 @@
+package zio
+package interop
+
+import _root_.java.net.InetSocketAddress
+import _root_.java.nio.ByteBuffer
+import _root_.java.nio.channels.{ AsynchronousServerSocketChannel, AsynchronousSocketChannel }
+import _root_.java.util.concurrent.{ CompletableFuture, CompletionStage, Future }
+import zio.interop.javaz._
+import zio.test.Assertion._
+import zio.test._
+
+object JavaSpec {
+  def spec = suite("JavaSpec")(
+    suite("`Task.fromFutureJava` must")(
+      testM("be lazy on the `Future` parameter") {
+        var evaluated         = false
+        def ftr: Future[Unit] = CompletableFuture.supplyAsync(() => evaluated = true)
+        assertM(ZIO.fromFutureJava(UIO.effectTotal(ftr)).when(false).as(evaluated), isFalse)
+      },
+      testM("catch exceptions thrown by lazy block") {
+        val ex                          = new Exception("no future for you!")
+        val noFuture: UIO[Future[Unit]] = UIO.effectTotal(throw ex)
+        assertM(ZIO.fromFutureJava(noFuture).run, dies(equalTo(ex)))
+      },
+      testM("return an `IO` that fails if `Future` fails (failedFuture)") {
+        val ex                         = new Exception("no value for you!")
+        val noValue: UIO[Future[Unit]] = UIO.effectTotal(CompletableFuture_.failedFuture(ex))
+        assertM(ZIO.fromFutureJava(noValue).run, fails[Throwable](equalTo(ex)))
+      },
+      testM("return an `IO` that fails if `Future` fails (supplyAsync)") {
+        val ex                         = new Exception("no value for you!")
+        val noValue: UIO[Future[Unit]] = UIO.effectTotal(CompletableFuture.supplyAsync(() => throw ex))
+        assertM(ZIO.fromFutureJava(noValue).run, fails[Throwable](equalTo(ex)))
+      },
+      testM("return an `IO` that produces the value from `Future`") {
+        val someValue: UIO[Future[Int]] = UIO.effectTotal(CompletableFuture.completedFuture(42))
+        assertM(ZIO.fromFutureJava(someValue), equalTo(42))
+      },
+      testM("handle null produced by the completed `Future`") {
+        val someValue: UIO[Future[String]] = UIO.effectTotal(CompletableFuture.completedFuture[String](null))
+        assertM(ZIO.fromFutureJava(someValue).map(Option(_)), isNone)
+      }
+    ),
+    suite("`Task.fromCompletionStage` must")(
+      testM("be lazy on the `Future` parameter") {
+        var evaluated                 = false
+        def cs: CompletionStage[Unit] = CompletableFuture.supplyAsync(() => evaluated = true)
+        assertM(ZIO.fromCompletionStage(UIO.effectTotal(cs)).when(false).as(evaluated), isFalse)
+      },
+      testM("catch exceptions thrown by lazy block") {
+        val ex                                   = new Exception("no future for you!")
+        val noFuture: UIO[CompletionStage[Unit]] = UIO.effectTotal(throw ex)
+        assertM(ZIO.fromCompletionStage(noFuture).run, dies(equalTo(ex)))
+      },
+      testM("return an `IO` that fails if `Future` fails (failedFuture)") {
+        val ex                                  = new Exception("no value for you!")
+        val noValue: UIO[CompletionStage[Unit]] = UIO.effectTotal(CompletableFuture_.failedFuture(ex))
+        assertM(ZIO.fromCompletionStage(noValue).run, fails[Throwable](equalTo(ex)))
+      },
+      testM("return an `IO` that fails if `Future` fails (supplyAsync)") {
+        val ex                                  = new Exception("no value for you!")
+        val noValue: UIO[CompletionStage[Unit]] = UIO.effectTotal(CompletableFuture.supplyAsync(() => throw ex))
+        assertM(ZIO.fromCompletionStage(noValue).run, fails[Throwable](equalTo(ex)))
+      },
+      testM("return an `IO` that produces the value from `Future`") {
+        val someValue: UIO[CompletionStage[Int]] = UIO.effectTotal(CompletableFuture.completedFuture(42))
+        assertM(ZIO.fromCompletionStage(someValue), equalTo(42))
+      },
+      testM("handle null produced by the completed `Future`") {
+        val someValue: UIO[CompletionStage[String]] = UIO.effectTotal(CompletableFuture.completedFuture[String](null))
+        assertM(ZIO.fromCompletionStage(someValue).map(Option(_)), isNone)
+      }
+    ),
+    suite("`Task.toCompletableFuture` must")(
+      testM("produce always a successful `IO` of `Future`") {
+        val failedIO = IO.fail[Throwable](new Exception("IOs also can fail"))
+        assertM(failedIO.toCompletableFuture, isSubtype[CompletableFuture[Unit]](anything))
+      },
+      test("be polymorphic in error type") {
+        val unitIO: Task[Unit]                          = Task.unit
+        val polyIO: IO[String, CompletableFuture[Unit]] = unitIO.toCompletableFuture
+        assert(polyIO, anything)
+      },
+      testM("return a `CompletableFuture` that fails if `IO` fails") {
+        val ex                       = new Exception("IOs also can fail")
+        val failedIO: Task[Unit]     = IO.fail[Throwable](ex)
+        val failedFuture: Task[Unit] = failedIO.toCompletableFuture.flatMap(f => Task(f.get()))
+        assertM(
+          failedFuture.run,
+          fails[Throwable](hasField("message", _.getMessage, equalTo("java.lang.Exception: IOs also can fail")))
+        )
+      },
+      testM("return a `CompletableFuture` that produces the value from `IO`") {
+        val someIO = Task.succeed[Int](42)
+        assertM(someIO.toCompletableFuture.map(_.get()), equalTo(42))
+      }
+    ),
+    suite("`Task.toCompletableFutureE` must")(
+      testM("convert error of type `E` to `Throwable`") {
+        val failedIO: IO[String, Unit] = IO.fail[String]("IOs also can fail")
+        val failedFuture: Task[Unit] =
+          failedIO.toCompletableFutureWith(new Exception(_)).flatMap(f => Task(f.get()))
+        assertM(
+          failedFuture.run,
+          fails[Throwable](hasField("message", _.getMessage, equalTo("java.lang.Exception: IOs also can fail")))
+        )
+      }
+    ),
+    suite("`Fiber.fromCompletionStage`")(
+      test("be lazy on the `Future` parameter") {
+        var evaluated                  = false
+        def ftr: CompletionStage[Unit] = CompletableFuture.supplyAsync(() => evaluated = true)
+        Fiber.fromCompletionStage(ftr)
+        assert(evaluated, isFalse)
+      },
+      testM("catch exceptions thrown by lazy block") {
+        val ex                              = new Exception("no future for you!")
+        def noFuture: CompletionStage[Unit] = throw ex
+        assertM(Fiber.fromCompletionStage(noFuture).join.run, dies(equalTo(ex)))
+      },
+      testM("return an `IO` that fails if `Future` fails (failedFuture)") {
+        val ex                             = new Exception("no value for you!")
+        def noValue: CompletionStage[Unit] = CompletableFuture_.failedFuture(ex)
+        assertM(Fiber.fromCompletionStage(noValue).join.run, fails[Throwable](equalTo(ex)))
+      },
+      testM("return an `IO` that fails if `Future` fails (supplyAsync)") {
+        val ex                             = new Exception("no value for you!")
+        def noValue: CompletionStage[Unit] = CompletableFuture.supplyAsync(() => throw ex)
+        assertM(Fiber.fromCompletionStage(noValue).join.run, fails[Throwable](equalTo(ex)))
+      },
+      testM("return an `IO` that produces the value from `Future`") {
+        def someValue: CompletionStage[Int] = CompletableFuture.completedFuture(42)
+        assertM(Fiber.fromCompletionStage(someValue).join.run, succeeds(equalTo(42)))
+      }
+    ),
+    suite("`Fiber.fromFutureJava` must")(
+      test("be lazy on the `Future` parameter") {
+        var evaluated         = false
+        def ftr: Future[Unit] = CompletableFuture.supplyAsync(() => evaluated = true)
+        Fiber.fromFutureJava(ftr)
+        assert(evaluated, isFalse)
+      },
+      testM("catch exceptions thrown by lazy block") {
+        val ex                     = new Exception("no future for you!")
+        def noFuture: Future[Unit] = throw ex
+        assertM(Fiber.fromFutureJava(noFuture).join.run, dies(equalTo(ex)))
+      },
+      testM("return an `IO` that fails if `Future` fails (failedFuture)") {
+        val ex                    = new Exception("no value for you!")
+        def noValue: Future[Unit] = CompletableFuture_.failedFuture(ex)
+        assertM(Fiber.fromFutureJava(noValue).join.run, fails[Throwable](equalTo(ex)))
+      },
+      testM("return an `IO` that fails if `Future` fails (failedFuture)") {
+        val ex                    = new Exception("no value for you!")
+        def noValue: Future[Unit] = CompletableFuture.supplyAsync(() => throw ex)
+        assertM(Fiber.fromFutureJava(noValue).join.run, fails[Throwable](equalTo(ex)))
+      },
+      testM("return an `IO` that produces the value from `Future`") {
+        def someValue: Future[Int] = CompletableFuture.completedFuture(42)
+        assertM(Fiber.fromFutureJava(someValue).join.run, succeeds(equalTo(42)))
+      }
+    ),
+    suite("`Task.withCompletionHandler` must")(
+      testM("write and read to and from AsynchronousSocketChannel") {
+        val list: List[Byte] = List(13)
+        val address          = new InetSocketAddress(54321)
+        val server           = AsynchronousServerSocketChannel.open().bind(address)
+        val client           = AsynchronousSocketChannel.open()
+
+        val taskServer = for {
+          c <- ZIO.withCompletionHandler[AsynchronousSocketChannel](server.accept((), _))
+          w <- ZIO.withCompletionHandler[Integer](c.write(ByteBuffer.wrap(list.toArray), (), _))
+        } yield w
+
+        val taskClient = for {
+          _      <- ZIO.withCompletionHandler[Void](client.connect(address, (), _))
+          buffer = ByteBuffer.allocate(1)
+          r      <- ZIO.withCompletionHandler[Integer](client.read(buffer, (), _))
+        } yield (r, buffer.array.toList)
+
+        val task = for {
+          fiberServer  <- taskServer.fork
+          fiberClient  <- taskClient.fork
+          resultServer <- fiberServer.join
+          resultClient <- fiberClient.join
+          _            <- ZIO.effectTotal(server.close())
+        } yield (resultServer, resultClient)
+
+        assertM(task.run, succeeds[(Integer, (Integer, List[Byte]))](equalTo((1, (1, list)))))
+      }
+    )
+  )
+}
+
+object JavaSpecMain extends DefaultRunnableSpec(JavaSpec.spec)
---
+++ docs/interop/javaz.scala
@@ -0,0 +1,214 @@
+/*
+ * Copyright 2017-2019 John A. De Goes and the ZIO Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package zio.interop
+
+import _root_.java.nio.channels.CompletionHandler
+import _root_.java.util.concurrent.{ CompletableFuture, CompletionException, CompletionStage, Future }
+
+import zio.Fiber.Status
+import zio._
+import zio.blocking.{ blocking, Blocking }
+
+import scala.concurrent.ExecutionException
+
+object javaz {
+  def withCompletionHandler[T](op: CompletionHandler[T, Any] => Unit): Task[T] =
+    Task.effectSuspendTotalWith { p =>
+      Task.effectAsync { k =>
+        val handler = new CompletionHandler[T, Any] {
+          def completed(result: T, u: Any): Unit = k(Task.succeed(result))
+
+          def failed(t: Throwable, u: Any): Unit = t match {
+            case e if !p.fatal(e) => k(Task.fail(e))
+            case _                => k(Task.die(t))
+          }
+        }
+
+        try {
+          op(handler)
+        } catch {
+          case e if !p.fatal(e) => k(Task.fail(e))
+        }
+      }
+    }
+
+  private def catchFromGet(isFatal: Throwable => Boolean): PartialFunction[Throwable, Task[Nothing]] = {
+    case e: CompletionException =>
+      Task.fail(e.getCause)
+    case e: ExecutionException =>
+      Task.fail(e.getCause)
+    case _: InterruptedException =>
+      Task.interrupt
+    case e if !isFatal(e) =>
+      Task.fail(e)
+  }
+
+  private def unwrapDone[A](isFatal: Throwable => Boolean)(f: Future[A]): Task[A] =
+    try {
+      Task.succeed(f.get())
+    } catch catchFromGet(isFatal)
+
+  def fromCompletionStage[A](csUio: UIO[CompletionStage[A]]): Task[A] =
+    csUio.flatMap { cs =>
+      Task.effectSuspendTotalWith { p =>
+        val cf = cs.toCompletableFuture
+        if (cf.isDone) {
+          unwrapDone(p.fatal)(cf)
+        } else {
+          Task.effectAsync { cb =>
+            val _ = cs.handle[Unit] { (v: A, t: Throwable) =>
+              val io = Option(t).fold[Task[A]](Task.succeed(v)) { t =>
+                catchFromGet(p.fatal).lift(t).getOrElse(Task.die(t))
+              }
+              cb(io)
+            }
+          }
+        }
+      }
+    }
+
+  /** WARNING: this uses the blocking Future#get, consider using `fromCompletionStage` */
+  def fromFutureJava[A](futureUio: UIO[Future[A]]): RIO[Blocking, A] =
+    futureUio.flatMap { future =>
+      RIO.effectSuspendTotalWith { p =>
+        if (future.isDone) {
+          unwrapDone(p.fatal)(future)
+        } else {
+          blocking(Task.effectSuspend(unwrapDone(p.fatal)(future)))
+        }
+      }
+    }
+
+  implicit class CompletionStageJavaconcurrentOps[A](private val csUio: UIO[CompletionStage[A]]) extends AnyVal {
+    def toZio: Task[A] = ZIO.fromCompletionStage(csUio)
+  }
+
+  implicit class FutureJavaconcurrentOps[A](private val futureUio: UIO[Future[A]]) extends AnyVal {
+
+    /** WARNING: this uses the blocking Future#get, consider using `CompletionStage` */
+    def toZio: RIO[Blocking, A] = ZIO.fromFutureJava(futureUio)
+  }
+
+  implicit class ZioObjJavaconcurrentOps(private val zioObj: ZIO.type) extends AnyVal {
+    def withCompletionHandler[T](op: CompletionHandler[T, Any] => Unit): Task[T] =
+      javaz.withCompletionHandler(op)
+
+    def fromCompletionStage[A](csUio: UIO[CompletionStage[A]]): Task[A] = javaz.fromCompletionStage(csUio)
+
+    /** WARNING: this uses the blocking Future#get, consider using `fromCompletionStage` */
+    def fromFutureJava[A](futureUio: UIO[Future[A]]): RIO[Blocking, A] = javaz.fromFutureJava(futureUio)
+  }
+
+  implicit class FiberObjOps(private val fiberObj: Fiber.type) extends AnyVal {
+    def fromCompletionStage[A](thunk: => CompletionStage[A]): Fiber[Throwable, A] = {
+      lazy val cs: CompletionStage[A] = thunk
+
+      new Fiber[Throwable, A] {
+        override def await: UIO[Exit[Throwable, A]] = ZIO.fromCompletionStage(UIO.effectTotal(cs)).run
+
+        override def poll: UIO[Option[Exit[Throwable, A]]] =
+          UIO.effectSuspendTotal {
+            val cf = cs.toCompletableFuture
+            if (cf.isDone) {
+              Task
+                .effectSuspendWith(p => unwrapDone(p.fatal)(cf))
+                .fold(Exit.fail, Exit.succeed)
+                .map(Some(_))
+            } else {
+              UIO.succeed(None)
+            }
+          }
+
+        final def children: UIO[Iterable[Fiber[Any, Any]]] = UIO(Nil)
+
+        final def getRef[A](ref: FiberRef[A]): UIO[A] = UIO(ref.initial)
+
+        final def id: UIO[Option[Fiber.Id]] = UIO.none
+
+        final def interruptAs(id: Fiber.Id): UIO[Exit[Throwable, A]] = join.fold(Exit.fail, Exit.succeed)
+
+        final def inheritRefs: UIO[Unit] = IO.unit
+
+        final def status: UIO[Fiber.Status] = UIO {
+          // TODO: Avoid toCompletableFuture?
+          if (thunk.toCompletableFuture.isDone) Status.Done else Status.Running
+        }
+
+        final def trace: UIO[Option[ZTrace]] = UIO.none
+      }
+    }
+
+    /** WARNING: this uses the blocking Future#get, consider using `fromCompletionStage` */
+    def fromFutureJava[A](thunk: => Future[A]): Fiber[Throwable, A] = {
+      lazy val ftr: Future[A] = thunk
+
+      new Fiber[Throwable, A] {
+        def await: UIO[Exit[Throwable, A]] =
+          ZIO.fromFutureJava(UIO.effectTotal(ftr)).provide(Blocking.Live).run
+
+        def poll: UIO[Option[Exit[Throwable, A]]] =
+          UIO.effectSuspendTotal {
+            if (ftr.isDone) {
+              Task
+                .effectSuspendWith(p => unwrapDone(p.fatal)(ftr))
+                .fold(Exit.fail, Exit.succeed)
+                .map(Some(_))
+            } else {
+              UIO.succeed(None)
+            }
+          }
+
+        def children: UIO[Iterable[Fiber[Any, Any]]] = UIO(Nil)
+
+        def getRef[A](ref: FiberRef[A]): UIO[A] = UIO(ref.initial)
+
+        def id: UIO[Option[Fiber.Id]] = UIO.none
+
+        def interruptAs(id: Fiber.Id): UIO[Exit[Throwable, A]] = join.fold(Exit.fail, Exit.succeed)
+
+        def inheritRefs: UIO[Unit] = UIO.unit
+
+        def status: UIO[Fiber.Status] = UIO {
+          if (thunk.isDone) Status.Done else Status.Running
+        }
+
+        def trace: UIO[Option[ZTrace]] = UIO.none
+      }
+    }
+  }
+
+  /**
+   * CompletableFuture#failedFuture(Throwable) available only since Java 9
+   */
+  object CompletableFuture_ {
+    def failedFuture[A](e: Throwable): CompletableFuture[A] = {
+      val f = new CompletableFuture[A]
+      f.completeExceptionally(e)
+      f
+    }
+  }
+
+  implicit class TaskCompletableFutureOps[A](private val io: Task[A]) extends AnyVal {
+    def toCompletableFuture: UIO[CompletableFuture[A]] =
+      io.fold(CompletableFuture_.failedFuture, CompletableFuture.completedFuture[A])
+  }
+
+  implicit class IOCompletableFutureOps[E, A](private val io: IO[E, A]) extends AnyVal {
+    def toCompletableFutureWith(f: E => Throwable): UIO[CompletableFuture[A]] =
+      io.mapError(f).toCompletableFuture
+  }
+}
