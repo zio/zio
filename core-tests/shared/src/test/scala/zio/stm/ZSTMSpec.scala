c33960853c9f89e230b65a6282de903fec21cb0a
Dejan Mijic <dmijic@acm.org>
2020-02-12 10:33:28 +0100

    Format the sources

--- core-tests/shared/src/test/scala/zio/stm/ZSTMSpec.scala
+++ core-tests/shared/src/test/scala/zio/stm/ZSTMSpec.scala
@@ -105,11 +105,11 @@ object ZSTMSpec extends ZIOBaseSpec {
         val as = Iterable(2, 4, 6, 3, 5, 6)
 
         val tx =
-        for {
-          ref     <- TRef.make(List.empty[Int])
-          results <- STM.filter(as)(a => ref.update(a :: _).as(a % 2 == 0))
-          effects <- ref.get.map(_.reverse)
-        } yield (results, effects)
+          for {
+            ref     <- TRef.make(List.empty[Int])
+            results <- STM.filter(as)(a => ref.update(a :: _).as(a % 2 == 0))
+            effects <- ref.get.map(_.reverse)
+          } yield (results, effects)
 
         val expected = (List(2, 4, 6, 6), List(2, 4, 6, 3, 5, 6))
 
