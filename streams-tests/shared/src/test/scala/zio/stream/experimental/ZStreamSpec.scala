f756d3be9c3466471270662af5c84baff14b4d12
Itamar Ravid <iravid@users.noreply.github.com>
2020-02-14 21:01:53 +0200

    Port ZStream#ensuring,take,runDrain; ZStream.bracket,bracketExit; ZSink.drain (#2899)

--- streams-tests/shared/src/test/scala/zio/stream/experimental/ZStreamSpec.scala
+++ streams-tests/shared/src/test/scala/zio/stream/experimental/ZStreamSpec.scala
@@ -10,6 +10,30 @@ import zio.test._
 object ZStreamSpec extends ZIOBaseSpec {
   def spec = suite("ZStreamSpec")(
     suite("Combinators")(
+      suite("ensuring")(
+        testM("Stream.ensuring") {
+          for {
+            log <- Ref.make[List[String]](Nil)
+            _ <- (for {
+                  _ <- ZStream.bracket(log.update("Acquire" :: _))(_ => log.update("Release" :: _))
+                  _ <- ZStream.fromEffect(log.update("Use" :: _))
+                } yield ()).ensuring(log.update("Ensuring" :: _)).runDrain
+            execution <- log.get
+          } yield assert(execution)(equalTo(List("Ensuring", "Release", "Use", "Acquire")))
+        }
+      ),
+      suite("ensuringFirst")(
+        testM("Stream.ensuringFirst") {
+          for {
+            log <- Ref.make[List[String]](Nil)
+            _ <- (for {
+                  _ <- ZStream.bracket(log.update("Acquire" :: _))(_ => log.update("Release" :: _))
+                  _ <- ZStream.fromEffect(log.update("Use" :: _))
+                } yield ()).ensuringFirst(log.update("Ensuring" :: _)).runDrain
+            execution <- log.get
+          } yield assert(execution)(equalTo(List("Release", "Ensuring", "Use", "Acquire")))
+        }
+      ),
       suite("flatMap")(
         testM("deep flatMap stack safety") {
           def fib(n: Int): UStream[Int] =
@@ -133,6 +157,59 @@ object ZStreamSpec extends ZIOBaseSpec {
       }
     ),
     suite("Constructors")(
+      suite("bracket")(
+        testM("bracket")(
+          for {
+            done           <- Ref.make(false)
+            iteratorStream = ZStream.bracket(UIO(0))(_ => done.set(true))
+            result         <- iteratorStream.runCollect
+            released       <- done.get
+          } yield assert(result)(equalTo(List(0))) && assert(released)(isTrue)
+        ),
+        testM("bracket short circuits")(
+          for {
+            done <- Ref.make(false)
+            stream = ZStream
+              .bracket(UIO(Chunk(0, 1, 2)))(_ => done.set(true))
+              .flatMap(ZStream.fromChunk(_))
+              .take(2)
+            result   <- stream.runCollect
+            released <- done.get
+          } yield assert(result)(equalTo(List(0, 1))) && assert(released)(isTrue)
+        ),
+        // testM("no acquisition when short circuiting")(
+        //   for {
+        //     acquired       <- Ref.make(false)
+        //     iteratorStream = (Stream(1) ++ Stream.bracket(acquired.set(true))(_ => UIO.unit)).take(0)
+        //     _              <- iteratorStream.run(Sink.drain)
+        //     result         <- acquired.get
+        //   } yield assert(result)(isFalse)
+        // ),
+        testM("releases when there are defects") {
+          for {
+            ref <- Ref.make(false)
+            _ <- ZStream
+                  .bracket(ZIO.unit)(_ => ref.set(true))
+                  .flatMap(_ => ZStream.fromEffect(ZIO.dieMessage("boom")))
+                  .runDrain
+                  .run
+            released <- ref.get
+          } yield assert(released)(isTrue)
+        },
+        testM("flatMap associativity doesn't affect bracket lifetime")(
+          for {
+            leftAssoc <- ZStream
+                          .bracket(Ref.make(true))(_.set(false))
+                          .flatMap(r => ZStream.fromEffect(UIO.succeedNow(r)))
+                          .flatMap(r => ZStream.fromEffect(r.get))
+                          .runCollect
+            rightAssoc <- ZStream
+                           .bracket(Ref.make(true))(_.set(false))
+                           .flatMap(r => ZStream.fromEffect(UIO(r)).flatMap(r => ZStream.fromEffect(r.get)))
+                           .runCollect
+          } yield assert(leftAssoc -> rightAssoc)(equalTo(List(true) -> List(true)))
+        )
+      ),
       suite("fromEffect")(
         testM("success") {
           ZStream
@@ -192,6 +269,32 @@ object ZStreamSpec extends ZIOBaseSpec {
           )
         }
       )
+      // suite("take")(
+      // testM("take")(checkM(streamOfBytes, Gen.anyInt) { (s: Stream[String, Byte], n: Int) =>
+      //   for {
+      //     takeStreamResult <- s.take(n.toLong).runCollect.run
+      //     takeListResult   <- s.runCollect.map(_.take(n)).run
+      //   } yield assert(takeListResult.succeeded)(isTrue) implies assert(takeStreamResult)(equalTo(takeListResult))
+      // }),
+      // testM("take short circuits")(
+      //   for {
+      //     ran    <- Ref.make(false)
+      //     stream = (ZStream(1) ++ Stream.fromEffect(ran.set(true)).drain).take(0)
+      //     _      <- stream.run(Sink.drain)
+      //     result <- ran.get
+      //   } yield assert(result)(isFalse)
+      // ),
+      //  testM("take(0) short circuits")(
+      //    for {
+      //      units <- Stream.never.take(0).run(Sink.collectAll[Unit])
+      //    } yield assert(units)(equalTo(Nil))
+      //  ),
+      //  testM("take(1) short circuits")(
+      //    for {
+      //      ints <- (Stream(1) ++ Stream.never).take(1).run(Sink.collectAll[Int])
+      //    } yield assert(ints)(equalTo(List(1)))
+      //  )
+      // )
     ),
     suite("Destructors")(
       testM("toQueue")(checkM(smallChunks(Gen.anyInt)) { (c: Chunk[Int]) =>
