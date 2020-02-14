13838e6e7f3e102d74cc1d989dab4541e4bde780
Regis Kuckaertz <regis.kuckaertz@gmail.com>
2020-02-14 10:53:29 +0000

    Add ZSink.collectAll

--- streams/shared/src/main/scala/zio/stream/experimental/ZSink.scala
+++ streams/shared/src/main/scala/zio/stream/experimental/ZSink.scala
@@ -46,4 +46,21 @@ object ZSink extends Serializable {
    */
   def apply[R, E, M, A, B](process: ZManaged[R, E, ZSink.Control[R, E, M, A, B]]): ZSink[R, E, M, A, B] =
     new ZSink(process)
+
+  /**
+    * Accumulates all incoming elements into a list.
+    *
+    * @tparam A the type of elements
+    * @return a sink accumulating (forever) incoming elements
+    */
+  def collectAll[A]: ZSink[Any, Nothing, List[A], A, Nothing] = {
+    import scala.collection.mutable.ListBuffer
+    ZSink[Any, Nothing, List[A], A, Nothing] {
+      for {
+        buf   <- Ref.make(ListBuffer.empty[A]).toManaged_
+        push  = (a: A) => buf.update(_ :+ a)
+        query = buf.get.map(_.toList)
+      } yield Control(push, query)
+    }
+  }
 }
