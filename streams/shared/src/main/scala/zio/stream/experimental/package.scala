e64901ad4b7bffaad586ab0ad13de976d0e5b044
Regis Kuckaertz <regis.kuckaertz@gmail.com>
2020-02-14 14:53:07 +0000

    Fix type alias end marker

--- streams/shared/src/main/scala/zio/stream/experimental/package.scala
+++ streams/shared/src/main/scala/zio/stream/experimental/package.scala
@@ -1,7 +1,7 @@
 package zio.stream
 
 package object experimental {
-  type Stream[+E, +A] = ZStream[Any, E, Any, Nothing, A]
-  type UStream[A]     = ZStream[Any, Nothing, Any, Nothing, A]
-  type URStream[R, A] = ZStream[R, Nothing, Any, Nothing, A]
+  type Stream[+E, +A] = ZStream[Any, E, Any, Unit, A]
+  type UStream[A]     = ZStream[Any, Nothing, Any, Unit, A]
+  type URStream[R, A] = ZStream[R, Nothing, Any, Unit, A]
 }
