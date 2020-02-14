0d4555a57c990d44ee6c6571f68ed589114fdfa8
Regis Kuckaertz <regis.kuckaertz@gmail.com>
2020-02-14 14:41:46 +0000

    Stub ZSinkSpec

---
+++ streams-tests/shared/src/test/scala/zio/stream/experimental/ZSinkUtils.scala
@@ -0,0 +1,31 @@
+package zio.stream.experimental
+
+import zio._
+import ZSink.Control
+
+object ZSinkUtils {
+  val initErrorSink = ZSink[Any, String, Int, Int, Int] {
+    Managed.failNow("Ouch")
+  }
+
+  val stepErrorSink = ZSink[Any, String, Int, Int, Int] {
+    Managed.succeedNow {
+      Control(
+        _ => IO.failNow(Left("Ouch")),
+        IO.failNow("Ouch")
+      )
+    }
+  }
+
+  def extractErrorSink = ZSink[Any, String, Int, Int, Int] {
+    Managed.succeedNow {
+      Control(
+        _ => IO.unit,
+        IO.failNow("Ouch")
+      )
+    }
+  }
+
+  def sinkIteration[R, E, M, B, A](sink: ZSink[R, E, B, A, Any], a: A): ZIO[R, E, B] =
+    sink.process.use(control => control.push(a).catchAll(_.fold(IO.fail(_), _ => IO.unit)) *> control.query)
+}
