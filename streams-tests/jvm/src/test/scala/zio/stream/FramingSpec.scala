9d5158957d5a5cd2cd5f1cfc10b5c30b0310f931
Lachlan O'Dea <lodea@mac.com>
2020-02-17 23:18:49 +1100

    ZSink for delimited framing of chunked streams.
    Idea stolen from Akka Streams.

---
+++ streams-tests/jvm/src/test/scala/zio/stream/FramingSpec.scala
@@ -0,0 +1,75 @@
+package zio.stream
+
+import zio.test.Assertion.{ anything, equalTo, fails, isSubtype }
+import zio.test._
+import zio.{ Chunk, ZIOBaseSpec }
+
+import scala.{ Stream => _ }
+
+object FramingSpec extends ZIOBaseSpec {
+
+  override def spec = suite("FramingSpec")(
+    testM("happy path") {
+      val sink = Framing.delimiter(Chunk.single(0), 1000)
+      val in   = Stream(Chunk(1), Chunk(2, 0, 3), Chunk(4, 0, 5), Chunk(6))
+      assertM(in.aggregate(sink).runCollect)(
+        equalTo(List(Chunk(1, 2), Chunk(3, 4), Chunk(5, 6)))
+      )
+    },
+    testM("split delimiter") {
+      val sink = Framing.delimiter(Chunk(-1, -2, -3), 1000)
+      val in   = Stream(Chunk(0, 1, -1, -2), Chunk(-3, 2, 3))
+      assertM(in.aggregate(sink).runCollect)(
+        equalTo(List(Chunk(0, 1), Chunk(2, 3)))
+      )
+    },
+    testM("partial delimiter") {
+      val sink = Framing.delimiter(Chunk(-1, -2), 1000)
+      val in   = Stream(Chunk(0, 1, -1, 2), Chunk(3, -2, 4))
+      assertM(in.aggregate(sink).runCollect)(
+        equalTo(List(Chunk(0, 1, -1, 2, 3, -2, 4)))
+      )
+    },
+    testM("deilimter last") {
+      val sink = Framing.delimiter(Chunk(-1, -2), 1000)
+      val in   = Stream(Chunk(1, 2), Chunk(3, -1, -2))
+      assertM(in.aggregate(sink).runCollect)(
+        equalTo(List(Chunk(1, 2, 3), Chunk.empty))
+      )
+    },
+    testM("delimiter first") {
+      val sink = Framing.delimiter(Chunk(-1, -2), 1000)
+      val in   = Stream(Chunk(-1, -2, 1, 2), Chunk(3, 4))
+      assertM(in.aggregate(sink).runCollect)(
+        equalTo(List(Chunk.empty, Chunk(1, 2, 3, 4)))
+      )
+    },
+    testM("no delimiter") {
+      val sink = Framing.delimiter(Chunk(-1), 1000)
+      val in   = Stream(Chunk(1, 2), Chunk(3, 4))
+      assertM(in.aggregate(sink).runCollect)(
+        equalTo(List(Chunk(1, 2, 3, 4)))
+      )
+    },
+    testM("empty stream") {
+      val sink = Framing.delimiter(Chunk(-1, -2), 1000)
+      val in   = Stream.empty
+      assertM(in.aggregate(sink).runCount)(equalTo(0L))
+    },
+    testM("fails if maximum frame length exceeded") {
+      val sink = Framing.delimiter(Chunk(-1), 3)
+      val in   = Stream(Chunk(1, 2), Chunk(3, 4, -1, 5))
+      assertM(in.aggregate(sink).runCollect.run)(
+        fails(isSubtype[IllegalArgumentException](anything))
+      )
+    },
+    testM("succeeds if maximum frame length hit exactly") {
+      val sink = Framing.delimiter(Chunk(-1), 4)
+      val in   = Stream(Chunk(1, 2), Chunk(3, 4, -1, 5))
+      assertM(in.aggregate(sink).runCollect.run)(
+        fails(isSubtype[IllegalArgumentException](anything))
+      )
+    }
+  )
+
+}
---
+++ streams/shared/src/main/scala/zio/stream/Framing.scala
@@ -0,0 +1,78 @@
+package zio.stream
+
+import zio.{ Chunk, ZIO }
+
+object Framing {
+
+  val lfDelimiter: Chunk[Char] = Chunk('\n')
+
+  val crLfDelimiter: Chunk[Char] = Chunk('\r', '\n')
+
+  /**
+   * Frames a stream of chunks according to a delimiter.
+   *
+   * This is designed for use with `ZStream#aggregate`.
+   * Regardless of how the input stream is chunked, the output stream will emit
+   * a single chunk for each occurrence of the delimiter. The delimiters are
+   * not included in the output.
+   *
+   * Example:
+   * {{{
+   *   val stream = Stream(Chunk(1), Chunk(2, 3, 100), Chunk(101, 4))
+   *   val sink = Framing.delimiter(Chunk(100, 101), 100)
+   *   stream.aggregate(sink).runCollect
+   *   // List(Chunk(1, 2, 3), Chunk(4))
+   * }}}
+   *
+   * @param delimiter The delimiter to use for framing.
+   * @param maxFrameLength The maximum frame length allowed. If more than this
+   *                       many elements appears without the delimiter,
+   *                       this sink fails with `IllegalArgumentException`.
+   *                       None of the chunks emitted will be larger than this.
+   */
+  def delimiter[A](
+    delimiter: Chunk[A],
+    maxFrameLength: Int
+  ): ZSink[Any, IllegalArgumentException, Chunk[A], Chunk[A], Chunk[A]] = {
+
+    val delimiterLength = delimiter.length
+
+    def findDelimiter[B](
+      pos: Int
+    )(chunk: Chunk[B]): Option[(Chunk[B], Chunk[B])] = {
+
+      @scala.annotation.tailrec
+      def go(pos: Int): Option[(Chunk[B], Chunk[B])] = {
+        val compare = chunk.drop(pos).take(delimiterLength)
+        if (compare.length < delimiterLength) {
+          None
+        } else if (compare == delimiter) {
+          val (matched, remaining) = chunk.splitAt(pos)
+          Some((matched, remaining.drop(delimiterLength)))
+        } else {
+          go(pos + 1)
+        }
+      }
+
+      go(pos)
+    }
+
+    ZSink
+      .foldM((true, Chunk.empty: Chunk[A]))(_._1) { (acc, in: Chunk[A]) =>
+        val buffer       = acc._2
+        val searchBuffer = buffer ++ in
+        if (searchBuffer.length > maxFrameLength) {
+          ZIO.failNow(new IllegalArgumentException(s"Delimiter not found within $maxFrameLength elements"))
+        } else {
+          findDelimiter(math.max(0, buffer.length - delimiterLength + 1))(searchBuffer).map {
+            case (found, remaining) =>
+              ZIO.succeedNow(((false, found), Chunk.single(remaining)))
+          }.getOrElse {
+            ZIO.succeedNow(((true, searchBuffer), Chunk.empty))
+          }
+        }
+      }
+      .map(_._2)
+  }
+
+}
