00d17d0dea1331db88ce9c657972bd7eb800321d
Dmitry Karlinsky <dmitryka@wix.com>
2020-02-17 13:56:51 +0200

    trying to fix dotty compilation

--- test/shared/src/main/scala/zio/test/diff/Patch.scala
+++ test/shared/src/main/scala/zio/test/diff/Patch.scala
@@ -1,6 +1,6 @@
 package zio.test.diff
 
-private[diff] case class Patch[T](deltas: Vector[Delta[T]] = Vector.empty) {
+private[diff] case class Patch[T](deltas: Vector[Delta[T]] = Vector.empty[Delta[T]]) {
   def addDelta(delta: Delta[T]): Patch[T] = copy(deltas = deltas :+ delta)
 
   def deltasSorted: Vector[Delta[T]] =
