22944587c2aaa5c64dd84fc0e927931ef549d5d5
sken <22631338+sken77@users.noreply.github.com>
2020-02-21 12:33:02 -0800

    add forever to ZSTM (#2953)
    * add forever to STM
    
    * minor refactor

--- core/shared/src/main/scala/zio/stm/ZSTM.scala
+++ core/shared/src/main/scala/zio/stm/ZSTM.scala
@@ -378,6 +378,11 @@ final class ZSTM[-R, +E, +A] private[stm] (
     }
 
   /**
+   * Repeats this effect forever (until the first error).
+   */
+  def forever: ZSTM[R, E, Nothing] = self *> self.forever
+
+  /**
    * Unwraps the optional success of this effect, but can fail with unit value.
    */
   def get[E1 >: E, B](implicit ev1: E1 =:= Nothing, ev2: A <:< Option[B]): ZSTM[R, Unit, B] =
@@ -620,7 +625,7 @@ final class ZSTM[-R, +E, +A] private[stm] (
   /**
    * Keeps some of the errors, and terminates the fiber with the rest.
    */
-  final def refineOrDie[E1](
+  def refineOrDie[E1](
     pf: PartialFunction[E, E1]
   )(implicit ev1: E <:< Throwable, ev2: CanFail[E]): ZSTM[R, E1, A] =
     refineOrDieWith(pf)(ev1)
