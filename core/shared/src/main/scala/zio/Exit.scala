9504516f04f50ecb7db1cee6b51f6dcbb2ab2bf7
Adam Fraser <adam.fraser@gmail.com>
2020-02-23 04:51:30 -0500

    trigger CI

--- core/shared/src/main/scala/zio/Exit.scala
+++ core/shared/src/main/scala/zio/Exit.scala
@@ -59,11 +59,6 @@ sealed trait Exit[+E, +A] extends Product with Serializable { self =>
   final def as[B](b: B): Exit[E, B] = map(_ => b)
 
   /**
-   * Replaces the error value with the one provided.
-   */
-  final def asError[E1](e1: E1): Exit[E1, A] = mapError(_ => e1)
-
-  /**
    * Maps over both the error and value type.
    */
   final def bimap[E1, A1](f: E => E1, g: A => A1): Exit[E1, A1] = mapError(f).map(g)
