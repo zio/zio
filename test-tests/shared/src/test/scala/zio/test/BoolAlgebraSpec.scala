a82693d10d8a36a6939782507cb006d091d4be4c
Wessel van Staal <w.vanstaal@eaglescience.nl>
2020-03-06 21:16:31 +0100

    fixed lint

--- test-tests/shared/src/test/scala/zio/test/BoolAlgebraSpec.scala
+++ test-tests/shared/src/test/scala/zio/test/BoolAlgebraSpec.scala
@@ -86,9 +86,9 @@ object BoolAlgebraSpec extends ZIOBaseSpec {
     },
     test("iff returns double implication of two values") {
       assert(success1 <==> success2)(isSuccess) &&
-        assert(success1 <==> failure1)(isFailure) &&
-        assert(failure1 <==> success1)(isFailure) &&
-        assert(failure1 <==> failure2)(isSuccess)
+      assert(success1 <==> failure1)(isFailure) &&
+      assert(failure1 <==> success1)(isFailure) &&
+      assert(failure1 <==> failure2)(isSuccess)
     },
     test("isFailure returns whether result is failure") {
       assert(!success1.isFailure && failure1.isFailure)(isTrue)
