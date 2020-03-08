3f22020b82fbaabe6557395a9ee59d8d434af1ea
Itamar Ravid <iravid@iravid.com>
2020-03-08 09:47:55 +0200

    Add more ZStream combinators

--- core/shared/src/main/scala/zio/Cause.scala
+++ core/shared/src/main/scala/zio/Cause.scala
@@ -566,27 +566,27 @@ object Cause extends Serializable {
    * Converts the specified `Cause[Either[E, A]]` to an `Either[Cause[E], A]` by
    * recursively stripping out any failures with the error `None`.
    */
-  def sequenceCauseEither[E, A](c: Cause[Either[E, A]]): Either[A, Cause[E]] =
+  def sequenceCauseEither[E, A](c: Cause[Either[E, A]]): Either[Cause[E], A] =
     c match {
-      case Internal.Empty                => Right(Internal.Empty)
-      case Internal.Traced(cause, trace) => sequenceCauseEither(cause).map(Internal.Traced(_, trace))
-      case Internal.Meta(cause, data)    => sequenceCauseEither(cause).map(Internal.Meta(_, data))
-      case Internal.Interrupt(id)        => Right(Internal.Interrupt(id))
-      case d @ Internal.Die(_)           => Right(d)
-      case Internal.Fail(Left(e))        => Right(Internal.Fail(e))
-      case Internal.Fail(Right(a))       => Left(a)
+      case Internal.Empty                => Left(Internal.Empty)
+      case Internal.Traced(cause, trace) => sequenceCauseEither(cause).left.map(Internal.Traced(_, trace))
+      case Internal.Meta(cause, data)    => sequenceCauseEither(cause).left.map(Internal.Meta(_, data))
+      case Internal.Interrupt(id)        => Left(Internal.Interrupt(id))
+      case d @ Internal.Die(_)           => Left(d)
+      case Internal.Fail(Left(e))        => Left(Internal.Fail(e))
+      case Internal.Fail(Right(a))       => Right(a)
       case Internal.Then(left, right) =>
         (sequenceCauseEither(left), sequenceCauseEither(right)) match {
-          case (Right(cl), Right(cr)) => Right(Internal.Then(cl, cr))
-          case (Left(a), _)           => Left(a)
-          case (_, Left(a))           => Left(a)
+          case (Left(cl), Left(cr)) => Left(Internal.Then(cl, cr))
+          case (Right(a), _)        => Right(a)
+          case (_, Right(a))        => Right(a)
         }
 
       case Internal.Both(left, right) =>
         (sequenceCauseEither(left), sequenceCauseEither(right)) match {
-          case (Right(cl), Right(cr)) => Right(Internal.Both(cl, cr))
-          case (Left(a), _)           => Left(a)
-          case (_, Left(a))           => Left(a)
+          case (Left(cl), Left(cr)) => Left(Internal.Both(cl, cr))
+          case (Right(a), _)        => Right(a)
+          case (_, Right(a))        => Right(a)
         }
     }
 
