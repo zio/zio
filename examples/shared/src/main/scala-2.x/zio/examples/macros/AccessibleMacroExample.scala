7b0e5023dcb9153f5fa2b4ca6cd38ce63772d18e
ioleo <ioleo+git@protonmail.com>
2020-03-18 18:43:52 +0100

    Polymorphic accessors

--- examples/shared/src/main/scala-2.x/zio/examples/macros/AccessibleMacroExample.scala
+++ examples/shared/src/main/scala-2.x/zio/examples/macros/AccessibleMacroExample.scala
@@ -14,6 +14,7 @@ object AccessibleMacroExample {
     val foo: UIO[Unit]
     def bar(n: Int): UIO[Unit]
     def baz(x: Int, y: Int): UIO[Int]
+    def poly[A](a: A): UIO[A]
   }
 
   val live: ZLayer[Console, Nothing, Has[Service]] =
@@ -21,15 +22,18 @@ object AccessibleMacroExample {
       val foo: UIO[Unit]                = console.putStrLn("foo")
       def bar(n: Int): UIO[Unit]        = console.putStrLn(s"bar $n")
       def baz(x: Int, y: Int): UIO[Int] = UIO.succeed(x + y)
+      def poly[A](a: A): UIO[A]         = UIO.succeed(a)
     })
 
   // can use accessors even in the same compilation unit
-  val program: URIO[AccessibleMacroExample, Int] =
+  val program: URIO[AccessibleMacroExample, (Int, String, Long)] =
     for {
-      _ <- AccessibleMacroExample.foo
-      _ <- AccessibleMacroExample.bar(1)
-      v <- AccessibleMacroExample.baz(2, 3)
-    } yield v
+      _  <- AccessibleMacroExample.foo
+      _  <- AccessibleMacroExample.bar(1)
+      v1 <- AccessibleMacroExample.baz(2, 3)
+      v2 <- AccessibleMacroExample.poly("foo")
+      v3 <- AccessibleMacroExample.poly(4L)
+    } yield (v1, v2, v3)
 
-  // macro autogenerates accessors for `foo`, `bar` and `baz` below
+  // macro autogenerates accessors for `foo`, `bar`, `baz` and `poly` below
 }
