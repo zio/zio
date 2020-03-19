c3060ab5c2aa60908c97584790a81c0f683e9db9
ioleo <ioleo+git@protonmail.com>
2020-03-19 14:52:59 +0100

    ZIO Macros: Polymorphic accessors (#3170)
    * Polymorphic accessors
    
    * Add example with type constraints
    
    * Add sanity checks

--- examples/shared/src/main/scala-2.x/zio/examples/macros/AccessibleMacroExample.scala
+++ examples/shared/src/main/scala-2.x/zio/examples/macros/AccessibleMacroExample.scala
@@ -1,6 +1,6 @@
 package zio.examples.macros
 
-import zio.{ Has, UIO, URIO, ZLayer }
+import zio.{ Has, UIO, URIO, ZIO, ZLayer }
 import zio.console.Console
 import zio.macros.accessible
 
@@ -9,27 +9,45 @@ object AccessibleMacroExample {
 
   type AccessibleMacroExample = Has[AccessibleMacroExample.Service]
 
+  trait Foo { val value: String }
+  case class Bar(value: String) extends Foo
+  case class Wrapped[T](value: T)
+
   trait Service {
 
     val foo: UIO[Unit]
     def bar(n: Int): UIO[Unit]
     def baz(x: Int, y: Int): UIO[Int]
+    def poly[A](a: A): UIO[A]
+    def poly2[A <: Foo](a: Wrapped[A]): UIO[List[A]]
   }
 
   val live: ZLayer[Console, Nothing, Has[Service]] =
     ZLayer.fromService(console => new Service {
-      val foo: UIO[Unit]                = console.putStrLn("foo")
-      def bar(n: Int): UIO[Unit]        = console.putStrLn(s"bar $n")
-      def baz(x: Int, y: Int): UIO[Int] = UIO.succeed(x + y)
+      val foo: UIO[Unit]                               = console.putStrLn("foo")
+      def bar(n: Int): UIO[Unit]                       = console.putStrLn(s"bar $n")
+      def baz(x: Int, y: Int): UIO[Int]                = UIO.succeed(x + y)
+      def poly[A](a: A): UIO[A]                        = UIO.succeed(a)
+      def poly2[A <: Foo](a: Wrapped[A]): UIO[List[A]] = UIO.succeed(List(a.value))
     })
 
   // can use accessors even in the same compilation unit
-  val program: URIO[AccessibleMacroExample, Int] =
+  val program: URIO[AccessibleMacroExample, (Int, String, Long, List[Foo])] =
     for {
-      _ <- AccessibleMacroExample.foo
-      _ <- AccessibleMacroExample.bar(1)
-      v <- AccessibleMacroExample.baz(2, 3)
-    } yield v
+      _  <- AccessibleMacroExample.foo
+      _  <- AccessibleMacroExample.bar(1)
+      v1 <- AccessibleMacroExample.baz(2, 3)
+      v2 <- AccessibleMacroExample.poly("foo")
+      v3 <- AccessibleMacroExample.poly(4L)
+      v4 <- AccessibleMacroExample.poly2(Wrapped(Bar("bar")))
+    } yield (v1, v2, v3, v4)
 
-  // macro autogenerates accessors for `foo`, `bar` and `baz` below
+  // sanity check
+  val _foo                            : ZIO[AccessibleMacroExample, Nothing, Unit]    = AccessibleMacroExample.foo
+  def _bar(n: Int)                    : ZIO[AccessibleMacroExample, Nothing, Unit]    = AccessibleMacroExample.bar(n)
+  def _baz(x: Int, y: Int)            : ZIO[AccessibleMacroExample, Nothing, Int]     = AccessibleMacroExample.baz(x, y)
+  def _poly[A](a: A)                  : ZIO[AccessibleMacroExample, Nothing, A]       = AccessibleMacroExample.poly(a)
+  def _poly2[A <: Foo](a: Wrapped[A]) : ZIO[AccessibleMacroExample, Nothing, List[A]] = AccessibleMacroExample.poly2(a)
+
+  // macro autogenerates accessors for `foo`, `bar`, `baz` and `poly` below
 }
