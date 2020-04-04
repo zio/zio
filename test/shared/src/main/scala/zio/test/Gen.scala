89aae0b0e1438d16afd5b15769ddc42cdae851b8
Adam Fraser <adam.fraser@gmail.com>
2020-04-04 11:08:12 -0400

    implement bigInt (#3282)

--- test/shared/src/main/scala/zio/test/Gen.scala
+++ test/shared/src/main/scala/zio/test/Gen.scala
@@ -261,6 +261,24 @@ object Gen extends GenZIO with FunctionVariants with TimeVariants {
     )
 
   /**
+   * A generator of big integers inside the specified range: [start, end].
+   * The shrinker will shrink toward the lower end of the range ("smallest").
+   */
+  def bigInt(min: BigInt, max: BigInt): Gen[Random, BigInt] =
+    Gen.fromEffectSample {
+      val bitLength  = (max - min).bitLength
+      val byteLength = ((bitLength.toLong + 7) / 8).toInt
+      val excessBits = byteLength * 8 - bitLength
+      val mask       = (1 << (8 - excessBits)) - 1
+      val effect = nextBytes(byteLength).map { bytes =>
+        val arr = bytes.toArray
+        arr(0) = (arr(0) & mask).toByte
+        min + BigInt(arr)
+      }.doUntil(n => min <= n && n <= max)
+      effect.map(Sample.shrinkIntegral(min))
+    }
+
+  /**
    * A generator of booleans. Shrinks toward 'false'.
    */
   val boolean: Gen[Random, Boolean] =
