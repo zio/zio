cc7990f43a9f84b7d4d75c93bf936dbc1efc7f4e
simpadjo <simpadjo@gmail.com>
2020-04-04 22:03:15 +0200

    ZSink.head

--- streams-tests/shared/src/test/scala/zio/stream/experimental/ZSinkSpec.scala
+++ streams-tests/shared/src/test/scala/zio/stream/experimental/ZSinkSpec.scala
@@ -1,11 +1,20 @@
 package zio.stream.experimental
 
 import zio._
+import zio.stream.ChunkUtils
+import zio.test.Assertion.equalTo
 import zio.test._
 
-object ZSinkSpec extends ZIOBaseSpec {
+object ZSinkSpec1 extends ZIOBaseSpec {
   def spec = suite("ZSinkSpec")(
-    suite("Constructors")(),
+    suite("Constructors")(
+      testM("head")(
+        checkM(Gen.listOf(ChunkUtils.smallChunks(Gen.anyInt))) { chunks: Seq[Chunk[Int]] =>
+          val headOpt = ZStream.fromChunks(chunks: _*).run(ZSink.head[Int]).either.map(_.toOption)
+          assertM(headOpt)(equalTo(chunks.flatMap(_.toSeq).headOption))
+        }
+      )
+    ),
     suite("Combinators")()
   )
 }
