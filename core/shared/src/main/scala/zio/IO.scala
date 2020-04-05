e8c0ec473b4918407f7f787a45339ff516a7bde5
Jonathan Winandy <jonathan@univalence.io>
2020-04-05 21:06:35 +0200

    Prototype for NonEmptyChunk (#3241)
    * Add NonEmptyChunk[A], a Chunk with a minimal size of 1
    
    * specialize if possible all the methods, duplicate when needed.
    * distribute the implementations between Empty, NonEmpty.
    * update ZIO, IO, Task, UIO, URIO, RIO with aliases.
    
    * NonEmptyChunk, remove custom slice implem
    
    * Chunk : added find on Arr
    
    * NonEmptyChunk, clean the API
    
    * NonEmptyChunk, improve test and subtyping
    
    * NonEmptyChunk, test update
    
    * NonEmptyChunk remove 'def slice', change tests
    
    * NonEmptyChunk, fix test

--- core/shared/src/main/scala/zio/IO.scala
+++ core/shared/src/main/scala/zio/IO.scala
@@ -92,6 +92,12 @@ object IO {
     ZIO.collectAll(in)
 
   /**
+   * @see See [[[zio.ZIO.collectAll[R,E,A](in:zio\.NonEmptyChunk*]]]
+   */
+  def collectAll[E, A](in: NonEmptyChunk[IO[E, A]]): IO[E, NonEmptyChunk[A]] =
+    ZIO.collectAll(in)
+
+  /**
    * @see See [[[zio.ZIO.collectAll_[R,E,A](in:Iterable*]]]
    */
   def collectAll_[E, A](in: Iterable[IO[E, A]]): IO[E, Unit] =
@@ -113,6 +119,12 @@ object IO {
    * @see See [[[zio.ZIO.collectAllPar[R,E,A](as:zio\.Chunk*]]]
    */
   def collectAllPar[E, A](as: Chunk[IO[E, A]]): IO[E, Chunk[A]] =
+    ZIO.collectAllPar(as)
+
+  /**
+   * @see See [[[zio.ZIO.collectAllPar[R,E,A](as:zio\.NonEmptyChunk*]]]
+   */
+  def collectAllPar[E, A](as: NonEmptyChunk[IO[E, A]]): IO[E, NonEmptyChunk[A]] =
     ZIO.collectAllPar(as)
 
   /**
@@ -328,6 +340,12 @@ object IO {
     ZIO.foreach(in)(f)
 
   /**
+   * @see See [[[zio.ZIO.foreach[R,E,A,B](in:zio\.NonEmptyChunk*]]]
+   */
+  def foreach[E, A, B](in: NonEmptyChunk[A])(f: A => IO[E, B]): IO[E, NonEmptyChunk[B]] =
+    ZIO.foreach(in)(f)
+
+  /**
    * @see See [[zio.ZIO.foreachExec]]
    */
   final def foreachExec[E, A, B](as: Iterable[A])(exec: ExecutionStrategy)(f: A => IO[E, B]): IO[E, List[B]] =
@@ -343,6 +361,12 @@ object IO {
    * @see See [[[zio.ZIO.foreachPar[R,E,A,B](as:zio\.Chunk*]]]
    */
   def foreachPar[E, A, B](as: Chunk[A])(fn: A => IO[E, B]): IO[E, Chunk[B]] =
+    ZIO.foreachPar(as)(fn)
+
+  /**
+   * @see See [[[zio.ZIO.foreachPar[R,E,A,B](as:zio\.NonEmptyChunk*]]]
+   */
+  def foreachPar[E, A, B](as: NonEmptyChunk[A])(fn: A => IO[E, B]): IO[E, NonEmptyChunk[B]] =
     ZIO.foreachPar(as)(fn)
 
   /**
