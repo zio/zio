49b6e4b4888f7f21419ea0dcd2d58027c590e83e
Adam Fraser <adam.fraser@gmail.com>
2020-04-05 10:52:47 -0400

    remove extraneous type parameters

--- streams/shared/src/main/scala/zio/stream/StreamEffectChunk.scala
+++ streams/shared/src/main/scala/zio/stream/StreamEffectChunk.scala
@@ -100,10 +100,10 @@ private[stream] final class StreamEffectChunk[-R, +E, +A](override val chunks: S
   override def filter(pred: A => Boolean): StreamEffectChunk[R, E, A] =
     StreamEffectChunk(chunks.map(_.filter(pred)))
 
-  final override def foldWhileManaged[A1 >: A, S](s: S)(cont: S => Boolean)(f: (S, A1) => S): ZManaged[R, E, S] =
+  final override def foldWhileManaged[S](s: S)(cont: S => Boolean)(f: (S, A) => S): ZManaged[R, E, S] =
     chunks.foldWhileManaged(s)(cont)((s, as) => as.foldWhile(s)(cont)(f))
 
-  override def fold[A1 >: A, S](s: S)(f: (S, A1) => S): ZIO[R, E, S] =
+  override def fold[S](s: S)(f: (S, A) => S): ZIO[R, E, S] =
     foldWhileManaged(s)(_ => true)(f).use(UIO.succeedNow)
 
   override def map[B](f: A => B): StreamEffectChunk[R, E, B] =
