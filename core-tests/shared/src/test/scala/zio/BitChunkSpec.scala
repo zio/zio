9a2e093cd4929c3dd558d5e8a600dfb80a72acc4
Adam Fraser <adam.fraser@gmail.com>
2020-04-10 23:45:31 -0400

    cleanup

--- core-tests/shared/src/test/scala/zio/BitChunkSpec.scala
+++ core-tests/shared/src/test/scala/zio/BitChunkSpec.scala
@@ -1,6 +1,5 @@
 package zio
 
-import zio.Chunk.BitChunk
 import zio.random.Random
 import zio.test.Assertion._
 import zio.test._
@@ -16,21 +15,10 @@ object BitChunkSpec extends ZIOBaseSpec {
     String.format("%8s", (byte.toInt & 0xFF).toBinaryString).replace(' ', '0')
 
   def spec = suite("BitChunkSpec")(
-    testM("foreach") {
-      check(genByteChunk) { bytes =>
-        val bitChunk = BitChunk.fromByteChunk(bytes)
-        val builder  = new scala.collection.mutable.StringBuilder
-        bitChunk.foreach(bit => if (bit) builder.append("1") else builder.append("0"))
-        val actual   = builder.toString
-        val expected = bitChunk.toBinaryString
-        assert(actual)(equalTo(expected))
-      }
-    },
     testM("toBinaryString") {
       check(genByteChunk) { bytes =>
-        val bitChunk = BitChunk.fromByteChunk(bytes)
-        val actual   = bitChunk.toBinaryString
-        val expected = bytes.map(toBinaryString).mkString("", "", "")
+        val actual   = bytes.asBits.toBinaryString
+        val expected = bytes.map(toBinaryString).mkString
         assert(actual)(equalTo(expected))
       }
     }
