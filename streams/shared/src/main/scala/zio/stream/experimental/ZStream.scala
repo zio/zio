585421711e6478b1de18c53ccee4807dc893cb48
Regis Kuckaertz <regis.kuckaertz@gmail.com>
2020-04-12 17:09:06 +0100

    Insert End signal on finish no matter what

--- streams/shared/src/main/scala/zio/stream/experimental/ZStream.scala
+++ streams/shared/src/main/scala/zio/stream/experimental/ZStream.scala
@@ -134,7 +134,7 @@ abstract class ZStream[-R, +E, +O](
           // Upstream is done, we need to flush the transducer. If the output is
           // empty, we send the end signal downstream.
           def finish(ps: Chunk[P]): URIO[R1, Boolean] =
-            bucket.offer(if (ps.isEmpty) Take.End else Exit.succeed(ps)) as false
+            bucket.offerAll(if (ps.isEmpty) List(Take.End) else List(Exit.succeed(ps), Take.End)) as false
 
           // We have to make progress in the transducer. If the output is empty,
           // we must keep making progress until it isn't.
