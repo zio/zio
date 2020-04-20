39c15d533304151c8da4cbaa75652163574a63ca
Dejan Mijic <dmijic@acm.org>
2020-04-20 11:16:33 +0200

    Make right retry explicit

--- core-tests/shared/src/test/scala/zio/stm/ZSTMSpec.scala
+++ core-tests/shared/src/test/scala/zio/stm/ZSTMSpec.scala
@@ -36,7 +36,7 @@ object ZSTMSpec extends ZIOBaseSpec {
           for {
             ref   <- TRef.makeCommit(0)
             left  = ref.get.flatMap(v => STM.check(v > 500).as("left"))
-            right = ref.get.flatMap(v => STM.check(v < 0).as("right"))
+            right = STM.retry
             f     <- ref.update(_ + 10).commit.forever.fork
             res   <- (left <|> right).commit
             _     <- f.interrupt
