869b6fd037f0b163aa27c04b328073bfb5bfe37a
Adam Fraser <adam.fraser@gmail.com>
2020-04-27 07:00:08 -0400

    add test

--- test-tests/shared/src/test/scala/zio/test/SpecSpec.scala
+++ test-tests/shared/src/test/scala/zio/test/SpecSpec.scala
@@ -1,27 +1,84 @@
 package zio.test
 
-import zio.test.Assertion.{ equalTo, hasSize, isFalse, isGreaterThan, isTrue }
-import zio.test.TestAspect.{ ifEnvSet, nondeterministic, only }
+import zio._
+import zio.test.Assertion._
+import zio.test.TestAspect._
 import zio.test.TestUtils._
-import zio.test.environment.TestEnvironment
-import zio.{ random, Has, NeedsEnv, Ref, ZIO, ZLayer }
+import zio.test.environment._
 
 object SpecSpec extends ZIOBaseSpec {
 
-  type Module = Has[Module.Service]
-
-  object Module {
-    trait Service
-  }
-
-  val layer: ZLayer[Any, Nothing, Module] = ZLayer.succeed(new Module.Service {})
+  val layer: ZLayer[Any, Nothing, Has[Unit]] =
+    ZLayer.succeed(())
 
   def spec: Spec[TestEnvironment, TestFailure[Nothing], TestSuccess] = suite("SpecSpec")(
+    suite("only")(
+      testM("ignores all tests except one tagged") {
+        val spec = suite("suite1")(
+          suite("suite2")(
+            test("test1") {
+              assert(1)(equalTo(2))
+            }
+          ),
+          suite("suite3")(
+            test("test2") {
+              assert(1)(equalTo(1))
+            } @@ only
+          )
+        )
+        assertM(succeeded(spec))(isTrue)
+      },
+      testM("ignores all tests except ones in the tagged suite") {
+        val spec = suite("suite1")(
+          suite("suite2")(
+            test("test1") {
+              assert(1)(equalTo(2))
+            }
+          ),
+          suite("suite3")(
+            test("test3") {
+              assert(1)(equalTo(1))
+            }
+          ) @@ only
+        )
+        assertM(succeeded(spec))(isTrue)
+      },
+      testM("runs everything if no tests are tagged") {
+        val spec = suite("suite1")(
+          suite("suite2")(
+            test("test1") {
+              assert(1)(equalTo(2))
+            }
+          ),
+          suite("suite3")(
+            test("test2") {
+              assert(1)(equalTo(1))
+            }
+          )
+        )
+        assertM(succeeded(spec))(isFalse)
+      }
+    ),
+    suite("provideCustomLayer")(
+      testM("provides the part of the environment that is not part of the `TestEnvironment`") {
+        for {
+          _ <- ZIO.environment[TestEnvironment]
+          _ <- ZIO.environment[Has[Unit]]
+        } yield assertCompletes
+      }.provideCustomLayer(layer)
+    ),
+    suite("provideLayer")(
+      testM("does not have early initialization issues") {
+        for {
+          _ <- ZIO.environment[Has[Unit]]
+        } yield assertCompletes
+      }.provideLayer(layer)
+    ),
     suite("provideLayerShared")(
       testM("gracefully handles fiber death") {
         implicit val needsEnv = NeedsEnv
-        val spec = suite("Suite1")(
-          test("Test1") {
+        val spec = suite("suite")(
+          test("test") {
             assert(true)(isTrue)
           }
         ).provideLayerShared(ZLayer.fromEffectMany(ZIO.dieMessage("everybody dies")))
@@ -30,12 +87,12 @@ object SpecSpec extends ZIOBaseSpec {
         } yield assertCompletes
       },
       testM("does not acquire the environment if the suite is ignored") {
-        val spec = suite("Suite1")(
-          testM("Test1") {
-            assertM(ZIO.accessM[Has[Ref[Boolean]]](_.get[Ref[Boolean]].get))(isTrue)
+        val spec = suite("suite")(
+          testM("test1") {
+            assertM(ZIO.service[Ref[Boolean]].flatMap(_.get))(isTrue)
           },
-          testM("another test") {
-            assertM(ZIO.accessM[Has[Ref[Boolean]]](_.get[Ref[Boolean]].get))(isTrue)
+          testM("test2") {
+            assertM(ZIO.service[Ref[Boolean]].flatMap(_.get))(isTrue)
           }
         )
         for {
@@ -46,15 +103,15 @@ object SpecSpec extends ZIOBaseSpec {
         } yield assert(result)(isTrue)
       },
       testM("is not interfered with by test level failures") {
-        val spec = suite("some suite")(
-          test("failing test") {
+        val spec = suite("suite")(
+          test("test1") {
             assert(1)(Assertion.equalTo(2))
           },
-          test("passing test") {
+          test("test2") {
             assert(1)(Assertion.equalTo(1))
           },
-          testM("test requires env") {
-            assertM(ZIO.access[Has[Int]](_.get[Int]))(Assertion.equalTo(42))
+          testM("test3") {
+            assertM(ZIO.service[Int])(Assertion.equalTo(42))
           }
         ).provideLayerShared(ZLayer.succeed(43))
         for {
@@ -87,73 +144,28 @@ object SpecSpec extends ZIOBaseSpec {
                 _ <- ref.update(_ + n)
               } yield assertCompletes
             }
-          ).provideSomeLayerShared[TestEnvironment](ZLayer.succeed(())) @@ nondeterministic
+          ).provideSomeLayerShared[TestEnvironment](layer) @@ nondeterministic
           _      <- execute(spec)
           result <- ref.get
         } yield assert(result)(hasSize(isGreaterThan(1)))
-      }
-    ),
-    suite("only")(
-      testM("ignores all tests except one tagged") {
-        val spec = suite("root suite")(
-          suite("failing suite")(
-            test("failing test") {
-              assert(1)(equalTo(2))
-            }
-          ),
-          suite("passing suite")(
-            test("passing test") {
-              assert(1)(equalTo(1))
-            } @@ only
-          )
-        )
-        assertM(succeeded(spec))(isTrue)
       },
-      testM("ignores all tests except ones in the tagged suite") {
-        val spec = suite("root suite")(
-          suite("failing suite")(
-            test("failing test") {
-              assert(1)(equalTo(2))
-            }
-          ),
-          suite("passing suite")(
-            test("passing test") {
-              assert(1)(equalTo(1))
-            }
-          ) @@ only
-        )
+      testM("does not cause the remainder to be shared") {
+        val spec = suite("suite")(
+          testM("test1") {
+            for {
+              _      <- console.putStrLn("Hello, World!")
+              output <- TestConsole.output
+            } yield assert(output)(equalTo(Vector("Hello, World!\n")))
+          },
+          testM("test2") {
+            for {
+              _      <- console.putStrLn("Hello, World!")
+              output <- TestConsole.output
+            } yield assert(output)(equalTo(Vector("Hello, World!\n")))
+          }
+        ).provideSomeLayerShared[TestEnvironment](layer) @@ silent
         assertM(succeeded(spec))(isTrue)
-      },
-      testM("runs everything if no tests are tagged") {
-        val spec = suite("root suite")(
-          suite("failing suite")(
-            test("failing test") {
-              assert(1)(equalTo(2))
-            }
-          ),
-          suite("passing suite")(
-            test("passing test") {
-              assert(1)(equalTo(1))
-            }
-          )
-        )
-        assertM(succeeded(spec))(isFalse)
       }
-    ),
-    suite("provideCustomLayer")(
-      testM("provides the part of the environment that is not part of the `TestEnvironment`") {
-        for {
-          _ <- ZIO.environment[TestEnvironment]
-          _ <- ZIO.environment[Module]
-        } yield assertCompletes
-      }.provideCustomLayer(layer)
-    ),
-    suite("provideLayer")(
-      testM("does not have early initialization issues") {
-        for {
-          _ <- ZIO.environment[Module]
-        } yield assertCompletes
-      }.provideLayer(layer)
     )
   )
 }
