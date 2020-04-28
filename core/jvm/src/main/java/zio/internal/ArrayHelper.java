dc9605d497329f508116dd936455e385f632f809
aleksandar.skrbic <aleksandar.skrbic@smartcat.io>
2020-04-28 18:12:36 +0200

    Fixed #3448 - Add Java class that can access nth element of each primitive type of array

---
+++ core/jvm/src/main/java/zio/internal/ArrayHelper.java
@@ -0,0 +1,35 @@
+package zio.internal;
+
+public abstract class ArrayHelper {
+    public static byte getByte(byte[] array, int i) {
+        return array[i];
+    }
+
+    public static char getChar(char[] array, int i) {
+        return array[i];
+    }
+
+    public static short getShort(short[] array, int i) {
+        return array[i];
+    }
+
+    public static int getInt(int[] array, int i) {
+        return array[i];
+    }
+
+    public static long getLong(long[] array, int i) {
+        return array[i];
+    }
+
+    public static float getFloat(float[] array, int i) {
+        return array[i];
+    }
+
+    public static double getDouble(double[] array, int i) {
+        return array[i];
+    }
+
+    public static boolean getBoolean(boolean[] array, int i) {
+        return array[i];
+    }
+}
