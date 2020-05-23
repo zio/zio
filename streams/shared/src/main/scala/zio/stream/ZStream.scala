f3d20db9c29137accb7d740c42e69792dc42889f
Luis Martinez <luis3m.23@gmail.com>
2020-05-23 09:59:06 -0400

    Check done before pulling

--- streams/shared/src/main/scala/zio/stream/ZStream.scala
+++ streams/shared/src/main/scala/zio/stream/ZStream.scala
@@ -1992,25 +1992,30 @@ abstract class ZStream[-R, +E, +O](val process: ZManaged[R, Nothing, ZIO[R, Opti
 
       for {
         queue   <- Queue.unbounded[Take[E1, O3]].toManaged(_.shutdown)
-        done    <- RefM.make[Option[Boolean]](None).toManaged_
+        done    <- RefM.makeManaged[Option[Boolean]](None)
         chunksL <- self.process
         chunksR <- that.process
         handler = (pull: Pull[R1, E1, O3], terminate: Boolean) => {
-          pull.run.flatMap { exit =>
-            done.modify {
-              (_, exit.fold(c => Left(Cause.sequenceCauseOption(c)), Right(_))) match {
-                case (state @ Some(true), _) =>
-                  ZIO.succeedNow((false, state))
-                case (state, Right(chunk)) =>
-                  queue.offer(Take.chunk(chunk)).as((true, state))
-                case (_, Left(Some(cause))) =>
-                  queue.offer(Take.halt(cause)).as((false, Some(true)))
-                case (option, Left(None)) if terminate || option.isDefined =>
-                  queue.offer(Take.end).as((false, Some(true)))
-                case (None, Left(None)) =>
-                  ZIO.succeedNow((false, Some(false)))
+          done.get.flatMap {
+            case Some(true) =>
+              ZIO.succeedNow(false)
+            case _ =>
+              pull.run.flatMap { exit =>
+                done.modify {
+                  (_, exit.fold(c => Left(Cause.sequenceCauseOption(c)), Right(_))) match {
+                    case (state @ Some(true), _) =>
+                      ZIO.succeedNow((false, state))
+                    case (state, Right(chunk)) =>
+                      queue.offer(Take.chunk(chunk)).as((true, state))
+                    case (_, Left(Some(cause))) =>
+                      queue.offer(Take.halt(cause)).as((false, Some(true)))
+                    case (option, Left(None)) if terminate || option.isDefined =>
+                      queue.offer(Take.end).as((false, Some(true)))
+                    case (None, Left(None)) =>
+                      ZIO.succeedNow((false, Some(false)))
+                  }
+                }
               }
-            }
           }.repeat(Schedule.doWhileEquals(true))
         }
         _ <- handler(chunksL.map(_.map(l)), List(L, E).contains(strategy)).forkManaged
