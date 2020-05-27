7a34eac34f681ae157999762e328f5b07ece58f0
lvitaly <lvitalii@gmail.com>
2020-05-27 21:36:08 +0300

    Add ZSink.fromFile functionality (scalafmt fix)

--- streams/jvm/src/main/scala/zio/stream/platform.scala
+++ streams/jvm/src/main/scala/zio/stream/platform.scala
@@ -1,11 +1,11 @@
 package zio.stream
 
-import java.io.{IOException, InputStream, OutputStream}
+import java.io.{ IOException, InputStream, OutputStream }
 import java.nio.ByteBuffer
 import java.nio.channels.FileChannel
-import java.nio.file.{Files, OpenOption, Path}
+import java.nio.file.{ Files, OpenOption, Path }
 import java.nio.file.StandardOpenOption._
-import java.{util => ju}
+import java.{ util => ju }
 
 import zio._
 import zio.blocking.Blocking
@@ -37,29 +37,28 @@ trait ZSinkPlatformSpecificConstructors { self: ZSink.type =>
    * Uses the provided `Path` to create a [[ZSink]] that consumes byte chunks
    * and writes them to the `File`. The sink will yield count of bytes written.
    */
-  final def fromFile(path: => Path,
-                     position: Long = 0L,
-                     options: Set[OpenOption] = Set(WRITE, TRUNCATE_EXISTING, CREATE)
-                    ): ZSink[Blocking, Throwable, Byte, Long] =
+  final def fromFile(
+    path: => Path,
+    position: Long = 0L,
+    options: Set[OpenOption] = Set(WRITE, TRUNCATE_EXISTING, CREATE)
+  ): ZSink[Blocking, Throwable, Byte, Long] =
     ZSink {
       for {
-        state    <- Ref.make(0L).toManaged_
-        channel  <- ZManaged.make(
-                      blocking.effectBlockingInterrupt(FileChannel.open(path, options.asJava).position(position)).orDie
-                    )(
-                      chan => blocking.effectBlocking(chan.close()).orDie
-                    )
+        state <- Ref.make(0L).toManaged_
+        channel <- ZManaged.make(
+                    blocking.effectBlockingInterrupt(FileChannel.open(path, options.asJava).position(position)).orDie
+                  )(chan => blocking.effectBlocking(chan.close()).orDie)
         push = (is: Option[Chunk[Byte]]) =>
           is match {
             case None => state.get.flatMap(Push.emit)
             case Some(byteChunk) =>
               for {
                 bytesWritten <- state.get
-                justWritten  <- blocking.effectBlockingInterrupt {
-                                  Files.write(path, byteChunk.toArray)
-                                  channel.write(ByteBuffer.wrap(byteChunk.toArray))
-                                }.mapError(Left(_))
-                more         <- state.set(bytesWritten + justWritten) *> Push.more
+                justWritten <- blocking.effectBlockingInterrupt {
+                                Files.write(path, byteChunk.toArray)
+                                channel.write(ByteBuffer.wrap(byteChunk.toArray))
+                              }.mapError(Left(_))
+                more <- state.set(bytesWritten + justWritten) *> Push.more
               } yield more
           }
       } yield push
