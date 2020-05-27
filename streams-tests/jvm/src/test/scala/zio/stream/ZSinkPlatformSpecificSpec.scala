ff382204ddf2fdda65d7aa8088ee718d88ffa3c5
lvitaly <lvitalii@gmail.com>
2020-05-27 21:08:07 +0300

    Add ZSink.fromFile functionality

---
+++ streams-tests/jvm/src/test/scala/zio/stream/ZSinkPlatformSpecificSpec.scala
@@ -0,0 +1,27 @@
+package zio.stream
+
+import java.nio.file.Files
+
+import zio._
+import zio.test._
+import zio.test.Assertion._
+
+object ZSinkPlatformSpecificSpec extends ZIOBaseSpec {
+  override def spec = suite("ZSink JVM")(
+    suite("fromFile")(
+      testM("writes to an existing file") {
+        val data = (0 to 100).mkString
+
+          Task(Files.createTempFile("stream", "fromFile"))
+            .bracket(path => Task(Files.delete(path)).orDie) { path =>
+              for {
+                bytes  <- Task(data.getBytes("UTF-8"))
+                length <- ZStream.fromIterable(bytes).run(ZSink.fromFile(path))
+                str    <- Task(new String(Files.readAllBytes(path)))
+              } yield assert(data)(equalTo(str)) && assert(bytes.length.toLong)(equalTo(length))
+            }
+
+      }
+    )
+  )
+}
--- streams/jvm/src/main/scala/zio/stream/platform.scala
+++ streams/jvm/src/main/scala/zio/stream/platform.scala
@@ -1,13 +1,16 @@
 package zio.stream
 
-import java.io.{ IOException, InputStream, OutputStream }
+import java.io.{IOException, InputStream, OutputStream}
 import java.nio.ByteBuffer
 import java.nio.channels.FileChannel
-import java.nio.file.Path
-import java.{ util => ju }
+import java.nio.file.{Files, OpenOption, Path}
+import java.nio.file.StandardOpenOption._
+import java.{util => ju}
 
 import zio._
 import zio.blocking.Blocking
+
+import scala.collection.JavaConverters._
 
 trait ZSinkPlatformSpecificConstructors { self: ZSink.type =>
 
@@ -28,6 +31,38 @@ trait ZSinkPlatformSpecificConstructors { self: ZSink.type =>
       }.refineOrDie {
         case e: IOException => e
       }
+    }
+
+  /**
+   * Uses the provided `Path` to create a [[ZSink]] that consumes byte chunks
+   * and writes them to the `File`. The sink will yield count of bytes written.
+   */
+  final def fromFile(path: => Path,
+                     position: Long = 0L,
+                     options: Set[OpenOption] = Set(WRITE, TRUNCATE_EXISTING, CREATE)
+                    ): ZSink[Blocking, Throwable, Byte, Long] =
+    ZSink {
+      for {
+        state    <- Ref.make(0L).toManaged_
+        channel  <- ZManaged.make(
+                      blocking.effectBlockingInterrupt(FileChannel.open(path, options.asJava).position(position)).orDie
+                    )(
+                      chan => blocking.effectBlocking(chan.close()).orDie
+                    )
+        push = (is: Option[Chunk[Byte]]) =>
+          is match {
+            case None => state.get.flatMap(Push.emit)
+            case Some(byteChunk) =>
+              for {
+                bytesWritten <- state.get
+                justWritten  <- blocking.effectBlockingInterrupt {
+                                  Files.write(path, byteChunk.toArray)
+                                  channel.write(ByteBuffer.wrap(byteChunk.toArray))
+                                }.mapError(Left(_))
+                more         <- state.set(bytesWritten + justWritten) *> Push.more
+              } yield more
+          }
+      } yield push
     }
 }
 
