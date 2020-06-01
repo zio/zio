9433d1e236ff04944f0014a9ecca4f464d26e658
Ajay Chandran <ajay.chandran.1980@gmail.com>
2020-06-01 21:17:44 +0530

    Removed throttleBytes

--- streams/shared/src/main/scala/zio/stream/ZTransducer.scala
+++ streams/shared/src/main/scala/zio/stream/ZTransducer.scala
@@ -728,12 +728,6 @@ object ZTransducer {
     }
 
   /**
-   * Creates a transducer that limits byte chunks to the given size.
-   */
-  def throttleBytes(limit: Int): ZTransducer[Any, Nothing, Byte, Chunk[Byte]] =
-    throttleChunks[Byte](limit)
-
-  /**
    * Creates a transducer that limits chunks to the given size.
    */
   def throttleChunks[A](limit: Int): ZTransducer[Any, Nothing, A, Chunk[A]] = {
