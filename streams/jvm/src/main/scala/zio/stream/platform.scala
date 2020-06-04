fcbf094a66b127c1dd13d686520d3ca926aee280
Vitalii Lagutin <lvitaly@users.noreply.github.com>
2020-06-04 22:40:43 +0300

    Add ZSink.fromFile functionality (#3647) (#3712)
    * Add ZSink.fromFile functionality
    
    * Add ZSink.fromFile functionality (scalafmt fix)
    
    * Add ZSink.fromFile functionality (scalafmt fix in test)
    
    * Add ZSink.fromFile functionality (updates after review)
    
    * Add ZSink.fromFile functionality (scalafix)
    
    * Add ZSink.fromFile functionality (scalafix)
    
    * Add ZSink.fromFile functionality (cross compile)
    
    * Add ZSink.fromFile functionality (unified)
    
    * Add ZSink.fromFile functionality (scalafmt fix)

--- streams/jvm/src/main/scala/zio/stream/platform.scala
+++ streams/jvm/src/main/scala/zio/stream/platform.scala
@@ -3,7 +3,8 @@ package zio.stream
 import java.io.{ IOException, InputStream, OutputStream }
 import java.nio.ByteBuffer
 import java.nio.channels.FileChannel
-import java.nio.file.Path
+import java.nio.file.StandardOpenOption._
+import java.nio.file.{ OpenOption, Path }
 import java.{ util => ju }
 
 import zio._
@@ -28,6 +29,46 @@ trait ZSinkPlatformSpecificConstructors { self: ZSink.type =>
       }.refineOrDie {
         case e: IOException => e
       }
+    }
+
+  /**
+   * Uses the provided `Path` to create a [[ZSink]] that consumes byte chunks
+   * and writes them to the `File`. The sink will yield count of bytes written.
+   */
+  final def fromFile(
+    path: => Path,
+    position: Long = 0L,
+    options: Set[OpenOption] = Set(WRITE, TRUNCATE_EXISTING, CREATE)
+  ): ZSink[Blocking, Throwable, Byte, Long] =
+    ZSink {
+      for {
+        state <- Ref.make(0L).toManaged_
+        channel <- ZManaged.make(
+                    blocking
+                      .effectBlockingInterrupt(
+                        FileChannel
+                          .open(
+                            path,
+                            options.foldLeft(new ju.HashSet[OpenOption]()) { (acc, op) =>
+                              acc.add(op); acc
+                            } // for avoiding usage of different Java collection converters for different scala versions
+                          )
+                          .position(position)
+                      )
+                      .orDie
+                  )(chan => blocking.effectBlocking(chan.close()).orDie)
+        push = (is: Option[Chunk[Byte]]) =>
+          is match {
+            case None => state.get.flatMap(Push.emit)
+            case Some(byteChunk) =>
+              for {
+                justWritten <- blocking.effectBlockingInterrupt {
+                                channel.write(ByteBuffer.wrap(byteChunk.toArray))
+                              }.mapError(Left(_))
+                more <- state.update(_ + justWritten) *> Push.more
+              } yield more
+          }
+      } yield push
     }
 }
 
