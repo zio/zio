67b016fac573b73db5577ae76fd808d771726b25
simpadjo <simpadjo@gmail.com>
2020-06-21 18:51:14 +0200

    delete dead code

--- streams-tests/jvm/src/test/scala/zio/stream/StreamUtils.scala
+++
@@ -1,8 +0,0 @@
-package zio.stream
-
-import zio._
-
-object StreamUtils {
-  def nPulls[R, E, A](pull: ZIO[R, Option[E], Chunk[A]], n: Int): ZIO[R, Nothing, List[Either[Option[E], Chunk[A]]]] =
-    ZIO.foreach(1 to n)(_ => pull.either)
-}
