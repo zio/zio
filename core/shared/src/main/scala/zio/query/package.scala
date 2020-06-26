6960a447328f92d6d1b4b1e82481e3ef365c1cc4
Adam Fraser <adam.fraser@gmail.com>
2020-06-26 09:27:36 -0700

    implement partitionMap

--- core/shared/src/main/scala/zio/query/package.scala
+++ core/shared/src/main/scala/zio/query/package.scala
@@ -7,4 +7,18 @@ package object query {
   type Query[+E, +A]   = ZQuery[Any, E, A]
   type UQuery[+A]      = ZQuery[Any, Nothing, A]
   type TaskQuery[+A]   = ZQuery[Any, Throwable, A]
+
+  implicit class VectorSytanx[A](private val self: Vector[A]) extends AnyVal {
+    def partitionMap[B, C](f: A => Either[B, C]): (Vector[B], Vector[C]) = {
+      val bs = Vector.newBuilder[B]
+      val cs = Vector.newBuilder[C]
+      self.foreach { a =>
+        f(a) match {
+          case Left(b)  => bs += b
+          case Right(c) => cs += c
+        }
+      }
+      (bs.result(), cs.result())
+    }
+  }
 }
