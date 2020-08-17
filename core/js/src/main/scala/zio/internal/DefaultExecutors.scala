b5f6fa6711d69484c5699bf07f001a3b12d213cc
Adam Fraser <adam.fraser@gmail.com>
2020-08-17 13:58:08 -0700

    use setImmediate

--- core/js/src/main/scala/zio/internal/DefaultExecutors.scala
+++ core/js/src/main/scala/zio/internal/DefaultExecutors.scala
@@ -1,3 +1,17 @@
 package zio.internal
 
-private[internal] abstract class DefaultExecutors
+import scala.concurrent.ExecutionContext
+import scala.scalajs.js
+
+private[internal] abstract class DefaultExecutors {
+  final def makeDefault(yieldOpCount: Int): Executor =
+    Executor.fromExecutionContext(yieldOpCount) {
+      new ExecutionContext {
+        def execute(runnable: Runnable): Unit = {
+          val _ = js.Dynamic.global.setImmediate(() => runnable.run())
+        }
+        def reportFailure(cause: Throwable): Unit =
+          cause.printStackTrace()
+      }
+    }
+}
