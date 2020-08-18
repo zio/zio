5f0c60afd5f3281d450f28f829fc89ac32bf1e3b
Adam Fraser <adam.fraser@gmail.com>
2020-08-18 10:23:49 -0700

    only use setImmediate after maxYieldOpCount

--- core/shared/src/main/scala/zio/internal/FiberContext.scala
+++ core/shared/src/main/scala/zio/internal/FiberContext.scala
@@ -535,7 +535,9 @@ private[zio] final class FiberContext[E, A](
                       else lock(zio.executor).bracket_(unlock, zio.zio)
 
                   case ZIO.Tags.Yield =>
-                    curZio = yieldNow
+                    evaluateLater(ZIO.unit)
+
+                    curZio = null
 
                   case ZIO.Tags.Access =>
                     val zio = curZio.asInstanceOf[ZIO.Read[Any, E, Any]]
@@ -663,9 +665,6 @@ private[zio] final class FiberContext[E, A](
 
   private[this] def unlock: UIO[Unit] =
     ZIO.effectTotal(executors.pop()) *> ZIO.yieldNow
-
-  private[this] def yieldNow: UIO[Unit] =
-    ZIO.effectAsync(cb => executor.submitAndYield(() => cb(ZIO.unit)))
 
   private[this] def getDescriptor(): Fiber.Descriptor =
     Fiber.Descriptor(
