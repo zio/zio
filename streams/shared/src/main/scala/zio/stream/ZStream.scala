afc8c02f13ef0142f8dc8184ca17463c35f990cc
ogresto <ogresto@gmail.com>
2020-08-31 18:08:36 +0200

    `ZStream#fromIterator(Managed)` should have an optional `chunkSize` parameter #3841

--- streams/shared/src/main/scala/zio/stream/ZStream.scala
+++ streams/shared/src/main/scala/zio/stream/ZStream.scala
@@ -3449,7 +3449,6 @@ object ZStream extends ZStreamPlatformSpecificConstructors {
     chunkSize: Int = ZStream.DefaultChunkSize
   ): ZStream[Any, Throwable, A] = {
     object StreamEnd extends Throwable
-
     ZStream.fromEffect(Task(iterator.grouped(chunkSize)) <*> ZIO.runtime[Any]).flatMap {
       case (it, rt) =>
         ZStream.repeatEffectChunkOption {
