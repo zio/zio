bdfe0f2fe6a98331be28366918578be37d779905
Adam Fraser <adam.fraser@gmail.com>
2020-09-03 15:44:54 -0700

    use specialized ExecutionContext

--- core/js/src/main/scala/zio/internal/DefaultExecutors.scala
+++ core/js/src/main/scala/zio/internal/DefaultExecutors.scala
@@ -1,24 +1,6 @@
 package zio.internal
 
-import scala.concurrent.ExecutionContext
-import scala.scalajs.js
-
 private[internal] abstract class DefaultExecutors {
   final def makeDefault(yieldOpCount: Int): Executor =
-    Executor.fromExecutionContext(yieldOpCount) {
-      new ExecutionContext {
-        def execute(runnable: Runnable): Unit = {
-          val _ = setImmediate(() => runnable.run())
-        }
-        def reportFailure(cause: Throwable): Unit =
-          cause.printStackTrace()
-      }
-    }
-
-  private val setImmediate =
-    if (js.typeOf(js.Dynamic.global.setImmediate) == "function") {
-      js.Dynamic.global.setImmediate
-    } else {
-      js.Dynamic.global.setTimeout
-    }
+    Executor.fromExecutionContext(yieldOpCount)(JSExecutionContext.default)
 }
---
+++ core/js/src/main/scala/zio/internal/JSExecutionContext.scala
@@ -0,0 +1,45 @@
+package zio.internal
+
+import java.util.ArrayList
+
+import scala.concurrent.ExecutionContext
+import scala.scalajs.js
+
+/**
+ * A specialized `ExecutionContext` optimized for Scala.js.
+ */
+private[zio] sealed abstract class JSExecutionContext extends ExecutionContext
+
+object JSExecutionContext {
+
+  val default: JSExecutionContext =
+    new JSExecutionContext {
+      var running = false
+      val queue: ArrayList[Runnable] =
+        new ArrayList[Runnable]
+      def execute(runnable: Runnable): Unit =
+        if (!running) {
+          running = true
+          queue.add(runnable)
+          val _ = setImmediate(() => runQueue())
+        } else {
+          val _ = queue.add(runnable)
+        }
+      def reportFailure(cause: Throwable): Unit =
+        cause.printStackTrace()
+      def runQueue(): Unit = {
+        while (!queue.isEmpty()) {
+          val runnable = queue.remove(0)
+          runnable.run()
+        }
+        running = false
+      }
+    }
+
+  private val setImmediate =
+    if (js.typeOf(js.Dynamic.global.setImmediate) == "function") {
+      js.Dynamic.global.setImmediate
+    } else {
+      js.Dynamic.global.setTimeout
+    }
+}
