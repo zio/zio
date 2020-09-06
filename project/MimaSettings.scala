a4a1f3aeac882419e0e3490b0cecfbd756b92a6b
Ondra Pelech <ondra.pelech@gmail.com>
2020-09-07 00:59:16 +0200

    MiMa ReversedMissingMethodProblem: suppress for Cause.equalsWith and Cause.hashWith

--- project/MimaSettings.scala
+++ project/MimaSettings.scala
@@ -1,6 +1,5 @@
 import sbt._
-import sbt.Keys.{ name, organization }
-
+import sbt.Keys.{ moduleName, organization }
 import com.typesafe.tools.mima.plugin.MimaKeys._
 import com.typesafe.tools.mima.core._
 import com.typesafe.tools.mima.core.ProblemFilters._
@@ -10,11 +9,13 @@ object MimaSettings {
 
   def mimaSettings(failOnProblem: Boolean) =
     Seq(
-      mimaPreviousArtifacts := Set(organization.value %% name.value % bincompatVersionToCompare),
+      mimaPreviousArtifacts := Set(organization.value %% moduleName.value % bincompatVersionToCompare),
       mimaBinaryIssueFilters ++= Seq(
         exclude[Problem]("zio.internal.*"),
         exclude[DirectMissingMethodProblem]("zio.ZManaged.reserve"),
-        exclude[DirectMissingMethodProblem]("zio.ZIO#Fork.this")
+        exclude[DirectMissingMethodProblem]("zio.ZIO#Fork.this"),
+        exclude[ReversedMissingMethodProblem]("zio.Cause.equalsWith"),
+        exclude[ReversedMissingMethodProblem]("zio.Cause.hashWith")
       ),
       mimaFailOnProblem := failOnProblem
     )
