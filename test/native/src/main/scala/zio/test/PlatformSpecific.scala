569376b3efc5fa94a1c484fbf44a787b9e2f3966
Ondra Pelech <ondra.pelech@gmail.com>
2020-09-29 21:37:50 +0200

    Build ZIO Test for Scala Native

---
+++ test/native/src/main/scala/zio/test/PlatformSpecific.scala
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2017-2020 John A. De Goes and the ZIO Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package zio.test
+
+import zio._
+import zio.test.environment._
+
+private[test] abstract class PlatformSpecific {
+  type TestEnvironment =
+    Annotations
+      with Live
+      with Sized
+      with TestClock
+      with TestConfig
+      with TestConsole
+      with TestRandom
+      with TestSystem
+      with ZEnv
+
+  object TestEnvironment {
+    val any: ZLayer[TestEnvironment, Nothing, TestEnvironment] =
+      ZLayer.requires[TestEnvironment]
+    val live: ZLayer[ZEnv, Nothing, TestEnvironment] =
+      Annotations.live ++
+        Live.default ++
+        Sized.live(100) ++
+        ((Live.default ++ Annotations.live) >>> TestClock.default) ++
+        TestConfig.live(100, 100, 200, 1000) ++
+        (Live.default >>> TestConsole.debug) ++
+        TestRandom.deterministic ++
+        TestSystem.default
+  }
+}
