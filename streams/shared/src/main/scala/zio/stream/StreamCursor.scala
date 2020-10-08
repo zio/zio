a4dbca09ef6ee2ee836506f9995379540b0cb47a
Maxim Schuwalow <maxim.schuwalow@gmail.com>
2020-10-08 22:44:25 +0200

    remove cursor

--- streams/shared/src/main/scala/zio/stream/StreamCursor.scala
+++
@@ -1,9 +0,0 @@
-package zio.stream
-
-import zio._
-
-trait StreamCursor[-R, +E, +A] {
-  val next: ZIO[R, Option[E], Chunk[A]]
-  val split: UIO[StreamCursor[R, E, A]]
-  final lazy val stream: ZStream[R, E, A] = ZStream(ZManaged.succeedNow(next))
-}
