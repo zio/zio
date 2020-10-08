95b38263b7dcd758db9ed64fe3d8efad28c834c9
Adam Fraser <adam.fraser@gmail.com>
2020-10-08 11:16:15 -0700

    rename

--- test/shared/src/main/scala/zio/test/TestAspect.scala
+++ test/shared/src/main/scala/zio/test/TestAspect.scala
@@ -451,7 +451,7 @@ object TestAspect extends TimeoutVariants {
   /**
    * An aspect that runs each test with the size set to the specified value.
    */
-  def size(n: Int): TestAspectAtLeastR[Sized] =
+  def sized(n: Int): TestAspectAtLeastR[Sized] =
     new PerTest.AtLeastR[Sized] {
       def perTest[R <: Sized, E](test: ZIO[R, TestFailure[E], TestSuccess]): ZIO[R, TestFailure[E], TestSuccess] =
         Sized.withSize(n)(test)
