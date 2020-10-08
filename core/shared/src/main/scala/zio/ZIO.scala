345aefe9752cb21aac48c25903a954b210e73203
Adam Fraser <adam.fraser@gmail.com>
2020-10-07 17:05:32 -0700

    implement ZIO#schedule

--- core/shared/src/main/scala/zio/ZIO.scala
+++ core/shared/src/main/scala/zio/ZIO.scala
@@ -1666,6 +1666,17 @@ sealed trait ZIO[-R, +E, +A] extends Serializable with ZIOPlatformSpecific[R, E,
   final def sandbox: ZIO[R, Cause[E], A] = foldCauseM(ZIO.fail(_), ZIO.succeedNow)
 
   /**
+   * Runs this effect according to the specified schedule.
+   */
+  final def schedule[R1 <: R, B](schedule: Schedule[R1, Any, B]): ZIO[R1 with Clock, E, B] =
+    schedule.driver.flatMap { driver =>
+      def loop: ZIO[R1 with Clock, E, B] =
+        driver.next(()).foldM(_ => driver.last.orDie, _ => self *> loop)
+
+      loop
+    }
+
+  /**
    * Converts an option on values into an option on errors.
    */
   final def some[B](implicit ev: A <:< Option[B]): ZIO[R, Option[E], B] =
