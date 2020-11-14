7c6b0c6d18137cedba51654e255feffe0eaa89fd
Adam Fraser <adam.fraser@gmail.com>
2020-11-14 07:29:53 -0800

    use transplant

--- core/shared/src/main/scala/zio/ZIO.scala
+++ core/shared/src/main/scala/zio/ZIO.scala
@@ -3007,7 +3007,7 @@ object ZIO extends ZIOCompanionPlatformSpecific {
                    .uninterruptible
                )
 
-        fibers <- ZIO.foreach(as)(a => task(a).fork)
+        fibers <- ZIO.transplant(graft => ZIO.foreach(as)(a => graft(task(a)).fork))
         interrupter = result.await
                         .catchAll(_ => ZIO.foreach(fibers)(_.interruptAs(parentId).fork) >>= Fiber.joinAll)
                         .forkManaged
