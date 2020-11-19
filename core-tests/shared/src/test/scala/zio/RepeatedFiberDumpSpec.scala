1070541af672ab5993dd5640ff23a0103df948a5
Andreas Gies <andreas@wayofquality.de>
2020-11-19 14:38:52 +0100

    Added some sample output

--- core-tests/shared/src/test/scala/zio/RepeatedFiberDumpSpec.scala
+++ core-tests/shared/src/test/scala/zio/RepeatedFiberDumpSpec.scala
@@ -12,6 +12,42 @@ import scala.collection.immutable.SortedSet
 import java.util.function.UnaryOperator
 import zio.test.environment.Live
 
+// Will produce something like
+// sbt:core-tests> test:testOnly *FiberDumpSpec
+// Starting test
+// AJIBECHGDKFFHBDA
+// GIKCJEEJCKIG
+// ADBHFFHBDA
+// GIKCJEEJCKIG
+// ADBHFFHBDA
+// GIKCJEEJCKIG
+// ADBHFFHBDA
+// Encountered null value in content of SingleThreadedRingBuffer, capacity=100, size=14, current=14, nullAt=0
+
+// [info] - The Fiber context should
+// [info]   - allow to regularly poll the dump of all current fibers
+// [info]     Fiber failed.
+// [info]     An interrupt was produced by #79.
+// [info]
+// [info]     Fiber:Id(1605792801846,78) was supposed to continue to:
+// [info]       a future continuation at zio.ZIO$._IdentityFn(ZIO.scala:3959)
+// [info]       a future continuation at zio.ZIO.onInterrupt(ZIO.scala:981)
+// [info]       a future continuation at zio.test.environment.package$TestClock$Test.sleep(package.scala:332)
+// [info]       a future continuation at zio.Schedule.driver(Schedule.scala:347)
+// [info]       a future continuation at zio.Schedule.driver(Schedule.scala:349)
+// [info]       a future continuation at zio.ZIO.scheduleFrom(ZIO.scala:1684)
+// [info]
+// [info]     Fiber:Id(1605792801846,78) execution trace:
+// [info]       at zio.Promise.await(Promise.scala:50)
+// [info]       at zio.ZIO$.effectAsyncInterrupt(ZIO.scala:2574)
+// [info]       at zio.ZIO$.effectAsyncInterrupt(ZIO.scala:2574)
+// [info]       at zio.test.environment.package$TestClock$Test.sleep(package.scala:332)
+// [info]       at zio.internal.FiberContext$InterruptExit$.apply(FiberContext.scala:161)
+// [info]       at zio.internal.FiberContext$InterruptExit$.apply(FiberContext.scala:154)
+// [info]       at zio.ZIO$.effectSuspendTotal(ZIO.scala:2640)
+// [info]       at zio.ZIO$.bracketExit(ZIO.scala:2273)
+// [info]       at zio.internal.FiberContext$InterruptExit$.apply(FiberContext.scala:161)
+
 object RepeatedFiberDumpSpec extends ZIOBaseSpec {
 
   override def runner: TestRunner[zio.test.environment.TestEnvironment, Any] =
@@ -27,13 +63,13 @@ object RepeatedFiberDumpSpec extends ZIOBaseSpec {
     // Start a number of busy Fibers
     _  <- ZIO.foreach(0.to(10))(i => recurringWork(('A' + i).toChar).fork)
     f  <- ZIO.unit.schedule(Schedule.duration(300.seconds)).fork
-    fd <- (dumpLoop.onError{ t =>
+    fd <- (dumpLoop.onError{ t => // Will die on the NPE reported in #4384
       putStrLn(t.failures.map(_.getMessage()).mkString("\n")) *> f.interrupt
     }).schedule(Schedule.spaced(1.second)).fork
     _  <- f.join
     _  <- putStrLn("Stopping test")
     _  <- fd.interrupt
-  } yield assertCompletes)
+  } yield assertCompletes) // If we end up here the bug might be fixed
 
   // Advance the test clock every 10 millis by a second
   private def timeWarp = for {
