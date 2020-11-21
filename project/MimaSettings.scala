0436add68804e412ca9c376c1c49da0a37c28771
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2020-11-21 18:31:34 +0100

    Filtering zio.Promise#internal from MIMA check

--- project/MimaSettings.scala
+++ project/MimaSettings.scala
@@ -13,6 +13,7 @@ object MimaSettings {
       mimaPreviousArtifacts := Set(organization.value %% name.value % bincompatVersionToCompare),
       mimaBinaryIssueFilters ++= Seq(
         exclude[Problem]("zio.internal.*"),
+        exclude[Problem]("zio.Promise#internal#*"),
         exclude[DirectMissingMethodProblem]("zio.ZManaged.reserve"),
         exclude[DirectMissingMethodProblem]("zio.ZIO#Fork.this")
       ),
