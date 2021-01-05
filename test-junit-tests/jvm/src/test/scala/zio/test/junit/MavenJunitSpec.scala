3de76fd24da4536a9ba5ac1b8500e45a199646f2
Dmitry Karlinsky <dmitryka@wix.com>
2021-01-05 11:23:33 +0200

    fixing MavenJunitSpec

--- test-junit-tests/jvm/src/test/scala/zio/test/junit/MavenJunitSpec.scala
+++ test-junit-tests/jvm/src/test/scala/zio/test/junit/MavenJunitSpec.scala
@@ -29,13 +29,14 @@ object MavenJunitSpec extends DefaultRunnableSpec {
             "should fail",
             s"""zio.test.junit.TestFailed:
                |11 did not satisfy equalTo(12)
-               |11 did not satisfy (equalTo(12) ?? "assert(`11`) (at ${mvn.mvnRoot}/src/test/scala/zio/test/junit/maven/FailingSpec.scala:10)")""".stripMargin
+               |at ${mvn.mvnRoot}/src/test/scala/zio/test/junit/maven/FailingSpec.scala:10""".stripMargin
           ) &&
             containsFailure(
               "should fail - isSome",
               s"""zio.test.junit.TestFailed:
                  |11 did not satisfy equalTo(12)
-                 |Some(11) did not satisfy (isSome(equalTo(12)) ?? "assert(`Some[Int](11)`) (at ${mvn.mvnRoot}/src/test/scala/zio/test/junit/maven/FailingSpec.scala:13)")""".stripMargin
+                 |`Some[Int](11)` = Some(11) did not satisfy isSome(equalTo(12))
+                 |at ${mvn.mvnRoot}/src/test/scala/zio/test/junit/maven/FailingSpec.scala:13""".stripMargin
             ) &&
             containsSuccess("should succeed")
         )
