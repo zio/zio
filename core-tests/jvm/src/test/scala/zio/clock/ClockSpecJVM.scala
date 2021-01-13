9cf246f97a888ab3462595238e9dfb8a77380b33
Simon Schenk <simon@schenk-online.net>
2021-01-13 13:00:57 +0100

    run test only on JRE 9 >= 9

--- core-tests/jvm/src/test/scala/zio/clock/ClockSpecJVM.scala
+++ core-tests/jvm/src/test/scala/zio/clock/ClockSpecJVM.scala
@@ -1,6 +1,7 @@
 package zio.clock
 
 import zio._
+import zio.system._
 import zio.test.Assertion._
 import zio.test._
 
@@ -9,13 +10,17 @@ import java.util.concurrent.TimeUnit
 object ClockSpecJVM extends ZIOBaseSpec {
 
   def spec: Spec[Annotations with TestConfig with ZTestEnv, TestFailure[Any], TestSuccess] = suite("ClockSpec")(
-    testM("currentDateTime has microsecond resolution") {
+    testM("currentTime has microsecond resolution on JRE >= 9") {
       val unit = TimeUnit.MICROSECONDS
       for {
-        a <- clock.currentTime(unit)
-        _ <- ZIO.foreach(1 to 1000)(_ => UIO.unit) // just pass some time
-        b <- clock.currentTime(unit)
-      } yield assert((b - a) % 1000)(not(equalTo(0L)))
-    }.provideLayer(Clock.live) @@ TestAspect.flaky
+        jreVersion <- property("java.version")
+        a          <- clock.currentTime(unit)
+        _          <- ZIO.foreach(1 to 1000)(_ => UIO.unit) // just pass some time
+        b          <- clock.currentTime(unit)
+      } yield
+      // This test should only run on JRE >= 9, which is when microsecond precision was introduced.
+      // Versions of JREs < 9 started with s"1.${majorVersion}", then with JEP 223 they switched to semantic versioning.
+      assert(jreVersion)(isSome(startsWithString("1."))) || assert((b - a) % 1000)(not(equalTo(0L)))
+    }.provideLayer(Clock.live ++ System.live) @@ TestAspect.flaky
   )
 }
