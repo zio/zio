678900c7413a5d974e6aedc930b4660fb9571a23
d948521 <afsal.taj06@gmail.com>
2021-01-17 23:25:58 +1100

    Add generic to all instances

--- test-refined/shared/src/main/scala/zio/test/refined/all.scala
+++ test-refined/shared/src/main/scala/zio/test/refined/all.scala
@@ -1,3 +1,7 @@
 package zio.test.refined
 
-object all extends BooleanInstances with CharInstances with StringInstances
+object all
+    extends BooleanInstances
+    with CharInstances
+    with GenericInstances
+    with StringInstances
