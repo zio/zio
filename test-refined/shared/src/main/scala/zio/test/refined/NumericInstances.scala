d4ab9f2076d108c5e91f3c97c1117f9d22d2773c
Pierangelo Cecchetto <pierangeloc@gmail.com>
2021-01-23 01:28:52 +0100

    test-refined: Make integer instances implicit

--- test-refined/shared/src/main/scala/zio/test/refined/NumericInstances.scala
+++ test-refined/shared/src/main/scala/zio/test/refined/NumericInstances.scala
@@ -10,52 +10,52 @@ object numeric extends NumericInstances
 
 trait NumericInstances {
 
-  def intGreaterThan[N](implicit
+  implicit def intGreaterThan[N](implicit
     wn: WitnessAs[N, Int]
   ): DeriveGen[Int Refined Greater[N]] =
     DeriveGen.instance(Gen.int(wn.snd, Int.MaxValue).map(Refined.unsafeApply))
 
-  def longGreaterThan[N](implicit
+  implicit def longGreaterThan[N](implicit
     wn: WitnessAs[N, Long]
   ): DeriveGen[Long Refined Greater[N]] =
     DeriveGen.instance(Gen.long(wn.snd, Long.MaxValue).map(Refined.unsafeApply))
 
-  def shortGreaterThan[N](implicit
+  implicit def shortGreaterThan[N](implicit
     wn: WitnessAs[N, Short]
   ): DeriveGen[Short Refined Greater[N]] =
     DeriveGen.instance(Gen.short(wn.snd, Short.MaxValue).map(Refined.unsafeApply))
 
-  def byteGreaterThan[N](implicit
+  implicit def byteGreaterThan[N](implicit
     wn: WitnessAs[N, Byte]
   ): DeriveGen[Byte Refined Greater[N]] =
     DeriveGen.instance(Gen.byte(wn.snd, Byte.MaxValue).map(Refined.unsafeApply))
 
-  def doubleGreaterThan[N](implicit
+  implicit def doubleGreaterThan[N](implicit
     wn: WitnessAs[N, Double]
   ): DeriveGen[Double Refined Greater[N]] =
     DeriveGen.instance(Gen.double(wn.snd, Double.MaxValue).map(Refined.unsafeApply))
 
-  def intLessThan[N](implicit
+  implicit def intLessThan[N](implicit
     wn: WitnessAs[N, Int]
   ): DeriveGen[Int Refined Less[N]] =
     DeriveGen.instance(Gen.int(Int.MinValue, wn.snd).map(Refined.unsafeApply))
 
-  def longLessThan[N](implicit
+  implicit def longLessThan[N](implicit
     wn: WitnessAs[N, Long]
   ): DeriveGen[Long Refined Less[N]] =
     DeriveGen.instance(Gen.long(Long.MinValue, wn.snd).map(Refined.unsafeApply))
 
-  def shortLessThan[N](implicit
+  implicit def shortLessThan[N](implicit
     wn: WitnessAs[N, Short]
   ): DeriveGen[Short Refined Less[N]] =
     DeriveGen.instance(Gen.short(Short.MinValue, wn.snd).map(Refined.unsafeApply))
 
-  def byteLessThan[N](implicit
+  implicit def byteLessThan[N](implicit
     wn: WitnessAs[N, Byte]
   ): DeriveGen[Byte Refined Less[N]] =
     DeriveGen.instance(Gen.byte(Byte.MinValue, wn.snd).map(Refined.unsafeApply))
 
-  def doubleLessThan[N](implicit
+  implicit def doubleLessThan[N](implicit
     wn: WitnessAs[N, Double]
   ): DeriveGen[Double Refined Less[N]] =
     DeriveGen.instance(Gen.double(Double.MinValue, wn.snd).map(Refined.unsafeApply))
