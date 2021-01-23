52f8413423ada3d13df2bd49079a641969fc28b4
Pierangelo Cecchetto <pierangeloc@gmail.com>
2021-01-23 01:14:10 +0100

    test-refined: Return DeriveGen instead of Gen

--- test-refined/shared/src/main/scala/zio/test/refined/NumericInstances.scala
+++ test-refined/shared/src/main/scala/zio/test/refined/NumericInstances.scala
@@ -3,8 +3,8 @@ package zio.test.refined
 import eu.timepit.refined.api.Refined
 import eu.timepit.refined.internal.WitnessAs
 import eu.timepit.refined.numeric.{Greater, Less}
-import zio.random.Random
 import zio.test.Gen
+import zio.test.magnolia.DeriveGen
 
 object numeric extends NumericInstances
 
@@ -12,52 +12,52 @@ trait NumericInstances {
 
   def intGreaterThan[N](implicit
     wn: WitnessAs[N, Int]
-  ): Gen[Random, Int Refined Greater[N]] =
-    Gen.int(wn.snd, Int.MaxValue).map(Refined.unsafeApply)
+  ): DeriveGen[Int Refined Greater[N]] =
+    DeriveGen.instance(Gen.int(wn.snd, Int.MaxValue).map(Refined.unsafeApply))
 
   def longGreaterThan[N](implicit
     wn: WitnessAs[N, Long]
-  ): Gen[Random, Long Refined Greater[N]] =
-    Gen.long(wn.snd, Long.MaxValue).map(Refined.unsafeApply)
+  ): DeriveGen[Long Refined Greater[N]] =
+    DeriveGen.instance(Gen.long(wn.snd, Long.MaxValue).map(Refined.unsafeApply))
 
   def shortGreaterThan[N](implicit
     wn: WitnessAs[N, Short]
-  ): Gen[Random, Short Refined Greater[N]] =
-    Gen.short(wn.snd, Short.MaxValue).map(Refined.unsafeApply)
+  ): DeriveGen[Short Refined Greater[N]] =
+    DeriveGen.instance(Gen.short(wn.snd, Short.MaxValue).map(Refined.unsafeApply))
 
   def byteGreaterThan[N](implicit
     wn: WitnessAs[N, Byte]
-  ): Gen[Random, Byte Refined Greater[N]] =
-    Gen.byte(wn.snd, Byte.MaxValue).map(Refined.unsafeApply)
+  ): DeriveGen[Byte Refined Greater[N]] =
+    DeriveGen.instance(Gen.byte(wn.snd, Byte.MaxValue).map(Refined.unsafeApply))
 
   def doubleGreaterThan[N](implicit
     wn: WitnessAs[N, Double]
-  ): Gen[Random, Double Refined Greater[N]] =
-    Gen.double(wn.snd, Double.MaxValue).map(Refined.unsafeApply)
+  ): DeriveGen[Double Refined Greater[N]] =
+    DeriveGen.instance(Gen.double(wn.snd, Double.MaxValue).map(Refined.unsafeApply))
 
   def intLessThan[N](implicit
     wn: WitnessAs[N, Int]
-  ): Gen[Random, Int Refined Less[N]] =
-    Gen.int(Int.MinValue, wn.snd).map(Refined.unsafeApply)
+  ): DeriveGen[Int Refined Less[N]] =
+    DeriveGen.instance(Gen.int(Int.MinValue, wn.snd).map(Refined.unsafeApply))
 
   def longLessThan[N](implicit
     wn: WitnessAs[N, Long]
-  ): Gen[Random, Long Refined Less[N]] =
-    Gen.long(Long.MinValue, wn.snd).map(Refined.unsafeApply)
+  ): DeriveGen[Long Refined Less[N]] =
+    DeriveGen.instance(Gen.long(Long.MinValue, wn.snd).map(Refined.unsafeApply))
 
   def shortLessThan[N](implicit
     wn: WitnessAs[N, Short]
-  ): Gen[Random, Short Refined Less[N]] =
-    Gen.short(Short.MinValue, wn.snd).map(Refined.unsafeApply)
+  ): DeriveGen[Short Refined Less[N]] =
+    DeriveGen.instance(Gen.short(Short.MinValue, wn.snd).map(Refined.unsafeApply))
 
   def byteLessThan[N](implicit
     wn: WitnessAs[N, Byte]
-  ): Gen[Random, Byte Refined Less[N]] =
-    Gen.byte(Byte.MinValue, wn.snd).map(Refined.unsafeApply)
+  ): DeriveGen[Byte Refined Less[N]] =
+    DeriveGen.instance(Gen.byte(Byte.MinValue, wn.snd).map(Refined.unsafeApply))
 
   def doubleLessThan[N](implicit
     wn: WitnessAs[N, Double]
-  ): Gen[Random, Double Refined Less[N]] =
-    Gen.double(Double.MinValue, wn.snd).map(Refined.unsafeApply)
+  ): DeriveGen[Double Refined Less[N]] =
+    DeriveGen.instance(Gen.double(Double.MinValue, wn.snd).map(Refined.unsafeApply))
 
 }
