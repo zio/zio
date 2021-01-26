6c1e27192271664f11d0f76efb45f63cd6839258
Vladislav Dolbilov <darl@yandex-team.ru>
2021-01-26 15:49:50 +0300

    add failing test

--- core-tests/jvm/src/test/scala/zio/clock/ClockSpecJVM.scala
+++ core-tests/jvm/src/test/scala/zio/clock/ClockSpecJVM.scala
@@ -1,10 +1,11 @@
 package zio.clock
 
+import java.time.Instant
+
 import zio._
 import zio.test.Assertion._
 import zio.test._
 import zio.test.environment.Live
-
 import java.util.concurrent.TimeUnit
 
 object ClockSpecJVM extends ZIOBaseSpec {
@@ -23,6 +24,15 @@ object ClockSpecJVM extends ZIOBaseSpec {
         @@ TestAspect.flaky
         // This test should only run on JRE >= 9, which is when microsecond precision was introduced.
         // Versions of JREs < 9 started with s"1.${majorVersion}", then with JEP 223 they switched to semantic versioning.
-        @@ TestAspect.ifProp("java.version", not(startsWithString("1.")))
+        @@ TestAspect.ifProp("java.version", not(startsWithString("1."))),
+      testM("currentTime has correct time") {
+        val unit = TimeUnit.MICROSECONDS
+        for {
+          start  <- ZIO.effectTotal(Instant.now).map(_.toEpochMilli)
+          time   <- clock.currentTime(unit).map(TimeUnit.MILLISECONDS.convert(_, unit))
+          finish <- ZIO.effectTotal(Instant.now).map(_.toEpochMilli)
+        } yield assert(time)(isGreaterThanEqualTo(start) && isLessThanEqualTo(finish))
+      }.provideLayer(Clock.live)
+        @@ TestAspect.nonFlaky
     )
 }
