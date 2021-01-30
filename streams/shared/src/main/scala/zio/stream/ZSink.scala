00906969788840497531430452ce45b3557efde7
Adam Fraser <adam.fraser@gmail.com>
2021-01-30 12:46:27 -0800

    rename (#4633)

--- streams/shared/src/main/scala/zio/stream/ZSink.scala
+++ streams/shared/src/main/scala/zio/stream/ZSink.scala
@@ -816,6 +816,19 @@ object ZSink extends ZSinkPlatformSpecificConstructors {
     ZSink(Managed.succeed(sink))
 
   /**
+   * Create a sink which enqueues each element into the specified queue.
+   */
+  def fromQueue[R, E, I](queue: ZQueue[R, Nothing, E, Any, I, Any]): ZSink[R, E, I, Nothing, Unit] =
+    foreachChunk(queue.offerAll)
+
+  /**
+   * Create a sink which enqueues each element into the specified queue.
+   * The queue will be shutdown once the stream is closed.
+   */
+  def fromQueueWithShutdown[R, E, I](queue: ZQueue[R, Nothing, E, Any, I, Any]): ZSink[R, E, I, Nothing, Unit] =
+    ZSink(ZManaged.make(ZIO.succeedNow(queue))(_.shutdown).map(fromQueue[R, E, I]).flatMap(_.push))
+
+  /**
    * Creates a sink halting with a specified cause.
    */
   def halt[E](e: => Cause[E]): ZSink[Any, E, Any, Nothing, Nothing] =
@@ -908,19 +921,6 @@ object ZSink extends ZSinkPlatformSpecificConstructors {
    * A sink with timed execution.
    */
   def timed: ZSink[Clock, Nothing, Any, Nothing, Duration] = ZSink.drain.timed.map(_._2)
-
-  /**
-   * Create a sink which enqueues each element into the specified queue.
-   */
-  def toQueue[R, E, I](queue: ZQueue[R, Nothing, E, Any, I, Any]): ZSink[R, E, I, Nothing, Unit] =
-    foreachChunk(queue.offerAll)
-
-  /**
-   * Create a sink which enqueues each element into the specified queue.
-   * The queue will be shutdown once the stream is closed.
-   */
-  def toQueueWithShutdown[R, E, I](queue: ZQueue[R, Nothing, E, Any, I, Any]): ZSink[R, E, I, Nothing, Unit] =
-    ZSink(ZManaged.make(ZIO.succeedNow(queue))(_.shutdown).map(toQueue[R, E, I]).flatMap(_.push))
 
   final class AccessSinkPartiallyApplied[R](private val dummy: Boolean = true) extends AnyVal {
     def apply[E, I, L, Z](f: R => ZSink[R, E, I, L, Z]): ZSink[R, E, I, L, Z] =
