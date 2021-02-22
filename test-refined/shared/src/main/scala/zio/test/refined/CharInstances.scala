bbbb0d1c0f1098af2688c71404e3f039af5a91d9
Maxim Grankin <maximgran@gmail.com>
2021-02-22 10:33:29 +0300

    First fixes for #4600 - moved generators from CharInstances to Gen

--- test-refined/shared/src/main/scala/zio/test/refined/CharInstances.scala
+++ test-refined/shared/src/main/scala/zio/test/refined/CharInstances.scala
@@ -9,34 +9,26 @@ import zio.test.magnolia.DeriveGen
 object char extends CharInstances
 
 trait CharInstances {
-  private val alphaCharGen: Gen[Random, Char] =
-    Gen.weighted(Gen.char(65, 90) -> 26, Gen.char(97, 122) -> 26)
-
-  private val numericCharGen: Gen[Random, Char] =
-    Gen.weighted(Gen.char(48, 57) -> 10)
-
-  private val whitespaceChars: Seq[Char] =
-    (Char.MinValue to Char.MaxValue).filter(_.isWhitespace)
 
   implicit def digitArbitrary: DeriveGen[Refined[Char, Digit]] =
-    DeriveGen.instance(numericCharGen.map(value => Refined.unsafeApply(value)))
+    DeriveGen.instance(Gen.numericChar.map(value => Refined.unsafeApply(value)))
 
   implicit def letterDeriveGen: DeriveGen[Refined[Char, Letter]] =
-    DeriveGen.instance(alphaCharGen.map(value => Refined.unsafeApply(value)))
+    DeriveGen.instance(Gen.alphaChar.map(value => Refined.unsafeApply(value)))
 
   implicit def lowerCaseDeriveGen: DeriveGen[Refined[Char, LowerCase]] =
     DeriveGen.instance(
-      alphaCharGen.map(value => Refined.unsafeApply(value.toLower))
+      Gen.alphaChar.map(value => Refined.unsafeApply(value.toLower))
     )
 
   implicit def upperCaseDeriveGen: DeriveGen[Refined[Char, UpperCase]] =
     DeriveGen.instance(
-      alphaCharGen.map(value => Refined.unsafeApply(value.toUpper))
+      Gen.alphaChar.map(value => Refined.unsafeApply(value.toUpper))
     )
 
   implicit def whitespaceDeriveGen: DeriveGen[Refined[Char, Whitespace]] = {
     val whiteSpaceGens: Seq[Gen[Random, Char]] =
-      whitespaceChars.map(Gen.const(_))
+      Gen.whitespaceChars.map(Gen.const(_))
 
     DeriveGen.instance(
       Gen
