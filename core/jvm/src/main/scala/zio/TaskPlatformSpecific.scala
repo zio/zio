d79eadde097f0873f49448a5272948ac7a45a9dd
sken <sken77@pm.me>
2021-03-23 12:34:54 -0700

    lint issue

--- core/jvm/src/main/scala/zio/TaskPlatformSpecific.scala
+++ core/jvm/src/main/scala/zio/TaskPlatformSpecific.scala
@@ -25,7 +25,7 @@ private[zio] trait TaskPlatformSpecific {
   def effectAsyncWithCompletionHandler[T](op: CompletionHandler[T, Any] => Any): Task[T] =
     javaz.effectAsyncWithCompletionHandler(op)
 
-  /** Alias for `formCompletionStage` for a concrete implementation of CompletionStage*/
+  /** Alias for `formCompletionStage` for a concrete implementation of CompletionStage */
   def fromCompletableFuture[A](cs: => CompletableFuture[A]): Task[A] = fromCompletionStage(cs)
 
   def fromCompletionStage[A](cs: => CompletionStage[A]): Task[A] = javaz.fromCompletionStage(cs)
