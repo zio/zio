84ec1747a5986e0a31f42e58c2f2ab2e4c022fe4
Ondra Pelech <ondra.pelech@gmail.com>
2021-04-04 15:40:42 +0200

    Scala 3.0.0-RC2

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -12,7 +12,7 @@ object BuildHelper {
   val Scala211   = "2.11.12"
   val Scala212   = "2.12.13"
   val Scala213   = "2.13.5"
-  val ScalaDotty = "3.0.0-RC1"
+  val ScalaDotty = "3.0.0-RC2"
 
   val SilencerVersion = "1.7.3"
 
