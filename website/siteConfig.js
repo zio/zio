9f75724f39877d0fb4adb5ef57dba2b2ea3799df
Milad Khajavi <khajavi@gmail.com>
2021-04-05 17:57:36 +0430

    enable edit button for each page.

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -8,6 +8,8 @@
 // See https://docusaurus.io/docs/site-config for all the possible
 // site configuration options.
 
+const repoUrl = "https://github.com/zio/zio";
+
 // List of projects/orgs using your project for the users page.
 const users = [
   /*
@@ -114,6 +116,9 @@ const siteConfig = {
   },
   
   docsSideNavCollapsible: true,
+  
+  editUrl: `${repoUrl}/edit/master/docs/`, 
+  
 };
 
 module.exports = siteConfig;
