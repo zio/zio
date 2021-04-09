e3107dbb49447dda7cbd9a96893239429aa521df
Dejan Mijic <dmijic@acm.org>
2021-04-09 18:49:10 +0200

    Add remark about chunk size

--- streams/shared/src/main/scala/zio/stream/experimental/ZSink.scala
+++ streams/shared/src/main/scala/zio/stream/experimental/ZSink.scala
@@ -343,7 +343,8 @@ object ZSink {
   }
 
   /**
-   * A sink that collects all of its inputs into chunks of maximum size `n`.
+   * A sink that collects all of its inputs into chunks of maximum size `n`. Note
+   * that the chunks are preallocated and must fit in memory.
    */
   def collectAllN[Err, In](n: Int): ZSink[Any, Err, In, Err, In, Chunk[In]] =
     fromEffect(UIO(ChunkBuilder.make[In](n)))
