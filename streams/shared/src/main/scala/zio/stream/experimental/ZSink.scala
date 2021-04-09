5df7a92c550967bf1a7f675155b7a2525efe6a14
Dejan Mijic <dmijic@acm.org>
2021-04-09 17:10:43 +0200

    Use `:+` instead of `appended`

--- streams/shared/src/main/scala/zio/stream/experimental/ZSink.scala
+++ streams/shared/src/main/scala/zio/stream/experimental/ZSink.scala
@@ -346,7 +346,7 @@ object ZSink {
    * A sink that collects all of its inputs into chunks of maximum size `n`.
    */
   def collectAllN[Err, In](n: Int): ZSink[Any, Err, In, Err, In, Chunk[In]] =
-    foldUntil(Chunk.empty: Chunk[In], n.toLong)(_ appended _)
+    foldUntil(Chunk.empty: Chunk[In], n.toLong)(_ :+ _)
 
   /**
    * A sink that collects all of its inputs into a map. The keys are extracted from inputs
