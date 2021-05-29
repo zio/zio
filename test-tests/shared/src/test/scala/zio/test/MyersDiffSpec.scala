92d6a8533e7d2e0d19b7ff34728fbe1ad6e6c0d1
Girish Kolanthra Ramadevan <girishkolantra@gmail.com>
2021-05-29 12:22:45 -0700

    First cut of the Myers diff algorith

---
+++ test-tests/shared/src/test/scala/zio/test/MyersDiffSpec.scala
@@ -0,0 +1,67 @@
+package zio.test
+
+import zio.Chunk
+import zio.test.Assertion._
+
+object MyersDiffSpec extends ZIOBaseSpec {
+
+  def spec: ZSpec[Environment, Failure] = suite("MyersDiffSpec")(
+    test("diffing works for only additions both ways") {
+      val original = ""
+      val modified = "ADDITIONS"
+
+      assert(MyersDiff.diff(original, modified).applyChanges(original))(equalTo(modified))
+      assert(MyersDiff.diff(original, modified).invert.applyChanges(modified))(equalTo(original))
+    },
+    test("diffing works for only deletions both ways") {
+      val original = "DELETIONS"
+      val modified = ""
+
+      assert(MyersDiff.diff(original, modified).applyChanges(original))(equalTo(modified))
+      assert(MyersDiff.diff(original, modified).invert.applyChanges(modified))(equalTo(original))
+    },
+    test("diffing works for two empty strings both ways") {
+      val original = ""
+      val modified = ""
+
+      assert(MyersDiff.diff(original, modified).applyChanges(original))(equalTo(modified))
+      assert(MyersDiff.diff(original, modified).invert.applyChanges(modified))(equalTo(original))
+    },
+    test("diffing works for Myers example both ways") {
+      val original = "ABCABBA"
+      val modified = "CBABAC"
+
+      assert(MyersDiff.diff(original, modified).applyChanges(original))(equalTo(modified))
+      assert(MyersDiff.diff(original, modified).invert.applyChanges(modified))(equalTo(original))
+    },
+    test("diffing for Myers example produces a sane DiffResult") {
+      val original = "ABCABBA"
+      val modified = "CBABAC"
+
+      import Action._
+      assert(MyersDiff.diff(original, modified))(
+        equalTo(
+          DiffResult(
+            Chunk(
+              Insert("C"),
+              Delete("A"),
+              Keep("B"),
+              Delete("C"),
+              Keep("A"),
+              Keep("B"),
+              Delete("B"),
+              Keep("A"),
+              Insert("C")
+            )
+          )
+        )
+      )
+    },
+    testM("diffing works for all random strings both ways") {
+      check(Gen.anyString, Gen.anyString) { (original, modified) =>
+        assert(MyersDiff.diff(original, modified).applyChanges(original))(equalTo(modified))
+        assert(MyersDiff.diff(original, modified).invert.applyChanges(modified))(equalTo(original))
+      }
+    }
+  )
+}
