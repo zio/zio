66315ee57b560a89a37a0868759dae21cc4e8b51
Paul Daniels <paul@leadiq.com>
2021-06-04 15:41:50 +0800

    Implement ZStream#effectAsync* operators (#5053)

---
+++ streams-tests/jvm/src/test/scala/zio/stream/experimental/ZStreamPlatformSpecificSpec.scala
@@ -0,0 +1,167 @@
+package zio.stream.experimental
+
+import zio.test.Assertion.{containsCause, equalTo, failsCause, isFalse, isTrue}
+import zio.test.{Gen, ZSpec, assert, assertM, checkM}
+import zio.{Cause, Chunk, IO, Promise, Ref, Task, UIO, ZIO, ZIOBaseSpec}
+
+import scala.concurrent.ExecutionContext.global
+
+object ZStreamPlatformSpecificSpec extends ZIOBaseSpec {
+  def spec: ZSpec[Environment, Failure] = suite("ZStream JVM experimental")(
+    suite("Constructors")(
+      suite("effectAsyncMaybe")(
+        testM("effectAsyncMaybe signal end stream") {
+          for {
+            result <- ZStream
+                        .effectAsyncMaybe[Any, Nothing, Int] { k =>
+                          k(IO.fail(None))
+                          None
+                        }
+                        .runCollect
+          } yield assert(result)(equalTo(Chunk.empty))
+        },
+        testM("effectAsyncMaybe Some")(checkM(Gen.chunkOf(Gen.anyInt)) { chunk =>
+          val s = ZStream.effectAsyncMaybe[Any, Throwable, Int](_ => Some(ZStream.fromIterable(chunk)))
+
+          assertM(s.runCollect.map(_.take(chunk.size)))(equalTo(chunk))
+        }),
+        testM("effectAsyncMaybe None")(checkM(Gen.chunkOf(Gen.anyInt)) { chunk =>
+          val s = ZStream.effectAsyncMaybe[Any, Throwable, Int] { k =>
+            global.execute(() => chunk.foreach(a => k(Task.succeed(Chunk.single(a)))))
+            None
+          }
+
+          assertM(s.take(chunk.size.toLong).runCollect)(equalTo(chunk))
+        }),
+        testM("effectAsyncMaybe back pressure") {
+          for {
+            refCnt  <- Ref.make(0)
+            refDone <- Ref.make[Boolean](false)
+            stream = ZStream.effectAsyncMaybe[Any, Throwable, Int](
+                       cb => {
+                         global.execute { () =>
+                           // 1st consumed by sink, 2-6 – in queue, 7th – back pressured
+                           (1 to 7).foreach(i => cb(refCnt.set(i) *> ZIO.succeedNow(Chunk.single(1))))
+                           cb(refDone.set(true) *> ZIO.fail(None))
+                         }
+                         None
+                       },
+                       5
+                     )
+            run    <- stream.run(ZSink.take(1) *> ZSink.never).fork
+            _      <- refCnt.get.repeatWhile(_ != 7)
+            isDone <- refDone.get
+            _      <- run.interrupt
+          } yield assert(isDone)(isFalse)
+        }
+      ),
+      suite("effectAsyncM")(
+        testM("effectAsyncM")(checkM(Gen.chunkOf(Gen.anyInt).filter(_.nonEmpty)) { chunk =>
+          for {
+            latch <- Promise.make[Nothing, Unit]
+            fiber <- ZStream
+                       .effectAsyncM[Any, Throwable, Int] { k =>
+                         global.execute(() => chunk.foreach(a => k(Task.succeed(Chunk.single(a)))))
+                         latch.succeed(()) *>
+                           Task.unit
+                       }
+                       .take(chunk.size.toLong)
+                       .run(ZSink.collectAll[Throwable, Int])
+                       .fork
+            _ <- latch.await
+            s <- fiber.join
+          } yield assert(s)(equalTo(chunk))
+        }),
+        testM("effectAsyncM signal end stream") {
+          for {
+            result <- ZStream
+                        .effectAsyncM[Any, Nothing, Int] { k =>
+                          global.execute(() => k(IO.fail(None)))
+                          UIO.unit
+                        }
+                        .runCollect
+          } yield assert(result)(equalTo(Chunk.empty))
+        },
+        testM("effectAsyncM back pressure") {
+          for {
+            refCnt  <- Ref.make(0)
+            refDone <- Ref.make[Boolean](false)
+            stream = ZStream.effectAsyncM[Any, Throwable, Int](
+                       cb => {
+                         global.execute { () =>
+                           println("Rawr")
+                           // 1st consumed by sink, 2-6 – in queue, 7th – back pressured
+                           (1 to 7).foreach(i => cb(refCnt.set(i) *> ZIO.succeedNow(Chunk.single(1))))
+                           cb(refDone.set(true) *> ZIO.fail(None))
+                         }
+                         UIO.unit
+                       },
+                       5
+                     )
+            run    <- stream.run(ZSink.take(1) *> ZSink.never).fork
+            _      <- refCnt.get.repeatWhile(_ != 7)
+            isDone <- refDone.get
+            _      <- run.interrupt
+          } yield assert(isDone)(isFalse)
+        }
+      ),
+      suite("effectAsyncInterrupt")(
+        testM("effectAsyncInterrupt Left") {
+          for {
+            cancelled <- Ref.make(false)
+            latch     <- Promise.make[Nothing, Unit]
+            fiber <- ZStream
+                       .effectAsyncInterrupt[Any, Nothing, Unit] { offer =>
+                         global.execute(() => offer(ZIO.succeedNow(Chunk.unit)))
+                         Left(cancelled.set(true))
+                       }
+                       .tap(_ => latch.succeed(()))
+                       .runDrain
+                       .fork
+            _      <- latch.await
+            _      <- fiber.interrupt
+            result <- cancelled.get
+          } yield assert(result)(isTrue)
+        },
+        testM("effectAsyncInterrupt Right")(checkM(Gen.chunkOf(Gen.anyInt)) { chunk =>
+          val s = ZStream.effectAsyncInterrupt[Any, Throwable, Int](_ => Right(ZStream.fromIterable(chunk)))
+
+          assertM(s.take(chunk.size.toLong).runCollect)(equalTo(chunk))
+        }),
+        testM("effectAsyncInterrupt signal end stream ") {
+          for {
+            result <- ZStream
+                        .effectAsyncInterrupt[Any, Nothing, Int] { k =>
+                          global.execute(() => k(IO.fail(None)))
+                          Left(UIO.succeedNow(()))
+                        }
+                        .runCollect
+          } yield assert(result)(equalTo(Chunk.empty))
+        },
+        testM("effectAsyncInterrupt back pressure") {
+          for {
+            selfId  <- ZIO.fiberId
+            refCnt  <- Ref.make(0)
+            refDone <- Ref.make[Boolean](false)
+            stream = ZStream.effectAsyncInterrupt[Any, Throwable, Int](
+                       cb => {
+                         global.execute { () =>
+                           // 1st consumed by sink, 2-6 – in queue, 7th – back pressured
+                           (1 to 7).foreach(i => cb(refCnt.set(i) *> ZIO.succeedNow(Chunk.single(1))))
+                           cb(refDone.set(true) *> ZIO.fail(None))
+                         }
+                         Left(UIO.unit)
+                       },
+                       5
+                     )
+            run    <- stream.run(ZSink.take(1) *> ZSink.never).fork
+            _      <- refCnt.get.repeatWhile(_ != 7)
+            isDone <- refDone.get
+            exit   <- run.interrupt
+          } yield assert(isDone)(isFalse) &&
+            assert(exit.untraced)(failsCause(containsCause(Cause.interrupt(selfId))))
+        }
+      )
+    )
+  )
+}
---
+++ streams/native/src/main/scala/zio/stream/experimental/platform.scala
@@ -0,0 +1,118 @@
+package zio.stream.experimental
+
+import zio.{Canceler, Chunk, FiberFailure, Queue, UIO, ZIO, ZManaged, stream}
+
+import scala.concurrent.Future
+
+trait ZStreamPlatformSpecificConstructors {
+  self: ZStream.type =>
+
+  /**
+   * Creates a stream from an asynchronous callback that can be called multiple times.
+   * The optionality of the error type `E` can be used to signal the end of the stream,
+   * by setting it to `None`.
+   */
+  def effectAsync[R, E, A](
+    register: (ZIO[R, Option[E], Chunk[A]] => Future[Boolean]) => Unit,
+    outputBuffer: Int = 16
+  ): ZStream[R, E, A] =
+    effectAsyncMaybe(
+      callback => {
+        register(callback)
+        None
+      },
+      outputBuffer
+    )
+
+  /**
+   * Creates a stream from an asynchronous callback that can be called multiple times.
+   * The registration of the callback returns either a canceler or synchronously returns a stream.
+   * The optionality of the error type `E` can be used to signal the end of the stream, by
+   * setting it to `None`.
+   */
+  def effectAsyncInterrupt[R, E, A](
+    register: (ZIO[R, Option[E], Chunk[A]] => Future[Boolean]) => Either[Canceler[R], ZStream[R, E, A]],
+    outputBuffer: Int = 16
+  ): ZStream[R, E, A] =
+    ZStream.unwrapManaged(for {
+      output  <- Queue.bounded[stream.Take[E, A]](outputBuffer).toManaged(_.shutdown)
+      runtime <- ZManaged.runtime[R]
+      eitherStream <- ZManaged.effectTotal {
+                        register { k =>
+                          try {
+                            runtime.unsafeRunToFuture(stream.Take.fromPull(k).flatMap(output.offer))
+                          } catch {
+                            case FiberFailure(c) if c.interrupted =>
+                              Future.successful(false)
+                          }
+                        }
+                      }
+    } yield {
+      eitherStream match {
+        case Right(value) => ZStream.unwrap(output.shutdown as value)
+        case Left(canceler) =>
+          lazy val loop: ZChannel[Any, Any, Any, Any, E, Chunk[A], Unit] =
+            ZChannel.unwrap(
+              output.take
+                .flatMap(_.done)
+                .fold(
+                  maybeError =>
+                    ZChannel.fromEffect(output.shutdown) *>
+                      maybeError
+                        .fold[ZChannel[Any, Any, Any, Any, E, Chunk[A], Unit]](ZChannel.end(()))(ZChannel.fail(_)),
+                  a => ZChannel.write(a) *> loop
+                )
+            )
+
+          new ZStream(loop).ensuring(canceler)
+      }
+    })
+
+  /**
+   * Creates a stream from an asynchronous callback that can be called multiple times
+   * The registration of the callback itself returns an effect. The optionality of the
+   * error type `E` can be used to signal the end of the stream, by setting it to `None`.
+   */
+  def effectAsyncM[R, E, A](
+    register: (ZIO[R, Option[E], Chunk[A]] => Future[Boolean]) => ZIO[R, E, Any],
+    outputBuffer: Int = 16
+  ): ZStream[R, E, A] =
+    new ZStream(ZChannel.unwrapManaged(for {
+      output  <- Queue.bounded[stream.Take[E, A]](outputBuffer).toManaged(_.shutdown)
+      runtime <- ZManaged.runtime[R]
+      _ <- register { k =>
+             try {
+               runtime.unsafeRunToFuture(stream.Take.fromPull(k).flatMap(output.offer))
+             } catch {
+               case FiberFailure(c) if c.interrupted =>
+                 Future.successful(false)
+             }
+           }.toManaged_
+    } yield {
+      lazy val loop: ZChannel[Any, Any, Any, Any, E, Chunk[A], Unit] = ZChannel.unwrap(
+        output.take
+          .flatMap(_.done)
+          .fold(
+            maybeError =>
+              ZChannel.fromEffect(output.shutdown) *>
+                maybeError.fold[ZChannel[Any, Any, Any, Any, E, Chunk[A], Unit]](ZChannel.end(()))(ZChannel.fail(_)),
+            a => ZChannel.write(a) *> loop
+          )
+      )
+
+      loop
+    }))
+
+  /**
+   * Creates a stream from an asynchronous callback that can be called multiple times.
+   * The registration of the callback can possibly return the stream synchronously.
+   * The optionality of the error type `E` can be used to signal the end of the stream,
+   * by setting it to `None`.
+   */
+  def effectAsyncMaybe[R, E, A](
+    register: (ZIO[R, Option[E], Chunk[A]] => Future[Boolean]) => Option[ZStream[R, E, A]],
+    outputBuffer: Int = 16
+  ): ZStream[R, E, A] =
+    effectAsyncInterrupt(k => register(k).toRight(UIO.unit), outputBuffer)
+
+}
