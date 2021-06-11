196785f7e0ee36304a96c7d3f6a3cc71113bb02b
Itamar Ravid <iravid@iravid.com>
2021-06-11 12:35:05 +0300

    Add ZStream#collectType to the new encoding

--- streams/shared/src/main/scala/zio/stream/experimental/ZStream.scala
+++ streams/shared/src/main/scala/zio/stream/experimental/ZStream.scala
@@ -4142,6 +4142,14 @@ object ZStream extends ZStreamPlatformSpecificConstructors {
       self.refineOrDie { case e: E1 => e }
   }
 
+  implicit final class SyntaxOps[-R, +E, O](self: ZStream[R, E, O]) {
+    /*
+     * Collect elements of the given type flowing through the stream, and filters out others.
+     */
+    def collectType[O1 <: O](implicit tag: ClassTag[O1]): ZStream[R, E, O1] =
+      self.collect({ case o if tag.runtimeClass.isInstance(o) => o.asInstanceOf[O1] })
+  }
+
   private[zio] class Rechunker[A](n: Int) {
     private var builder: ChunkBuilder[A] = ChunkBuilder.make(n)
     private var pos: Int                 = 0
