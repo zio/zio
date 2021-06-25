289e9c5c2626afb947f98988de1170f8be067d4e
Adam Fraser <adam.fraser@gmail.com>
2021-06-25 06:03:59 -0700

    format

--- core-tests/shared/src/test/scala/zio/ZIOConstructorSpec.scala
+++ core-tests/shared/src/test/scala/zio/ZIOConstructorSpec.scala
@@ -4,22 +4,23 @@ import zio.test._
 
 object ZIOConstructorSpec extends ZIOBaseSpec {
 
-  def spec = suite("ZIOConstructorSpec")(
-    test("either") {
-      val either: Either[Int, String] = Left(12)
-      val zio                         = ZIO(either)
-      val _: ZIO[Any, Int, String]    = zio
-      assertCompletes
-    },
-    test("function") {
-      val zio                       = ZIO((x: Int) => x * x)
-      val _: ZIO[Int, Nothing, Int] = zio
-      assertCompletes
-    },
-    test("left") {
-      val zio                       = ZIO(Left(12))
-      val _: ZIO[Any, Int, Nothing] = zio
-      assertCompletes
-    }
-  )
+  def spec: ZSpec[Environment, Failure] =
+    suite("ZIOConstructorSpec")(
+      test("either") {
+        val either: Either[Int, String] = Left(12)
+        val zio                         = ZIO(either)
+        val _: ZIO[Any, Int, String]    = zio
+        assertCompletes
+      },
+      test("function") {
+        val zio                       = ZIO((x: Int) => x * x)
+        val _: ZIO[Int, Nothing, Int] = zio
+        assertCompletes
+      },
+      test("left") {
+        val zio                       = ZIO(Left(12))
+        val _: ZIO[Any, Int, Nothing] = zio
+        assertCompletes
+      }
+    )
 }
--- core/shared/src/main/scala/zio/ZIOConstructor.scala
+++ core/shared/src/main/scala/zio/ZIOConstructor.scala
@@ -5,37 +5,25 @@ import scala.util.Try
 sealed trait ZIOConstructor[Input] {
   type Out
 
-  def make(input: Input): Out
+  def make(input: => Input): Out
 }
 object ZIOConstructor extends ZIOConstructorLowPriority {
 
   implicit def EitherConstructor[E, A]: WithOut[Either[E, A], ZIO[Any, E, A]] = new ZIOConstructor[Either[E, A]] {
     type Out = ZIO[Any, E, A]
-    def make(input: Either[E, A]): ZIO[Any, E, A] =
+    def make(input: => Either[E, A]): ZIO[Any, E, A] =
       ZIO.fromEither(input)
   }
 
   implicit def FunctionConstructor[A, B]: WithOut[A => B, ZIO[A, Nothing, B]] = new ZIOConstructor[A => B] {
     type Out = ZIO[A, Nothing, B]
-    def make(input: (A => B)): ZIO[A, Nothing, B] =
+    def make(input: => (A => B)): ZIO[A, Nothing, B] =
       ZIO.fromFunction(input)
   }
 
-  // implicit def LeftConstructor[E, A]: WithOut[Left[E, A], ZIO[Any, E, A]] = new ZIOConstructor[Left[E, A]] {
-  //   type Out = ZIO[Any, E, A]
-  //   def make(input: Left[E, A]): ZIO[Any, E, A] =
-  //     ZIO.fromEither(input)
-  // }
-
-  // implicit def RightConstructor[E, A]: WithOut[Right[E, A], ZIO[Any, E, A]] = new ZIOConstructor[Right[E, A]] {
-  //   type Out = ZIO[Any, E, A]
-  //   def make(input: Right[E, A]): ZIO[Any, E, A] =
-  //     ZIO.fromEither(input)
-  // }
-
   implicit def TryConstructor[A]: WithOut[Try[A], ZIO[Any, Throwable, A]] = new ZIOConstructor[Try[A]] {
     type Out = ZIO[Any, Throwable, A]
-    def make(input: Try[A]): ZIO[Any, Throwable, A] =
+    def make(input: => Try[A]): ZIO[Any, Throwable, A] =
       ZIO.fromTry(input)
   }
 }
@@ -45,6 +33,7 @@ trait ZIOConstructorLowPriority {
 
   implicit def AttemptConstructor[A]: WithOut[A, ZIO[Any, Throwable, A]] = new ZIOConstructor[A] {
     type Out = ZIO[Any, Throwable, A]
-    def make(input: A): ZIO[Any, Throwable, A] = ZIO.attempt(input)
+    def make(input: => A): ZIO[Any, Throwable, A] =
+      ZIO.attempt(input)
   }
 }
