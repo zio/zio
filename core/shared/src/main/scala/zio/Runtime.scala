cab227851728e78f1573ebb9a3e2087b97eb01a6
Tushar Mathur <tusharmath@gmail.com>
2021-07-18 00:15:00 +0530

    Update Runtime.scala

--- core/shared/src/main/scala/zio/Runtime.scala
+++ core/shared/src/main/scala/zio/Runtime.scala
@@ -102,8 +102,7 @@ trait Runtime[+R] {
    * This method is effectful and should only be invoked at the edges of your program.
    */
   final def unsafeRunAsyncCancelable[E, A](zio: => ZIO[R, E, A])(k: Exit[E, A] => Any): Fiber.Id => Exit[E, A] = {
-    lazy val curZio = if (Platform.isJVM) ZIO.yieldNow *> zio else zio
-    val canceler    = unsafeRunWith(curZio)(k)
+    val canceler    = unsafeRunWith(zio)(k)
     fiberId => {
       val result = internal.OneShot.make[Exit[E, A]]
       canceler(fiberId)(result.set)
