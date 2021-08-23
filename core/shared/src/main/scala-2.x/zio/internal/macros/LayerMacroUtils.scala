30ff90eb6dfd39f320795c97d6b10170287bb77d
Kit Langton <kit.langton@gmail.com>
2021-08-23 13:03:42 -0700

    implement extensible ZLayer debugging

--- core/shared/src/main/scala-2.x/zio/internal/macros/LayerMacroUtils.scala
+++ core/shared/src/main/scala-2.x/zio/internal/macros/LayerMacroUtils.scala
@@ -3,6 +3,8 @@ package zio.internal.macros
 import zio._
 import zio.internal.ansi.AnsiStringOps
 
+import java.nio.charset.StandardCharsets
+import java.util.Base64
 import scala.reflect.macros.blackbox
 
 private[zio] trait LayerMacroUtils {
@@ -68,30 +70,81 @@ private[zio] trait LayerMacroUtils {
   def isValidHasType(tpe: Type): Boolean =
     tpe.isHas || tpe.isAny
 
-  def injectBaseImpl[F[_, _, _], R: c.WeakTypeTag, E, A](
-    layers: Seq[c.Expr[ZLayer[_, E, _]]],
-    method: String
-  ): c.Expr[F[Any, E, A]] = {
-    assertProperVarArgs(layers)
-    val expr = buildMemoizedLayer(generateExprGraph(layers), getRequirements[R])
-    c.Expr[F[Any, E, A]](q"${c.prefix}.${TermName(method)}(${expr.tree})")
-  }
-
-  def injectSomeBaseImpl[F[_, _, _], R0: c.WeakTypeTag, R: c.WeakTypeTag, E, A](
+  def injectBaseImpl[F[_, _, _], R0: c.WeakTypeTag, R: c.WeakTypeTag, E, A](
     layers: Seq[c.Expr[ZLayer[_, E, _]]],
     method: String
   ): c.Expr[F[R0, E, A]] = {
-    assertProperVarArgs(layers)
-    val remainderRequirements = getRequirements[R0]
+    val expr = constructLayer[R0, R, E](layers)
+    c.Expr[F[R0, E, A]](q"${c.prefix}.${TermName(method)}(${expr.tree})")
+  }
+
+  def constructLayer[R0: c.WeakTypeTag, R: c.WeakTypeTag, E](
+    layers0: Seq[c.Expr[ZLayer[_, E, _]]]
+  ): c.Expr[ZLayer[Any, E, R]] = {
+    assertProperVarArgs(layers0)
+
+    val debug = layers0.collectFirst {
+      _.tree match {
+        case q"zio.ZLayer.Debug.tree"    => ZLayer.Debug.Tree
+        case q"zio.ZLayer.Debug.mermaid" => ZLayer.Debug.Mermaid
+      }
+    }
+    val layers = layers0.filter {
+      _.tree match {
+        case q"zio.ZLayer.Debug.tree" | q"zio.ZLayer.Debug.mermaid" => false
+        case _                                                      => true
+      }
+    }
+
     val remainderExpr =
       if (weakTypeOf[R0] =:= weakTypeOf[ZEnv]) reify(ZEnv.any)
       else reify(ZLayer.environment[R0])
+    val remainderNode =
+      if (weakTypeOf[R0] =:= weakTypeOf[Any]) List.empty
+      else List(Node(List.empty, getRequirements[R0], remainderExpr))
+    val nodes = remainderNode ++ layers.map(getNode)
 
-    val remainderNode = Node(List.empty, remainderRequirements, remainderExpr)
-    val nodes         = (remainderNode +: layers.map(getNode)).toList
+    val graph        = generateExprGraph(nodes)
+    val requirements = getRequirements[R]
+    val expr         = buildMemoizedLayer(graph, requirements)
+    debug.foreach { debug =>
+      debugLayers(debug, graph, requirements)
+    }
+    expr.asInstanceOf[c.Expr[ZLayer[Any, E, R]]]
+  }
 
-    val layerExpr = buildMemoizedLayer(generateExprGraph(nodes), getRequirements[R])
-    c.Expr[F[R0, E, A]](q"${c.prefix}.${TermName(method)}(${layerExpr.tree})")
+  private def debugLayers(
+    debug: ZLayer.Debug,
+    graph: ZLayerExprBuilder[c.Type, LayerExpr],
+    requirements: List[c.Type]
+  ): Unit = {
+    val graphString: String =
+      eitherToOption(
+        graph.graph
+          .map(layer => RenderedGraph(layer.showTree))
+          .buildComplete(requirements)
+      ).get
+        .fold[RenderedGraph](RenderedGraph.Row(List.empty), identity, _ ++ _, _ >>> _)
+        .render
+
+    val title   = "  ZLayer Wiring Graph  ".yellow.bold.inverted
+    val builder = new StringBuilder
+    builder ++= "\n" + title + "\n\n" + graphString + "\n\n"
+
+    if (debug == ZLayer.Debug.Mermaid) {
+      val mermaidLink: String = generateMermaidJsLink(requirements, graph)
+      builder ++= "Mermaid Live Editor Link".underlined + "\n" + mermaidLink.faint + "\n\n"
+    }
+
+    c.info(c.enclosingPosition, builder.result(), true)
+  }
+
+  /**
+   * Scala 2.11 doesn't have `Either.toOption`
+   */
+  private def eitherToOption[A](either: Either[_, A]): Option[A] = either match {
+    case Left(_)      => None
+    case Right(value) => Some(value)
   }
 
   def getRequirements(tpe: Type): List[c.Type] = {
@@ -143,6 +196,52 @@ private[zio] trait LayerMacroUtils {
   implicit class TreeOps(self: c.Expr[_]) {
     def showTree: String = CleanCodePrinter.show(c)(self.tree)
   }
+
+  /**
+   * Generates a link of the Layer graph for the Mermaid.js graph viz
+   * library's live-editor (https://mermaid-js.github.io/mermaid-live-editor)
+   */
+  private def generateMermaidJsLink[R <: Has[_]: c.WeakTypeTag, R0: c.WeakTypeTag, E](
+    requirements: List[c.Type],
+    graph: ZLayerExprBuilder[c.Type, LayerExpr]
+  ): String = {
+    val cool = eitherToOption(
+      graph.graph
+        .map(layer => layer.showTree)
+        .buildComplete(requirements)
+    ).get
+
+    val map = cool.fold[Map[String, Chunk[String]]](
+      z = Map.empty,
+      value = str => Map(str -> Chunk.empty),
+      composeH = _ ++ _,
+      composeV = (m1, m2) =>
+        m2.map { case (key, values) =>
+          val result = m1.keys.toSet -- m1.values.flatten.toSet
+          key -> (values ++ Chunk.fromIterable(result))
+        } ++ m1
+    )
+
+    val mermaidGraphString = map.flatMap {
+      case (key, children) if children.isEmpty =>
+        List(s"    $key")
+      case (key, children) =>
+        children.map { child =>
+          s"    $key --> $child"
+        }
+    }
+      .mkString("\\n")
+
+    val mermaidGraph =
+      s"""{"code":"graph\\n$mermaidGraphString\\n    ","mermaid": "{\\n  \\"theme\\": \\"default\\"\\n}", "updateEditor": true, "autoSync": true, "updateDiagram": true}"""
+
+    val encodedMermaidGraph: String =
+      new String(Base64.getEncoder.encode(mermaidGraph.getBytes(StandardCharsets.UTF_8)), StandardCharsets.UTF_8)
+
+    val mermaidLink = s"https://mermaid-js.github.io/mermaid-live-editor/edit/#$encodedMermaidGraph"
+    mermaidLink
+  }
+
 }
 
 trait ExprGraphCompileVariants {}
--- core/shared/src/main/scala-2.x/zio/internal/macros/WireMacros.scala
+++ core/shared/src/main/scala-2.x/zio/internal/macros/WireMacros.scala
@@ -1,7 +1,6 @@
 package zio.internal.macros
 
 import zio.internal.ansi.AnsiStringOps
-import zio.internal.macros.StringUtils.StringOps
 import zio.{Has, ZLayer}
 
 import scala.reflect.macros.blackbox
@@ -19,76 +18,15 @@ final class WireMacros(val c: blackbox.Context) extends LayerMacroUtils {
   ): c.Expr[ZLayer[R0, E, R]] = {
     val _ = (dummyK, dummyKRemainder)
     assertEnvIsNotNothing[R]()
-    assertProperVarArgs(layers)
-
-    val deferredRequirements = getRequirements[R0]
-    val requirements         = getRequirements[R] diff deferredRequirements
-
-    val deferredLayer =
-      if (deferredRequirements.nonEmpty) Seq(Node(List.empty, deferredRequirements, reify(ZLayer.environment[R0])))
-      else Nil
-
-    val nodes = (deferredLayer ++ layers.map(getNode)).toList
-
-    buildMemoizedLayer(generateExprGraph(nodes), deferredRequirements ++ requirements)
-      .asInstanceOf[c.Expr[ZLayer[R0, E, R]]]
-  }
-
-  def wireDebugImpl[
-    E,
-    R0: c.WeakTypeTag,
-    R <: Has[_]: c.WeakTypeTag
-  ](layers: c.Expr[ZLayer[_, E, _]]*)(
-    dummyKRemainder: c.Expr[DummyK[R0]],
-    dummyK: c.Expr[DummyK[R]]
-  ): c.Expr[ZLayer[R0, E, R]] = {
-    val _ = (dummyK, dummyKRemainder)
-    assertEnvIsNotNothing[R]()
-    assertProperVarArgs(layers)
-
-    val deferredRequirements = getRequirements[R0]
-    val requirements         = getRequirements[R] diff deferredRequirements
-
-    val deferredLayer =
-      if (deferredRequirements.isEmpty) List.empty
-      else List(Node(List.empty, deferredRequirements, reify(ZLayer.environment[R0])))
-    val nodes = deferredLayer ++ layers.map(getNode)
-
-    val graph = generateExprGraph(nodes)
-    graph.buildLayerFor(requirements)
-
-    val graphString: String = {
-      eitherToOption(
-        graph.graph
-          .map(layer => RenderedGraph(layer.showTree))
-          .buildComplete(requirements)
-      ).get
-        .fold[RenderedGraph](RenderedGraph.Row(List.empty), identity, _ ++ _, _ >>> _)
-        .render
-    }
-
-    val maxWidth = graphString.maxLineWidth
-    val title    = "  ZLayer Wiring Graph  ".yellow.bold.inverted
-    val adjust   = (maxWidth - title.length) / 2
-    val rendered = "\n" + (" " * adjust) + title + "\n\n" + graphString + "\n\n"
-
-    c.abort(c.enclosingPosition, rendered)
-  }
-
-  /**
-   * Scala 2.11 doesn't have `Either.toOption`
-   */
-  private def eitherToOption[A](either: Either[_, A]): Option[A] = either match {
-    case Left(_)      => None
-    case Right(value) => Some(value)
+    constructLayer[R0, R, E](layers)
   }
 
   /**
    * Ensures the macro has been annotated with the intended result type.
    * The macro will not behave correctly otherwise.
    */
-  private def assertEnvIsNotNothing[Out <: Has[_]: c.WeakTypeTag](): Unit = {
-    val outType     = weakTypeOf[Out]
+  private def assertEnvIsNotNothing[R <: Has[_]: c.WeakTypeTag](): Unit = {
+    val outType     = weakTypeOf[R]
     val nothingType = weakTypeOf[Nothing]
     if (outType == nothingType) {
       val errorMessage =
