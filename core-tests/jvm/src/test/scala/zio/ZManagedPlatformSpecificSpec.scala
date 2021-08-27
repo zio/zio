5d1f804cac70e568b7e17651cf4dbbb50b40f69b
John A. De Goes <john@degoes.net>
2021-08-27 11:42:52 +0100

    fmt

--- core-tests/jvm/src/test/scala/zio/ZManagedPlatformSpecificSpec.scala
+++ core-tests/jvm/src/test/scala/zio/ZManagedPlatformSpecificSpec.scala
@@ -9,7 +9,7 @@ import java.{util => ju}
 
 object ZManagedPlatformSpecificSpec extends ZIOBaseSpec {
 
-  def spec = suite("ZManagedPlatformSpecificSpec")(
+  def spec: ZSpec[Environment, Failure] = suite("ZManagedPlatformSpecificSpec")(
     test("writeFile & readFile & OutputStream.write & InputStream.readAll") {
       val fixture = Chunk[Byte](1, 2, 3, 6, 5, 4)
       for {
