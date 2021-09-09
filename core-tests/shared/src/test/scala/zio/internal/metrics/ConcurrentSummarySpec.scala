fbd83f7d729992e07844ea0575d24a32235154ed
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2021-09-09 20:19:38 +0200

    Migrated ConcurrentSummary thread safety fix from zio-zmx (#5541)

---
+++ core-tests/shared/src/test/scala/zio/internal/metrics/ConcurrentSummarySpec.scala
@@ -0,0 +1,133 @@
+package zio.internal.metrics
+
+import zio.ZIOBaseSpec
+import zio._
+import zio.test._
+import zio.test.environment.TestClock
+
+object ConcurrentSummarySpec extends ZIOBaseSpec {
+  override def spec: ZSpec[Environment, Any] =
+    suite("ConcurrentSummary")(
+      test("single observe works with maxSize = 0") {
+        val summary = ConcurrentSummary.manual(maxSize = 0, maxAge = 10.seconds, error = 0.0, quantiles = Chunk.empty)
+        val observe = Clock.instant.flatMap(now => ZIO.attempt(summary.observe(11.0, now)))
+
+        for {
+          _        <- observe
+          now      <- Clock.instant
+          snapshot <- ZIO.attempt(summary.snapshot(now))
+          count    <- ZIO.attempt(summary.getCount())
+          sum      <- ZIO.attempt(summary.getSum())
+        } yield assertTrue(
+          snapshot.isEmpty,
+          count == 1L,
+          sum == 11.0
+        )
+      },
+      test("single observe works with arbitrary maxSize") {
+        checkM(Gen.int(0, 100000)) { maxSize =>
+          val summary = ConcurrentSummary.manual(maxSize, maxAge = 10.seconds, error = 0.0, quantiles = Chunk.empty)
+          val observe = Clock.instant.flatMap(now => ZIO.attempt(summary.observe(11.0, now)))
+
+          for {
+            _        <- observe
+            now      <- Clock.instant
+            snapshot <- ZIO.attempt(summary.snapshot(now))
+            count    <- ZIO.attempt(summary.getCount())
+            sum      <- ZIO.attempt(summary.getSum())
+          } yield assertTrue(
+            snapshot.length <= 1,
+            count == 1L,
+            sum == 11.0
+          )
+        }
+      },
+      zio.test.suite("stable under load")(
+        Seq(0, 1, 100, 100000).map { maxSize =>
+          test(s"maxSize = $maxSize") {
+            val summary     = ConcurrentSummary.manual(maxSize, maxAge = 10.seconds, error = 0.0, quantiles = Chunk.empty)
+            val observe     = Clock.instant.flatMap(now => ZIO.attempt(summary.observe(11.0, now)))
+            val getSnapshot = Clock.instant.flatMap(now => ZIO.attempt(summary.snapshot(now)))
+
+            val test =
+              for {
+                f1       <- observe.repeat(Schedule.upTo(2.seconds) *> Schedule.count).forkDaemon
+                f2       <- observe.repeat(Schedule.upTo(2.seconds) *> Schedule.count).forkDaemon
+                _        <- getSnapshot.repeat(Schedule.upTo(2.seconds))
+                snapshot <- getSnapshot
+                f1Count  <- f1.join
+                f2Count  <- f2.join
+                count    <- ZIO.attempt(summary.getCount())
+                sum      <- ZIO.attempt(summary.getSum())
+              } yield assertTrue(
+                snapshot.length <= maxSize,
+                count == (f1Count + f2Count + 2),
+                sum == (f1Count + f2Count + 2) * 11.0
+              )
+
+            test.provideLayer(Clock.live)
+          }
+        }: _*
+      ),
+      test(s"old measurements not used for quantiles with non-full buffer") {
+        val summary =
+          ConcurrentSummary.manual(maxSize = 10, maxAge = 1.seconds, error = 0.0, quantiles = Chunk(0.5, 1.0))
+        def observe(v: Double) = Clock.instant.flatMap(now => ZIO.attempt(summary.observe(v, now)))
+        val getSnapshot        = Clock.instant.flatMap(now => ZIO.attempt(summary.snapshot(now)))
+
+        for {
+          _        <- observe(1.0) // old
+          _        <- TestClock.adjust(300.millis)
+          _        <- observe(2.0) // old
+          _        <- TestClock.adjust(300.millis)
+          _        <- observe(3.0)
+          _        <- TestClock.adjust(300.millis)
+          _        <- observe(4.0)
+          _        <- TestClock.adjust(300.millis)
+          _        <- observe(5.0)
+          _        <- TestClock.adjust(300.millis)
+          snapshot <- getSnapshot
+          count    <- ZIO.attempt(summary.getCount())
+          sum      <- ZIO.attempt(summary.getSum())
+          s0        = (0.5, Some(3.0))
+          s1        = (1.0, Some(5.0))
+        } yield assertTrue(
+          snapshot.length == 2,
+          snapshot(0) == s0,
+          snapshot(1) == s1,
+          count == 5L,
+          sum == 1.0 + 2.0 + 3.0 + 4.0 + 5.0
+        )
+      },
+      test(s"old measurements not used for quantiles with full buffer") {
+        val summary =
+          ConcurrentSummary.manual(maxSize = 3, maxAge = 1.seconds, error = 0.0, quantiles = Chunk(0.5, 1.0))
+        def observe(v: Double) = Clock.instant.flatMap(now => ZIO.attempt(summary.observe(v, now)))
+        val getSnapshot        = Clock.instant.flatMap(now => ZIO.attempt(summary.snapshot(now)))
+
+        for {
+          _        <- observe(1.0) // old
+          _        <- TestClock.adjust(300.millis)
+          _        <- observe(2.0) // old
+          _        <- TestClock.adjust(300.millis)
+          _        <- observe(3.0)
+          _        <- TestClock.adjust(300.millis)
+          _        <- observe(4.0)
+          _        <- TestClock.adjust(300.millis)
+          _        <- observe(5.0)
+          _        <- TestClock.adjust(300.millis)
+          snapshot <- getSnapshot
+          count    <- ZIO.attempt(summary.getCount())
+          sum      <- ZIO.attempt(summary.getSum())
+          s0        = (0.5, Some(3.0))
+          s1        = (1.0, Some(5.0))
+        } yield assertTrue(
+          snapshot.length == 2,
+          snapshot(0) == s0,
+          snapshot(1) == s1,
+          count == 5L,
+          sum == 1.0 + 2.0 + 3.0 + 4.0 + 5.0
+        )
+      }
+    )
+}
--- core/shared/src/main/scala/zio/internal/metrics/ConcurrentSummary.scala
+++ core/shared/src/main/scala/zio/internal/metrics/ConcurrentSummary.scala
@@ -2,8 +2,8 @@ package zio.internal.metrics
 
 import zio._
 
-import java.util.concurrent.ConcurrentLinkedDeque
-import java.util.concurrent.atomic.{DoubleAdder, LongAdder}
+import java.util.concurrent.atomic.{AtomicInteger, AtomicReferenceArray, DoubleAdder, LongAdder}
+import scala.annotation.tailrec
 
 private[zio] sealed abstract class ConcurrentSummary {
 
@@ -28,9 +28,9 @@ private[zio] object ConcurrentSummary {
 
   def manual(maxSize: Int, maxAge: Duration, error: Double, quantiles: Chunk[Double]): ConcurrentSummary =
     new ConcurrentSummary {
-      private[this] val values          = new ConcurrentLinkedDeque[(java.time.Instant, Double)]
+      private[this] val values          = new AtomicReferenceArray[(java.time.Instant, Double)](maxSize)
+      private[this] val head            = new AtomicInteger(0)
       private[this] val count           = new LongAdder
-      private[this] val currentCount    = new LongAdder
       private[this] val sum             = new DoubleAdder
       private[this] val sortedQuantiles = quantiles.sorted(DoubleOrdering)
 
@@ -46,20 +46,22 @@ private[zio] object ConcurrentSummary {
       def snapshot(now: java.time.Instant): Chunk[(Double, Option[Double])] = {
         val builder = ChunkBuilder.make[Double]()
 
-        val predicate: java.util.function.Predicate[(java.time.Instant, Double)] =
-          new java.util.function.Predicate[(java.time.Instant, Double)] {
-            override def test(t: (java.time.Instant, Double)): Boolean =
-              Duration.fromInterval(t._1, now).compareTo(maxAge) > 0
+        // If the buffer is not full yet it contains valid items at the 0..last indices
+        // and null values at the rest of the positions.
+        // If the buffer is already full then all elements contains a valid measurement with timestamp.
+        // At any given point in time we can enumerate all the non-null elements in the buffer and filter
+        // them by timestamp to get a valid view of a time window.
+        // The order does not matter because it gets sorted before passing to calculateQuantiles.
+
+        for (idx <- 0 until maxSize) {
+          val item = values.get(idx)
+          if (item != null) {
+            val (t, v) = item
+            val age    = Duration.fromInterval(t, now)
+            if (!age.isNegative && age.compareTo(maxAge) <= 0) {
+              builder += v
+            }
           }
-
-        values.removeIf(predicate)
-        currentCount.reset()
-
-        val it = values.iterator()
-        while (it.hasNext()) {
-          val (_, v) = it.next()
-          currentCount.increment()
-          builder += v
         }
 
         calculateQuantiles(builder.result().sorted(DoubleOrdering))
@@ -68,12 +70,10 @@ private[zio] object ConcurrentSummary {
       // Assuming that the instant of observed values is continuously increasing
       // While Observing we cut off the first sample if we have already maxSize samples
       def observe(value: Double, t: java.time.Instant): Unit = {
-        if (currentCount.intValue() == maxSize) {
-          values.removeFirst()
-        } else {
-          currentCount.increment()
+        if (maxSize > 0) {
+          val target = head.incrementAndGet() % maxSize
+          values.set(target, (t, value))
         }
-        values.add((t, value))
 
         count.increment()
         sum.add(value)
@@ -87,6 +87,7 @@ private[zio] object ConcurrentSummary {
         // The number of the samples examined
         val sampleCnt = sortedSamples.size
 
+        @tailrec
         def get(
           current: Option[Double],
           consumed: Int,
