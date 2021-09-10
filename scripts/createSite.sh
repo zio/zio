92cf70f7f2d441e2ccd0e3e96473ea079b93f723
Andreas Gies <andreas@wayofquality.de>
2021-09-10 07:20:29 +0200

    Create first cut of versioned site

--- scripts/createSite.sh
+++ scripts/createSite.sh
@@ -12,9 +12,6 @@ sbt docs/mdoc
 mkdir -p website/versioned_docs/version-1.x
 mv zio-docs/target/mdoc/* website/versioned_docs/version-1.x
 
-mkdir -p website/versioned_sidebars
-cp website/sidebars.json website/versioned_sidebars/version-1.x-sidebars.json
-
 git checkout series/2.x
 sbt docs/mdoc
 
