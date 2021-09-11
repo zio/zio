c9c1563ea73e6fd952ceb5e7521fb848d71fafa3
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2021-09-11 12:04:36 +0200

    A ZIOApp that collects the default JVM metrics

--- core/jvm/src/main/scala/zio/metrics/jvm/DefaultJvmMetrics.scala
+++ core/jvm/src/main/scala/zio/metrics/jvm/DefaultJvmMetrics.scala
@@ -22,3 +22,16 @@ object DefaultJvmMetrics {
   val live: ZLayer[Has[Clock] with Has[System], Throwable, Has[DefaultJvmMetrics]] =
     collectDefaultJvmMetrics.as(new DefaultJvmMetrics {}).toLayer
 }
+
+/** A ZIO application that collects the same JVM metrics as the Prometheus Java client's default exporters. */
+object DefaultJvmMetricsExporter extends ZIOApp {
+  override val tag: Tag[Environment] = Tag[Has[DefaultJvmMetrics]]
+
+  override type Environment = Has[DefaultJvmMetrics]
+
+  override def layer: ZLayer[Has[ZIOAppArgs], Any, Has[DefaultJvmMetrics]] =
+    Clock.live ++ System.live >>> DefaultJvmMetrics.live
+
+  override def run: ZIO[Has[DefaultJvmMetrics] with Has[ZIOAppArgs], Any, Any] =
+    ZIO.unit
+}
