f40d4b10a0a307c4e193c45217dde99463a468dd
Andreas Gies <andreas@wayofquality.de>
2021-09-13 10:26:36 +0200

    Adjust site deploy script to deploy site on releases and changes on the "documantation" branch

--- scripts/createSite.sh
+++ scripts/createSite.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
+OLDDIR=`pwd`
 # Clean existing build and mdoc output directory
 rm -Rf target
 rm -Rf website/docs
 rm -Rf website/versioned_docs
 
 # Checkout latest released version of 1.x 
-git checkout docs-1.x
+git checkout v1.0.11
 sbt docs/mdoc
 
 mkdir -p website/versioned_docs/version-1.x
 mv zio-docs/target/mdoc/* website/versioned_docs/version-1.x
 
-git checkout documentation
+# No we need to checkout the branch that originally has triggered the site build 
+git checkout $1
 sbt docs/mdoc
 
 cd website 
 rm -Rf node_modules
 yarn install 
-yarn build 
+
+cd $OLDDIR
+
