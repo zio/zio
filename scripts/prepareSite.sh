f4cd9618622aaedaf48566d51613602fcf4ec86b
Andreas Gies <andreas@wayofquality.de>
2021-09-16 11:30:45 +0200

    Optimize for site deployment

--- scripts/prepareSite.sh
+++ scripts/prepareSite.sh
@@ -17,7 +17,7 @@ mv zio-docs/target/mdoc/* website/versioned_docs/version-1.x
 
 # No we need to checkout the branch that originally has triggered the site build 
 git checkout $1
-sbt docs/mdoc
+sbt docs/unidoc docs/mdoc
 
 cd website 
 rm -Rf node_modules
