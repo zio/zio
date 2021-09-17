b7cdc7aaa621e0270f584a3cd26f1f73c22d73f2
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-09-17 21:31:35 +0200

    Update scalafmt-core to 3.0.4 (#5635)
    * Update scalafmt-core to 3.0.4
    
    * Reformat with scalafmt 3.0.4

--- test-scalacheck/shared/src/main/scala/zio/test/scalacheck/package.scala
+++ test-scalacheck/shared/src/main/scala/zio/test/scalacheck/package.scala
@@ -8,29 +8,29 @@ import zio.random.Random
 import zio.test.Sized
 
 /**
-  * Provides functionality for converting legacy ScalaCheck generators to ZIO
-  * Test generators to support upgrading to ZIO Test without having to
-  * reimplement existing generators. To use it import this module and then call
-  * `toGenZIO` on any existing ScalaCheck generator. For example:
-  * 
-  * {{{
-  * import org.scalacheck.Arbitrary
-  * 
-  * import zio._
-  * import zio.test._
-  * import zio.test.scalacheck._
-  * 
-  * val anyInt: Gen[Random with Sized, Int] =
-  *   Arbitrary.arbitrary[Int].toGenZIO
-  * }}}
-  */
+ * Provides functionality for converting legacy ScalaCheck generators to ZIO
+ * Test generators to support upgrading to ZIO Test without having to
+ * reimplement existing generators. To use it import this module and then call
+ * `toGenZIO` on any existing ScalaCheck generator. For example:
+ *
+ * {{{
+ * import org.scalacheck.Arbitrary
+ *
+ * import zio._
+ * import zio.test._
+ * import zio.test.scalacheck._
+ *
+ * val anyInt: Gen[Random with Sized, Int] =
+ *   Arbitrary.arbitrary[Int].toGenZIO
+ * }}}
+ */
 package object scalacheck {
 
   implicit final class ScalaCheckGenSyntax[A](private val self: scalacheck.Gen[A]) extends AnyVal {
 
     /**
-      * Converts a legacy ScalaCheck `Gen` to a ZIO Test `Gen`.
-      */
+     * Converts a legacy ScalaCheck `Gen` to a ZIO Test `Gen`.
+     */
     def toGenZIO: Gen[Random with Sized, A] =
       Gen.fromEffect {
         for {
