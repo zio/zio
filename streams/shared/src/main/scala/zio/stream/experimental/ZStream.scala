7658d8f676ecc159cd25ee42bc20f6ad8db3e0e3
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2021-09-24 14:45:49 +0200

    Format

--- streams/shared/src/main/scala/zio/stream/experimental/ZStream.scala
+++ streams/shared/src/main/scala/zio/stream/experimental/ZStream.scala
@@ -5044,7 +5044,7 @@ object ZStream extends ZStreamPlatformSpecificConstructors {
      * Collect elements of the given type flowing through the stream, and filters out others.
      */
     def collectType[O1 <: O](implicit tag: ClassTag[O1]): ZStream[R, E, O1] =
-      self.collect({ case o if tag.runtimeClass.isInstance(o) => o.asInstanceOf[O1] })
+      self.collect { case o if tag.runtimeClass.isInstance(o) => o.asInstanceOf[O1] }
   }
 
   private[zio] class Rechunker[A](n: Int) {
