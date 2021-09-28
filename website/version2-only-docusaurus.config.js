e2015b5d7c8b39cf64695679abfcff4a98c95e65
Milad Khajavi <khajavi@gmail.com>
2021-09-28 11:51:47 +0330

    enable edit button.

--- website/version2-only-docusaurus.config.js
+++ website/version2-only-docusaurus.config.js
@@ -126,6 +126,7 @@ module.exports = {
             [require('blended-include-code-plugin'), { marker: 'CODE_INCLUDE' }],
             [require('remark-kroki-plugin'), { krokiBase: 'https://kroki.io', lang: "kroki", imgRefDir: "/img/kroki", imgDir: "static/img/kroki" }]
           ],
+          editUrl: 'https://github.com/zio/zio/edit/series/2.x', 
         },
         theme: {
           customCss: [require.resolve('./src/css/custom.css')],
