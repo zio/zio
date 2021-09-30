d4ffc747ea995ec1854e3cd5f7cce71f0f72ee37
Andreas Gies <andreas@wayofquality.de>
2021-09-30 11:40:02 +0200

    Just adjusting the config

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -111,7 +111,6 @@ module.exports = {
         {
           items: [
             {
-              title: 'Deployment support',
               html: `
                 <a href="https://www.netlify.com">
                   <img src="https://www.netlify.com/img/global/badges/netlify-color-accent.svg" alt="Deploys by Netlify"/>
