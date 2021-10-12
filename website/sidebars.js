4f9cc1eda8a998536f8fe8665c7846785291eee6
Milad Khajavi <khajavi@gmail.com>
2021-10-13 02:32:07 +0330

    ZState Documentation (#5704)
    * initial work.
    
    * a note about the fiberref behaviour of zstate.

--- website/sidebars.js
+++ website/sidebars.js
@@ -179,7 +179,8 @@ module.exports = {
     "Miscellaneous": [
       "datatypes/misc/chunk",
       "datatypes/misc/schedule",
-      "datatypes/misc/supervisor"
+      "datatypes/misc/supervisor",
+      "datatypes/misc/zstate",
     ]
   },
   "usecases-sidebar": {
