c74cc5aa616c34db0c5810d10aed44724d3541ee
Andreas Gies <andreas@wayofquality.de>
2021-10-22 21:25:07 +0200

    Added script to check site

---
+++ scripts/checkSite.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e 
+
+ZIO_LATEST_2=`git describe --tags --abbrev=0 ` sbt docs/unidoc docs/mdoc
+cd website
+mv docusaurus.config.js docusaurus.config.js.org 
+cp version2-only-docusaurus.config.js docusaurus.config.js 
+mv versions.json versions.json.org
+yarn install 
+yarn build 
+mv versions.json.org versions.json
+mv docusaurus.config.js.org docusaurus.config.js
+cd ..
