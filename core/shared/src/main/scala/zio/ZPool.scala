3b0a8553c5ed0748aa255a8ffb7274e8a855ad47
Milad Khajavi <khajavi@gmail.com>
2021-10-27 05:32:46 +0330

    fmt

--- core/shared/src/main/scala/zio/ZPool.scala
+++ core/shared/src/main/scala/zio/ZPool.scala
@@ -20,7 +20,7 @@ import zio.stacktracer.TracingImplicits.disableAutoTrace
 
 /**
  * A `ZPool[E, A]` is a pool of items of type `A`, each of which may be
- * associated with the acquisition and release of resources. An attempt to get 
+ * associated with the acquisition and release of resources. An attempt to get
  * an item `A` from a pool may fail with an error of type `E`.
  */
 trait ZPool[+Error, Item] {
