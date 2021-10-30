6e00d7b28e6b456ec71bf88da75138b2646884ed
Kit Langton <kit.langton@gmail.com>
2021-10-30 14:24:19 -0600

    add concurrent set methods

--- core/shared/src/main/scala/zio/concurrent/ConcurrentSet.scala
+++ core/shared/src/main/scala/zio/concurrent/ConcurrentSet.scala
@@ -1,8 +1,11 @@
 package zio.concurrent
 
-import zio.UIO
+import zio.{UIO, ZIO}
 
 import java.util.concurrent.ConcurrentHashMap
+import java.util.function.Predicate
+import collection.JavaConverters._
+import scala.collection.mutable
 
 final class ConcurrentSet[A] private (private val underlying: ConcurrentHashMap.KeySetView[A, java.lang.Boolean])
     extends AnyVal {
@@ -11,14 +14,46 @@ final class ConcurrentSet[A] private (private val underlying: ConcurrentHashMap.
     UIO(underlying.add(x))
 
   def addAll(xs: Iterable[A]): UIO[Boolean] =
-    UIO {
-      var added = false
-      xs.foreach(x => added = underlying.add(x))
-      added
-    }
+    UIO(underlying.addAll(xs.asJavaCollection))
+
+  def collectFirst[B](pf: PartialFunction[A, B]): UIO[Option[B]] =
+    UIO(underlying.asScala.toSet.collectFirst(pf))
+
+  def exists(p: A => Boolean): UIO[Boolean] =
+    UIO(underlying.asScala.toSet.exists(p))
+
+  def fold[R, E, S](zero: S)(f: (S, A) => S): UIO[S] =
+    UIO(underlying.asScala.toSet.foldLeft(zero)(f))
+
+  def foldZIO[R, E, S](zero: S)(f: (S, A) => ZIO[R, E, S]): ZIO[R, E, S] =
+    ZIO.foldLeft(underlying.asScala.toSet)(zero)(f)
+
+  def foreach[R, E, B](f: A => ZIO[R, E, B]): ZIO[R, E, Set[B]] =
+    ZIO.foreach(underlying.asScala.toSet)(f)
+
+  def foreach_[R, E](f: A => ZIO[R, E, Any]): ZIO[R, E, Unit] =
+    ZIO.foreach_(underlying.asScala.toSet)(f)
+
+  def forall(p: A => Boolean): UIO[Boolean] =
+    UIO(underlying.asScala.toSet.forall(p))
+
+  def find[B](p: A => Boolean): UIO[Option[A]] =
+    UIO(underlying.asScala.toSet.find(p))
 
   def remove(x: A): UIO[Boolean] =
     UIO(underlying.remove(x))
+
+  def removeAll(xs: Iterable[A]): UIO[Boolean] =
+    UIO(underlying.removeAll(xs.asJavaCollection))
+
+  def removeIf(p: A => Boolean): UIO[Boolean] =
+    UIO(underlying.removeIf((t: A) => !p(t)))
+
+  def retainAll(xs: Iterable[A]): UIO[Boolean] =
+    UIO(underlying.retainAll(xs.asJavaCollection))
+
+  def retainIf(p: A => Boolean): UIO[Boolean] =
+    UIO(underlying.removeIf((t: A) => p(t)))
 
   def clear: UIO[Unit] =
     UIO(underlying.clear())
@@ -34,24 +69,45 @@ final class ConcurrentSet[A] private (private val underlying: ConcurrentHashMap.
 
   def isEmpty: UIO[Boolean] =
     UIO(underlying.isEmpty)
+
+  def toSet: UIO[Set[A]] =
+    UIO(underlying.asScala.toSet)
+
+  def transform(f: A => A): UIO[Unit] = UIO {
+    val set = underlying.asScala.toSet
+    underlying.removeAll(set.asJavaCollection)
+    underlying.addAll(set.map(f).asJavaCollection)
+  }
 }
 
 object ConcurrentSet {
 
-  def make[A](xs: A*): UIO[ConcurrentSet[A]] =
-    UIO.effectSuspendTotal {
-      UIO {
-        val keySetView = ConcurrentHashMap.newKeySet[A]()
-        xs.foreach(x => keySetView.add(x))
-        new ConcurrentSet(keySetView)
-      }
+  def empty[A]: UIO[ConcurrentSet[A]] =
+    UIO {
+      val keySetView = ConcurrentHashMap.newKeySet[A]()
+      new ConcurrentSet(keySetView)
     }
 
-  def make[A](size: Int): UIO[ConcurrentSet[A]] =
-    UIO.effectSuspendTotal {
-      UIO {
-        val keySetView = ConcurrentHashMap.newKeySet[A](size)
-        new ConcurrentSet(keySetView)
-      }
+  def empty[A](initialCapacity: Int): UIO[ConcurrentSet[A]] =
+    UIO {
+      val keySetView = ConcurrentHashMap.newKeySet[A](initialCapacity)
+      new ConcurrentSet(keySetView)
+    }
+
+  /**
+   * Makes a new `ConcurrentSet` initialized with provided collection.
+   */
+  def fromIterable[A](as: Iterable[A]): UIO[ConcurrentSet[A]] =
+    UIO {
+      val keySetView = ConcurrentHashMap.newKeySet[A]()
+      as.foreach(x => keySetView.add(x))
+      new ConcurrentSet(keySetView)
+    }
+
+  def make[A](as: A*): UIO[ConcurrentSet[A]] =
+    UIO {
+      val keySetView = ConcurrentHashMap.newKeySet[A]()
+      as.foreach(x => keySetView.add(x))
+      new ConcurrentSet(keySetView)
     }
 }
