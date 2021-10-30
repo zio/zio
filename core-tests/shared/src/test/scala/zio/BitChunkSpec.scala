a0ada5de7a07d983cb5127db882aba8064d3de08
Ilias Antoniou <helias1990@hotmail.com>
2021-10-30 16:34:04 +0000

    construct Chunk[Boolean] from Chunk[Int] and Chunk[Long] based on endianness

--- core-tests/shared/src/test/scala/zio/BitChunkSpec.scala
+++ core-tests/shared/src/test/scala/zio/BitChunkSpec.scala
@@ -4,6 +4,8 @@ import zio.random.Random
 import zio.test.Assertion._
 import zio.test._
 
+import java.nio.ByteOrder
+
 object BitChunkSpec extends ZIOBaseSpec {
 
   val genByteChunk: Gen[Random with Sized, Chunk[Byte]] =
@@ -14,8 +16,22 @@ object BitChunkSpec extends ZIOBaseSpec {
   val genInt: Gen[Random with Sized, Int] =
     Gen.small(Gen.const(_))
 
+  val genIntChunk: Gen[Random with Sized, Chunk[Int]] = for {
+    ints <- Gen.listOf(Gen.anyInt)
+  } yield Chunk.fromIterable(ints)
+
+  val genLongChunk: Gen[Random with Sized, Chunk[Long]] = for {
+    longs <- Gen.listOf(Gen.anyLong)
+  } yield Chunk.fromIterable(longs)
+
   def toBinaryString(byte: Byte): String =
     String.format("%8s", (byte.toInt & 0xff).toBinaryString).replace(' ', '0')
+
+  def toBinaryString(int: Int): String =
+    String.format("%32s", int.toBinaryString).replace(' ', '0')
+
+  def toBinaryString(long: Long): String =
+    String.format("%64s", long.toBinaryString).replace(' ', '0')
 
   def spec: ZSpec[Environment, Failure] = suite("BitChunkSpec")(
     testM("drop") {
@@ -65,6 +81,34 @@ object BitChunkSpec extends ZIOBaseSpec {
         val actual   = bytes.asBits.toBinaryString
         val expected = bytes.map(toBinaryString).mkString
         assert(actual)(equalTo(expected))
+      }
+    },
+    testM("integers in big endian bit representation") {
+      check(genIntChunk) { ints =>
+        val bits     = ints.intsAsBits(ByteOrder.BIG_ENDIAN).toBinaryString
+        val expected = ints.map(toBinaryString).mkString
+        assert(bits)(equalTo(expected))
+      }
+    },
+    testM("integers in little endian bit representation") {
+      check(genIntChunk) { ints =>
+        val bits     = ints.intsAsBits(ByteOrder.LITTLE_ENDIAN).toBinaryString
+        val expected = ints.map(Integer.reverseBytes).map(toBinaryString).mkString
+        assert(bits)(equalTo(expected))
+      }
+    },
+    testM("longs in big endian bit representation") {
+      check(genLongChunk) { longs =>
+        val bits     = longs.longsAsBits(ByteOrder.BIG_ENDIAN).toBinaryString
+        val expected = longs.map(toBinaryString).mkString
+        assert(bits)(equalTo(expected))
+      }
+    },
+    testM("longs in little endian bit representation") {
+      check(genLongChunk) { longs =>
+        val bits     = longs.longsAsBits(ByteOrder.LITTLE_ENDIAN).toBinaryString
+        val expected = longs.map(java.lang.Long.reverseBytes).map(toBinaryString).mkString
+        assert(bits)(equalTo(expected))
       }
     }
   )
--- core/shared/src/main/scala/zio/Chunk.scala
+++ core/shared/src/main/scala/zio/Chunk.scala
@@ -100,6 +100,36 @@ sealed abstract class Chunk[+A] extends ChunkLike[A] { self =>
   final def asBits(implicit ev: A <:< Byte): Chunk[Boolean] =
     Chunk.BitChunk(self.map(ev), 0, length << 3)
 
+  /** Converts a chunk of longs to a chunk of bits using the specified endianness */
+  final def longsAsBits(endian: ByteOrder)(implicit ev: A <:< Long): Chunk[Boolean] = {
+    val byteChunk: Chunk[Byte] = self
+      .map(ev)
+      .flatMap(
+        ByteBuffer
+          .allocate(java.lang.Long.BYTES)
+          .order(endian)
+          .putLong(_)
+          .array()
+      )
+
+    Chunk.BitChunk(byteChunk, 0, length << 6)
+  }
+
+  /** Converts a chunk of ints to a chunk of bits using the specified endianness */
+  final def intsAsBits(endian: ByteOrder)(implicit ev: A <:< Int): Chunk[Boolean] = {
+    val byteChunk: Chunk[Byte] = self
+      .map(ev)
+      .flatMap(
+        ByteBuffer
+          .allocate(Integer.BYTES)
+          .order(endian)
+          .putInt(_)
+          .array()
+      )
+
+    Chunk.BitChunk(byteChunk, 0, length << 5)
+  }
+
   /**
    * Get the element at the specified index.
    */
