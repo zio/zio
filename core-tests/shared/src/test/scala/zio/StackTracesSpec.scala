a032684601d0b011d3d43c654b6e3cf48b0c1d18
szaloma <szaloma@hlag.com>
2021-10-31 11:36:18 +0100

    Adjust code to let compiler infer the type
    It should fix a problem for 2.11 and 2.12.

--- core-tests/shared/src/test/scala/zio/StackTracesSpec.scala
+++ core-tests/shared/src/test/scala/zio/StackTracesSpec.scala
@@ -7,23 +7,22 @@ object StackTracesSpec extends ZIOBaseSpec {
   def spec: ZSpec[Environment, Failure] = suite("StackTracesSpec")(
     suite("captureSimpleCause")(
       test("captures a simple failure") {
-        val failureStackTraceContent: IO[String, Nothing] => ZIO[Any, Serializable, String] = {
-          case fail: ZIO[Any, String, Unit] =>
-            fail.catchAllCause {
-              case c: Cause[String] => ZIO(c.prettyPrint)
-              case _                => UnsupportedTestPath
-            }
-          case _ => UnsupportedTestPath
-        }
         val checkExpectations: String => Boolean = {
           case stack: String =>
             stack.startsWith("Exception in thread") && includesAll(Seq("zio-fiber", "java.lang.String: Oh no!"))(stack)
           case _ => false
         }
         for {
-          _          <- ZIO.succeed(15)
-          stackTrace <- failureStackTraceContent(ZIO.fail("Oh no!"))
-          result      = checkExpectations(stackTrace)
+          _ <- ZIO.succeed(15)
+          stackTrace <- ZIO.fail("Oh no!") match {
+                          case fail: ZIO[Any, Serializable, String] =>
+                            fail.catchAllCause {
+                              case c: Cause[String] => ZIO(c.prettyPrint)
+                              case _                => UnsupportedTestPath
+                            }
+                          case _ => UnsupportedTestPath
+                        }
+          result = checkExpectations(stackTrace)
         } yield assertTrue(result)
       }
     )
@@ -31,5 +30,5 @@ object StackTracesSpec extends ZIOBaseSpec {
 
   private def includesAll(texts: Seq[String]): String => Boolean = stack => texts.map(stack.contains).forall(r => r)
 
-  private val UnsupportedTestPath = ZIO.fail("not considered scenario")
+  private val UnsupportedTestPath: Task[String] = ZIO("not considered scenario")
 }
