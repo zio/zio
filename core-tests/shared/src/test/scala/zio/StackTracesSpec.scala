f4fef5d2e211d123860b604c17651c6930b93dda
szaloma <szaloma@hlag.com>
2021-10-31 08:50:49 +0100

    #5917 Clean up code

--- core-tests/shared/src/test/scala/zio/StackTracesSpec.scala
+++ core-tests/shared/src/test/scala/zio/StackTracesSpec.scala
@@ -1,31 +1,35 @@
 package zio
 
-import zio.test.{ZSpec, assertTrue}
+import zio.test.{AssertionValue, BoolAlgebra, ZSpec, assertTrue}
 
 object StackTracesSpec extends ZIOBaseSpec {
 
   def spec: ZSpec[Environment, Failure] = suite("StackTracesSpec")(
     suite("captureSimpleCause")(
       test("captures a simple failure") {
-        val failure =
-          for {
-            _ <- ZIO.succeed(15)
-            _ <- ZIO.fail("Oh no!")
-          } yield ()
-
-        val run = failure.catchAllCause(cause => ZIO(cause.prettyPrint))
-
+        val failureStackTraceContent: IO[String, Nothing] => ZIO[Any, Serializable, String] = {
+          case fail: ZIO[Any, String, Unit] =>
+            fail.catchAllCause {
+              case c: Cause[String] => ZIO(c.prettyPrint)
+              case _                => UnsupportedTestPath
+            }
+          case _ => UnsupportedTestPath
+        }
+        val checkExpectations: String => Boolean = {
+          case stack: String =>
+            stack.startsWith("Exception in thread") &&
+              stack.contains("zio-fiber") &&
+              stack.contains("java.lang.String: Oh no!")
+          case _ => false
+        }
         for {
-          stackTrace <- run
-          result = stackTrace match {
-                     case stack: String =>
-                       stack.startsWith("Exception in thread") &&
-                         stack.contains("zio-fiber") &&
-                         stack.contains("java.lang.String: Oh no!")
-                     case _ => false
-                   }
+          _          <- ZIO.succeed(15)
+          stackTrace <- failureStackTraceContent(ZIO.fail("Oh no!"))
+          result      = checkExpectations(stackTrace)
         } yield assertTrue(result)
       }
     )
   )
+
+  private val UnsupportedTestPath = ZIO.fail("not considered scenario")
 }
