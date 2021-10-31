7768a5e079373e8920b25f71e114fd13bd52733d
szaloma <szaloma@hlag.com>
2021-10-31 16:25:55 +0100

    #5849 Create test suite for Cause.prettyPrint

---
+++ core-tests/shared/src/test/scala/zio/StackTracesSpec.scala
@@ -0,0 +1,90 @@
+package zio
+
+import zio.test.{ZSpec, assertTrue}
+
+object StackTracesSpec extends ZIOBaseSpec {
+
+  def spec: ZSpec[Environment, Failure] = suite("StackTracesSpec")(
+    suite("captureSimpleCause")(
+      test("captures a simple failure") {
+        for {
+          _          <- ZIO.succeed(15)
+          value       = ZIO.fail("Oh no!")
+            stackTrace <- matchPrettyPrintCause(value)
+        } yield {
+          assertTrue(stackTrace.startsWith("Exception in thread")) &&
+            assertTrue(includesAll(Seq("zio-fiber", "java.lang.String: Oh no!"))(stackTrace))
+          assertTrue(excludesAll(Seq("An unchecked error was produced"))(stackTrace))
+        }
+      }
+    ),
+    suite("captureMultiMethod")(
+      test("captures a deep embedded failure") {
+        val deepUnderlyingFailure =
+          for {
+            _ <- ZIO.succeed(5)
+            f <- ZIO.fail("Oh no!").ensuring(ZIO.dieMessage("deep failure"))
+          } yield f
+
+        val underlyingFailure =
+          for {
+            _ <- ZIO.succeed(15)
+            f <- deepUnderlyingFailure.ensuring(ZIO.dieMessage("other failure"))
+          } yield f
+
+        for {
+          _          <- ZIO.succeed(15)
+          value       = underlyingFailure
+          stackTrace <- matchPrettyPrintCause(value)
+        } yield {
+          assertTrue(stackTrace.startsWith("Exception in thread")) &&
+          assertTrue(
+            includesAll(
+              Seq(
+                "zio-fiber",
+                "java.lang.String: Oh no!",
+                "Suppressed: java.lang.RuntimeException: deep failure",
+                "Suppressed: java.lang.RuntimeException: other failure"
+              )
+            )(stackTrace)
+          )
+        }
+      },
+      test("captures the embedded failure") {
+        val underlyingFailure =
+          for {
+            _ <- ZIO.succeed(15)
+            f <- ZIO.fail("Oh no!").ensuring(ZIO.dieMessage("other failure"))
+          } yield f
+
+        for {
+          _          <- ZIO.succeed(15)
+          value       = underlyingFailure
+          stackTrace <- matchPrettyPrintCause(value)
+        } yield {
+          assertTrue(stackTrace.startsWith("Exception in thread")) &&
+          assertTrue(
+            includesAll(
+              Seq("zio-fiber", "java.lang.String: Oh no!", "Suppressed: java.lang.RuntimeException: other failure")
+            )(stackTrace)
+          )
+        }
+      }
+    )
+  )
+
+  private def includesAll(texts: Seq[String]): String => Boolean = stack => texts.map(stack.contains).forall(r => r)
+
+  private def excludesAll(texts: Seq[String]): String => Boolean = stack => texts.map(stack.contains).forall(!_)
+
+  private val UnsupportedTestPath: Task[String] = ZIO("not considered scenario")
+
+  private val matchPrettyPrintCause: ZIO[Any, String, Nothing] => ZIO[Any, Throwable, String] = {
+    case fail: IO[String, Nothing] =>
+      fail.catchAllCause {
+        case c: Cause[String] => ZIO(c.prettyPrint)
+        case _                => UnsupportedTestPath
+      }
+    case _ => UnsupportedTestPath
+  }
+}
