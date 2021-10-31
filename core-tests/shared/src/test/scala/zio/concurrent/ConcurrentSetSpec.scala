103aaa7851beaae0b96850ea903032f4c4f07be5
Kit Langton <kit.langton@gmail.com>
2021-10-31 11:16:38 -0600

    Add concurrent set methods (#5921)
    * #5823 - Add ConcurrentSet
    
    * Add remove method
    
    * Refactor ConcurrentSet according to suggestions
    
    * Add tests
    
    * Remove Scala Java conversion
    
    * add concurrent set methods
    
    * improve atomicity
    
    * Update core/shared/src/main/scala/zio/concurrent/ConcurrentSet.scala
    
    Co-authored-by: Regis Kuckaertz <629976+regiskuckaertz@users.noreply.github.com>
    
    * fix Consumer scala.js issue
    
    Co-authored-by: Aleksandar Skrbic <skrbic.alexa@gmail.com>
    Co-authored-by: Regis Kuckaertz <629976+regiskuckaertz@users.noreply.github.com>

--- core-tests/shared/src/test/scala/zio/concurrent/ConcurrentSetSpec.scala
+++ core-tests/shared/src/test/scala/zio/concurrent/ConcurrentSetSpec.scala
@@ -1,54 +1,130 @@
 package zio.concurrent
 
+import zio._
 import zio.test._
-import zio.test.Assertion._
-import zio.{Chunk, ZIOBaseSpec}
 
 object ConcurrentSetSpec extends ZIOBaseSpec {
 
   override def spec: ZSpec[Environment, Failure] = suite("ConcurrentSetSpec")(
     testM("add") {
       for {
-        concurrentSet <- ConcurrentSet.make[Int](3)
-        added         <- concurrentSet.add(1)
-      } yield assert(added)(equalTo(true))
+        set    <- ConcurrentSet.empty[Int]
+        added  <- set.add(1)
+        result <- set.toSet
+      } yield assertTrue(added, result == Set(1))
     },
     testM("addAll") {
       for {
-        concurrentSet <- ConcurrentSet.make[Int](3)
-        added         <- concurrentSet.addAll(Chunk(1, 2))
-      } yield assert(added)(equalTo(true))
+        set    <- ConcurrentSet.make[Int](3, 4)
+        added  <- set.addAll(Chunk(1, 2))
+        result <- set.toSet
+      } yield assertTrue(added, result == Set(1, 2, 3, 4))
     },
     testM("remove") {
       for {
-        concurrentSet <- ConcurrentSet.make(1, 2, 3)
-        isRemoved     <- concurrentSet.remove(1)
-      } yield assert(isRemoved)(equalTo(true))
+        set       <- ConcurrentSet.make(1, 2, 3)
+        isRemoved <- set.remove(1)
+        result    <- set.toSet
+      } yield assertTrue(isRemoved, result == Set(2, 3))
+    },
+    testM("removeAll") {
+      for {
+        set       <- ConcurrentSet.make(1, 2, 3)
+        isRemoved <- set.removeAll(List(1, 3))
+        result    <- set.toSet
+      } yield assertTrue(isRemoved, result == Set(2))
+    },
+    testM("retainAll") {
+      for {
+        set       <- ConcurrentSet.make(1, 2, 3, 4)
+        isRemoved <- set.retainAll(List(1, 3, 5, 6))
+        result    <- set.toSet
+      } yield assertTrue(isRemoved, result == Set(1, 3))
     },
     testM("clear and isEmpty") {
       for {
-        concurrentSet <- ConcurrentSet.make(1, 2, 3)
-        _             <- concurrentSet.clear
-        isEmpty       <- concurrentSet.isEmpty
-      } yield assert(isEmpty)(equalTo(true))
+        set     <- ConcurrentSet.make(1, 2, 3)
+        _       <- set.clear
+        isEmpty <- set.isEmpty
+        result  <- set.toSet
+      } yield assertTrue(isEmpty, result == Set.empty[Int])
     },
     testM("contains") {
       for {
-        concurrentSet <- ConcurrentSet.make(1, 2, 3)
-        contains      <- concurrentSet.contains(5)
-      } yield assert(contains)(equalTo(false))
+        set      <- ConcurrentSet.make(1, 2, 3)
+        contains <- set.contains(5)
+      } yield assertTrue(!contains)
     },
     testM("containsAll") {
       for {
-        concurrentSet <- ConcurrentSet.make(1, 2, 3)
-        containsAll   <- concurrentSet.containsAll(Chunk(1, 2, 3))
-      } yield assert(containsAll)(equalTo(true))
+        set         <- ConcurrentSet.make(1, 2, 3)
+        containsAll <- set.containsAll(Chunk(1, 2, 3))
+      } yield assertTrue(containsAll)
+    },
+    testM("exists") {
+      for {
+        set       <- ConcurrentSet.make(1, 2, 3)
+        exists    <- set.exists(_ > 2)
+        notExists <- set.exists(_ > 3)
+      } yield assertTrue(exists, !notExists)
+    },
+    testM("forall") {
+      for {
+        set       <- ConcurrentSet.make(1, 2, 3)
+        forall    <- set.forall(_ < 4)
+        notForall <- set.forall(_ > 5)
+      } yield assertTrue(forall, !notForall)
+    },
+    testM("filter") {
+      for {
+        set    <- ConcurrentSet.make(1, 2, 3, 4)
+        _      <- set.removeIf(_ % 2 == 0)
+        result <- set.toSet
+      } yield assertTrue(result == Set(2, 4))
+    },
+    testM("filterNot") {
+      for {
+        set    <- ConcurrentSet.make(1, 2, 3, 4)
+        _      <- set.retainIf(_ % 2 == 0)
+        result <- set.toSet
+      } yield assertTrue(result == Set(1, 3))
+    },
+    testM("find") {
+      for {
+        set    <- ConcurrentSet.make(1, 2, 3)
+        result <- set.find(_ > 2)
+      } yield assertTrue(result.get == 3)
+    },
+    testM("collectFirst") {
+      for {
+        set    <- ConcurrentSet.make(1, 2, 3)
+        result <- set.collectFirst { case 3 => "Three" }
+      } yield assertTrue(result.get == "Three")
     },
     testM("size") {
       for {
-        concurrentSet <- ConcurrentSet.make(1, 2, 3, 4, 5)
-        size          <- concurrentSet.size
-      } yield assert(size)(equalTo(5))
+        set  <- ConcurrentSet.make(1, 2, 3, 4, 5)
+        size <- set.size
+      } yield assertTrue(size == 5)
+    },
+    testM("toSet") {
+      for {
+        set    <- ConcurrentSet.make(1, 2, 3, 4, 5)
+        result <- set.toSet
+      } yield assertTrue(result == Set(1, 2, 3, 4, 5))
+    },
+    testM("transform") {
+      for {
+        set    <- ConcurrentSet.make(1, 2, 3, 4, 5)
+        _      <- set.transform(_ + 10)
+        result <- set.toSet
+      } yield assertTrue(result == Set(11, 12, 13, 14, 15))
+    },
+    testM("fold") {
+      for {
+        set    <- ConcurrentSet.make(1, 2, 3, 4, 5)
+        result <- set.fold(0)(_ + _)
+      } yield assertTrue(result == 15)
     }
   )
 }
--- core/shared/src/main/scala/zio/concurrent/ConcurrentSet.scala
+++ core/shared/src/main/scala/zio/concurrent/ConcurrentSet.scala
@@ -1,8 +1,11 @@
 package zio.concurrent
 
+import com.github.ghik.silencer.silent
 import zio.UIO
 
 import java.util.concurrent.ConcurrentHashMap
+import java.util.function.Consumer
+import scala.collection.JavaConverters._
 
 final class ConcurrentSet[A] private (private val underlying: ConcurrentHashMap.KeySetView[A, java.lang.Boolean])
     extends AnyVal {
@@ -11,14 +14,82 @@ final class ConcurrentSet[A] private (private val underlying: ConcurrentHashMap.
     UIO(underlying.add(x))
 
   def addAll(xs: Iterable[A]): UIO[Boolean] =
+    UIO(underlying.addAll(xs.asJavaCollection): @silent("JavaConverters"))
+
+  def collectFirst[B](pf: PartialFunction[A, B]): UIO[Option[B]] =
     UIO {
-      var added = false
-      xs.foreach(x => added = underlying.add(x))
-      added
+      var result = Option.empty[B]
+      underlying.forEach {
+        makeConsumer { (a: A) =>
+          if (result.isEmpty && pf.isDefinedAt(a)) {
+            result = Some(pf(a))
+          }
+        }
+      }
+      result
+    }
+
+  def exists(p: A => Boolean): UIO[Boolean] =
+    UIO {
+      var result = false
+      underlying.forEach {
+        makeConsumer { (a: A) =>
+          if (!result && p(a))
+            result = true
+        }
+      }
+      result
+    }
+
+  def fold[R, E, S](zero: S)(f: (S, A) => S): UIO[S] =
+    UIO {
+      var result: S = zero
+      underlying.forEach {
+        makeConsumer { (a: A) =>
+          result = f(result, a)
+        }
+      }
+      result
+    }
+
+  def forall(p: A => Boolean): UIO[Boolean] =
+    UIO {
+      var result = true
+      underlying.forEach {
+        makeConsumer { (a: A) =>
+          if (result && !p(a))
+            result = false
+        }
+      }
+      result
+    }
+
+  def find[B](p: A => Boolean): UIO[Option[A]] =
+    UIO {
+      var result = Option.empty[A]
+      underlying.forEach {
+        makeConsumer { (a: A) =>
+          if (result.isEmpty && p(a))
+            result = Some(a)
+        }
+      }
+      result
     }
 
   def remove(x: A): UIO[Boolean] =
     UIO(underlying.remove(x))
+
+  def removeAll(xs: Iterable[A]): UIO[Boolean] =
+    UIO(underlying.removeAll(xs.asJavaCollection): @silent("JavaConverters"))
+
+  def removeIf(p: A => Boolean): UIO[Boolean] =
+    UIO(underlying.removeIf((t: A) => !p(t)))
+
+  def retainAll(xs: Iterable[A]): UIO[Boolean] =
+    UIO(underlying.retainAll(xs.asJavaCollection): @silent("JavaConverters"))
+
+  def retainIf(p: A => Boolean): UIO[Boolean] =
+    UIO(underlying.removeIf((t: A) => p(t)))
 
   def clear: UIO[Unit] =
     UIO(underlying.clear())
@@ -34,24 +105,51 @@ final class ConcurrentSet[A] private (private val underlying: ConcurrentHashMap.
 
   def isEmpty: UIO[Boolean] =
     UIO(underlying.isEmpty)
+
+  def toSet: UIO[Set[A]] =
+    UIO(underlying.asScala.toSet: @silent("JavaConverters"))
+
+  @silent("JavaConverters")
+  def transform(f: A => A): UIO[Unit] = UIO {
+    val set = underlying.asScala.toSet
+    underlying.removeAll(set.asJavaCollection)
+    val _ = underlying.addAll(set.map(f).asJavaCollection)
+  }
+
+  private def makeConsumer(f: A => Unit): Consumer[A] =
+    new java.util.function.Consumer[A] {
+      override def accept(t: A): Unit = f(t)
+    }
 }
 
 object ConcurrentSet {
 
-  def make[A](xs: A*): UIO[ConcurrentSet[A]] =
-    UIO.effectSuspendTotal {
-      UIO {
-        val keySetView = ConcurrentHashMap.newKeySet[A]()
-        xs.foreach(x => keySetView.add(x))
-        new ConcurrentSet(keySetView)
-      }
+  def empty[A]: UIO[ConcurrentSet[A]] =
+    UIO {
+      val keySetView = ConcurrentHashMap.newKeySet[A]()
+      new ConcurrentSet(keySetView)
     }
 
-  def make[A](size: Int): UIO[ConcurrentSet[A]] =
-    UIO.effectSuspendTotal {
-      UIO {
-        val keySetView = ConcurrentHashMap.newKeySet[A](size)
-        new ConcurrentSet(keySetView)
-      }
+  def empty[A](initialCapacity: Int): UIO[ConcurrentSet[A]] =
+    UIO {
+      val keySetView = ConcurrentHashMap.newKeySet[A](initialCapacity)
+      new ConcurrentSet(keySetView)
+    }
+
+  /**
+   * Makes a new `ConcurrentSet` initialized with provided collection.
+   */
+  def fromIterable[A](as: Iterable[A]): UIO[ConcurrentSet[A]] =
+    UIO {
+      val keySetView = ConcurrentHashMap.newKeySet[A]()
+      as.foreach(x => keySetView.add(x))
+      new ConcurrentSet(keySetView)
+    }
+
+  def make[A](as: A*): UIO[ConcurrentSet[A]] =
+    UIO {
+      val keySetView = ConcurrentHashMap.newKeySet[A]()
+      as.foreach(x => keySetView.add(x))
+      new ConcurrentSet(keySetView)
     }
 }
