992fdcd44537c85c98af3d77709f500d875e190e
Adam Fraser <adam.fraser@gmail.com>
2021-10-31 22:54:32 -0600

    delete test

--- streams-tests/shared/src/test/scala/zio/stream/experimental/ZStreamSpec.scala
+++ streams-tests/shared/src/test/scala/zio/stream/experimental/ZStreamSpec.scala
@@ -1752,39 +1752,6 @@ object ZStreamSpec extends ZIOBaseSpec {
             sum <- ref.get
           } yield assert(sum)(equalTo(10))
         },
-        suite("groupAdjacentBy")(
-          test("groupAdjacentBy 1")(check(Gen.chunkOf(Gen.chunkOf(Gen.int))) { iss =>
-            val keyFn = (x: Int) => x % 2 == 0
-
-            ZStream.fromChunk(iss).flattenChunks.groupAdjacentBy(keyFn).runCollect.map { oss =>
-              val splat = oss.foldLeft[Chunk[Int]](Chunk.empty) { case (acc, (_, is)) => acc ++ is.toChunk }
-
-              def verifyInside(in: Chunk[(Boolean, NonEmptyChunk[Int])]) =
-                in.map { case (k, xs) => xs.forall(keyFn(_) == k) }
-
-              def verifyAdjacentKeys(in: Chunk[(Boolean, Any)]): Boolean =
-                in.sliding(2, 1)
-                  .foldLeft(true)((res, chunk) => res && (chunk.length == 1 || chunk(0)._1 != chunk(1)._1))
-
-              assert(splat)(equalTo(iss.flatten)) &&
-              assert(verifyInside(oss))(forall(isTrue)) &&
-              assert(verifyAdjacentKeys(oss))(isTrue)
-            }
-          }),
-          test("groupAdjacentBy 2") {
-            assertM(
-              ZStream((1, 1), (1, 2), (1, 3), (2, 1), (2, 2), (1, 4)).groupAdjacentBy(_._1).runCollect
-            )(
-              equalTo(
-                Chunk(
-                  (1, NonEmptyChunk((1, 1), (1, 2), (1, 3))),
-                  (2, NonEmptyChunk((2, 1), (2, 2))),
-                  (1, NonEmptyChunk((1, 4)))
-                )
-              )
-            )
-          }
-        ),
         suite("groupBy")(
           test("values XYZ") {
             val words = List.fill(100)(0 to 100).flatten.map(_.toString())
