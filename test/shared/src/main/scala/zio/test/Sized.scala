a371d7603141c43a23d866262db41c5691995c76
Adam Fraser <adam.fraser@gmail.com>
2021-11-03 06:41:28 -0700

    implement gen#resize

--- test/shared/src/main/scala/zio/test/Sized.scala
+++ test/shared/src/main/scala/zio/test/Sized.scala
@@ -1,11 +1,13 @@
 package zio.test
 
 import zio.{FiberRef, Has, Layer, UIO, URIO, ZIO, ZLayer, ZTraceElement}
+import zio.stream.ZStream
 import zio.stacktracer.TracingImplicits.disableAutoTrace
 
 trait Sized extends Serializable {
   def size(implicit trace: ZTraceElement): UIO[Int]
   def withSize[R, E, A](size: Int)(zio: ZIO[R, E, A])(implicit trace: ZTraceElement): ZIO[R, E, A]
+  def withSizeGen[R, A](size: Int)(gen: Gen[R, A])(implicit trace: ZTraceElement): Gen[R, A]
 }
 
 object Sized {
@@ -17,6 +19,14 @@ object Sized {
           fiberRef.get
         def withSize[R, E, A](size: Int)(zio: ZIO[R, E, A])(implicit trace: ZTraceElement): ZIO[R, E, A] =
           fiberRef.locally(size)(zio)
+        def withSizeGen[R, A](size: Int)(gen: Gen[R, A])(implicit trace: ZTraceElement): Gen[R, A] =
+          Gen {
+            ZStream
+              .fromZIO(fiberRef.get)
+              .flatMap { oldSize =>
+                ZStream.managed(fiberRef.locallyManaged(size)) *> gen.sample.mapZIO(a => fiberRef.set(oldSize).as(a))
+              }
+          }
       }
     })
 
@@ -25,4 +35,7 @@ object Sized {
 
   def withSize[R <: Has[Sized], E, A](size: Int)(zio: ZIO[R, E, A])(implicit trace: ZTraceElement): ZIO[R, E, A] =
     ZIO.accessZIO[R](_.get.withSize(size)(zio))
+
+  def withSizeGen[R <: Has[Sized], A](size: Int)(gen: Gen[R, A])(implicit trace: ZTraceElement): Gen[R, A] =
+    Gen.fromZIO(ZIO.service[Sized]).flatMap(_.withSizeGen(size)(gen))
 }
