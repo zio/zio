7c2ad270e57c3ce6d361a66860b948e0f5ba2e6f
NavidJalali <nvd.eth0@gmail.com>
2021-11-05 18:51:27 +0100

    less squigles

--- streams/shared/src/main/scala/zio/stream/experimental/ZStream.scala
+++ streams/shared/src/main/scala/zio/stream/experimental/ZStream.scala
@@ -3206,12 +3206,8 @@ class ZStream[-R, +E, +A](val channel: ZChannel[R, Any, Any, Any, E, Chunk[A], A
                 if (buffer eq null) buffer = collection.mutable.ArrayBuffer[Chunk[A]]()
                 buffer += concatenated.slice(0, concatenated.length - delimiter.length)
                 (Chunk.empty, 0)
-              } else {
-                (concatenated, delimiterCursor + 1)
-              }
-            } else {
-              (concatenated, if (a == delimiter(0)) 1 else 0)
-            }
+              } else (concatenated, delimiterCursor + 1)
+            } else (concatenated, if (a == delimiter(0)) 1 else 0)
           } match {
             case (carry, delimiterCursor) =>
               ZChannel.write(
