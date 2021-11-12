a114f12c7d95753239c4d4584b82179c3fec91d7
Andreas Gies <andreas@wayofquality.de>
2021-11-12 12:45:15 +0100

    Adding version 2 specific index file to make the checkSite script work for CI

--- scripts/checkSite.sh
+++ scripts/checkSite.sh
@@ -4,11 +4,20 @@ set -e
 
 ZIO_LATEST_2=`git describe --tags --abbrev=0 ` sbt docs/unidoc docs/mdoc
 cd website
+
 mv docusaurus.config.js docusaurus.config.js.org 
 cp version2-only-docusaurus.config.js docusaurus.config.js 
+
+mv src/pages/index.js src/pages/index.js.org
+cp src/pages/version2-only-index.js src/pages/index.js
+
 mv versions.json versions.json.org
+
 yarn install 
 yarn build 
+
 mv versions.json.org versions.json
 mv docusaurus.config.js.org docusaurus.config.js
+mv src/pages/index.js.org src/pages/index.js
+
 cd ..
