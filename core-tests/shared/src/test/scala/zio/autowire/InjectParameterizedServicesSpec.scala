e34ecda8156c3b1f205bd9841cf94c7f02ea55ca
Adam Fraser <adam.fraser@gmail.com>
2021-11-15 20:53:49 -0800

    enable automatic layer construction tests

--- core-tests/shared/src/test/scala/zio/autowire/InjectParameterizedServicesSpec.scala
+++ core-tests/shared/src/test/scala/zio/autowire/InjectParameterizedServicesSpec.scala
@@ -1,54 +1,54 @@
-// package zio.autowire
+package zio.autowire
 
-// import zio.{Tag, UIO, ZIO, ZServiceBuilder}
-// import zio.test._
-// import zio.UServiceBuilder
+import zio.{Tag, UIO, ZIO, ZServiceBuilder}
+import zio.test._
+import zio.UServiceBuilder
 
-// // https://github.com/kitlangton/zio-magic/issues/76
-// object InjectParameterizedServicesSpec extends DefaultRunnableSpec {
+// https://github.com/kitlangton/zio-magic/issues/76
+object InjectParameterizedServicesSpec extends DefaultRunnableSpec {
 
-//   def spec: ZSpec[Environment, Failure] = suite("Samples")(
-//     test("compiles with ParameterisedServiceImpl1 direct usage") {
-//       ParameterizedService.something[String].as(assertCompletes)
-//     }.inject(ParameterisedServiceWithoutTypeAlias.live),
-//     test("compiles using the type alias directly") {
-//       val huh: ZIO[ParameterizedService[ParameterizedServiceWithTypeAlias.Alias], Nothing, TestResult] =
-//         ParameterizedService.something[ParameterizedServiceWithTypeAlias.Alias].as(assertCompletes)
-//       huh
-//     }.inject(ParameterizedServiceWithTypeAlias.live),
-//     test("fails to compile using the type directly") {
-//       val huh: ZIO[ParameterizedService[String], Nothing, TestResult] =
-//         ParameterizedService.something[String].as(assertCompletes)
-//       huh
-//     }.inject(ParameterizedServiceWithTypeAlias.live),
-//     test("compile using the type directly if not using wire macro") {
-//       ParameterizedService.something[String].as(assertCompletes)
-//     }.provideServices(ParameterizedServiceWithTypeAlias.live)
-//   ) @@ TestAspect.exceptDotty
+  def spec: ZSpec[Environment, Failure] = suite("Samples")(
+    test("compiles with ParameterisedServiceImpl1 direct usage") {
+      ParameterizedService.something[String].as(assertCompletes)
+    }.inject(ParameterisedServiceWithoutTypeAlias.live),
+    test("compiles using the type alias directly") {
+      val huh: ZIO[ParameterizedService[ParameterizedServiceWithTypeAlias.Alias], Nothing, TestResult] =
+        ParameterizedService.something[ParameterizedServiceWithTypeAlias.Alias].as(assertCompletes)
+      huh
+    }.inject(ParameterizedServiceWithTypeAlias.live),
+    test("fails to compile using the type directly") {
+      val huh: ZIO[ParameterizedService[String], Nothing, TestResult] =
+        ParameterizedService.something[String].as(assertCompletes)
+      huh
+    }.inject(ParameterizedServiceWithTypeAlias.live),
+    test("compile using the type directly if not using wire macro") {
+      ParameterizedService.something[String].as(assertCompletes)
+    }.provideServices(ParameterizedServiceWithTypeAlias.live)
+  ) @@ TestAspect.exceptDotty
 
-//   trait ParameterizedService[A] {
-//     def something: UIO[Unit]
-//   }
+  trait ParameterizedService[A] {
+    def something: UIO[Unit]
+  }
 
-//   object ParameterizedService {
-//     def something[A: Tag]: ZIO[ParameterizedService[A], Nothing, Unit] =
-//       ZIO.serviceWith[ParameterizedService[A]](_.something)
-//   }
+  object ParameterizedService {
+    def something[A: Tag]: ZIO[ParameterizedService[A], Nothing, Unit] =
+      ZIO.serviceWith[ParameterizedService[A]](_.something)
+  }
 
-//   object ParameterizedServiceWithTypeAlias {
-//     type Alias = String
+  object ParameterizedServiceWithTypeAlias {
+    type Alias = String
 
-//     val live: UServiceBuilder[ParameterizedService[Alias]] =
-//       ZServiceBuilder.succeed(new ParameterizedService[Alias] {
-//         override def something: UIO[Unit] = ZIO.unit
-//       })
-//   }
+    val live: UServiceBuilder[ParameterizedService[Alias]] =
+      ZServiceBuilder.succeed(new ParameterizedService[Alias] {
+        override def something: UIO[Unit] = ZIO.unit
+      })
+  }
 
-//   object ParameterisedServiceWithoutTypeAlias {
-//     val live: UServiceBuilder[ParameterizedService[String]] =
-//       ZServiceBuilder.succeed(new ParameterizedService[String] {
-//         override def something: UIO[Unit] = ZIO.unit
-//       })
-//   }
+  object ParameterisedServiceWithoutTypeAlias {
+    val live: UServiceBuilder[ParameterizedService[String]] =
+      ZServiceBuilder.succeed(new ParameterizedService[String] {
+        override def something: UIO[Unit] = ZIO.unit
+      })
+  }
 
-// }
+}
