c059d3eb7a1bc26188242f1d1454f5fa27855d22
Adam Fraser <adam.fraser@gmail.com>
2021-11-16 18:33:07 -0800

    comment out examples

--- examples/shared/src/main/scala/zio/examples/ServiceBuilderDefinitionExample.scala
+++ examples/shared/src/main/scala/zio/examples/ServiceBuilderDefinitionExample.scala
@@ -1,31 +1,31 @@
-package zio.examples
-import zio._
+// package zio.examples
+// import zio._
 
-object ServiceBuilderDefinitionExample extends ZIOAppDefault {
-  trait Foo {
-    def bar: UIO[Unit]
-  }
+// object ServiceBuilderDefinitionExample extends ZIOAppDefault {
+//   trait Foo {
+//     def bar: UIO[Unit]
+//   }
 
-  object Foo {
-    val live: URServiceBuilder[Console with String with Int, Foo] =
-      (FooLive.apply _).toServiceBuilder
+//   object Foo {
+//     val live: URServiceBuilder[Console with String with Int, Foo] =
+//       (FooLive.apply _).toServiceBuilder
 
-    case class FooLive(console: Console, string: String, int: Int) extends Foo {
-      override def bar: UIO[Unit] = console.printLine(s"$string and $int").orDie
-    }
-  }
+//     case class FooLive(console: Console, string: String, int: Int) extends Foo {
+//       override def bar: UIO[Unit] = console.printLine(s"$string and $int").orDie
+//     }
+//   }
 
-  override def run: ZIO[ZEnv with ZIOAppArgs, Any, Any] = {
+//   override def run: ZIO[ZEnv with ZIOAppArgs, Any, Any] = {
 
-    val program: ZIO[Foo, Nothing, Unit] = ZIO.serviceWith[Foo](_.bar)
+//     val program: ZIO[Foo, Nothing, Unit] = ZIO.serviceWith[Foo](_.bar)
 
-    program
-      .inject(
-        Console.live,
-        ZServiceBuilder.succeed("Hello"),
-        ZServiceBuilder.succeed(3),
-        Foo.live
-      )
-  }
+//     program
+//       .inject(
+//         Console.live,
+//         ZServiceBuilder.succeed("Hello"),
+//         ZServiceBuilder.succeed(3),
+//         Foo.live
+//       )
+//   }
 
-}
+// }
