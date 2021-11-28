562331d2d2c48cc3f841acd92f4c4aa171c86045
John A. De Goes <john@degoes.net>
2021-11-28 15:51:13 +0000

    Add full version of evalOn

--- core/shared/src/main/scala/zio/Fiber.scala
+++ core/shared/src/main/scala/zio/Fiber.scala
@@ -486,9 +486,25 @@ object Fiber extends FiberPlatformSpecific {
     final def dump(implicit trace: ZTraceElement): UIO[Fiber.Dump] = dumpWith(true)
 
     /**
-     * Evaluates the specified
+     * Evaluates the specified effect on the fiber. If this is not possible,
+     * because the fiber has already ended life, then the specified alternate
+     * effect will be executed instead.
      */
     def evalOn(effect: UIO[Any], orElse: UIO[Any])(implicit trace: ZTraceElement): UIO[Unit]
+
+    /**
+     * A fully-featured, but much slower version of `evalOn`, which is useful
+     * when environment and error are required.
+     */
+    def evalOnZIO[R, E2, A2](effect: ZIO[R, E2, A2], orElse: ZIO[R, E2, A2])(implicit
+      trace: ZTraceElement
+    ): ZIO[R, E2, A2] =
+      for {
+        r <- ZIO.environment[R]
+        p <- Promise.make[E2, A2]
+        _ <- evalOn(effect.provideEnvironment(r).intoPromise(p), orElse.provideEnvironment(r).intoPromise(p))
+        a <- p.await
+      } yield a
 
     /**
      * The identity of the fiber.
