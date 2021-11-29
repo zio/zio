1f56dc8b90339e5ad7589e2bfc0ad533e6f57efe
John A. De Goes <john@degoes.net>
2021-11-29 20:14:42 +0000

    Moar tests

--- core-tests/shared/src/test/scala/zio/EvalOnSpec.scala
+++ core-tests/shared/src/test/scala/zio/EvalOnSpec.scala
@@ -5,14 +5,22 @@ import zio.test.TestAspect._
 
 object EvalOnSpec extends ZIOBaseSpec {
   def spec = suite("EvalOnSpec") {
-    test("evalOn") {
+    test("evalOn - promise") {
       for {
         promise <- Promise.make[Nothing, Int]
         fiber   <- ZIO.succeed(12).forever.fork
         _       <- fiber.evalOn(promise.succeed(42), UIO.unit)
-        v       <- promise.await
+        v       <- promise.await <* fiber.interrupt
       } yield assertTrue(v == 42)
     } @@ nonFlaky +
+      test("evalOn - fiberId") {
+        for {
+          promise <- Promise.make[Nothing, FiberId]
+          fiber   <- ZIO.succeed(12).forever.fork
+          _       <- fiber.evalOn(ZIO.fiberId.intoPromise(promise), UIO.never)
+          v       <- promise.await <* fiber.interrupt
+        } yield assertTrue((fiber.id: FiberId) == v)
+      } @@ nonFlaky +
       test("evalOnZIO orElse") {
         for {
           ref   <- Ref.make(0)
