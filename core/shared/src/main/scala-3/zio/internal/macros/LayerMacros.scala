75131ad28b9eaed38741dd3f4bc9f2d203f7d9f8
Kit Langton <kit.langton@gmail.com>
2021-12-05 19:37:35 +0000

    scala 3 layer debug

--- core/shared/src/main/scala-3/zio/internal/macros/LayerMacros.scala
+++ core/shared/src/main/scala-3/zio/internal/macros/LayerMacros.scala
@@ -5,6 +5,9 @@ import zio._
 import scala.quoted._
 import scala.compiletime._
 import zio.internal.macros.StringUtils.StringOps
+import java.nio.charset.StandardCharsets
+import java.util.Base64
+
 
 import LayerMacroUtils._
 
@@ -38,9 +41,90 @@ object LayerMacros {
 
     val nodes = zEnvLayer ++ getNodes(layer)
 
-    val dep = buildMemoizedLayer(ctx)(ZLayerExprBuilder.fromNodes(ctx)(nodes), requirements)
+    val graph = ZLayerExprBuilder.fromNodes(ctx)(nodes)
+    val dep = buildMemoizedLayer(ctx)(graph, requirements)
+
+    debug.foreach { debug =>
+      debugLayer(debug, graph, requirements)
+    }
+
     '{$dep.asInstanceOf[ZLayer[R0,E,R]] }
   }
+
+
+  private def debugLayer(using ctx: Quotes)(
+                          debug: ZLayer.Debug,
+                          graph: ZLayerExprBuilder[ctx.reflect.TypeRepr, LayerExpr],
+                          requirements: List[ctx.reflect.TypeRepr]
+                        ): Unit = {
+    import ctx.reflect._
+
+    val graphString: String =
+
+        graph.graph
+          .map(layer => RenderedGraph(layer.show))
+          .buildComplete(requirements)
+        .toOption.get
+        .fold[RenderedGraph](RenderedGraph.Row(List.empty), identity, _ ++ _, _ >>> _)
+        .render
+
+    val title   = "  ZLayer Wiring Graph  ".yellow.bold.inverted
+    val builder = new StringBuilder
+    builder ++= "\n" + title + "\n\n" + graphString + "\n\n"
+
+    if (debug == ZLayer.Debug.Mermaid) {
+      val mermaidLink: String = generateMermaidJsLink(requirements, graph)
+      builder ++= "Mermaid Live Editor Link".underlined + "\n" + mermaidLink.faint + "\n\n"
+    }
+
+    report.info(builder.result())
+  }
+
+  /**
+   * Generates a link of the layer graph for the Mermaid.js graph viz library's
+   * live-editor (https://mermaid-js.github.io/mermaid-live-editor)
+   */
+  private def generateMermaidJsLink(using ctx: Quotes)(
+    requirements: List[ctx.reflect.TypeRepr],
+    graph: ZLayerExprBuilder[ctx.reflect.TypeRepr, LayerExpr]
+  ): String = {
+    val cool =
+      graph.graph
+        .map(layer => layer.show)
+        .buildComplete(requirements)
+    .toOption.get
+
+    val map = cool.fold[Map[String, Chunk[String]]](
+      z = Map.empty,
+      value = str => Map(str -> Chunk.empty),
+      composeH = _ ++ _,
+      composeV = (m1, m2) =>
+        m2.map { case (key, values) =>
+          val result = m1.keys.toSet -- m1.values.flatten.toSet
+          key -> (values ++ Chunk.fromIterable(result))
+        } ++ m1
+    )
+
+    val mermaidGraphString = map.flatMap {
+      case (key, children) if children.isEmpty =>
+        List(s"    $key")
+      case (key, children) =>
+        children.map { child =>
+          s"    $key --> $child"
+        }
+    }
+      .mkString("\\n")
+
+    val mermaidGraph =
+      s"""{"code":"graph\\n$mermaidGraphString\\n    ","mermaid": "{\\n  \\"theme\\": \\"default\\"\\n}", "updateEditor": true, "autoSync": true, "updateDiagram": true}"""
+
+    val encodedMermaidGraph: String =
+      new String(Base64.getEncoder.encode(mermaidGraph.getBytes(StandardCharsets.UTF_8)), StandardCharsets.UTF_8)
+
+    val mermaidLink = s"https://mermaid-js.github.io/mermaid-live-editor/edit/#$encodedMermaidGraph"
+    mermaidLink
+  }
+
 }
 
 
@@ -87,3 +171,4 @@ trait ExprGraphCompileVariants { self : ZLayerExprBuilder.type =>
       )
   }
 }
+
