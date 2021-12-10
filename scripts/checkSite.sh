7f6d54bd8c84a28807fac751e51fcfd480231e4d
Andreas Gies <andreas@wayofquality.de>
2021-12-10 10:03:35 +0100

    Hopefully final tweak for check website

--- scripts/checkSite.sh
+++ scripts/checkSite.sh
@@ -8,8 +8,6 @@ cd website
 mv docusaurus.config.js docusaurus.config.js.org 
 cp version2-only-docusaurus.config.js docusaurus.config.js 
 
-mv src/theme src/theme.org
-
 mv src/pages/index.js src/pages/index.js.org
 cp src/pages/version2-only-index.js.txt src/pages/index.js
 
@@ -24,6 +22,5 @@ yarn build
 mv versions.json.org versions.json
 mv docusaurus.config.js.org docusaurus.config.js
 mv src/pages/index.js.org src/pages/index.js
-mv src/theme.org src/theme
 
 cd ..
