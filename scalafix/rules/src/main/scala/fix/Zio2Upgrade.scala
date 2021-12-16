210151c1e4e95bcffed97e1f18d454702affb019
Bill Frasure <bill.frasure@gmail.com>
2021-12-16 08:39:12 -0700

    Consolidate Scalafix rule into 1 file

--- scalafix/rules/src/main/scala/fix/Zio2Upgrade.scala
+++ scalafix/rules/src/main/scala/fix/Zio2Upgrade.scala
@@ -686,6 +686,31 @@ class Zio2Upgrade extends SemanticRule("Zio2Upgrade") {
     case Some(t: Term.Select) => unwindSelect(t)
     case _                    => t
   }
+  
+  object Zio2ZIOSpec extends SemanticRule("ZIOSpecMigration"){
+    val zio2UpgradeRule = new Zio2Upgrade()
+    val AbstractRunnableSpecRenames = zio2UpgradeRule.Renames(
+      List("zio.test.DefaultRunnableSpec" /* TODO What other types here? */),
+      Map(
+        "Failure"            -> "Any",
+      )
+    )
+
+    override def fix(implicit doc: SemanticDocument): Patch =
+      doc.tree.collect {
+        case AbstractRunnableSpecRenames.Matcher(patch) => patch
+
+        // TODO Check if we really want to do this, or if we want to keep it now that we might have a
+        //    meaningful Failure type
+        case t @ q"override def spec: $tpe = $body" if tpe.toString().contains("ZSpec[Environment, Failure]") =>
+          Patch.replaceTree(t, s"override def spec = $body")
+      }.asPatch + replaceSymbols
+
+    def replaceSymbols(implicit doc: SemanticDocument) = Patch.replaceSymbols(
+      "zio.test.DefaultRunnableSpec" -> "zio.test.ZIOSpecDefault"
+    )
+
+  }
 }
 
 private object ImporteeNameOrRename {
