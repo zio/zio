179b66615fb295ff2fbf8d65d52d226bf6103b9f
Adam Fraser <adam.fraser@gmail.com>
2021-12-16 12:03:27 -0800

    Implement ZPipeline#fromPush (#6177)
    * implement pipeline from push
    
    * propagate tracing information

--- streams/shared/src/main/scala/zio/stream/ZPipeline.scala
+++ streams/shared/src/main/scala/zio/stream/ZPipeline.scala
@@ -203,6 +203,39 @@ object ZPipeline extends ZPipelinePlatformSpecificConstructors {
     }
 
   /**
+   * Creates a pipeline from a chunk processing function.
+   */
+  def fromPush[Env, Err, In, Out](
+    push: => ZManaged[Env, Nothing, Option[Chunk[In]] => ZIO[Env, Err, Chunk[Out]]]
+  ): ZPipeline[Env, Err, In, Out] =
+    new ZPipeline[Env, Err, In, Out] {
+      def apply[Env1 <: Env, Err1 >: Err](stream: ZStream[Env1, Err1, In])(implicit
+        trace: ZTraceElement
+      ): ZStream[Env1, Err1, Out] = {
+
+        def pull(
+          push: Option[Chunk[In]] => ZIO[Env, Err, Chunk[Out]]
+        ): ZChannel[Env, Nothing, Chunk[In], Any, Err, Chunk[Out], Any] =
+          ZChannel.readWith[Env, Nothing, Chunk[In], Any, Err, Chunk[Out], Any](
+            in =>
+              ZChannel
+                .fromZIO(push(Some(in)))
+                .flatMap(out => ZChannel.write(out))
+                .zipRight[Env, Nothing, Chunk[In], Any, Err, Chunk[Out], Any](pull(push)),
+            err => ZChannel.fail(err),
+            _ => ZChannel.fromZIO(push(None)).flatMap(out => ZChannel.write(out))
+          )
+
+        val channel: ZChannel[Env, Nothing, Chunk[In], Any, Err, Chunk[Out], Any] =
+          ZChannel.unwrapManaged[Env, Nothing, Chunk[In], Any, Err, Chunk[Out], Any] {
+            push.map(pull)
+          }
+
+        stream.pipeThroughChannelOrFail(channel)
+      }
+    }
+
+  /**
    * The identity pipeline, which does not modify streams in any way.
    */
   def identity[In]: ZPipeline[Any, Nothing, In, In] =
