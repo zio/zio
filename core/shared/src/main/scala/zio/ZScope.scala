1b603c479ee1edf54e742da3f1811efa4d637bab
Adam Fraser <adam.fraser@gmail.com>
2021-12-24 12:24:23 -0800

    format

--- core/shared/src/main/scala/zio/ZScope.scala
+++ core/shared/src/main/scala/zio/ZScope.scala
@@ -23,8 +23,8 @@ import java.lang.ref.WeakReference
 
 /**
  * A `ZScope` represents the scope of a fiber lifetime. The scope of a fiber can
- * be retrieved using [[ZIO.scope]], and when forking fibers, you can
- * specify a custom scope to fork them on by using the [[ZIO#forkIn]].
+ * be retrieved using [[ZIO.scope]], and when forking fibers, you can specify a
+ * custom scope to fork them on by using the [[ZIO#forkIn]].
  */
 sealed trait ZScope {
   def fiberId: FiberId
