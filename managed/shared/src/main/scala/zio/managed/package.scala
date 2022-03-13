70748f6386228af47799185901dd57e3234e1d00
Adam Fraser <adam.fraser@gmail.com>
2022-03-13 08:25:33 +0100

    add zmanaged zio extension methods

--- managed/shared/src/main/scala/zio/managed/package.scala
+++ managed/shared/src/main/scala/zio/managed/package.scala
@@ -16,7 +16,7 @@
 
 package zio
 
-package object managed extends LowPriority {
+package object managed {
 
   type Managed[+E, +A]   = ZManaged[Any, E, A]         //Manage an `A`, may fail with `E`        , no requirements
   type TaskManaged[+A]   = ZManaged[Any, Throwable, A] //Manage an `A`, may fail with `Throwable`, no requirements
@@ -29,13 +29,64 @@ package object managed extends LowPriority {
       ???
   }
 
-  implicit final class ZIOSyntax[R, E, A](private val self: ZIO[R, E, A]) {
-    def toManaged: ZManaged[R, E, A] =
-      ???
+  implicit final class ZManagedZIOSyntax[R, E, A](private val self: ZIO[R, E, A]) {
+
+    /**
+     * Forks the fiber in a [[ZManaged]]. Using the [[ZManaged]] value will
+     * execute the effect in the fiber, while ensuring its interruption when the
+     * effect supplied to [[ZManaged#use]] completes.
+     */
+    final def forkManaged(implicit trace: ZTraceElement): ZManaged[R, Nothing, Fiber.Runtime[E, A]] =
+      self.toManaged.fork
+
+    /**
+     * Converts this ZIO to [[ZManaged]] with no release action. It will be
+     * performed interruptibly.
+     */
+    final def toManaged(implicit trace: ZTraceElement): ZManaged[R, E, A] =
+      ZManaged.fromZIO(self)
+
+    /**
+     * Converts this ZIO to [[Managed]]. This ZIO and the provided release
+     * action will be performed uninterruptibly.
+     */
+    final def toManagedWith[R1 <: R](release: A => URIO[R1, Any])(implicit trace: ZTraceElement): ZManaged[R1, E, A] =
+      ZManaged.acquireReleaseWith(self)(release)
+
+    /**
+     * Converts this ZIO to [[ZManaged]] with no release action. It will be
+     * performed interruptibly.
+     */
+    @deprecated("use toManaged", "2.0.0")
+    final def toManaged_(implicit trace: ZTraceElement): ZManaged[R, E, A] =
+      self.toManaged
   }
-}
 
-trait LowPriority {
-  import zio.managed.ZManaged
+  implicit final class ZManagedZIOAutoCloseableSyntax[R, E, A <: AutoCloseable](private val self: ZIO[R, E, A])
+      extends AnyVal {
 
+    /**
+     * Converts this ZIO value to a ZManaged value. See
+     * [[ZManaged.fromAutoCloseable]].
+     */
+    def toManagedAuto(implicit trace: ZTraceElement): ZManaged[R, E, A] =
+      ZManaged.fromAutoCloseable(self)
+  }
+
+  implicit final class ZManagedZIOCompanionSyntax(private val self: ZIO.type) extends AnyVal {
+
+    /**
+     * Acquires a resource, uses the resource, and then releases the resource.
+     * However, unlike `acquireReleaseWith`, the separation of these phases
+     * allows the acquisition to be interruptible.
+     *
+     * Useful for concurrent data structures and other cases where the
+     * 'deallocator' can tell if the allocation succeeded or not just by
+     * inspecting internal / external state.
+     */
+    def reserve[R, E, A, B](reservation: => ZIO[R, E, Reservation[R, E, A]])(use: A => ZIO[R, E, B])(implicit
+      trace: ZTraceElement
+    ): ZIO[R, E, B] =
+      ZManaged.fromReservationZIO(reservation).use(use)
+  }
 }
