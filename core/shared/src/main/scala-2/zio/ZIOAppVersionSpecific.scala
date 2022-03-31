2de11224c290bf00e4c55c66343d6c0c8e576ab9
Kit Langton <kit.langton@gmail.com>
2022-03-31 14:16:15 -0400

    fix provide some

--- core/shared/src/main/scala-2/zio/ZIOAppVersionSpecific.scala
+++ core/shared/src/main/scala-2/zio/ZIOAppVersionSpecific.scala
@@ -9,7 +9,7 @@ trait ZIOAppVersionSpecific { self: ZIOApp =>
    * If it is missing requirements, it will report a descriptive error message.
    * Otherwise, the effect will be returned unmodified.
    */
-  implicit def validateEnv[R, E, A](zio: ZIO[R, E, A]): ZIO[Environment with ZEnv with ZIOAppArgs, E, A] =
-    macro internal.macros.LayerMacros.validate[Environment with ZEnv with ZIOAppArgs, R]
+  implicit def validateEnv[R, E, A](zio: ZIO[R, E, A]): ZIO[Environment with ZIOAppArgs with Scope, E, A] =
+    macro internal.macros.LayerMacros.validate[Environment with ZIOAppArgs with Scope, R]
 
 }
