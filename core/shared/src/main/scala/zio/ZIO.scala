2b9ee3d82a87ac0e3dab3e89426a4ba2d4429073
Adam Fraser <adam.fraser@gmail.com>
2022-03-31 04:14:53 -0700

    Slightly Simplify ZIO#scheduleBackground Implementation (#6496)
    * simplify
    
    * rename
    
    * rename

--- core/shared/src/main/scala/zio/ZIO.scala
+++ core/shared/src/main/scala/zio/ZIO.scala
@@ -2035,15 +2035,6 @@ sealed trait ZIO[-R, +E, +A] extends Serializable with ZIOPlatformSpecific[R, E,
     Clock.schedule(self)(schedule)
 
   /**
-   * Runs this effect according to the specified schedule in a new fiber
-   * attached to the global scope
-   */
-  final def scheduleBackground[R1 <: R, B](schedule: => Schedule[R1, Any, B])(implicit
-    trace: ZTraceElement
-  ): ZIO[R1 with Clock with Scope, E, Fiber.Runtime[Any, B]] =
-    ZIO.acquireRelease(ZIO.interruptible(self.schedule(schedule)).forkDaemon)(_.interrupt)
-
-  /**
    * Runs this effect according to the specified schedule starting from the
    * specified input value.
    */
@@ -2051,6 +2042,15 @@ sealed trait ZIO[-R, +E, +A] extends Serializable with ZIOPlatformSpecific[R, E,
     schedule: => Schedule[R1, A1, B]
   )(implicit trace: ZTraceElement): ZIO[R1 with Clock, E, B] =
     Clock.scheduleFrom[R, R1, E, A, A1, B](self)(a)(schedule)
+
+  /**
+   * Runs this effect according to the specified schedule in a new fiber
+   * attached to the current scope.
+   */
+  final def scheduleFork[R1 <: R, B](schedule: => Schedule[R1, Any, B])(implicit
+    trace: ZTraceElement
+  ): ZIO[R1 with Clock with Scope, E, Fiber.Runtime[Any, B]] =
+    self.schedule(schedule).forkScoped
 
   /**
    * Converts an option on values into an option on errors.
