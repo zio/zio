227b7f24771aca11c6cb537f06fbe6741365390f
Adam Fraser <adam.fraser@gmail.com>
2022-04-01 06:58:08 -0700

    delete canceler type alias

--- streams/jvm/src/main/scala/zio/stream/platform.scala
+++ streams/jvm/src/main/scala/zio/stream/platform.scala
@@ -57,7 +57,7 @@ trait ZStreamPlatformSpecificConstructors {
    * be used to signal the end of the stream, by setting it to `None`.
    */
   def asyncInterrupt[R, E, A](
-    register: ZStream.Emit[R, E, A, Unit] => Either[Canceler[R], ZStream[R, E, A]],
+    register: ZStream.Emit[R, E, A, Unit] => Either[URIO[R, Any], ZStream[R, E, A]],
     outputBuffer: => Int = 16
   )(implicit trace: ZTraceElement): ZStream[R, E, A] =
     ZStream.unwrapScoped[R](for {
@@ -200,7 +200,7 @@ trait ZStreamPlatformSpecificConstructors {
    */
   @deprecated("use asyncInterrupt", "2.0.0")
   def effectAsyncInterrupt[R, E, A](
-    register: ZStream.Emit[R, E, A, Unit] => Either[Canceler[R], ZStream[R, E, A]],
+    register: ZStream.Emit[R, E, A, Unit] => Either[URIO[R, Any], ZStream[R, E, A]],
     outputBuffer: => Int = 16
   )(implicit trace: ZTraceElement): ZStream[R, E, A] =
     asyncInterrupt(register, outputBuffer)
