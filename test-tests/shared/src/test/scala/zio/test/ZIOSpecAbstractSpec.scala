62bfd2b52b85da9cb5e2b7b5ceeae6d096236dbc
Bill Frasure <bill.frasure@gmail.com>
2022-04-13 12:00:21 -0600

    Reliably replicate InterruptedException via nonFlaky

--- test-tests/shared/src/test/scala/zio/test/ZIOSpecAbstractSpec.scala
+++ test-tests/shared/src/test/scala/zio/test/ZIOSpecAbstractSpec.scala
@@ -3,10 +3,18 @@ import zio.{Scope, ZIO, ZIOAppArgs}
 
 object ZIOSpecAbstractSpec extends ZIOSpecDefault {
   override def spec = test("highlighting composed layer failures")(for {
-    _ <- ZIO.debug("Let's compose things")
+    _ <- ZIO.debug("==================== New Test Run ====================")
     composedSpec: ZIOSpecAbstract = AMinimalSpec <> BMinimalSpec <> MultiCMinimalSpec <> SmallMinimalSpec <> SlowMinimalSpec <> SingleMinimalSpec
     _ <- ZIO.consoleWith(console =>  composedSpec.runSpec(composedSpec.spec, TestArgs.empty, console))
+    output <- testOutput
 
-  } yield assertCompletes
-  ).provide(ZIOAppArgs.empty, testEnvironment, Scope.default)
+  } yield assertTrue(output.mkString("\n").contains("NotImplementedError"))
+  ).provide(ZIOAppArgs.empty, testEnvironment, Scope.default) @@ TestAspect.nonFlaky
+
+  private val testOutput =
+    for {
+      console <- testConsole
+      output  <- console.output
+    } yield output
+
 }
