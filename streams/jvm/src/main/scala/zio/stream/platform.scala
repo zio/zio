7cb50b3189b656ca023e25e48a74c37da85c4943
Adam Fraser <adam.fraser@gmail.com>
2022-04-20 17:40:30 -0700

    make more of stream api private

--- streams/jvm/src/main/scala/zio/stream/platform.scala
+++ streams/jvm/src/main/scala/zio/stream/platform.scala
@@ -30,7 +30,7 @@ import java.util.zip.{DataFormatException, Inflater}
 import java.{util => ju}
 import scala.annotation.tailrec
 
-trait ZStreamPlatformSpecificConstructors {
+private[stream] trait ZStreamPlatformSpecificConstructors {
   self: ZStream.type =>
 
   /**
@@ -524,7 +524,7 @@ trait ZStreamPlatformSpecificConstructors {
   }
 }
 
-trait ZSinkPlatformSpecificConstructors {
+private[stream] trait ZSinkPlatformSpecificConstructors {
   self: ZSink.type =>
 
   /**
@@ -677,7 +677,7 @@ trait ZSinkPlatformSpecificConstructors {
 
 }
 
-trait ZPipelinePlatformSpecificConstructors {
+private[stream] trait ZPipelinePlatformSpecificConstructors {
   def deflate(
     bufferSize: => Int = 64 * 1024,
     noWrap: => Boolean = false,
