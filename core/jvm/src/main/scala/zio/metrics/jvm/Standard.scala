8381f10eeaa8dbbbbd948cfcd6eadc2cc68f6806
Kit Langton <kit.langton@gmail.com>
2022-04-25 17:25:08 -0400

    Add readFileString and writeFileString convenience methods (#6655)
    * implement readFileString & writeFileString
    
    * rename file read/write methods

--- core/jvm/src/main/scala/zio/metrics/jvm/Standard.scala
+++ core/jvm/src/main/scala/zio/metrics/jvm/Standard.scala
@@ -62,7 +62,7 @@ object Standard {
     residentMemorySize: Gauge[Double]
   ): ZIO[Any, Throwable, Unit] =
     ZIO.scoped {
-      ZIO.readFile("/proc/self/status").flatMap { stream =>
+      ZIO.readFileInputStream("/proc/self/status").flatMap { stream =>
         stream
           .readAll(8192)
           .catchAll {
