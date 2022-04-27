5200a3ae214cfd7d2cd233e09dc4570c7f386dd5
Bill Frasure <bill.frasure@gmail.com>
2022-04-27 09:59:06 -0600

    Work on input/output for Managed/Scope migration

---
+++ scalafix/output/src/main/scala/fix/ManagedPieces.scala
@@ -0,0 +1,32 @@
+package fix
+
+import zio.{Scope, ZIO}
+
+import java.io.{FileInputStream, IOException}
+
+object ManagedPieces {
+  def make(): ZIO[Int with Scope, IOException, String] = ???
+
+  val resource = make()
+  ZIO.scoped {
+    resource.flatMap(f)
+  }
+
+  val f = (s: String) => ZIO.unit
+
+  ZIO.scoped {
+    resource.flatMap(f)
+  }
+
+  ZIO.scoped {
+    ZIO
+    .fromAutoCloseable(ZIO.attemptBlockingIO(scala.io.Source.fromFile("file.txt")))
+    .flatMap(x => ZIO.succeed(x.getLines().length))
+  }
+
+  ZIO.acquireRelease(ZIO.succeed("hi"))(s => ZIO.succeed("End" + s))
+
+  ZIO.fromAutoCloseable(
+    ZIO.attemptBlockingIO(new FileInputStream("file.txt"))
+  )
+}
