10fe2d2ef5fb4e7eab40fa54042ce0bf64501804
Kit Langton <kit.langton@gmail.com>
2022-04-29 18:05:50 -0400

    update magnolia and fix DeriveDiff (#6743)
    * update magnolia and fix DeriveDiff
    
    * DeriveDiff dotty done

--- test-magnolia/shared/src/main/scala-2.12-2.13/zio/test/magnolia/diff/package.scala
+++
@@ -1,9 +0,0 @@
-package zio.test.magnolia
-
-import zio.test.diff.DiffInstances
-
-/**
- * Use `import zio.test.magnolia.diff._` to get automatic case class diffing
- * when using `smartAssert(result == SomeCaseClass("Nice", 123))`.
- */
-package object diff extends DiffInstances with DeriveDiff {}
