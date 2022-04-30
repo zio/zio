1be7f82642899adef6235b1c4d5f56a2bce1f607
Adam Fraser <adam.fraser@gmail.com>
2022-04-29 18:00:51 -0700

    cleanup

--- core/js/src/main/scala/zio/RuntimePlatformSpecific.scala
+++ core/js/src/main/scala/zio/RuntimePlatformSpecific.scala
@@ -1,24 +1,52 @@
+/*
+ * Copyright 2017-2022 John A. De Goes and the ZIO Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package zio
+
+import zio.stacktracer.TracingImplicits.disableAutoTrace
 
 import scala.concurrent.ExecutionContext
 import scala.scalajs.js.Dynamic.{global => jsglobal}
 
 private[zio] trait RuntimePlatformSpecific {
 
+  /**
+   * The default number of operations the ZIO runtime should execute before
+   * yielding to other fibers.
+   */
   val defaultYieldOpCount: Int =
     2048
+
+  val defaultExecutor: Executor =
+    Executor.fromExecutionContext(defaultYieldOpCount)(ExecutionContext.global)
+
+  val defaultBlockingExecutor: Executor =
+    defaultExecutor
+
+  val defaultFatal: Set[Class[_ <: Throwable]] =
+    Set.empty
+
+  val defaultFlags: Set[RuntimeConfigFlag] =
+    Set(RuntimeConfigFlag.EnableFiberRoots)
 
   val defaultReportFatal: Throwable => Nothing =
     (t: Throwable) => {
       t.printStackTrace()
       throw t
     }
-
-  val defaultSupervisors: Set[Supervisor[Any]] =
-    Set.empty
-
-  val defaultFlags: Set[RuntimeConfigFlag] =
-    Set(RuntimeConfigFlag.EnableFiberRoots)
 
   val defaultLoggers: Set[ZLogger[String, Any]] = {
     val logger: ZLogger[String, Unit] =
@@ -47,15 +75,9 @@ private[zio] trait RuntimePlatformSpecific {
         }
       }
 
-    Set(logger)
+    Set(logger.filterLogLevel(_ >= LogLevel.Info))
   }
 
-  val defaultFatal: Set[Class[_ <: Throwable]] =
+  val defaultSupervisors: Set[Supervisor[Any]] =
     Set.empty
-
-  val defaultExecutor: Executor =
-    Executor.fromExecutionContext(defaultYieldOpCount)(ExecutionContext.global)
-
-  val defaultBlockingExecutor: Executor =
-    defaultExecutor
 }
--- core/jvm/src/main/scala/zio/RuntimePlatformSpecific.scala
+++ core/jvm/src/main/scala/zio/RuntimePlatformSpecific.scala
@@ -1,11 +1,47 @@
+/*
+ * Copyright 2017-2022 John A. De Goes and the ZIO Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package zio
 
 import zio.internal.Blocking
+import zio.stacktracer.TracingImplicits.disableAutoTrace
 
 private[zio] trait RuntimePlatformSpecific {
 
+  /**
+   * The default number of operations the ZIO runtime should execute before
+   * yielding to other fibers.
+   */
   val defaultYieldOpCount: Int =
     2048
+
+  val defaultExecutor: Executor =
+    Executor.makeDefault(defaultYieldOpCount)
+
+  val defaultBlockingExecutor: Executor =
+    Blocking.blockingExecutor
+
+  val defaultFatal: Set[Class[_ <: Throwable]] =
+    Set(classOf[VirtualMachineError])
+
+  val defaultFlags: Set[RuntimeConfigFlag] =
+    Set(RuntimeConfigFlag.EnableFiberRoots)
+
+  val defaultLoggers: Set[ZLogger[String, Any]] =
+    Set(ZLogger.default.map(println(_)).filterLogLevel(_ >= LogLevel.Info))
 
   val defaultReportFatal: Throwable => Nothing =
     (t: Throwable) => {
@@ -18,19 +54,4 @@ private[zio] trait RuntimePlatformSpecific {
 
   val defaultSupervisors: Set[Supervisor[Any]] =
     Set.empty
-
-  val defaultFlags: Set[RuntimeConfigFlag] =
-    Set(RuntimeConfigFlag.EnableFiberRoots)
-
-  val defaultLoggers: Set[ZLogger[String, Any]] =
-    Set(ZLogger.default.map(println(_)).filterLogLevel(_ >= LogLevel.Info))
-
-  val defaultFatal: Set[Class[_ <: Throwable]] =
-    Set(classOf[VirtualMachineError])
-
-  val defaultExecutor: Executor =
-    Executor.makeDefault(defaultYieldOpCount)
-
-  val defaultBlockingExecutor: Executor =
-    Blocking.blockingExecutor
 }
