b7e9864de151b2ea570400ae303b03ba2bc5f7f6
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2022-05-25 16:40:50 +0100

    Always add TestAspect.fibers

--- test-tests/shared/src/test/scala/zio/test/ZIOBaseSpec.scala
+++ test-tests/shared/src/test/scala/zio/test/ZIOBaseSpec.scala
@@ -4,6 +4,6 @@ import zio._
 
 trait ZIOBaseSpec extends ZIOSpecDefault {
   override def aspects: Chunk[TestAspectAtLeastR[TestEnvironment]] =
-    if (TestPlatform.isJVM) Chunk(TestAspect.timeout(120.seconds), TestAspect.fibers)
-    else Chunk(TestAspect.timeout(120.seconds), TestAspect.sequential, TestAspect.fibers)
+    if (TestPlatform.isJVM) Chunk(TestAspect.timeout(120.seconds))
+    else Chunk(TestAspect.timeout(120.seconds), TestAspect.sequential)
 }
