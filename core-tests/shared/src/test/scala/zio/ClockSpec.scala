8192aa2a12975d4b027ed6664b6ada01f1d1cddd
Simon Popugaev <senia.psm@gmail.com>
2022-06-08 18:57:20 +0300

    remove some tests

--- core-tests/shared/src/test/scala/zio/ClockSpec.scala
+++ core-tests/shared/src/test/scala/zio/ClockSpec.scala
@@ -49,23 +49,6 @@ object ClockSpec extends ZIOBaseSpec {
           } yield assertTrue(now == instant)
         }
       },
-      test("should allow sleep") {
-        for {
-          queue <- Queue.unbounded[Int]
-          fiber <- ZIO.foreachDiscard(1 to 3)(i => ZIO.sleep(1.second) *> queue.offer(i)).fork
-          res   <- ZIO.foreach(Vector.fill(3)(0))(_ => TestClock.adjust(1.second) *> queue.take <*> queue.poll)
-          _     <- fiber.interrupt
-        } yield assertTrue(res == Vector((1, None), (2, None), (3, None)))
-      },
-      test("should allow multiple sleeps") {
-        for {
-          queue <- Queue.unbounded[Int]
-          fiber <- ZIO.foreachDiscard(1 to 3)(i => ZIO.sleep(1.second) *> queue.offer(i)).fork
-          _     <- TestClock.adjust(3.seconds)
-          res   <- ZIO.foreach(Vector.fill(3)(0))(_ => queue.take)
-          _     <- fiber.interrupt
-        } yield assertTrue(res == Vector(1, 2, 3))
-      },
       test("should allow long sleep") {
         val maxMillisDuration = Duration.fromMillis(Long.MaxValue)
 
