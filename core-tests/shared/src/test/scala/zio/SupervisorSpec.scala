67f709ed4dc1da5f1244d69b23aa40c9574782da
Adam Fraser <adam.fraser@gmail.com>
2022-06-10 10:03:52 -0700

    fix supervisor patching and add tests

---
+++ core-tests/shared/src/test/scala/zio/SupervisorSpec.scala
@@ -0,0 +1,60 @@
+package zio
+
+import zio.test._
+import zio.UIO
+
+object SupervisorSpec extends ZIOSpecDefault {
+
+  def spec = suite("SupervisorSpec")(
+    test("++") {
+      for {
+        ref       <- ZIO.succeed(Ref.unsafeMake(0))
+        left      <- makeSupervisor(ref)
+        right     <- makeSupervisor(ref)
+        supervisor = left ++ right
+        fiber     <- ZIO.unit.fork.supervised(supervisor)
+        _         <- fiber.join
+        value     <- ref.get
+      } yield assertTrue(value == 2)
+    },
+    suite("laws") {
+      DifferSpec.diffLaws(Differ.supervisor)(genSupervisor)((left, right) => Supervisor.toSet(left) == Supervisor.toSet(right))
+    }
+  )
+
+  val genSupervisor: Gen[Any, Supervisor[Any]] =
+    Gen.fromZIO {
+      ZIO.succeed {
+        new Supervisor[Any] {
+          def value(implicit trace: zio.Trace): UIO[Any] =
+            ZIO.unit
+          def unsafeOnStart[R, E, A](
+            environment: ZEnvironment[R],
+            effect: ZIO[R, E, A],
+            parent: Option[Fiber.Runtime[Any, Any]],
+            fiber: Fiber.Runtime[E, A]
+          ): Unit =
+            ()
+          def unsafeOnEnd[R, E, A](value: Exit[E, A], fiber: Fiber.Runtime[E, A]): Unit =
+            ()
+        }
+      }
+    }
+
+  def makeSupervisor(ref: Ref.Atomic[Int]): UIO[Supervisor[Any]] =
+    ZIO.succeed {
+      new Supervisor[Any] {
+        def value(implicit trace: zio.Trace): UIO[Any] =
+          ZIO.unit
+        def unsafeOnStart[R, E, A](
+          environment: ZEnvironment[R],
+          effect: ZIO[R, E, A],
+          parent: Option[Fiber.Runtime[Any, Any]],
+          fiber: Fiber.Runtime[E, A]
+        ): Unit =
+          ref.unsafeUpdate(_ + 1)
+        def unsafeOnEnd[R, E, A](value: Exit[E, A], fiber: Fiber.Runtime[E, A]): Unit =
+          ()
+      }
+    }
+}
