45dccd4024c7482f80f75592d09839a566035263
John A. De Goes <john@degoes.net>
2022-06-13 11:18:41 +0100

    Add bug isolator

---
+++ streams-tests/shared/src/test/scala/zio/stream/StreamBugIsolator.scala
@@ -0,0 +1,14 @@
+package zio.stream 
+
+import zio._
+
+object StreamBugIsolator extends ZIOAppDefault {
+  override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] = {
+    val N = 100000
+    (1 to N)
+      .foldLeft(ZChannel.write(1L)) { case (channel, n) =>
+        channel.mapOut(_ + n)
+      }
+      .runCollect.debug("Result")  <&> (ZIO.sleep(1.second) *> Fiber.dumpAll)
+  }
+}
