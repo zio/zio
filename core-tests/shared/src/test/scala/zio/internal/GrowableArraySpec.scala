6f4f9df652df278f0b2cbd601b4505d5751abab5
John A. De Goes <john@degoes.net>
2022-06-20 06:30:05 -0500

    More tests

--- core-tests/shared/src/test/scala/zio/internal/GrowableArraySpec.scala
+++ core-tests/shared/src/test/scala/zio/internal/GrowableArraySpec.scala
@@ -55,11 +55,28 @@ object GrowableArraySpec extends ZIOBaseSpec {
       assertTrue(chunkRange == a.build() && a.size == 0)
     }
 
+  val iterating =
+    test("iteration") {
+      val range      = (0 to 100).map(_.toString)
+      val chunkRange = Chunk.fromIterable(range)
+      val a          = make(1)
+
+      (0 to 100).foreach { number =>
+        a += number.toString
+      }
+
+      var i = ""
+      a.foreach(i += _)
+
+      assertTrue(i == chunkRange.reduce(_ + _))
+    }
+
   def spec =
     suite("GrowableArraySpec")(
       initialState,
       addAFew,
       addMany,
-      buildResets
+      buildResets,
+      iterating
     )
 }
