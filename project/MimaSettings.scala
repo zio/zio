ae453dc47f819ec9afaf08835130e20212bc9bb3
Adam Fraser <adam.fraser@gmail.com>
2022-06-25 10:58:55 -0700

    format

--- project/MimaSettings.scala
+++ project/MimaSettings.scala
@@ -12,7 +12,7 @@ object MimaSettings {
     Seq(
       mimaPreviousArtifacts := Set(organization.value %% name.value % bincompatVersionToCompare),
       mimaBinaryIssueFilters ++= Seq(
-        exclude[Problem]("zio.internal.*"),
+        exclude[Problem]("zio.internal.*")
       ),
       mimaFailOnProblem := failOnProblem
     )
