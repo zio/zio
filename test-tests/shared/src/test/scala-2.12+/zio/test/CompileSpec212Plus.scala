0ddc8ad8fdb0e262b4331136a5b931b2c060f58f
Ondra Pelech <ondra.pelech@gmail.com>
2022-06-29 00:42:33 +0200

    fix 2.11 Native

--- test-tests/shared/src/test/scala-2.12+/zio/test/CompileSpec212Plus.scala
+++ test-tests/shared/src/test/scala-2.12+/zio/test/CompileSpec212Plus.scala
@@ -4,8 +4,8 @@ import zio.test.Assertion._
 
 object CompileSpec212Plus extends ZIOBaseSpec {
 
-  // doesn't work on 2.11 Native, any other combination works
   def spec: ZSpec[Environment, Failure] = suite("CompileSpec")(
+    // doesn't work on 2.11 Native, any other combination works
     testM("typeCheck expansion must use Left and Right from scala.util") {
       // if this does not compile the macro expansion probably picks up the wrong Left or Right
       case class Left(s: Nothing)
