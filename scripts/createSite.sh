f5a01f56f2fffdb171981ef4b2fb62a1d6d05621
Milad Khajavi <khajavi@gmail.com>
2022-07-20 09:07:50 +0430

    Check The Whole Website on Pull Requests (#7098)

--- scripts/createSite.sh
+++ scripts/createSite.sh
@@ -9,21 +9,25 @@ rm -Rf website/docs
 rm -Rf website/versioned_docs
 
 # Checkout latest version of the website from the 1.x series
+
+original_dir=$(pwd)
+cd /tmp
+git clone https://github.com/zio/zio.git
+cd zio
 git checkout series/1.x
-git clean -df
 sbt docs/mdoc
-#sbt docs/unidoc
+sbt docs/unidoc
 
-mkdir -p website/versioned_docs/version-1.x
-mv zio-docs/target/mdoc/* website/versioned_docs/version-1.x
+mkdir -p "$original_dir"/website/versioned_docs/version-1.x
+mv zio-docs/target/mdoc/* "$original_dir"/website/versioned_docs/version-1.x
 
-mkdir -p website/static/api/1.x
-mv website/static/api website/static/api-1.x
+mkdir -p "$original_dir"/website/static/api/1.x
+mv website/static/api "$original_dir"/website/static/api-1.x
 
 # Now we need to checkout the branch that originally has triggered the site build
-git checkout $1
+cd "$original_dir"
 git fetch --tags
-#ZIO_LATEST_2=`git describe --tags --abbrev=0 ` sbt docs/unidoc
+ZIO_LATEST_2=`git describe --tags --abbrev=0 ` sbt docs/unidoc
 ZIO_LATEST_2=`git describe --tags --abbrev=0 ` sbt docs/mdoc
 
 cd website 
