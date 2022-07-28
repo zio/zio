caf29eea3ddba71e4cc617c658648ca981917766
Milad Khajavi <khajavi@gmail.com>
2022-07-28 13:46:20 +0430

    fix the sidebar.

--- website/sidebars.js
+++ website/sidebars.js
@@ -96,7 +96,7 @@ module.exports = {
             link: { type: "doc", id: "reference/state/concurrent" },
             items:
               [
-                "reference/fiber/fiberref",
+                "reference/state/fiberref",
                 "reference/state/zstate",
               ]
           },
