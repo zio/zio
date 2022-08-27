80ccff8e72cf5d23941b26f76c348d6d91dca529
Bill Frasure <bill.frasure@gmail.com>
2022-08-26 23:04:36 -0600

    Cleanup SummaryBuilderSpec

--- test-tests/shared/src/test/scala/zio/test/SummaryBuilderSpec.scala
+++ test-tests/shared/src/test/scala/zio/test/SummaryBuilderSpec.scala
@@ -3,49 +3,38 @@ package zio.test
 import zio.test.Assertion._
 import zio.test.ReportingTestUtils._
 import zio.test.TestAspect.silent
+import zio.internal.macros.StringUtils.StringOps
 
 object SummaryBuilderSpec extends ZIOBaseSpec {
 
   def summarize(log: Vector[String]): String =
-//     log.filter(!_.contains(green("+"))).mkString.stripLineEnd + "\n"
     log.mkString("\n").stripLineEnd + "\n"
 
   def labelOnly(log: Vector[String]): String =
     log.take(1).mkString.stripLineEnd
 
-  import zio.internal.macros.StringUtils.StringOps
-//  import zio.internal.stacktracer.SourceLocation
   def containsUnstyled(string: String, substring: String): TestResult =
     assertTrue(string.unstyled.contains(substring.unstyled))
 
   def spec =
     suite("SummaryBuilderSpec")(
-      test("doesn't generate summary for a successful test") {
+      test("doesn't generate summary for a successful test") (
         assertZIO(runSummary(test1))(equalTo(""))
-      },
-      test("includes a failed test") {
-//        for {
-//         str <- runSummary(test3)
-//          _ <- ZIO.debug("RES: \n" + str.unstyled + "\n===========")
-//         _ <- ZIO.debug("EXPECTED: \n" + summarize(test3ExpectedZ).unstyled + "\n===========")
-//        } yield assertCompletes && containsUnstyled(str, summarize(test3ExpectedZ))
-
+      ),
+      test("includes a failed test") (
         runSummary(test3).map(str => containsUnstyled(str, summarize(test3ExpectedZ)))
-//          _ <- ZIO.debug("RES: \n" + str.unstyled + "\n===========")
-//          _ <- ZIO.debug("EXPECTED: \n" + summarize(test3ExpectedZ).unstyled + "\n===========")
-      },
-      test("doesn't generate summary for a successful test suite") {
+      ),
+      test("doesn't generate summary for a successful test suite") (
         assertZIO(runSummary(suite1))(equalTo(""))
-      },
-      test("correctly reports failed test suite") {
+      ),
+      test("correctly reports failed test suite")(
         runSummary(suite2).map(res => containsUnstyled( res, summarize(suite2ExpectedZ)))
-      },
-      test("correctly reports multiple test suites") {
+      ),
+      test("correctly reports multiple test suites") (
         runSummary(suite3).map(res => containsUnstyled( res, summarize(suite3ExpectedZ)))
-//        runSummary(suite3).map{str => println(str.unstyled); println(summarize(suite3ExpectedZ).unstyled); assertTrue(str == summarize(suite3ExpectedZ))}
-      },
-      test("correctly reports failure of simple assertion") {
+      ),
+      test("correctly reports failure of simple assertion")(
         runSummary(test5).map(str => containsUnstyled(str, summarize(test5Expected)))
-      }
-    ) @@ silent // TODO Restore in next PR
+      )
+    ) @@ silent
 }
