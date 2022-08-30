07e0fd0b333ad536b06cb8ba5662cba46e77a746
Adam Fraser <adam.fraser@gmail.com>
2022-08-30 07:44:43 -0700

    implement unsafely

--- core-tests/shared/src/test/scala-3/zio/UnsafeSpecVersionSpecific.scala
+++ core-tests/shared/src/test/scala-3/zio/UnsafeSpecVersionSpecific.scala
@@ -5,15 +5,21 @@ import zio.test._
 object UnsafeSpecVersionSpecific extends ZIOSpecDefault {
 
   def spec = suite("UnsafeSpecVersionSpecific") {
-    suite("unsafe")(
+    suite("unsafely")(
       test("provides capability to method with implicit parameter") {
-        Unsafe.unsafe(doSomethingUnsafe())
+        Unsafe.unsafely(doSomethingUnsafe())
         assertCompletes
       },
       test("provides capability to implicit function") {
         def succeed[A](block: Unsafe ?=> A): ZIO[Any, Nothing, A] =
-          ZIO.succeed(Unsafe.unsafe(block))
+          ZIO.succeed(Unsafe.unsafely(block))
         assertCompletes
+      },
+      test("provides capability to implicit function with multiple parameters") {
+        val result = Unsafe.unsafely {
+          Runtime.default.unsafe.run(ZIO.succeed(42)).getOrThrowFiberFailure()
+        }
+        assertTrue(result == 42)
       }
     )
   }
