27bf27da5634cf11443798a984bf079d5ce5a7cb
LUC DUZAN <strokyl@users.noreply.github.com>
2022-09-09 16:44:07 +0200

    Fix Gen.instant when upper and lower bound second part are the same (#7330)

--- test-tests/shared/src/test/scala/zio/test/GenSpec.scala
+++ test-tests/shared/src/test/scala/zio/test/GenSpec.scala
@@ -39,6 +39,7 @@ object GenSpec extends ZIOBaseSpec {
             val p = (as ++ bs).reverse == (as.reverse ++ bs.reverse)
             if (p) assert(())(Assertion.anything) else assert((as, bs))(Assertion.nothing)
         }
+
         assertZIO(CheckN(100)(gen)(test).map { result =>
           result.failures.fold(false) { trace =>
             val values = trace.values
@@ -246,6 +247,22 @@ object GenSpec extends ZIOBaseSpec {
         val min = Instant.ofEpochSecond(-38457693893669L, 435345)
         val max = Instant.ofEpochSecond(74576982873324L, 345345345)
         checkSample(Gen.instant(min, max))(forall(isGreaterThanEqualTo(min) && isLessThanEqualTo(max)))
+      },
+      test("instant generates values in range when upper and lower bound share same second ") {
+        val min = Instant.ofEpochSecond(1, 2)
+        val max = Instant.ofEpochSecond(1, 3)
+        checkSample(Gen.instant(min, max))(forall(isGreaterThanEqualTo(min) && isLessThanEqualTo(max)))
+      },
+      test("instant generates values in range when upper bound nano part is smaller than lower bound one") {
+        val min = Instant.ofEpochSecond(2, 1)
+        val max = Instant.ofEpochSecond(3, 9)
+        checkSample(Gen.instant(min, max))(forall(isGreaterThanEqualTo(min) && isLessThanEqualTo(max)))
+      },
+      test("instant generates one value when upper and lower bound are equals") {
+        val anInstant = Instant.ofEpochSecond(1, 1)
+        checkSample(Gen.instant(anInstant, anInstant))(
+          forall(equalTo(anInstant))
+        )
       },
       test("int generates values in range") {
         checkSample(smallInt)(forall(isGreaterThanEqualTo(-10) && isLessThanEqualTo(10)))
@@ -687,7 +704,8 @@ object GenSpec extends ZIOBaseSpec {
       case object Pop                   extends Command
       final case class Push(value: Int) extends Command
 
-      val genPop: Gen[Any, Command]  = Gen.const(Pop)
+      val genPop: Gen[Any, Command] = Gen.const(Pop)
+
       def genPush: Gen[Any, Command] = Gen.int.map(value => Push(value))
 
       val genCommands: Gen[Any, List[Command]] =
