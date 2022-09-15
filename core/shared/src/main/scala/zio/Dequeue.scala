84996fe510be9a775d071c14970c4ff0855e1437
Jeff Shaw <shawjef3@gmail.com>
2022-09-15 13:53:05 -0400

    Add some documentation for Queue#size

--- core/shared/src/main/scala/zio/Dequeue.scala
+++ core/shared/src/main/scala/zio/Dequeue.scala
@@ -47,7 +47,8 @@ trait Dequeue[+A] extends Serializable {
   /**
    * Retrieves the size of the queue, which is equal to the number of elements
    * in the queue. This may be negative if fibers are suspended waiting for
-   * elements to be added to the queue.
+   * elements to be added to the queue. It may be greater than the capacity if
+   * fibers are suspended waiting for elements to be removed from the queue.
    */
   def size(implicit trace: Trace): UIO[Int]
 
