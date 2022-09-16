58a838383c8a7892df80f215661edc766e1d15ef
Jeff Shaw <shawjef3@gmail.com>
2022-09-16 11:31:35 -0400

    Ran scalafmt again.

--- core-tests/shared/src/test/scala/zio/stm/TSemaphoreSpec.scala
+++ core-tests/shared/src/test/scala/zio/stm/TSemaphoreSpec.scala
@@ -123,7 +123,7 @@ object TSemaphoreSpec extends ZIOBaseSpec {
         val transaction =
           for {
             semaphore <- TSemaphore.make(2L)
-            actual <- semaphore.acquireBetween(3L, 5L)
+            actual    <- semaphore.acquireBetween(3L, 5L)
           } yield actual
         transaction.commitEither *> assertTrue(false)
       } @@ timeout(1.second) @@ failing,
