1af093f4b0063590f7774f10b3bdbb9c6380d3de
Adam Fraser <adam.fraser@gmail.com>
2022-11-01 22:57:24 +0100

    upgrade binary compatibility version

--- project/MimaSettings.scala
+++ project/MimaSettings.scala
@@ -6,7 +6,7 @@ import com.typesafe.tools.mima.core._
 import com.typesafe.tools.mima.core.ProblemFilters._
 
 object MimaSettings {
-  lazy val bincompatVersionToCompare = "2.0.2"
+  lazy val bincompatVersionToCompare = "2.0.3"
 
   def mimaSettings(failOnProblem: Boolean) =
     Seq(
