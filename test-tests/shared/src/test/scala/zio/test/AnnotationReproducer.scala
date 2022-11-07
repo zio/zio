38e111bbf422cc4e344f6b3141bce831009d6b8e
Bill Frasure <bill.frasure@gmail.com>
2022-11-07 09:13:08 -0700

    Expand reproducer to include tags

--- test-tests/shared/src/test/scala/zio/test/AnnotationReproducer.scala
+++ test-tests/shared/src/test/scala/zio/test/AnnotationReproducer.scala
@@ -5,7 +5,7 @@ import zio.test.TestAspect._
 object AnnotationReproducer extends ZIOSpecDefault {
   override def spec = suite("Suite1")(
     test("a")(assertTrue(true)),
-    test("b")(assertTrue(false)),
+    test("b")(assertTrue(false)) @@ tag("Important"),
     test("c")(assertTrue(false)) @@ ignore,
   ) @@ timed
 }
