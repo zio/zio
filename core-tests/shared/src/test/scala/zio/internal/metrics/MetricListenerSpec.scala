17a5f7f973ceeeb59e88071c1220306039bada42
Albert Peto <petoalbert32@gmail.com>
2022-11-17 14:45:49 +0100

    Add MetricListenerSpec

---
+++ core-tests/shared/src/test/scala/zio/internal/metrics/MetricListenerSpec.scala
@@ -0,0 +1,48 @@
+package zio.internal.metrics
+
+import zio.metrics.MetricKeyType.Histogram.Boundaries
+import zio.metrics.MetricKeyType.{Counter, Frequency, Gauge}
+import zio.metrics._
+import zio.test.Assertion._
+import zio.test.TestAspect.timeout
+import zio.test.{Spec, _}
+import zio.{ZIOBaseSpec, _}
+
+import java.time.Instant
+
+object MetricListenerSpec extends ZIOBaseSpec {
+
+  case class HistogramListener(queue: Queue[(MetricKey[MetricKeyType.Histogram], Double)]) extends MetricListener {
+    override def updateHistogram(key: MetricKey[MetricKeyType.Histogram], value: Double)(implicit
+      unsafe: Unsafe
+    ): Unit = {
+      val _ = Runtime.default.unsafe.run(queue.offer((key, value)))
+    }
+
+    override def updateGauge(key: MetricKey[Gauge], value: Double)(implicit unsafe: Unsafe): Unit = ()
+
+    override def updateFrequency(key: MetricKey[Frequency], value: String)(implicit unsafe: Unsafe): Unit = ()
+
+    override def updateSummary(key: MetricKey[MetricKeyType.Summary], value: Double, instant: Instant)(implicit
+      unsafe: Unsafe
+    ): Unit = ()
+
+    override def updateCounter(key: MetricKey[Counter], value: Double)(implicit unsafe: Unsafe): Unit = ()
+  }
+
+  override def spec: Spec[Environment, Any] =
+    suite("MetricListenerSpec")(
+      test("listeners get notified") {
+        for {
+          listenerQueue <- Queue.bounded[(MetricKey[MetricKeyType.Histogram], Double)](1)
+          listener       = HistogramListener(listenerQueue)
+          _              = MetricClient.addListener(listener)
+          metric         = Metric.histogram("test", Boundaries(Chunk.empty))
+          _             <- ZIO.succeed(3.3) @@ metric
+          event         <- listenerQueue.take
+          (key, value)   = event
+        } yield assert(key.name)(equalTo("test")) && assert(value)(equalTo(3.3))
+      } @@ timeout(1.second)
+    )
+
+}
