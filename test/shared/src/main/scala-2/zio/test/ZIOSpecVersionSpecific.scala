ed6cc1354d8074f997133a1df5294c3172b8eb3b
Bill Frasure <bill.frasure@gmail.com>
2022-12-16 14:49:24 -0700

    Fix suiteAll error channel

--- test/shared/src/main/scala-2/zio/test/ZIOSpecVersionSpecific.scala
+++ test/shared/src/main/scala-2/zio/test/ZIOSpecVersionSpecific.scala
@@ -3,6 +3,6 @@ package zio.test
 trait ZIOSpecVersionSpecific[R] {
   // SCALA 2
 
-  def suiteAll(name: String)(spec: Any): Spec[Nothing, Nothing] =
+  def suiteAll(name: String)(spec: Any): Spec[Nothing, Any] =
     macro SmartSpecMacros.suiteImpl
 }
