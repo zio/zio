1ea39d6d0b09eb1d47e98a44daec333f87808231
Bill Frasure <bill.frasure@gmail.com>
2022-12-21 11:31:09 -0700

    Notes and consultation with John

---
+++ test-tests/shared/src/test/scala/zio/test/DemoSpec.scala
@@ -0,0 +1,22 @@
+package zio.test
+
+import zio.Clock.ClockLive
+import zio._
+
+object DemoSpec extends ZIOSpecDefault {
+
+  def spec = suite("outter suite")(
+    suite("inner suite 1")(
+      durationTest("A", 1.seconds),
+      durationTest("B", 2.seconds),
+    ),
+    suite("inner suite 2")(
+      durationTest("C", 1.seconds),
+      durationTest("D", 3.seconds),
+      durationTest("E", 2.seconds),
+    ),
+  )
+
+  private def durationTest(name: String, duration: Duration) =
+    test(name)(ZIO.sleep(duration).withClock(ClockLive) *> assertCompletes)
+}
