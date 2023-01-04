209ded7b04572127c22e49ba9206486b8c86a458
Bill Frasure <bill.frasure@gmail.com>
2023-01-04 11:02:57 -0700

    Remove Using in spec to fix Scala 2.11/2.12

--- test-tests/jvm-native/src/test/scala/zio/test/results/ResultFileOpsJsonSpec.scala
+++ test-tests/jvm-native/src/test/scala/zio/test/results/ResultFileOpsJsonSpec.scala
@@ -4,7 +4,6 @@ import zio._
 import zio.test._
 
 import java.nio.file.Path
-import scala.util.Using
 
 object ResultFileOpsJsonSpec extends ZIOSpecDefault {
   def spec = suite("ResultFileOpsJsonSpec")(
@@ -13,7 +12,7 @@ object ResultFileOpsJsonSpec extends ZIOSpecDefault {
         _       <- writeToTestFile("a")
         results <- readFile
       } yield assertTrue(results == List("a"))
-    ).provide(test),
+    ).provide(test, Scope.default),
     test("clobbered concurrent writes") {
       val linesToWrite =
         List(
@@ -29,9 +28,9 @@ object ResultFileOpsJsonSpec extends ZIOSpecDefault {
             linesToWrite
           )(writeToTestFile)
         results <- readFile
-      } yield assertTrue(linesToWrite.forall(results.contains))
+      } yield assertTrue(linesToWrite.forall(results.contains(_)))
     }
-      .provide(test)
+      .provide(test, Scope.default)
       @@ TestAspect.nonFlaky,
     test("generated concurrent writes clean") {
       checkN(10)(Gen.listOfN(3)(Gen.alphaNumericStringBounded(0, 700))) { linesToWrite =>
@@ -41,21 +40,21 @@ object ResultFileOpsJsonSpec extends ZIOSpecDefault {
               linesToWrite
             )(writeToTestFile)
           results <- readFile
-        } yield assertTrue(linesToWrite.forall(results.contains))
+        } yield assertTrue(linesToWrite.forall(results.contains(_)))
       }
-    }.provide(test)
+    }.provide(test, Scope.default)
   )
 
   private def writeToTestFile(content: String) =
     ZIO.serviceWithZIO[ResultFileOpsJson](_.write(content + "\n", append = true))
 
-  val readFile: ZIO[Path, Nothing, List[String]] =
+  val readFile: ZIO[Path with Scope, Nothing, List[String]] =
     for {
       tmpFilePath <- ZIO.service[Path]
+      source       = scala.io.Source.fromFile(tmpFilePath.toString)
+      _           <- ZIO.addFinalizer(ZIO.succeed(source.close()))
       lines <- ZIO.attempt {
-                 Using(scala.io.Source.fromFile(tmpFilePath.toString)) { source =>
-                   source.getLines().toList
-                 }.get
+                 source.getLines().toList
                }.orDie
     } yield lines
 
