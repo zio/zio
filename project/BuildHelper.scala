87a590f49617ed78bc1baa52fa7a2c08467b8813
Ondra Pelech <ondra.pelech@gmail.com>
2023-02-28 03:02:44 +0100

    Scala Native: run tests (#7031)
    * Scala Native: run tests
    
    * Native: Run scala-native-loop in OneShot#get
    
    * Native: Test / fork := false
    
    * ci.yaml: testPlatforms on Scala 3
    
    * ci.yaml: testPlatforms 180 min timeout
    
    * ci.yaml: testPlatforms 360 min timeout
    
    * ci.yaml: testPlatforms on Scala 3
    
    * Update versions
    
    * Update versions
    
    * Update munit
    
    * testPlatforms on Scala 2.13
    
    * EventLoop.run() behind if (value == null)
    
    * Set up swap for testing Native
    
    * More swap
    
    * More swap
    
    * More swap
    
    * Monitor memory
    
    * Add scala-native-crypto
    
    * scala-native-crypto 0.0.4
    
    * -Xmx3G -Xss3M
    
    * Global / concurrentRestrictions += Tags.limit(Tags.Test, 1)
    
    * smem
    
    * don't test core-test
    
    * with core-test
    
    * Update ci.yml
    
    * debug test
    
    * lower number of fibers
    
    * lower number of fibers
    
    * Scala Native: lower number of fibers in tests
    
    * revert timeout change

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -259,7 +259,6 @@ object BuildHelper {
   )
 
   def nativeSettings = Seq(
-    Test / test := (Test / compile).value,
     Test / fork := crossProjectPlatform.value == JVMPlatform // set fork to `true` on JVM to improve log readability, JS and Native need `false`
   )
 
