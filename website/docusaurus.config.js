71d224f096ee406f7cbf0bcc10ab2a39ff49f0cb
Milad Khajavi <khajavi@gmail.com>
2023-03-17 14:13:23 +0330

    add mermaid.

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -161,10 +161,10 @@ const config = {
   plugins: [
     [path.join(__dirname, './plugins/zio-ecosystem-docusaurus'), {}],
   ],
-//  markdown: {
-//    mermaid: true,
-//  },
-//  themes: ['@docusaurus/theme-mermaid'],
+  markdown: {
+    mermaid: true,
+  },
+  themes: ['@docusaurus/theme-mermaid'],
 }
 
 module.exports = config;
