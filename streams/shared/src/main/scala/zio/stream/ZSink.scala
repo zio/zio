abf78397f3e84928ac39deab6631a4172c37d7b9
Adam Fraser <adam.fraser@gmail.com>
2023-03-23 21:13:03 -0700

    optimize zsink sum

--- streams/shared/src/main/scala/zio/stream/ZSink.scala
+++ streams/shared/src/main/scala/zio/stream/ZSink.scala
@@ -1618,7 +1618,7 @@ object ZSink extends ZSinkPlatformSpecificConstructors {
    * A sink that sums incoming numeric values.
    */
   def sum[A](implicit A: Numeric[A], trace: Trace): ZSink[Any, Nothing, A, Nothing, A] =
-    foldLeft(A.zero)(A.plus)
+    foldLeftChunks(A.zero)((acc, chunk) => A.plus(acc, chunk.foldLeft(A.zero)(A.plus)))
 
   /**
    * Tags each metric in this sink with the specific tag.
