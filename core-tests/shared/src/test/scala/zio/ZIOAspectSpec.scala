7051558ec2ade758de3ffca144de7404b67974cc
Adam Fraser <adam.fraser@gmail.com>
2023-03-29 01:26:41 -0700

    implement zio aspect nested (#7960)

---
+++ core-tests/shared/src/test/scala/zio/ZIOAspectSpec.scala
@@ -0,0 +1,16 @@
+package zio
+
+import zio.test._
+import zio.test.TestAspect._
+
+object ZIOAspectSpec extends ZIOBaseSpec {
+  import ZIOAspect._
+
+  def spec = suite("ZIOAspectSpec")(
+    test("nested nests configuration under the specified name") {
+      for {
+        config <- ZIO.config(Config.string("key")) @@ nested("nested")
+      } yield assertTrue(config == "value")
+    } @@ withConfigProvider(ConfigProvider.fromMap(Map("nested.key" -> "value")))
+  )
+}
