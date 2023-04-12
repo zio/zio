43c2dbf20cf3ba2c331febfe31fda5847bad1416
Velimir Milinković <81649656+mvelimir@users.noreply.github.com>
2023-04-12 23:24:27 +0200

    Replace last else if with else (#8013)

--- test-sbt/jvm/src/main/scala/zio/test/sbt/ZTestRunnerJVM.scala
+++ test-sbt/jvm/src/main/scala/zio/test/sbt/ZTestRunnerJVM.scala
@@ -60,7 +60,7 @@ final class ZTestRunnerJVM(val args: Array[String], val remoteArgs: Array[String
         colored(renderedSummary)
       else if (ignore > 0)
         s"${Console.YELLOW}All eligible tests are currently ignored ${Console.RESET}"
-      else if (total == 0)
+      else
         s"${Console.YELLOW}No tests were executed${Console.RESET}"
 
     // We eagerly print out the info here, rather than returning it
