a8ce9703fb68e232d5434579833c0db52b27e275
Bill Frasure <bill.frasure@gmail.com>
2023-04-13 16:14:48 -0600

    rm more CSV references

--- test/shared/src/main/scala/zio/test/results/ResultSerializer.scala
+++ test/shared/src/main/scala/zio/test/results/ResultSerializer.scala
@@ -9,7 +9,6 @@ trait ResultSerializer {
 
 object ResultSerializer {
   val live: ULayer[ResultSerializer] = ZLayer.succeed(Json)
-  val csv: ULayer[ResultSerializer] = ZLayer.succeed(CSV)
 
   object Json extends ResultSerializer {
     def render[E](executionEvent: ExecutionEvent.Test[E]): String =
@@ -43,43 +42,4 @@ object ResultSerializer {
         .mkString(" : ")
   }
 
-  object CSV extends ResultSerializer {
-    def render[E](executionEvent: ExecutionEvent.Test[E]): String =
-      executionEvent match {
-        /*
-          CSV format
-          name, status, durationMillis, annotations?, fullyQualifiedClassName, labels
-         */
-        case ExecutionEvent.Test(labelsReversed, test, annotations, ancestors, duration, id, fullyQualifiedName) =>
-          s"""
-             |${render(fullyQualifiedName, labelsReversed)},
-             |${render(test)},
-             |$duration,
-             |${render(annotations)},
-             |$fullyQualifiedName,
-             |["${render(labelsReversed)}"]
-             |""".stripMargin
-      }
-
-    private def render(fullyQualifiedName: String, labelsReversed: List[String]) =
-      s"$fullyQualifiedName/${ labelsReversed.reverse .map(s => s.replace("/", "\\/")) .mkString("/") }"
-
-    private def render(labelsReversed: List[String]) =
-      labelsReversed.reverse.map(s => s.replace("/", "\\/")).mkString("\", \"")
-
-
-    private def render[E](test: Either[TestFailure[E], TestSuccess]): String =
-      test match {
-        case Left(value) =>
-          "Failure"
-        case Right(value) =>
-          "Success"
-      }
-
-    private[results] def render(testAnnotationMap: TestAnnotationMap): String =
-      TestAnnotationRenderer.default
-        .run(List.empty, testAnnotationMap)
-        .map(s => s.replace("\"", "\\\""))
-        .mkString(" : ")
-  }
 }
