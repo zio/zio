dfa5cf8b11310b15e23554daec75681adca883aa
Dani Rey <dani.rey.ch@gmail.com>
2023-04-18 09:11:51 +0200

    Fixed #8051 Handle special case where the offset of min is bigger then the offset of max in Gen.offsetDateTime(min, max)

--- test/shared/src/main/scala/zio/test/TimeVariants.scala
+++ test/shared/src/main/scala/zio/test/TimeVariants.scala
@@ -20,7 +20,7 @@ import com.github.ghik.silencer.silent
 import zio.{Duration, Trace}
 
 import java.time._
-import scala.jdk.CollectionConverters._
+import scala.collection.JavaConverters._
 
 trait TimeVariants {
 
