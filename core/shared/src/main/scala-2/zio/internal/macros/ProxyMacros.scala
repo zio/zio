f261ee0139a0b830106d7490829f282b7c70aa34
som <solver.som@gmail.com>
2023-04-29 21:06:30 +0900

    wip: Rewrite Scala 2 macros

--- core/shared/src/main/scala-2/zio/internal/macros/ProxyMacros.scala
+++ core/shared/src/main/scala-2/zio/internal/macros/ProxyMacros.scala
@@ -17,26 +17,31 @@ class ProxyMacros(val c: blackbox.Context) {
 
     val resultType = appliedType(tpe.typeConstructor, tpe.typeArgs)
     val forwarders = tpe.members.view
-      .filter(m => m.isMethod && m.isAbstract)
+      .filter(m => m.isMethod && (m.asMethod.returnType <:< c.weakTypeOf[zio.ZIO[_, _, _]]))
       .map { sym =>
-        val m       = sym.asMethod.typeSignatureIn(resultType)
-        val tparams = m.typeParams.map(c.internal.typeDef)
+        if (sym.asMethod.isVal)
+          c.abort(
+            c.enclosingPosition,
+            s"Cannot generate a proxy for ${weakTypeOf[A]} due to a val method ${sym.name}(...)"
+          )
 
+        val m = sym.asMethod.typeSignatureIn(resultType)
+        log(sym.name)
+        val tparams = m.typeParams.map(c.internal.typeDef)
         val params = m.paramLists.map(
           _.map { p =>
             val mods = if (p.isImplicit) Modifiers(Flag.IMPLICIT) else Modifiers()
             val tpe  = TypeTree(p.typeSignatureIn(m))
-            log("tpe", showRaw(tpe), tq"A")
             ValDef(mods, p.name.toTermName, tpe, EmptyTree)
           }
         )
 
         val args = m.paramLists.map(_.map(p => p.name.toTermName))
 
-        val tree =
+        if (sym.asMethod.isAbstract)
           q"def ${sym.name.toTermName}[..$tparams](...$params): ${m.finalResultType} = ${service.tree}.get.flatMap(_.${sym.name.toTermName}(...$args))"
-        log(showRaw(tree))
-        tree
+        else
+          q"override def ${sym.name.toTermName}[..$tparams](...$params): ${m.finalResultType} = ${service.tree}.get.flatMap(_.${sym.name.toTermName}(...$args))"
       }
       .toList
 
