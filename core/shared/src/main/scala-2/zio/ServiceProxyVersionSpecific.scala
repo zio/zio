a6d8dd92d23eadce8a4f97dbbe10cfc2090eb772
Jisoo Park <xxxyel@gmail.com>
2023-05-01 18:27:25 +0900

    Add scaladoc

--- core/shared/src/main/scala-2/zio/ServiceProxyVersionSpecific.scala
+++ core/shared/src/main/scala-2/zio/ServiceProxyVersionSpecific.scala
@@ -3,5 +3,14 @@ package zio
 import zio.internal.macros.ServiceProxyMacros
 
 trait ServiceProxyVersionSpecific {
+
+  /** 
+   * Generates a proxy instance of the specified service.
+   *
+   * @tparam A The type of the service.
+   * @param service The `ScopedRef` containing the service for which a proxy is to be generated.
+   * @return A proxy instance of the service that forwards ZIO method calls to the underlying service
+   *         and allows the service to change its behavior at runtime.
+   */
   def generate[A](service: ScopedRef[A]): A = macro ServiceProxyMacros.makeImpl[A]
 }
--- core/shared/src/main/scala/zio/ServiceProxy.scala
+++ core/shared/src/main/scala/zio/ServiceProxy.scala
@@ -1,3 +1,31 @@
 package zio
 
+/** 
+ * The `ServiceProxy` object provides a utility to generate a proxy instance for a given service.
+ *
+ * The `generate` function creates a proxy instance of the service that forwards every ZIO method call to the 
+ * underlying service, wrapped in a [[zio.ScopedRef]]. The generated proxy enables the service to change
+ * its behavior at runtime.
+ *
+ * @note
+ *   In order to successfully generate a service proxy, the type `A` must meet the following requirements:
+ *    - `A` should be either a trait or a class with a primary constructor without any term parameters.
+ *    - `A` should contain only ZIO methods or vals.
+ *    - `A` should not have any abstract type members.
+ *
+ * @example
+ * {{{
+ *   trait MyService { def foo: UIO[String] }
+ *
+ *   val service1: MyService = new MyService { def foo = ZIO.succeed("zio1") }
+ *   val service2: MyService = new MyService { def foo = ZIO.succeed("zio2") }
+ *   for {
+ *     ref  <- ScopedRef.make(service1)
+ *     proxy = ServiceProxy.generate(ref)
+ *     res1 <- proxy.foo
+ *     _    <- ref.set(ZIO.succeed(service2))
+ *     res2 <- proxy.foo
+ *   } yield assertTrue(res1 == "zio1" && res2 == "zio2")
+ * }}}
+ */
 object ServiceProxy extends ServiceProxyVersionSpecific
