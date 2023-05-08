53ab23212548d9a258f8ee693a7706d23ad599e0
dzikoysk <dzikoysk@dzikoysk.net>
2023-05-08 23:54:54 +0200

    GH-8093 Add `contains` benchmark

--- benchmarks/src/main/scala/zio/internal/ConcurrentWeakHashSetBenchmark.scala
+++ benchmarks/src/main/scala/zio/internal/ConcurrentWeakHashSetBenchmark.scala
@@ -184,6 +184,55 @@ private[this] class ConcurrentWeakHashSetIterateBenchmark {
 
 }
 
+@State(Scope.Benchmark)
+private[this] class ContainsContext extends BaseContext {
+  private val sampleSize             = 100_000
+  private val values: Array[TestKey] = (0 to this.sampleSize).map(TestKey).toArray
+
+  @Setup(Level.Iteration)
+  def setup(): Unit = {
+    this.javaSetInitializer = { _ => createJavaSet(this.values) }
+    this.springMapInitializer = { _ => createSpringMap(this.values) }
+    this.zioSetInitializer = { _ => createZioSet(this.values) }
+    this.setupBase()
+  }
+}
+
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Warmup(iterations = 2, time = 2)
+@Measurement(iterations = 2, time = 2)
+@Fork(1)
+private[this] class ConcurrentWeakHashSetContainsBenchmark {
+
+  @Benchmark
+  def javaContainsSerial(ctx: IterateContext, blackhole: Blackhole): Unit =
+    blackhole.consume(ctx.javaSet.contains(TestKey(50_000)))
+
+  @Threads(6)
+  @Benchmark
+  def javaContainsConcurrent(ctx: IterateContext, blackhole: Blackhole): Unit =
+    blackhole.consume(ctx.javaSet.contains(TestKey(50_000)))
+
+  @Benchmark
+  def springContainsSerial(ctx: IterateContext, blackhole: Blackhole): Unit =
+    blackhole.consume(ctx.springMap.containsKey(TestKey(50_000)))
+
+  @Threads(6)
+  @Benchmark
+  def springContainsConcurrent(ctx: IterateContext, blackhole: Blackhole): Unit =
+    blackhole.consume(ctx.springMap.containsKey(TestKey(50_000)))
+
+  @Benchmark
+  def zioContainsSerial(ctx: IterateContext, blackhole: Blackhole): Unit =
+    blackhole.consume(ctx.zioSet.contains(TestKey(50_000)))
+
+  @Threads(6)
+  @Benchmark
+  def zioContainsConcurrent(ctx: IterateContext, blackhole: Blackhole): Unit =
+    blackhole.consume(ctx.zioSet.contains(TestKey(50_000)))
+
+}
+
 private[this] case class TestKey(name: Int)
 
 private[this] class BaseContext {
