32983a53ffa90e689894a484245a43235252b07a
Adam Fraser <adam.fraser@gmail.com>
2023-05-26 09:38:56 -0700

    Improve Reporting Of ChannelFailure (#8141)
    * improve reporting of channel failure
    
    * jvm only
    
    * format

--- streams-tests/shared/src/test/scala/zio/stream/ZStreamSpec.scala
+++ streams-tests/shared/src/test/scala/zio/stream/ZStreamSpec.scala
@@ -3977,7 +3977,19 @@ object ZStreamSpec extends ZIOBaseSpec {
                 )
               }
             )(equalTo(bytes))
-          }
+          },
+          test("Preserves errors") {
+            val stream = ZStream.fail(new Throwable("fail"))
+            val pipeline = ZPipeline.fromFunction[Scope, Throwable, Byte, Unit] { s =>
+              ZStream.fromZIO {
+                for {
+                  is <- s.toInputStream.debug("toInputStream")
+                  _  <- ZIO.attemptBlocking(is.read())
+                } yield ()
+              }
+            }
+            assertZIO(stream.via(pipeline).runCollect.exit)(fails(hasMessage(containsString("fail"))))
+          } @@ TestAspect.jvmOnly
         ),
         test("toIterator") {
           ZIO.scoped {
