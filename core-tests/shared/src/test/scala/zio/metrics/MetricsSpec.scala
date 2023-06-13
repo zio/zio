e01f02da31e4f2317fc296075feba37b2eb6c98a
Vladislav Ugryumov <vlad.ugrum@gmail.com>
2023-06-13 18:46:15 +0300

    Fixed sharing metrics state among all tests.

--- core-tests/shared/src/test/scala/zio/metrics/MetricsSpec.scala
+++ core-tests/shared/src/test/scala/zio/metrics/MetricsSpec.scala
@@ -7,33 +7,51 @@ import zio.{Chunk, ZIO, ZIOBaseSpec, durationInt}
 object MetricsSpec extends ZIOBaseSpec {
   private val labels = Set(MetricLabel("x", "a"), MetricLabel("y", "b"))
 
-  private val counter   = Metric.counter("test_counter").tagged(labels).fromConst(1L)
-  private val gauge     = Metric.gauge("test_gauge").tagged(labels)
-  private val frequency = Metric.frequency("test_frequency").tagged(labels)
+  private val counterName   = "test_counter"
+  private val gaugeName     = "test_gauge"
+  private val frequencyName = "test_frequency"
+  private val histogramName = "test_histogram"
+  private val summaryName   = "test_summary"
+
+  private val counter   = Metric.counter(counterName).tagged(labels).fromConst(1L)
+  private val gauge     = Metric.gauge(gaugeName).tagged(labels)
+  private val frequency = Metric.frequency(frequencyName).tagged(labels)
   private val histogram =
-    Metric.histogram("test_histogram", Histogram.Boundaries.fromChunk(Chunk(1.0, 2.0, 3.0))).tagged(labels)
+    Metric.histogram(histogramName, Histogram.Boundaries.fromChunk(Chunk(1.0, 2.0, 3.0))).tagged(labels)
   private val summary =
-    Metric.summary("test_summary", 1.minute, 10, 0.0, Chunk(0.1, 0.5, 0.9)).tagged(labels)
+    Metric.summary(summaryName, 1.minute, 10, 0.0, Chunk(0.1, 0.5, 0.9)).tagged(labels)
 
   def spec = suite("CurrentMetrics")(
+    test("Metrics prettyPrint should correctly process empty Metric set") {
+      for {
+        currentMetrics <- ZIO.succeed(Metrics(Set.empty))
+        str            <- currentMetrics.prettyPrint
+      } yield assertTrue(str == "")
+    },
     test("should be pretty printed correctly") {
       for {
-        snapshotBefore <- ZIO.metrics
-        str0           <- snapshotBefore.prettyPrint
-        _              <- ZIO.succeed(1.0) @@ counter @@ gauge @@ histogram @@ summary
-        _              <- ZIO.succeed(3.0) @@ counter @@ gauge @@ histogram @@ summary
-        _              <- ZIO.succeed("strValue1") @@ frequency
-        _              <- ZIO.succeed("strValue2") @@ frequency
-        _              <- ZIO.succeed("strValue1") @@ frequency
-        snapshotAfter  <- ZIO.metrics
-        str1           <- snapshotAfter.prettyPrint
+        _ <- ZIO.succeed(1.0) @@ counter @@ gauge @@ histogram @@ summary
+        _ <- ZIO.succeed(3.0) @@ counter @@ gauge @@ histogram @@ summary
+        _ <- ZIO.succeed("strValue1") @@ frequency
+        _ <- ZIO.succeed("strValue2") @@ frequency
+        _ <- ZIO.succeed("strValue1") @@ frequency
+        //set of metrics in the current snapshot may include metrics from other tests
+        allMetricsSnapshot <- ZIO.metrics
+        testSnapshot <-
+          ZIO.succeed(
+            Metrics(
+              allMetricsSnapshot.metrics.filter(m =>
+                Set(counterName, gaugeName, frequencyName, histogramName, summaryName).contains(m.metricKey.name)
+              )
+            )
+          )
+        str <- testSnapshot.prettyPrint
       } yield assertTrue(
-        str0 == "" &&
-          str1 == s"""test_counter    tags[x: a, y: b]  Counter[${2.0}]
-                     |test_frequency  tags[x: a, y: b]  Frequency[(strValue2 -> 1), (strValue1 -> 2)]
-                     |test_gauge      tags[x: a, y: b]  Gauge[${3.0}]
-                     |test_histogram  tags[x: a, y: b]  Histogram[buckets: [(${1.0} -> 1), (${2.0} -> 1), (${3.0} -> 2), (${1.7976931348623157e308} -> 2)], count: [2], min: [${1.0}], max: [${3.0}], sum: [${4.0}]]
-                     |test_summary    tags[x: a, y: b]  Summary[quantiles: [(0.1 -> None), (0.5 -> Some(${1.0})), (0.9 -> Some(${1.0}))], count: [2], min: [${1.0}], max: [${3.0}], sum: [${4.0}]]""".stripMargin
+        str == s"""test_counter    tags[x: a, y: b]  Counter[${2.0}]
+                  |test_frequency  tags[x: a, y: b]  Frequency[(strValue2 -> 1), (strValue1 -> 2)]
+                  |test_gauge      tags[x: a, y: b]  Gauge[${3.0}]
+                  |test_histogram  tags[x: a, y: b]  Histogram[buckets: [(${1.0} -> 1), (${2.0} -> 1), (${3.0} -> 2), (${1.7976931348623157e308} -> 2)], count: [2], min: [${1.0}], max: [${3.0}], sum: [${4.0}]]
+                  |test_summary    tags[x: a, y: b]  Summary[quantiles: [(0.1 -> None), (0.5 -> Some(${1.0})), (0.9 -> Some(${1.0}))], count: [2], min: [${1.0}], max: [${3.0}], sum: [${4.0}]]""".stripMargin
       )
     }
   )
