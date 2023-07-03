960c9718ab04c1eaeabdf6bcf9312a3ae7c8a236
Nabil Abdel-Hafeez <7283535+987Nabil@users.noreply.github.com>
2023-07-03 23:28:36 +0700

    Fix: Enable `ZLayer.derive` to handle arity 0 case classes (#8236) (#8237)

--- core-tests/shared/src/test/scala-3/zio/ZLayerDerivationSpec.scala
+++ core-tests/shared/src/test/scala-3/zio/ZLayerDerivationSpec.scala
@@ -3,12 +3,19 @@ package zio
 import zio.test._
 
 object ZLayerDerivationSpec extends ZIOBaseSpec {
+  case class ZeroDependencies()
   case class OneDependency(d1: String)
   case class TwoDependencies(d1: String, d2: Int)
 
+  val derivedZero = ZLayer.derive[ZeroDependencies]
   val derivedOne = ZLayer.derive[OneDependency]
   val derivedTwo = ZLayer.derive[TwoDependencies]
   override def spec = suite("ZLayerDerivationSpec")(
+    test("ZLayer.derive[ZeroDependencies]") {
+      for {
+        d1 <- ZIO.service[ZeroDependencies]
+      } yield assertTrue(d1 == ZeroDependencies())
+    },
     test("ZLayer.derive[OneDependency]") {
       for {
         d1 <- ZIO.service[OneDependency]
@@ -20,6 +27,7 @@ object ZLayerDerivationSpec extends ZIOBaseSpec {
       } yield assertTrue(d1 == TwoDependencies("one", 2))
     }
   ).provide(
+    derivedZero,
     derivedOne,
     derivedTwo,
     ZLayer.succeed("one"),
