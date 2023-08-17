591b30e1dcc6652d2a5486f8e24577c06a06733c
Adam Fraser <adam.fraser@gmail.com>
2023-08-17 01:46:41 -0700

    Ensure Queue Will Be Shutdown Before Awaiting It In ZStream#tapSink (#8311)
    * ensure queue will be shut down before awaiting it
    
    * fork in

--- streams/shared/src/main/scala/zio/stream/ZChannel.scala
+++ streams/shared/src/main/scala/zio/stream/ZChannel.scala
@@ -764,6 +764,7 @@ sealed trait ZChannel[-Env, -InErr, -InElem, -InDone, +OutErr, +OutElem, +OutDon
         queueReader = ZChannel.fromInput(input)
         pullL      <- (queueReader >>> self).toPull
         pullR      <- (queueReader >>> that).toPull
+        scope      <- ZIO.scope
       } yield {
         def handleSide[Err, Done, Err2, Done2](
           exit: Exit[Err, Either[Done, OutElem1]],
@@ -794,7 +795,7 @@ sealed trait ZChannel[-Env, -InErr, -InElem, -InDone, +OutErr, +OutElem, +OutDon
           exit match {
             case Exit.Success(Right(elem)) =>
               ZIO.succeed {
-                ZChannel.write(elem) *> ZChannel.fromZIO(pull.forkDaemon).flatMap { leftFiber =>
+                ZChannel.write(elem) *> ZChannel.fromZIO(pull.forkIn(scope)).flatMap { leftFiber =>
                   go(both(leftFiber, fiber))
                 }
               }
@@ -856,7 +857,7 @@ sealed trait ZChannel[-Env, -InErr, -InElem, -InDone, +OutErr, +OutElem, +OutDon
           }
 
         ZChannel
-          .fromZIO(pullL.forkDaemon.zipWith(pullR.forkDaemon)(BothRunning(_, _): MergeState))
+          .fromZIO(pullL.forkIn(scope).zipWith(pullR.forkIn(scope))(BothRunning(_, _): MergeState))
           .flatMap(go)
           .embedInput(input)
       }
