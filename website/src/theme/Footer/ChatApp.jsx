9db7af0a94dd67a6b7d9acf1d73cf4c491645e19
Milad Khajavi <khajavi@gmail.com>
2023-08-24 17:25:35 +0330

    make chat button, more visible. (#8349)

--- website/src/theme/Footer/ChatApp.jsx
+++ website/src/theme/Footer/ChatApp.jsx
@@ -10,7 +10,7 @@ export const ChatApp = (props) => {
     <>
       <button
         id='chat-button'
-        className='fixed border-0 font-sans  bg-orange-400 rounded-tl-md text-black px-3 py-2 hover:scale-95 transition text-ms bottom-0 right-0'
+        className='fixed border-0 font-sans font-medium bg-orange-400 rounded-tl-md text-black px-4 py-2 hover:scale-95 transition text-lg bottom-0 right-0'
         onClick={open}
       >
         ZIO Chat!
