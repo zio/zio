c6ab172d0f4f43e3d8feb76af51e2eff1169cdf9
Adam Fraser <adam.fraser@gmail.com>
2023-11-21 18:29:32 -0800

    simplify quantiles calculation

--- core/shared/src/main/scala/zio/internal/metrics/package.scala
+++ core/shared/src/main/scala/zio/internal/metrics/package.scala
@@ -14,67 +14,22 @@ package object metrics {
     (l, r) => java.lang.Double.compare(l, r)
 
   private[zio] def calculateQuantiles(
-    error: Double,
     sortedQuantiles: Chunk[Double],
     sortedSamples: Chunk[Double]
   ): Chunk[(Double, Option[Double])] = {
-
-    // The number of the samples examined
-    val sampleCnt = sortedSamples.size
-
-    @tailrec
-    def get(
-      current: Option[Double],
-      consumed: Int,
-      q: Double,
-      rest: Chunk[Double]
-    ): ResolvedQuantile =
-      rest match {
-        // if the remaining list of samples is empty there is nothing more to resolve
-        case c if c.isEmpty => ResolvedQuantile(q, None, consumed, Chunk.empty)
-        // if the quantile is the 100% Quantile, we can take the max of all remaining values as the result
-        case c if q == 1.0d => ResolvedQuantile(q, Some(c.last), consumed + c.length, Chunk.empty)
-        case c              =>
-          // Split in 2 chunks, the first chunk contains all elements of the same value as the chunk head
-          val sameHead = c.splitWhere(_ > c.head)
-          // How many elements do we want to accept for this quantile
-          val desired = q * sampleCnt
-          // The error margin
-          val allowedError = error / 2 * desired
-          // Taking into account the elements consumed from the samples so far and the number of
-          // same elements at the beginning of the chunk
-          // calculate the number of elements we would have if we selected the current head as result
-          val candConsumed = consumed + sameHead._1.length
-          val candError    = Math.abs(candConsumed - desired)
-
-          // If we haven't got enough elements yet, recurse
-          if (candConsumed < desired - allowedError)
-            get(c.headOption, candConsumed, q, sameHead._2)
-          // If we have too many elements, select the previous value and hand back the the rest as leftover
-          else if (candConsumed > desired + allowedError) ResolvedQuantile(q, current, consumed, c)
-          // If we are in the target interval, select the current head and hand back the leftover after dropping all elements
-          // from the sample chunk that are equal to the current head
-          else {
-            current match {
-              case None => get(c.headOption, candConsumed, q, sameHead._2)
-              case Some(current) =>
-                val prevError = Math.abs(desired - current)
-                if (candError < prevError) get(c.headOption, candConsumed, q, sameHead._2)
-                else ResolvedQuantile(q, Some(current), consumed, rest)
-            }
-          }
+    val length = sortedSamples.length
+    if (length == 0) sortedQuantiles.map((_, None))
+    else {
+      sortedQuantiles.map { quantile =>
+        if (quantile <= 0.0) (quantile, Some(sortedSamples(0)))
+        else if (quantile >= 1.0) (quantile, Some(sortedSamples(length - 1)))
+        else {
+          val index = math.ceil(quantile * length).toInt - 1
+          val value = sortedSamples(index)
+          (quantile, Some(value))
+        }
       }
-
-    val resolved = sortedQuantiles match {
-      case e if e.isEmpty => Chunk.empty
-      case c =>
-        sortedQuantiles.tail
-          .foldLeft(Chunk(get(None, 0, c.head, sortedSamples))) { case (cur, q) =>
-            cur ++ Chunk(get(cur.head.value, cur.head.consumed, q, cur.head.rest))
-          }
     }
-
-    resolved.map(rq => (rq.quantile, rq.value))
   }
 
   private[metrics] case class ResolvedQuantile(
