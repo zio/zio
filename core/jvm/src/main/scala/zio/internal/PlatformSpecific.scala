99e9c7a1a201c0c059c236745a9baecfec82226a
Adam Fraser <adam.fraser@gmail.com>
2024-01-01 04:10:00 -0800

    update copyright (#8596)

--- core/jvm/src/main/scala/zio/internal/PlatformSpecific.scala
+++ core/jvm/src/main/scala/zio/internal/PlatformSpecific.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2023 John A. De Goes and the ZIO Contributors
+ * Copyright 2017-2024 John A. De Goes and the ZIO Contributors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- core/shared/src/main/scala/zio/internal/Platform.scala
+++ core/shared/src/main/scala/zio/internal/Platform.scala
@@ -5,7 +5,7 @@ import zio.stacktracer.TracingImplicits.disableAutoTrace
 import scala.concurrent.ExecutionContext
 
 /*
- * Copyright 2017-2023 John A. De Goes and the ZIO Contributors
+ * Copyright 2017-2024 John A. De Goes and the ZIO Contributors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
