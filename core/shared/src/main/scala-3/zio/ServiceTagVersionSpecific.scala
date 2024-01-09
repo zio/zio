ecfc0b87767766c47ff7b9e10232977979a22804
Georgi Krastev <joro.kr.21@gmail.com>
2024-01-09 16:10:27 +0200

    [Scala 3] Prevent Tag materialization for intersection types in covariant position (#8612)
    Unfortunately the custom error message from the macro is suppressed.
    Also return type check errors from `typeCheck` to enable testing.

--- core/shared/src/main/scala-3/zio/ServiceTagVersionSpecific.scala
+++ core/shared/src/main/scala-3/zio/ServiceTagVersionSpecific.scala
@@ -11,22 +11,29 @@ private object TagMacros {
 
   def materialize[A: Type](using Quotes): Expr[Tag[A]] = {
     import quotes.reflect.*
-    TypeRepr.of[A].dealias match {
-      case tpe if tpe.typeSymbol.isTypeParam || tpe.typeSymbol.isAbstractType =>
-        Expr.summon[Tag[A]] match {
-          case Some(tag) => tag
-          case None => report.errorAndAbort( s"Cannot find implicit Tag[${tpe.show}]" )
-        }
-      case AndType(_, _) =>
-        report.errorAndAbort(s"You must not use an intersection type, yet have provided ${Type.show[A]}")
-      case tpe =>
-        '{
-          val tag0 = EnvironmentTag[A]
-          new Tag[A] {
-            def tag: LightTypeTag =  tag0.tag
-            def closestClass: Class[_] = tag0.closestClass
-          }
-        }
+
+    def isCovariant(typeParam: Symbol) = typeParam.flags.is(Flags.Covariant)
+    def isContravariant(typeParam: Symbol) = typeParam.flags.is(Flags.Contravariant)
+
+    def findIntersectionInCovariantPosition(tpe: TypeRepr, covariant: Boolean = true): Option[TypeRepr] = tpe match {
+      case AndType(_, _) => Option.when(covariant)(tpe)
+      case OrType(_, _) => Option.unless(covariant)(tpe)
+      case AppliedType(constructor, typeArgs) =>
+        val typeParams = constructor.typeSymbol.typeMembers.iterator.filter(_.isTypeParam)
+        typeParams.zip(typeArgs).collectFirst(Function.unlift { (typeParam, typeArg) =>
+          if isContravariant(typeParam) then findIntersectionInCovariantPosition(typeArg, !covariant)
+          else if isCovariant(typeParam) then findIntersectionInCovariantPosition(typeArg, covariant)
+          else None
+        })
+      case _ => None
     }
+
+    // Note: this error message is currently suppressed by the @implicitNotFound annotation on the parent of Tag.
+    def intersectionFound(tpe: TypeRepr) =
+      report.errorAndAbort(s"You must not use an intersection type, yet have provided ${tpe.show}")
+
+    val tpe = TypeRepr.of[A].dealias
+    if tpe.typeSymbol.isTypeParam || tpe.typeSymbol.isAbstractType then '{Tag[A]}
+    else findIntersectionInCovariantPosition(tpe).fold('{Tag[A]})(intersectionFound)
   }
 }
