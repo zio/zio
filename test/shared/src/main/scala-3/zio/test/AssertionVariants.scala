8b2b14040cfd32452b26eef500600781feab1e16
urbit-pilled <urbit-pilled@proton.me>
2024-01-12 10:16:05 +1300

    modified equalTo function to shorten error message comparisons

--- test/shared/src/main/scala-3/zio/test/AssertionVariants.scala
+++ test/shared/src/main/scala-3/zio/test/AssertionVariants.scala
@@ -22,6 +22,29 @@ import zio.test.Assertion.Arguments.valueArgument
 
 trait AssertionVariants {
 
+  def diffProduct(obj1: Any, obj2: Any, paramNames: List[String] = Nil, rootClassName: Option[String] = None): String = {
+    val currClassName = rootClassName.getOrElse(obj1.getClass.getSimpleName)
+
+    (obj1, obj2) match {
+      case (seq1: Iterable[Any], seq2: Iterable[Any]) if seq1.size == seq2.size =>
+        seq1.zip(seq2).zipWithIndex.flatMap {
+          case ((subObj1, subObj2), index) =>
+            val newParamName = s"[$index]"
+            diffProduct(subObj1, subObj2, newParamName :: paramNames, Some(currClassName))
+        }.mkString
+      case (obj1: Product, obj2: Product) if obj1.productArity == obj2.productArity =>
+        obj1.productIterator.zip(obj2.productIterator).zip(obj1.productElementNames).flatMap {
+          case ((subObj1, subObj2), paramName) =>
+            val newParamName = if (paramName.nonEmpty) s".$paramName" else ""
+            if (subObj1 != subObj2 && !subObj1.isInstanceOf[Product])
+              s"$currClassName${paramNames.reverse.mkString("")}$newParamName : expected '$subObj1' got '$subObj2'\n"
+            else
+              diffProduct(subObj1, subObj2, newParamName :: paramNames, Some(currClassName))
+        }.mkString
+      case _ => ""
+    }
+  }
+
   /**
    * Makes a new assertion that requires a value equal the specified value.
    */
@@ -35,7 +58,11 @@ trait AssertionVariants {
             case (left, right)                     => left == right
           }
           TestTrace.boolean(result) {
-            M.pretty(actual) + M.equals + M.pretty(expected)
+            if(expected.isInstanceOf[Product]){
+              M.text(diffProduct(actual, expected))
+            }else{
+              M.pretty(actual) + M.equals + M.pretty(expected)
+            }
           }
         }
         .withCode("equalTo", valueArgument(expected))
