bfa590e0b516e5dc4bee60e093f3836e42a41b74
urbit-pilled <urbit-pilled@proton.me>
2024-01-12 18:58:07 +1300

    fixed 2.12 compatibility

---
+++ test/shared/src/main/scala-2.12/zio/test/AssertionVariants.scala
@@ -0,0 +1,78 @@
+
+/*
+ * Copyright 2019-2024 John A. De Goes and the ZIO Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package zio.test
+
+import zio.stacktracer.TracingImplicits.disableAutoTrace
+import zio.test.Assertion.Arguments.valueArgument
+import zio.test.{ErrorMessage => M}
+
+trait AssertionVariants {
+  def diffProduct(
+    obj1: Any,
+    obj2: Any,
+    paramNames: List[String] = Nil,
+    rootClassName: Option[String] = None
+  ): String = {
+    val currClassName = rootClassName.getOrElse(obj1.getClass.getSimpleName)
+
+    (obj1, obj2) match {
+      case (seq1: Iterable[_], seq2: Iterable[_]) if seq1.size == seq2.size =>
+        seq1
+          .zip(seq2)
+          .zipWithIndex
+          .flatMap { case ((subObj1, subObj2), index) =>
+            val newParamName = s"[$index]"
+            diffProduct(subObj1, subObj2, newParamName :: paramNames, Some(currClassName))
+          }
+          .mkString
+      case (obj1: Product, obj2: Product) if obj1.productArity == obj2.productArity =>
+        obj1.productIterator
+          .zip(obj2.productIterator)
+          .zip(obj1.getClass.getDeclaredFields.iterator.map(_.getName))
+          .flatMap { case ((subObj1, subObj2), paramName) =>
+            val newParamName = if (paramName.nonEmpty) s".$paramName" else ""
+            if (subObj1 != subObj2 && !subObj1.isInstanceOf[Product])
+              s"$currClassName${paramNames.reverse.mkString("")}$newParamName : expected '$subObj1' got '$subObj2'\n"
+            else
+              diffProduct(subObj1, subObj2, newParamName :: paramNames, Some(currClassName))
+          }
+          .mkString
+      case _ => ""
+    }
+  }
+
+  def equalTo[A, B](expected: A)(implicit eql: Eql[A, B]): Assertion[B] =
+    Assertion[B](
+      TestArrow
+        .make[B, Boolean] { actual =>
+          val result = (actual, expected) match {
+            case (left: Array[_], right: Array[_])         => left.sameElements[Any](right)
+            case (left: CharSequence, right: CharSequence) => left.toString == right.toString
+            case (left, right)                             => left == right
+          }
+          TestTrace.boolean(result) {
+            if (expected.isInstanceOf[Product]) {
+              M.text(diffProduct(actual, expected))
+            } else {
+              M.pretty(actual) + M.equals + M.pretty(expected)
+            }
+          }
+        }
+        .withCode("equalTo", valueArgument(expected))
+    )
+}
