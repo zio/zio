97f11dd34fe0b63a42c0f4ee9509b639623c1a8f
guptadeepak8 <vectord7738@gmail.com>
2024-01-15 12:14:48 +0530

    updated renderFailureDetails return only message

--- test-junit/jvm/src/main/scala/zio/test/junit/ZTestJUnitRunner.scala
+++ test-junit/jvm/src/main/scala/zio/test/junit/ZTestJUnitRunner.scala
@@ -124,10 +124,7 @@ class ZTestJUnitRunner(klass: Class[_]) extends Runner with Filterable {
   }
 
   private def renderFailureDetails(label: String, result: TestResult): Message = {
-    val assertResultLines = scala.util.Try(ConsoleRenderer.renderAssertionResult(result.result, 0)) match {
-      case scala.util.Success(message) => message
-      case scala.util.Failure(e)       => Message(s"Detected exception as rendering assertion results: $e")
-    }
+    val assertResultLines = ConsoleRenderer.renderAssertionResult(result.result, 0)
     Message(
       ConsoleRenderer
         .rendered(
