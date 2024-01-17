e7dff6cd4f3da5bc5c91fbdb91e3ac10c70b7b89
kyri-petrou <67301607+kyri-petrou@users.noreply.github.com>
2024-01-17 19:31:37 +1100

    Fix sequence config with shorter one issue (#8624)
    * Fix shorter sequence config value issue
    
    * PR comments

--- core/shared/src/main/scala/zio/ConfigProvider.scala
+++ core/shared/src/main/scala/zio/ConfigProvider.scala
@@ -209,7 +209,7 @@ object ConfigProvider {
                         case (Left(e1), Left(e2)) => ZIO.fail(e1 && e2)
                         case (Left(_), Right(r))  => ZIO.succeed(r)
                         case (Right(l), Left(_))  => ZIO.succeed(l)
-                        case (Right(l), Right(r)) => ZIO.succeed(l ++ r)
+                        case (Right(l), Right(r)) => ZIO.succeed(if (l.nonEmpty) l else r)
                       }
           } yield result
 
