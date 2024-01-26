e6c14043293c41041638208ca7c3511d4d797fb9
roopsai <roop.sai.surampudi@proton.me>
2024-01-26 10:49:42 +0530

    Fixed #8647 - Enable Redirection Plugin for ZIO Website

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -223,7 +223,7 @@ const config = {
       };
     },
     [path.join(__dirname, './plugins/zio-ecosystem-docusaurus'), {}],
-    ['@docusaurus/plugin-client-redirects',{}]
+    ['@docusaurus/plugin-client-redirects', {}],
   ],
   markdown: {
     mermaid: true,
