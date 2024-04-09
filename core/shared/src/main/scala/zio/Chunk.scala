0b4afbeedd567228305334625b55e9eaf5188acd
Pierre Ricadat <pierre.ricadat@devsisters.com>
2024-04-09 18:39:53 +0900

    Optimize fromIterable

--- core/shared/src/main/scala/zio/Chunk.scala
+++ core/shared/src/main/scala/zio/Chunk.scala
@@ -1266,9 +1266,10 @@ object Chunk extends ChunkFactory with ChunkPlatformSpecific {
    */
   def fromIterable[A](it: Iterable[A]): Chunk[A] =
     it match {
-      case chunk: Chunk[A]              => chunk
-      case iterable if iterable.isEmpty => Empty
-      case vector: Vector[A]            => VectorChunk(vector)
+      case chunk: Chunk[A]                     => chunk
+      case iterable if iterable.isEmpty        => Empty
+      case iterable if iterable.sizeCompare(1) => single(iterable.head)
+      case vector: Vector[A]                   => VectorChunk(vector)
       case iterable =>
         val builder = ChunkBuilder.make[A]()
         builder.sizeHint(iterable)
