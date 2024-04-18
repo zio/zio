42e31dd8a3577dfe97e49040f511ce24e0383b05
jules Ivanic <jules.ivanic@gmail.com>
2024-04-18 12:13:55 +0800

    Remove compilation errors

--- streams-tests/jvm/src/test/scala/zio/stream/GunzipSpec.scala
+++ streams-tests/jvm/src/test/scala/zio/stream/GunzipSpec.scala
@@ -34,7 +34,7 @@ object GunzipSpec extends ZIOBaseSpec {
       ),
       test("long input, no SYNC_FLUSH")(
         assertZIO(
-          (jdkGzippedStream(longText, false).channel >>> Gunzip.makeGunzipper(64)).runCollect.map(_._1.flatten)
+          (jdkGzippedStream(longText, syncFlush = false).channel >>> Gunzip.makeGunzipper(64)).runCollect.map(_._1.flatten)
         )(equalTo(Chunk.fromArray(longText)))
       ),
       test("long input, buffer smaller than chunks")(
