99be468101a0e63f40967feb97f670ff4a55ca26
M-Vamshi <21211a05f1@bvrit.ac.in>
2024-05-05 15:32:49 +0530

    fix test

--- test/shared/src/main/scala/zio/test/internal/SmartAssertions.scala
+++ test/shared/src/main/scala/zio/test/internal/SmartAssertions.scala
@@ -373,6 +373,37 @@ object SmartAssertions {
         s"${M.pretty(a)}${M.equals}${M.pretty(that)}"
     }
 
+  def equalToL[A, B](that: B)(implicit diff: OptionalImplicit[Diff[B]], conv: (A => B)): TestArrow[A, Boolean] =
+    TestArrow
+      .make[A, Boolean] { a =>
+        val result = (a, that) match {
+          case (a: Array[_], that: Array[_]) => a.sameElements[Any](that)
+          case _                             => a == that
+        }
+
+        TestTrace.boolean(result) {
+          diff.value match {
+            case Some(diff) if !diff.isLowPriority && !result =>
+              val diffResult = diff.diff(that, conv(a))
+              diffResult match {
+                case DiffResult.Different(_, _, None) =>
+                  M.pretty(a) + M.equals + M.pretty(that)
+                case diffResult =>
+                  M.choice("There was no difference", "There was a difference") ++
+                    M.custom(ConsoleUtils.underlined("Expected")) ++ M.custom(PrettyPrint(that)) ++
+                    M.custom(
+                      ConsoleUtils.underlined(
+                        "Diff"
+                      ) + s" ${scala.Console.RED}-expected ${scala.Console.GREEN}+obtained".faint
+                    ) ++
+                    M.custom(scala.Console.RESET + diffResult.render)
+              }
+            case _ =>
+              M.pretty(a) + M.equals + M.pretty(that)
+          }
+        }
+      }
+
   def equalToR[A, B](that: B)(implicit diff: OptionalImplicit[Diff[A]], conv: (B => A)): TestArrow[A, Boolean] =
     TestArrow
       .make[A, Boolean] { a =>
