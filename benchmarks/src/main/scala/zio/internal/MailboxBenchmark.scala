446ca542aa3b29799007460ba61c9a8ab935e1e3
Ajay Chandran <ajay.chandran.1980@gmail.com>
2024-05-31 13:58:08 +0530

    Revise benchmarks

--- benchmarks/src/main/scala/zio/internal/MailboxBenchmark.scala
+++ benchmarks/src/main/scala/zio/internal/MailboxBenchmark.scala
@@ -4,18 +4,25 @@ import org.openjdk.jmh.annotations._
 
 import java.util.concurrent.TimeUnit
 
-@BenchmarkMode(Array(Mode.AverageTime))
+/**
+ * Defines benchmarks to measure the relative performance of MPSC queues in the
+ * presence of concurrent operations. The benchmarks are organized into groups
+ * where each group consists of one or more producer threads that enqueue an
+ * element and a consumer thread that dequeues all elements.
+ *
+ * @note
+ *   A `SingleShotTime` mode with a `batchSize` is used because
+ *   - a precise number of elements need to be dequeued
+ *   - the operation does not have a steady state
+ *   - the operation measurement is too small
+ */
+@BenchmarkMode(Array(Mode.SingleShotTime))
+@Fork(1)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
 @State(Scope.Group)
-@OperationsPerInvocation(1)
-@Fork(2)
-@Measurement(iterations = 15, time = 1, timeUnit = TimeUnit.SECONDS)
-@Warmup(iterations = 15, time = 1, timeUnit = TimeUnit.SECONDS)
-@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Measurement(batchSize = 100000, iterations = 10)
+@Warmup(batchSize = 100000, iterations = 10)
 class MailboxBenchmark {
-
-  // CLI usage:
-  // -opi 1024 -jvmArgsAppend -Dopi=1024
-  val opi = Option(Integer.getInteger("opi")).fold(1)(_.intValue())
 
   @Param(
     Array(
@@ -24,79 +31,74 @@ class MailboxBenchmark {
       "MpscLinkedQueue"
     )
   )
-  var name: String = _
+  var queue: String = _
 
   var q: MailboxQueue[AnyRef] = _
 
-  def element(): AnyRef =
-    new Object()
-
   @Setup(Level.Iteration)
-  def create(): Unit =
-    q = MailboxQueue(name)
+  def setup(): Unit =
+    q = MailboxQueue(queue)
 
-  def dequeue(): Int = {
+  def add() = {
+    val a = new Object()
+    q.add(a)
+    a
+  }
+
+  def poll(n: Int) = {
     var i = 0
-    val n = opi
     while (i < n) {
-      q.add(element())
       while (null == q.poll()) {}
       i += 1
     }
     i
   }
 
-  def enqueue(): Int = {
-    var i = 0
-    val n = opi
-    while (i < n) {
-      val a = element()
-      q.add(a)
-      i += 1
-    }
-    i
-  }
+  @Benchmark
+  @Group("p01")
+  @GroupThreads(1)
+  def add_01() =
+    add()
 
   @Benchmark
   @Group("p01")
   @GroupThreads(1)
-  def dequeue_01() = dequeue()
-
-  @Benchmark
-  @Group("p01")
-  @GroupThreads(1)
-  def enqueue_01() = enqueue()
-
-  @Benchmark
-  @Group("p02")
-  @GroupThreads(1)
-  def dequeue_02() = dequeue()
+  def poll_01() =
+    poll(1)
 
   @Benchmark
   @Group("p02")
   @GroupThreads(2)
-  def enqueue_02() = enqueue()
+  def add_02() =
+    add()
+
+  @Benchmark
+  @Group("p02")
+  @GroupThreads(1)
+  def poll_02() =
+    poll(2)
+
+  @Benchmark
+  @Group("p03")
+  @GroupThreads(3)
+  def add_03() =
+    add()
 
   @Benchmark
   @Group("p03")
   @GroupThreads(1)
-  def dequeue_03() = dequeue()
+  def poll_03() =
+    poll(3)
 
   @Benchmark
-  @Group("p03")
-  @GroupThreads(2)
-  def enqueue_03() = enqueue()
+  @Group("p07")
+  @GroupThreads(7)
+  def add_07() =
+    add()
 
   @Benchmark
-  @Group("dequeue")
+  @Group("p07")
   @GroupThreads(1)
-  def empty() = {
-    var i = 0
-    val n = opi
-    while (i < n) {
-      q.poll()
-      i += 1
-    }
-    i
-  }
+  def poll_07() =
+    poll(7)
 }
