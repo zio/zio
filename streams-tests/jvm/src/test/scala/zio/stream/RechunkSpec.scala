464c6d2142191cd3e45d3bb1de3c4210462aff94
davidl <davidl@pagero.com>
2024-07-02 15:34:13 +0200

    2.12 lint fix

--- streams-tests/jvm/src/test/scala/zio/stream/RechunkSpec.scala
+++ streams-tests/jvm/src/test/scala/zio/stream/RechunkSpec.scala
@@ -1,10 +1,9 @@
 package zio.stream
 
 import zio._
+import zio.stream.ZStream.fromChunk
 import zio.test.Assertion._
 import zio.test._
-
-import scala.collection.immutable.ArraySeq.unsafeWrapArray
 
 object RechunkSpec extends ZIOBaseSpec {
   override def spec =
@@ -20,19 +19,20 @@ object RechunkSpec extends ZIOBaseSpec {
         )
       ),
       test("rechunk large") {
-        val array = (1 to 51).toArray
+        val elems = (1 to 51).toList
         for {
-          result <- ZStream(unsafeWrapArray(array): _*).rechunk(2).chunks.runCollect
+          result <- ZStream.from(elems).rechunk(2).chunks.runCollect
         } yield {
           assertTrue(result.size == 26) &&
           assertTrue(result.dropRight(1).forall(_.size == 2)) &&
-          assertTrue(result.flatten sameElements array)
+          assertTrue(result.flatten.toList == elems)
         }
       },
       test("rechunk mixed large/small sizes")(
         check(Gen.chunkOfN(10)(Gen.chunkOfBounded(0, 50)(Gen.int(1, 100))), Gen.int(10, 60)) { (c, n) =>
+          val in = ZStream.fromChunk(c).flatMap(fromChunk(_))
           for {
-            result  <- ZStream.fromChunks(unsafeWrapArray(c.toArray): _*).rechunk(n).chunks.runCollect
+            result  <- in.rechunk(n).chunks.runCollect
             expected = c.flatten.grouped(n).toList
           } yield assertTrue(result.toList == expected)
         }
