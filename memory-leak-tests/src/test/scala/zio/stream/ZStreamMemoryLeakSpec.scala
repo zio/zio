11c15395ac0b32b77cbb9d7a10476aa84a2ef955
Sergey Rublev <sergey.rublev@dataspike.io>
2024-07-09 17:45:52 +0300

    Use parallelism 1 for flatMapPar - still leak

--- memory-leak-tests/src/test/scala/zio/stream/ZStreamMemoryLeakSpec.scala
+++ memory-leak-tests/src/test/scala/zio/stream/ZStreamMemoryLeakSpec.scala
@@ -4,16 +4,16 @@ import zio._
 
 object ZStreamMemoryLeakSpec extends MemoryLeakSpec {
   def spec = suite("ZStream Memory Leak spec")(
-    leakTest("mapZIOParUnordered not leak") {
+    leakTest("mapZIOParUnordered(1) doesn't leak") {
       ZStream
         .iterate(1)(_ + 1)
         .mapZIOParUnordered(1)(i => ZIO.when(i % 1000 == 0)(ZIO.logDebug(s"$i")))
         .runDrain
     },
-    leakTest("flatMapPar with Int.MaxValue not leak") {
+    leakTest("flatMapPar(1) doesn't leak") {
       ZStream
         .iterate(MemoryHolder())(_ => MemoryHolder())
-        .flatMapPar(Int.MaxValue) { i =>
+        .flatMapPar(1) { i =>
           ZStream.fromZIO(ZIO.logDebug(s"$i"))
         }
         .runDrain
