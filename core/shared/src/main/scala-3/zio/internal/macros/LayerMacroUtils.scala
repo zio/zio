3ee6e584f1b9d61391d915dc3f722c318f54fbad
kyri-petrou <67301607+kyri-petrou@users.noreply.github.com>
2024-07-22 09:27:54 +0300

    Add scalafmt for Scala 3 files (#9032)

--- core/shared/src/main/scala-3/zio/internal/macros/LayerMacroUtils.scala
+++ core/shared/src/main/scala-3/zio/internal/macros/LayerMacroUtils.scala
@@ -7,7 +7,7 @@ import zio.internal.ansi.AnsiStringOps
 import zio.internal.macros.StringUtils.StringOps
 import zio.internal.stacktracer.Tracer
 
-private [zio] object LayerMacroUtils {
+private[zio] object LayerMacroUtils {
   type LayerExpr[E] = Expr[ZLayer[_, E, _]]
 
   def composeLayer[R1, E, O1, O2](lhs: ZLayer[R1, E, O1], rhs: ZLayer[O1, E, O2])(using Trace): ZLayer[R1, E, O2] =
@@ -24,7 +24,7 @@ private [zio] object LayerMacroUtils {
 
     def getNode(layer: LayerExpr[E]): Node[TypeRepr, LayerExpr[E]] = layer match {
       case '{ $layer: ZLayer[in, e, out] } =>
-        val inputs = getRequirements[in]
+        val inputs  = getRequirements[in]
         val outputs = getRequirements[out]
         Node(inputs, outputs, layer)
     }
@@ -32,21 +32,21 @@ private [zio] object LayerMacroUtils {
     def getRequirements[T: Type]: List[TypeRepr] = {
       def loop(tpe: TypeRepr): List[TypeRepr] =
         tpe.dealias.simplified match {
-          case AndType(lhs, rhs) => loop(lhs) ++ loop(rhs)
-          case AppliedType(_, TypeBounds(_,_) :: _) => Nil
-          case other if other =:= TypeRepr.of[Any] => Nil
+          case AndType(lhs, rhs)                          => loop(lhs) ++ loop(rhs)
+          case AppliedType(_, TypeBounds(_, _) :: _)      => Nil
+          case other if other =:= TypeRepr.of[Any]        => Nil
           case other if other.dealias.simplified != other => loop(other)
-          case other => List(other.dealias)
+          case other                                      => List(other.dealias)
         }
 
       loop(TypeRepr.of[T])
     }
 
-    val targetTypes = getRequirements[R]
+    val targetTypes    = getRequirements[R]
     val remainderTypes = getRequirements[R0]
     val layerToDebug: PartialFunction[LayerExpr[E], ZLayer.Debug] = {
-      case '{ZLayer.Debug.tree} => ZLayer.Debug.Tree
-      case '{ZLayer.Debug.mermaid} => ZLayer.Debug.Mermaid
+      case '{ ZLayer.Debug.tree }    => ZLayer.Debug.Tree
+      case '{ ZLayer.Debug.mermaid } => ZLayer.Debug.Mermaid
     }
 
     '{
@@ -54,30 +54,39 @@ private [zio] object LayerMacroUtils {
 
       ${
         def typeToNode(tpe: TypeRepr): Node[TypeRepr, LayerExpr[E]] =
-          Node(Nil, List(tpe), tpe.asType match { case '[t] => '{ZLayer.environment[t](trace)} })
+          Node(Nil, List(tpe), tpe.asType match { case '[t] => '{ ZLayer.environment[t](trace) } })
 
         def composeH(lhs: LayerExpr[E], rhs: LayerExpr[E]): LayerExpr[E] =
-          lhs match { case '{$lhs: ZLayer[i, E, o]} =>
-            rhs match { case '{$rhs: ZLayer[i2, E, o2]} =>
-              val tag = Expr.summon[EnvironmentTag[o2]]
-                .getOrElse(report.errorAndAbort(s"Cannot find EnvironmentTag[${TypeRepr.of[o2].show}] in implicit scope"))
-              '{$lhs.++($rhs)($tag)}
-            }
+          lhs match {
+            case '{ $lhs: ZLayer[i, E, o] } =>
+              rhs match {
+                case '{ $rhs: ZLayer[i2, E, o2] } =>
+                  val tag = Expr
+                    .summon[EnvironmentTag[o2]]
+                    .getOrElse(
+                      report.errorAndAbort(s"Cannot find EnvironmentTag[${TypeRepr.of[o2].show}] in implicit scope")
+                    )
+                  '{ $lhs.++($rhs)($tag) }
+              }
           }
 
         def composeV(lhs: LayerExpr[E], rhs: LayerExpr[E]): LayerExpr[E] =
-          lhs match { case '{$lhs: ZLayer[i, E, o]} =>
-            rhs match { case '{$rhs: ZLayer[`o`, E, o2]} =>
-              '{composeLayer($lhs, $rhs)(using trace)}
-            }
+          lhs match {
+            case '{ $lhs: ZLayer[i, E, o] } =>
+              rhs match {
+                case '{ $rhs: ZLayer[`o`, E, o2] } =>
+                  '{ composeLayer($lhs, $rhs)(using trace) }
+              }
           }
 
         def buildFinalTree(tree: LayerTree[LayerExpr[E]]): LayerExpr[E] = {
           val layerExprs = tree.toList
-          ValDef.let(Symbol.spliceOwner, layerExprs.map(_.asTerm)) { idents =>
-            val exprMap = layerExprs.zip(idents.map(_.asExprOf[ZLayer[_, E, _]])).toMap
-            tree.fold('{ZLayer.unit}, exprMap, composeH, composeV).asTerm
-          }.asExprOf[ZLayer[_, E, _]]
+          ValDef
+            .let(Symbol.spliceOwner, layerExprs.map(_.asTerm)) { idents =>
+              val exprMap = layerExprs.zip(idents.map(_.asExprOf[ZLayer[_, E, _]])).toMap
+              tree.fold('{ ZLayer.unit }, exprMap, composeH, composeV).asTerm
+            }
+            .asExprOf[ZLayer[_, E, _]]
         }
 
         val builder = LayerBuilder[TypeRepr, LayerExpr[E]](
