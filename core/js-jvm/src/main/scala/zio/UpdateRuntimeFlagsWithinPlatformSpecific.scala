d7dcc133e45e8cfeac407bbca9fb0a6bc3e74129
kyri-petrou <67301607+kyri-petrou@users.noreply.github.com>
2024-07-30 08:25:38 +0300

    Improve performance of updating interruption by 1 order of magnitude (#9033)
    * Optimizations for updating runtime flags
    
    * Maintain bin-compatibility
    
    * fmt
    
    * Fix Scala 3 compiling
    
    * Move emptyTrace in companion object

--- core/js-jvm/src/main/scala/zio/UpdateRuntimeFlagsWithinPlatformSpecific.scala
+++
@@ -1,9 +0,0 @@
-package zio
-
-import java.util.function.IntFunction
-
-private[zio] trait UpdateRuntimeFlagsWithinPlatformSpecific {
-  // By the virtue of using IntFunction, it does not box.
-  // Soon available for Scala Native too: https://github.com/scala-native/scala-native/pull/3127
-  type RuntimeFlagsFunc[A] = IntFunction[A]
-}
