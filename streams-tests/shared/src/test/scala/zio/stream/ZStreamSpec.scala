d3236675fcdab4f3e5d3b675a5f638320be958f9
Regis Kuckaertz <regiskuckaertz@users.noreply.github.com>
2024-08-01 19:31:14 +0100

    fix: remove bufferSize in test

--- streams-tests/shared/src/test/scala/zio/stream/ZStreamSpec.scala
+++ streams-tests/shared/src/test/scala/zio/stream/ZStreamSpec.scala
@@ -2762,7 +2762,7 @@ object ZStreamSpec extends ZIOBaseSpec {
                 latch <- CountdownLatch.make(parallelism + 1)
                 f <- ZStream
                        .range(0, iterations)
-                       .mapZIOParUnordered(parallelism, parallelism)(_ => latch.countDown *> latch.await)
+                       .mapZIOParUnordered(parallelism)(_ => latch.countDown *> latch.await)
                        .runDrain
                        .fork
                 _     <- Live.live(latch.count.delay(100.micros)).repeatUntil(_ == 1)
