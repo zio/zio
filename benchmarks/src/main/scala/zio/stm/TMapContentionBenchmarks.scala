b0a1e3f7d02d1102bac7c65e7e7e53a9c74d395d
Kyri Petrou <kyri.petrou@outlook.com>
2024-08-06 16:30:54 +0300

    Update benchmarks

--- benchmarks/src/main/scala/zio/stm/TMapContentionBenchmarks.scala
+++ benchmarks/src/main/scala/zio/stm/TMapContentionBenchmarks.scala
@@ -3,39 +3,56 @@ package zio.stm
 import org.openjdk.jmh.annotations.{Scope => JScope, _}
 import zio._
 
-import java.util.concurrent.TimeUnit
+import java.util.concurrent.{ThreadLocalRandom, TimeUnit}
 
 @State(JScope.Thread)
 @BenchmarkMode(Array(Mode.Throughput))
 @OutputTimeUnit(TimeUnit.SECONDS)
-@Measurement(iterations = 15, timeUnit = TimeUnit.SECONDS, time = 10)
-@Warmup(iterations = 15, timeUnit = TimeUnit.SECONDS, time = 10)
-@Fork(1)
+@Warmup(iterations = 5, timeUnit = TimeUnit.SECONDS, time = 1)
+@Measurement(iterations = 4, timeUnit = TimeUnit.SECONDS, time = 1)
+@Fork(4)
 class TMapContentionBenchmarks {
   import BenchmarkUtil.unsafeRun
 
-  @Param(Array("100", "1000", "10000"))
-  var repeatedUpdates: Int = _
+  @Param(Array("2", "10"))
+  var fibers: Int = _
 
-  private var mapUpdates: UIO[Unit] = _
-  private var refUpdates: UIO[Unit] = _
+  val ops: Int = 1000
+
+  private var tmapUpdates: UIO[Unit] = _
+  private var trefUpdates: UIO[Unit] = _
+  private var refUpdates: UIO[Unit]  = _
 
   @Setup(Level.Trial)
   def setup(): Unit = {
-    val keysToUpdate = (1 to 100).toList
-    val data         = (1 to 1000).toList.zipWithIndex
-    val map          = unsafeRun(TMap.fromIterable(data).commit)
-    val ref          = TRef.unsafeMake(data.toMap)
+    val keysToUpdate = (1 to fibers).toList
+    val data         = (1 to ops).toList.zipWithIndex
+    val tmap         = unsafeRun(TMap.fromIterable(data).commit)
+    val tref         = TRef.unsafeMake(data.toMap)
+    val ref          = Ref.unsafe.make(data.toMap)(Unsafe.unsafe)
 
-    mapUpdates = ZIO.foreachParDiscard(keysToUpdate)(i => map.put(i, i).commit.repeatN(repeatedUpdates))
-    refUpdates = ZIO.foreachParDiscard(keysToUpdate)(i => ref.update(_.updated(i, i)).commit.repeatN(repeatedUpdates))
+    def ri() = ThreadLocalRandom.current().nextInt()
+
+    tmapUpdates = ZIO.foreachParDiscard(keysToUpdate) { i =>
+      ZIO.suspendSucceed(tmap.put(i, ri()).commit).repeatN(ops)
+    }
+    trefUpdates = ZIO.foreachParDiscard(keysToUpdate) { i =>
+      ZIO.suspendSucceed(tref.update(_.updated(i, ri())).commit).repeatN(ops)
+    }
+    refUpdates = ZIO.foreachParDiscard(keysToUpdate) { i =>
+      ZIO.suspendSucceed(ref.update(_.updated(i, ri()))).repeatN(ops)
+    }
   }
 
   @Benchmark
-  def contentionMap(): Unit =
-    unsafeRun(mapUpdates)
+  def refMapContention(): Unit =
+    unsafeRun(refUpdates)
 
   @Benchmark
-  def contentionRef(): Unit =
-    unsafeRun(refUpdates)
+  def trefMapContention(): Unit =
+    unsafeRun(trefUpdates)
+
+  @Benchmark
+  def tmapContention(): Unit =
+    unsafeRun(tmapUpdates)
 }
