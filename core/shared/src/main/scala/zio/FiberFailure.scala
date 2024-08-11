a7370f267b0039f8e56df42929d867e2e43c453d
asr2003 <162500856+asr2003@users.noreply.github.com>
2024-08-12 00:35:10 +0530

    fmt

--- core/shared/src/main/scala/zio/FiberFailure.scala
+++ core/shared/src/main/scala/zio/FiberFailure.scala
@@ -38,8 +38,7 @@ final case class FiberFailure(cause: Cause[Any]) extends Throwable(null, null, t
   override def getMessage: String = cause.unified.headOption.fold("<unknown>")(_.message)
 
   override def getStackTrace(): Array[StackTraceElement] = {
-    val zioStackTrace  = cause.unified.headOption.fold[Chunk[StackTraceElement]](Chunk.empty)(_.trace).toArray
-    val javaStackTrace = super.getStackTrace()
+    val zioStackTrace = cause.unified.headOption.fold[Chunk[StackTraceElement]](Chunk.empty)(_.trace).toArray
 
     // Combine both stack traces into a single array with minimal allocations
     val combinedStackTrace = new Array[StackTraceElement](zioStackTrace.length + javaStackTrace.length)
