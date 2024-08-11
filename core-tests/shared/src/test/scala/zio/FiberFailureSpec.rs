4f1dcbf1cb67e8b463100f265e235084f3d27ef7
asr2003 <162500856+asr2003@users.noreply.github.com>
2024-08-12 00:38:30 +0530

    add tests to new file

---
+++ core-tests/shared/src/test/scala/zio/FiberFailureSpec.rs
@@ -0,0 +1,39 @@
+package zio
+
+import zio.test.TestAspect._
+import zio.test._
+
+object FiberFailureSpec extends ZIOBaseSpec {
+
+  def spec = suite("FiberFailureSpec")(
+    test("FiberFailure captures the stack trace for ZIO.fail with String") {
+      def subcall(): Unit =
+        Unsafe.unsafe { implicit unsafe =>
+          Runtime.default.unsafe.run(ZIO.fail("boom")).getOrThrowFiberFailure()
+        }
+      def call1(): Unit = subcall()
+
+      val fiberFailureTest = ZIO
+        .attempt(call1())
+        .catchAll {
+          case fiberFailure: FiberFailure =>
+            val stackTrace = fiberFailure.getStackTrace.mkString("\n")
+            ZIO.log(s"Captured Stack Trace:\n$stackTrace") *>
+              ZIO.succeed(stackTrace)
+          case other =>
+            ZIO.succeed(s"Unexpected failure: ${other.getMessage}")
+        }
+        .asInstanceOf[ZIO[Any, Nothing, String]]
+
+      fiberFailureTest.flatMap { stackTrace =>
+        ZIO.succeed {
+          assertTrue(
+            stackTrace.contains("call1") &&
+              stackTrace.contains("subcall") &&
+              stackTrace.contains("FiberFailureSpec")
+          )
+        }
+      }
+    } @@ exceptJS
+  )
+}
