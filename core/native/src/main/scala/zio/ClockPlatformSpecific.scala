5746ab2e0530dada241e0afb8ee8bd910c458ea7
Vamshi Maskuri <117595548+varshith257@users.noreply.github.com>
2024-08-16 05:19:36 +0530

    increase pool size

--- core/native/src/main/scala/zio/ClockPlatformSpecific.scala
+++ core/native/src/main/scala/zio/ClockPlatformSpecific.scala
@@ -55,7 +55,7 @@ private[zio] trait ClockPlatformSpecific {
 
 private object ClockPlatformSpecific {
   // Multi-threaded scheduler using ScheduledExecutorService
-  val scheduler: ScheduledExecutorService = Executors.newScheduledThreadPool(2)
+  val scheduler: ScheduledExecutorService = Executors.newScheduledThreadPool(4)
 
   final class Timer private (private val scheduledFuture: java.util.concurrent.ScheduledFuture[_]) extends AnyVal {
     def clear(): Unit =
