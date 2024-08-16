fa8f7f62e4f91508610e70e41e60dd078d62f031
Alejandro Navas <caeus@Alejandros-MacBook-Pro.local>
2024-08-16 18:02:06 +0200

    Added feature #9102

--- core-tests/shared/src/test/scala/zio/SemaphoreSpec.scala
+++ core-tests/shared/src/test/scala/zio/SemaphoreSpec.scala
@@ -1,6 +1,5 @@
 package zio
 
-import zio._
 import zio.test.Assertion._
 import zio.test.TestAspect._
 import zio.test._
@@ -32,6 +31,26 @@ object SemaphoreSpec extends ZIOBaseSpec {
         _         <- ZIO.scoped(semaphore.withPermitsScoped(2))
         permits   <- semaphore.available
       } yield assertTrue(permits == 2L)
+    },
+    test("awaiting returns the count of waiting fibers") {
+      for {
+        semaphore <- Semaphore.make(1)
+        promise   <- Promise.make[Nothing, Unit]
+        _         <- semaphore.withPermit(promise.await).fork
+        _         <- ZIO.foreach(List.fill(10)(()))(_ => semaphore.withPermit(ZIO.unit).fork)
+        awaiting0 <- semaphore.awaiting
+                       .flatMap(awaiting => if (awaiting < 10) ZIO.fail(awaiting) else ZIO.succeed(awaiting))
+                       .retryUntil(_ == 10)
+                       .catchAll(n => ZIO.succeed(n))
+        _ <- promise.succeed(())
+        //
+        awaiting1 <- semaphore.awaiting
+                       // We fail to allow for retrying, to give fibers time to acquire a permit and release it
+                       .flatMap(awaiting => if (awaiting > 0) ZIO.fail(awaiting) else ZIO.succeed(awaiting))
+                       // Fibers may take time to acquire a permit
+                       .retryUntil(_ == 0)
+                       .catchAll(n => ZIO.succeed(n))
+      } yield assertTrue(awaiting0 == 10) && assertTrue(awaiting1 == 0)
     }
   )
 }
