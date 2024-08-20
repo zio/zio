348731a7999c1c9038f8b055a1885ebbf9eb3f3d
Thayne McCombs <astrothayne@gmail.com>
2024-08-20 00:57:43 -0600

    perf: Use list of promises instead of building ZIO

--- core/shared/src/main/scala/zio/Semaphore.scala
+++ core/shared/src/main/scala/zio/Semaphore.scala
@@ -152,8 +152,8 @@ object Semaphore {
           def loop(
             n: Long,
             state: Either[ScalaQueue[(Promise[Nothing, Unit], Long)], Long],
-            acc: UIO[Any]
-          ): (UIO[Any], Either[ScalaQueue[(Promise[Nothing, Unit], Long)], Long]) =
+            acc: List[Promise[Nothing, Unit]]
+          ): (List[Promise[Nothing, Unit]], Either[ScalaQueue[(Promise[Nothing, Unit], Long)], Long]) =
             state match {
               case Right(permits) => acc -> Right(permits + n)
               case Left(queue) =>
@@ -161,15 +161,15 @@ object Semaphore {
                   case None => acc -> Right(n)
                   case Some(((promise, permits), queue)) =>
                     if (n > permits)
-                      loop(n - permits, Left(queue), acc *> promise.succeed(()))
+                      loop(n - permits, Left(queue), promise :: acc)
                     else if (n == permits)
-                      (acc *> promise.succeed(())) -> Left(queue)
+                      (promise :: acc) -> Left(queue)
                     else
                       acc -> Left((promise -> (permits - n)) +: queue)
                 }
             }
 
-          ref.modify(loop(n, _, ZIO.unit)).flatten
+          ref.modify(loop(n, _, Nil)).map(l => l.foreach(_.unsafe.done(Exit.unit)(Unsafe)))
         }
       }
   }
