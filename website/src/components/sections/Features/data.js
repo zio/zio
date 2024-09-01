0a1fc4ab9635f01e2321d5e914c5f735276c6921
Sviatoslav <sviat.mak@gmail.com>
2024-09-01 19:16:48 +0200

    Restyling welcome page (#9112)
    * add: dependencies
    
    * add: prettier config
    
    * add: google-fonts plugin & setup Inter font
    
    * add: Embedded Video component
    
    * setup: tailwindcss, css vars, global config
    
    * add: welcome page components
    
    * refactoring welcome page
    
    * change: welcome page name

---
+++ website/src/components/sections/Features/data.js
@@ -0,0 +1,41 @@
+export const features = [
+  {
+    title: 'High-performance',
+    content: 'Build scalable applications with minimal runtime overhead',
+  },
+  {
+    title: 'Type-safe',
+    content:
+      'Use the full power of the Scala compiler to catch bugs at compile time',
+  },
+  {
+    title: 'Concurrent',
+    content:
+      'Easily build concurrent apps without deadlocks, race conditions, or complexity',
+  },
+  {
+    title: 'Asynchronous',
+    content:
+      'Write sequential code that looks the same whether it’s asynchronous or synchronous',
+  },
+  {
+    title: 'Resource-safe',
+    content:
+      'Build apps that never leak resources (including threads!), even when they fail',
+  },
+  {
+    title: 'Testable',
+    content:
+      'Inject test services into your app for fast, deterministic, and type-safe testing',
+  },
+  {
+    title: 'Resilient',
+    content:
+      'Build apps that never lose errors, and which respond to failure locally and flexibly',
+  },
+  {
+    title: 'Functional',
+    content:
+      'Rapidly compose solutions to complex problems from simple building blocks',
+  },
+];
