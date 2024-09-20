a5862ee5abef8c3bc4f8f15f410d80fd45629324
kyri-petrou <67301607+kyri-petrou@users.noreply.github.com>
2024-09-20 13:04:42 +0300

    Use reference equality in `ZEnvironment` when applying optimizations (#9207)
    * Use `ZEnvironment#relaxedEquals` in `unionAll` and `Patch#apply`
    
    * Check keys and values separately

--- core/shared/src/main/scala/zio/ZEnvironment.scala
+++ core/shared/src/main/scala/zio/ZEnvironment.scala
@@ -69,6 +69,34 @@ final class ZEnvironment[+R] private (
   }
 
   /**
+   * Similar to `equals` but uses reference equality on the map's elements.
+   * Therefore, this method might result in false negatives but never in false
+   * positives.
+   *
+   * Useful for cases where failing the equality check will not produce an
+   * invalid state (e.g., applying optimizations) and we want to avoid the
+   * overhead of strict equality.
+   */
+  private def relaxedEquals(that: ZEnvironment[_]): Boolean =
+    if (self eq that) true
+    else if (self.scope ne that.scope) false
+    else if (self.map eq that.map) true
+    else if (self.map.size != that.map.size) false
+    else {
+      // We check in the reverse order since this is an update-ordered map
+      // We could potentially check only the last element but that might result in a false positive so better be safe
+      val l   = self.map.reverseIterator
+      val r   = that.map.reverseIterator
+      var res = true
+      while (l.hasNext && res) {
+        val (lk, lv) = l.next().asInstanceOf[(LightTypeTag, AnyRef)]
+        val (rk, rv) = r.next().asInstanceOf[(LightTypeTag, AnyRef)]
+        res = lk == rk && (lv eq rv)
+      }
+      res
+    }
+
+  /**
    * Retrieves a service from the environment.
    */
   def get[A >: R](implicit tag: Tag[A]): A =
@@ -184,7 +212,7 @@ final class ZEnvironment[+R] private (
    * the right hand side will be preferred.
    */
   def unionAll[R1](that: ZEnvironment[R1]): ZEnvironment[R with R1] =
-    if (self == that) that.asInstanceOf[ZEnvironment[R with R1]]
+    if (self.relaxedEquals(that)) that.asInstanceOf[ZEnvironment[R with R1]]
     else {
       val newMap = that.map.iterator.foldLeft(self.map) { case (map, (k, v)) =>
         map.updated(k, v)
@@ -395,9 +423,7 @@ object ZEnvironment {
       if (isEmpty) env0
       else {
         val out = loop(environment, self.asInstanceOf[Patch[Any, Any]] :: Nil).asInstanceOf[ZEnvironment[Out]]
-        // Unfortunately we can't rely on eq here. However, the ZEnvironment equals method uses a cached hashCode
-        // so it's pretty fast
-        if (env0 == out) env0 else out
+        if (env0.relaxedEquals(out)) env0 else out
       }
     }
 
