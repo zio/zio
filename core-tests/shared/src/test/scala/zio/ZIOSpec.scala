e14cd8d73bdd0c53f5182625a9aaa67f4e8ade3f
Jules Ivanic <jules.ivanic@gmail.com>
2024-12-14 01:19:14 +1100

    Optimisation: Implement `ZIO.suspendSucceed` with `ZIO.FlatMap` (#9388)
    * POC: Implement `ZIO.suspendSucceed` with `ZIO.FlatMap`
    
    * POC: Implement `ZIO.suspendSucceed` with `ZIO.FlatMap`, `Exit.unit`
    
    * POC: Implement `ZIO.suspendSucceed` with `ZIO.FlatMap`, `null`
    
    * suspendSucceedUnsafe
    
    * POC: Implement `ZIO.suspendSucceed` with `ZIO.FlatMap`, `Exit.unit`
    
    * Review: Prefer `ZIO.unit` over `Exit.unit`
    
    * Review: Add test
    
    * fmt

--- core-tests/shared/src/test/scala/zio/ZIOSpec.scala
+++ core-tests/shared/src/test/scala/zio/ZIOSpec.scala
@@ -2257,6 +2257,12 @@ object ZIOSpec extends ZIOBaseSpec {
       test("suspendSucceed must be evaluatable") {
         assertZIO(ZIO.suspendSucceed(ZIO.succeed(42)))(equalTo(42))
       },
+      test("ZIO.suspendSucceed is implemented with a ZIO.unit as first") {
+        ZIO.suspendSucceed(ZIO.succeed(42)) match {
+          case ZIO.FlatMap(_, first, _) => assertTrue(first eq ZIO.unit)
+          case _                        => assertNever("ZIO.suspendSucceed is not implemented as a FlatMap")
+        }
+      },
       test("point, bind, map") {
         def fibIo(n: Int): Task[BigInt] =
           if (n <= 1) ZIO.succeed(n)
--- core/shared/src/main/scala/zio/ZIO.scala
+++ core/shared/src/main/scala/zio/ZIO.scala
@@ -4818,10 +4818,10 @@ object ZIO extends ZIOCompanionPlatformSpecific with ZIOCompanionVersionSpecific
    * not use this method, use [[ZIO.suspend]].
    */
   def suspendSucceed[R, E, A](zio: => ZIO[R, E, A])(implicit trace: Trace): ZIO[R, E, A] =
-    ZIO.succeed(zio).flatMap(identityFn)
+    ZIO.FlatMap(trace, ZIO.unit, (_: Any) => zio)
 
   def suspendSucceedUnsafe[R, E, A](zio: Unsafe => ZIO[R, E, A])(implicit trace: Trace): ZIO[R, E, A] =
-    ZIO.succeedUnsafe(zio).flatMap(identityFn)
+    ZIO.FlatMap(trace, ZIO.unit, (_: Any) => zio(Unsafe))
 
   /**
    * Retrieves the `System` service for this workflow.
