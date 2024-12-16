492726d871eff4ecee3ed6171980f53848192bb1
kyri-petrou <67301607+kyri-petrou@users.noreply.github.com>
2024-12-16 18:40:06 +0530

    Ensure that TestClock methods are thread-safe (#9408)

--- test-tests/jvm-native/src/test/scala/zio/test/environment/TestClockSpecJVM.scala
+++ test-tests/jvm-native/src/test/scala/zio/test/environment/TestClockSpecJVM.scala
@@ -2,8 +2,8 @@ package zio.test.environment
 
 import zio._
 import zio.test.Assertion._
-import zio.test.TestAspect.nonFlaky
-import zio.test.{TestClock, ZIOBaseSpec, assert}
+import zio.test.TestAspect.{nonFlaky, timeout}
+import zio.test.{TestClock, ZIOBaseSpec, assert, assertCompletes}
 
 import java.util.concurrent.TimeUnit
 
@@ -126,6 +126,11 @@ object TestClockSpecJVM extends ZIOBaseSpec {
             _      <- ZIO.logInfo(s"Values after interruption: $values")
           } yield assert(values.reverse)(equalTo(List(5L)))
         }
+      ),
+      suite("adjust")(
+        test("is thread safe") {
+          (TestClock.adjust(1.second) &> TestClock.adjust(1.second)).as(assertCompletes)
+        } @@ timeout(10.seconds)
       )
     ) @@ nonFlaky(20)
 }
