632488f282b74539137c6b750a7b17321fe64817
Jongbeom Kim <kajebiii@gmail.com>
2025-03-17 19:42:26 +0900

    Change iterableFactory from val to def in ChunkLike

--- core/shared/src/main/scala-2.13+/zio/ChunkLike.scala
+++ core/shared/src/main/scala-2.13+/zio/ChunkLike.scala
@@ -111,7 +111,7 @@ trait ChunkLike[+A]
    * exposes a `newBuilder` method that is not referentially transparent because
    * it allocates mutable state.
    */
-  override val iterableFactory: SeqFactory[Chunk] =
+  override def iterableFactory: SeqFactory[Chunk] =
     Chunk
 
   /**
