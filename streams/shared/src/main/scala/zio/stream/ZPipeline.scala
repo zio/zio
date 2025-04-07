9feeca3587b903f0d3b7da684c35bed424a925db
Jules Ivanic <jules.ivanic@gmail.com>
2025-04-07 16:36:09 +1000

    Optimize `ZPipeline::mapStream`

--- streams/shared/src/main/scala/zio/stream/ZPipeline.scala
+++ streams/shared/src/main/scala/zio/stream/ZPipeline.scala
@@ -1785,7 +1785,7 @@ object ZPipeline extends ZPipelinePlatformSpecificConstructors {
     f: In => ZStream[Env, Err, Out]
   )(implicit trace: Trace): ZPipeline[Env, Err, In, Out] =
     new ZPipeline(
-      ZChannel.identity[Nothing, Chunk[In], Any].concatMap(_.map(f).map(_.channel).fold(ZChannel.unit)(_ *> _))
+      ZChannel.identity[Nothing, Chunk[In], Any].concatMap(_.foldLeft(ZChannel.unit)((acc, elem) => acc *> f(elem).channel))
     )
 
   /**
