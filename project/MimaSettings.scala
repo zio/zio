1150900ed83494679c51c959a3ea95158893c621
Kyri Petrou <kyri.petrou@outlook.com>
2025-04-09 18:24:34 +0300

    Optimize `Queue#shutdown`

--- project/MimaSettings.scala
+++ project/MimaSettings.scala
@@ -34,7 +34,8 @@ object MimaSettings {
         exclude[NewMixinForwarderProblem]("zio.Exit.mapBoth"),
         exclude[NewMixinForwarderProblem]("zio.Exit.mapError"),
         exclude[NewMixinForwarderProblem]("zio.Exit.mapErrorCause"),
-        exclude[NewMixinForwarderProblem]("zio.Exit.unit")
+        exclude[NewMixinForwarderProblem]("zio.Exit.unit"),
+        exclude[Problem]("zio.Queue#Strategy*.shutdown")
       ),
       mimaFailOnProblem := failOnProblem
     )
