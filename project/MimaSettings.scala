ee4236547755c6e5f0d8f07da27502fcb9cbfa86
Eyal Farago-Hagag <eyal.farago@gmail.com>
2025-04-21 11:37:55 +0300

    stream_par_ops_strm_first: mima

--- project/MimaSettings.scala
+++ project/MimaSettings.scala
@@ -35,7 +35,8 @@ object MimaSettings {
         exclude[NewMixinForwarderProblem]("zio.Exit.mapError"),
         exclude[NewMixinForwarderProblem]("zio.Exit.mapErrorCause"),
         exclude[NewMixinForwarderProblem]("zio.Exit.unit"),
-        exclude[Problem]("zio.Queue#Strategy*.shutdown")
+        exclude[Problem]("zio.Queue#Strategy*.shutdown"),
+        ProblemFilters.exclude[ReversedMissingMethodProblem]("zio.Scope.forkSingle")
       ),
       mimaFailOnProblem := failOnProblem
     )
