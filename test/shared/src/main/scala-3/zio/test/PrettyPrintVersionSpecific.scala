6ac46208c9607cb8e451c354b5116be0529ccf28
Jules Ivanic <jules.ivanic@gmail.com>
2025-05-29 10:24:41 +1000

    Optimize moaaaaaaar

--- test/shared/src/main/scala-3/zio/test/PrettyPrintVersionSpecific.scala
+++ test/shared/src/main/scala-3/zio/test/PrettyPrintVersionSpecific.scala
@@ -12,44 +12,38 @@ private[test] trait PrettyPrintVersionSpecific {
     if (size < 1) s"$name()"
     else {
       val isMultiLine = size > 1
-      val indentation = if (isMultiLine) "  " else ""
-      val spacer      = if (isMultiLine) '\n' else ""
 
       val acc = new java.lang.StringBuilder
 
       acc.append(name)
       acc.append('(')
-      acc.append(spacer)
+      if (isMultiLine) acc.append('\n')
 
-      // First line handling
-      val key0            = product.productElementName(0)
-      val value0          = product.productElement(0)
-      val firstLineSuffix = if (isMultiLine) ',' else ""
-      acc.append(indentation)
+      val key0   = product.productElementName(0)
+      val value0 = product.productElement(0)
+      if (isMultiLine) acc.append("  ") // indentation
       acc.append(key0)
       acc.append(" = ")
       acc.append(PrettyPrint(value0))
-      acc.append(firstLineSuffix)
+      if (isMultiLine) acc.append(',')
 
-      // Remaining lines handling
       var i           = 1
       val lastElement = size - 1
       while (i < size) {
-        val key        = product.productElementName(i)
-        val value      = product.productElement(i)
-        val isLastLine = i == lastElement
-        val suffix     = if (isLastLine) "" else ','
+        val key         = product.productElementName(i)
+        val value       = product.productElement(i)
+        val notLastLine = i != lastElement
         acc.append('\n')
-        acc.append(indentation)
+        if (isMultiLine) acc.append("  ") // indentation
         acc.append(key)
         acc.append(" = ")
         acc.append(PrettyPrint(value))
-        acc.append(suffix)
+        if (notLastLine) acc.append(',')
 
         i += 1
       }
 
-      acc.append(spacer)
+      if (isMultiLine) acc.append('\n')
       acc.append(')')
       acc.toString
     }
