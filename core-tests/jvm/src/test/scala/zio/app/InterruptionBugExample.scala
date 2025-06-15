6a04f0078a3e46962abed37aed94575991ab41a9
promisingcoder <hiet122230340@Sinaieng.edu.eg>
2025-06-15 08:06:12 -0700

    fix(tests): Correct compilation errors in InterruptionBugExample

--- core-tests/jvm/src/test/scala/zio/app/InterruptionBugExample.scala
+++ core-tests/jvm/src/test/scala/zio/app/InterruptionBugExample.scala
@@ -2,22 +2,7 @@ package zio.app
 
 import zio._
 
-import java.util.concurrent.TimeUnit
-
 object InterruptionBugExample extends ZIOAppDefault {
-
-  // This finalizer simulates a slow cleanup task that takes 3 seconds.
-  private val slowFinalizer =
-    ZIO.acquireReleaseWith(
-      acquire = Console.printLine("Resource acquired. Application is running, press Ctrl+C to interrupt.").orDie
-    )(
-      release =
-        Console.printLine("Finalizer started, will take 3 seconds to complete...").orDie *>
-          ZIO.sleep(3.seconds) *>
-          Console.printLine("Finalizer finished.").orDie
-    )(
-      use = _ => ZIO.never // App runs forever until interrupted
-    )
 
   /**
    * This hardcoded timeout of 1 second will be used, ignoring any
@@ -28,12 +13,24 @@ object InterruptionBugExample extends ZIOAppDefault {
   /**
    * When this app is interrupted externally (e.g., via Ctrl+C), the
    * `exitCode` logic in `ZIOAppPlatformSpecific` incorrectly treats the
-   * interruption as a failure, causing the app to return exit code 1
-   * instead of 0.
+   * interruption as a failure, returning exit code 1.
    *
-   * Additionally, the 1-second `gracefulShutdownTimeout` defined above
-   * will cause the shutdown to time out before the 3-second finalizer can
-   * complete, preventing "Finalizer finished." from being printed.
+   * Additionally, the `gracefulShutdownTimeout` above (1 second) will be
+   * used, causing the 3-second finalizer to be interrupted prematurely.
+   * The test expects "Finalizer finished" to be printed, which will not happen.
    */
-  override def run = slowFinalizer
-} 
+  override val run =
+    ZIO.acquireReleaseWith(
+      // acquire
+      Console.printLine("Resource acquired. Application is running, press Ctrl+C to interrupt.").orDie
+    )(
+      // release
+      _ =>
+        Console.printLine("Finalizer started, will take 3 seconds to complete...").orDie *>
+          ZIO.sleep(3.seconds) *>
+          Console.printLine("Finalizer finished.").orDie
+    )(
+      // use
+      _ => ZIO.never // App runs forever until interrupted
+    )
+}
