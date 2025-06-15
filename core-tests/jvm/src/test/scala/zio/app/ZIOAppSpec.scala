e828db71c42ae4e1e1fb90da3cf816c5a5e94f18
promisingcoder <hiet122230340@Sinaieng.edu.eg>
2025-06-15 12:18:15 -0700

    removed wrong exit code

--- core-tests/jvm/src/test/scala/zio/app/ZIOAppSpec.scala
+++ core-tests/jvm/src/test/scala/zio/app/ZIOAppSpec.scala
@@ -185,7 +185,7 @@ object ZIOAppSpec extends ZIOSpecDefault {
           exitCode <- process.waitForExit()
           // Note: We don't expect finalizers to run with SIGKILL
           _ <- process.destroy
-        } yield assert(exitCode)(isOneOf(equalTo(139), equalTo(137))) // SIGKILL typically gives 137 or 139
+        } yield assert(exitCode)(isOneOf(equalTo(139))) // SIGKILL typically gives 137 or 139
       }
     ) @@ jvmOnly @@ withLiveClock
   )
