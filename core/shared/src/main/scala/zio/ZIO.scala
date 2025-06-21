c8cc925b817101616a1f5f7b775561f74fcf51ef
Adam Hearn <22334119+hearnadam@users.noreply.github.com>
2025-06-20 20:43:03 -0700

    Deprecate `ZIO.infinity`

--- core/shared/src/main/scala/zio/ZIO.scala
+++ core/shared/src/main/scala/zio/ZIO.scala
@@ -3934,6 +3934,7 @@ object ZIO extends ZIOCompanionPlatformSpecific with ZIOCompanionVersionSpecific
    * Like [[never]], but fibers that running this effect won't be garbage
    * collected unless interrupted.
    */
+  @deprecated("Use `ZIO.never` instead", "2.1.20")
   def infinity(implicit trace: Trace): UIO[Nothing] =
     ZIO.sleep(Duration.fromNanos(Long.MaxValue)) *> ZIO.never
 
