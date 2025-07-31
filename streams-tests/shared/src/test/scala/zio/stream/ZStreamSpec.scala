5b7e27e139360b24d20ee6eba65e2b50aa13f2d0
Jules Ivanic <jules.ivanic@gmail.com>
2025-07-31 11:43:02 +1000

    Review: Simplify

--- streams-tests/shared/src/test/scala/zio/stream/ZStreamSpec.scala
+++ streams-tests/shared/src/test/scala/zio/stream/ZStreamSpec.scala
@@ -5630,11 +5630,7 @@ object ZStreamSpec extends ZIOBaseSpec {
           test("handles exceptions from making the iterator") {
             check(Gen.small(Gen.const(_), min = 1)) { maxChunkSize =>
               val exception = new RuntimeException("Iterator error")
-              def failingIterator = {
-                throw exception
-
-                List(1, 2).iterator
-              }
+              def failingIterator: Iterator[Int] = throw exception
 
               assertZIO(ZStream.fromIterator(failingIterator, maxChunkSize).runCollect.exit)(fails(equalTo(exception)))
             }
