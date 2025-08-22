01f2c9e1c1d6da4e71f7644391d04ddc2d4f77fe
Jules Ivanic <jules.ivanic@gmail.com>
2025-08-22 15:28:33 +1000

    Prefer using `computeIfAbsent` over multiple `get` and `set` calls

--- core/shared/src/main/scala/zio/internal/metrics/ConcurrentMetricRegistry.scala
+++ core/shared/src/main/scala/zio/internal/metrics/ConcurrentMetricRegistry.scala
@@ -18,15 +18,15 @@ private[zio] class ConcurrentMetricRegistry {
 
   def snapshot()(implicit unsafe: Unsafe): Set[MetricPair.Untyped] = {
     val iterator = map.entrySet().iterator()
-    val result   = scala.collection.mutable.Set[MetricPair.Untyped]()
+    val result   = Set.newBuilder[MetricPair.Untyped]
     result.sizeHint(map.size())
     while (iterator.hasNext) {
       val value = iterator.next()
       val key   = value.getKey
       val hook  = value.getValue
-      result.add(MetricPair.make(key, hook.get()))
+      result += MetricPair.make(key, hook.get())
     }
-    result.toSet
+    result.result()
   }
 
   def get[Type <: MetricKeyType](
@@ -34,17 +34,39 @@ private[zio] class ConcurrentMetricRegistry {
   )(implicit unsafe: Unsafe): MetricHook[key.keyType.In, key.keyType.Out] = {
     type Result = MetricHook[key.keyType.In, key.keyType.Out]
 
-    val hook0: MetricHook[_, zio.metrics.MetricState.Untyped] = map.get(key)
-
-    if (hook0 eq null) {
-      (key.keyType match {
-        case MetricKeyType.Counter             => getCounter(key.asInstanceOf[MetricKey.Counter])
-        case MetricKeyType.Frequency           => getSetCount(key.asInstanceOf[MetricKey.Frequency])
-        case MetricKeyType.Gauge               => getGauge(key.asInstanceOf[MetricKey.Gauge])
-        case MetricKeyType.Histogram(_)        => getHistogram(key.asInstanceOf[MetricKey.Histogram])
-        case MetricKeyType.Summary(_, _, _, _) => getSummary(key.asInstanceOf[MetricKey.Summary])
-      }).asInstanceOf[Result]
-    } else hook0.asInstanceOf[Result]
+    (
+      key.keyType match {
+        case MetricKeyType.Counter =>
+          map
+            .computeIfAbsent(
+              key.asInstanceOf[MetricKey.Counter],
+              k => ConcurrentMetricHooks.counter(k.asInstanceOf[MetricKey.Counter])
+            )
+        case MetricKeyType.Frequency =>
+          map
+            .computeIfAbsent(
+              key.asInstanceOf[MetricKey.Frequency],
+              k => ConcurrentMetricHooks.frequency(k.asInstanceOf[MetricKey.Frequency])
+            )
+        case MetricKeyType.Gauge =>
+          map
+            .computeIfAbsent(
+              key.asInstanceOf[MetricKey.Gauge],
+              k => ConcurrentMetricHooks.gauge(k.asInstanceOf[MetricKey.Gauge], 0.0)
+            )
+        case _: MetricKeyType.Histogram =>
+          map.computeIfAbsent(
+            key.asInstanceOf[MetricKey.Histogram],
+            k => ConcurrentMetricHooks.histogram(k.asInstanceOf[MetricKey.Histogram])
+          )
+        case _: MetricKeyType.Summary =>
+          map
+            .computeIfAbsent(
+              key.asInstanceOf[MetricKey.Summary],
+              k => ConcurrentMetricHooks.summary(k.asInstanceOf[MetricKey.Summary])
+            )
+      }
+    ).asInstanceOf[Result]
   }
 
   def remove[Type <: MetricKeyType](key: MetricKey[Type])(implicit unsafe: Unsafe): Boolean =
@@ -115,56 +137,4 @@ private[zio] class ConcurrentMetricRegistry {
     }
   }
 
-  private def getCounter(key: MetricKey.Counter)(implicit unsafe: Unsafe): MetricHook.Counter = {
-    var value = map.get(key)
-    if (value eq null) {
-      val counter = ConcurrentMetricHooks.counter(key)
-      map.putIfAbsent(key, counter)
-      value = map.get(key)
-    }
-    value.asInstanceOf[MetricHook.Counter]
-  }
-
-  private def getGauge(key: MetricKey.Gauge)(implicit unsafe: Unsafe): MetricHook.Gauge = {
-    var value = map.get(key)
-    if (value eq null) {
-      val gauge = ConcurrentMetricHooks.gauge(key, 0.0)
-      map.putIfAbsent(key, gauge)
-      value = map.get(key)
-    }
-    value.asInstanceOf[MetricHook.Gauge]
-  }
-
-  private def getHistogram(key: MetricKey.Histogram)(implicit unsafe: Unsafe): MetricHook.Histogram = {
-    var value = map.get(key)
-    if (value eq null) {
-      val histogram =
-        ConcurrentMetricHooks.histogram(key)
-      map.putIfAbsent(key, histogram)
-      value = map.get(key)
-    }
-    value.asInstanceOf[MetricHook.Histogram]
-  }
-
-  private def getSummary(
-    key: MetricKey.Summary
-  )(implicit unsafe: Unsafe): MetricHook.Summary = {
-    var value = map.get(key)
-    if (value eq null) {
-      val summary = ConcurrentMetricHooks.summary(key)
-      map.putIfAbsent(key, summary)
-      value = map.get(key)
-    }
-    value.asInstanceOf[MetricHook.Summary]
-  }
-
-  private def getSetCount(key: MetricKey.Frequency)(implicit unsafe: Unsafe): MetricHook.Frequency = {
-    var value = map.get(key)
-    if (value eq null) {
-      val frequency = ConcurrentMetricHooks.frequency(key)
-      map.putIfAbsent(key, frequency)
-      value = map.get(key)
-    }
-    value.asInstanceOf[MetricHook.Frequency]
-  }
 }
