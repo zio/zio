463332021fc51dbe3b7ec44a5acc589eb9a53892
kyri-petrou <67301607+kyri-petrou@users.noreply.github.com>
2025-09-01 07:43:26 +0300

    Fix for Scala 3

--- test/shared/src/main/scala/zio/test/TestConstructor.scala
+++ test/shared/src/main/scala/zio/test/TestConstructor.scala
@@ -1,7 +1,7 @@
 package zio.test
 
 import zio.internal.stacktracer.SourceLocation
-import zio.{Exit, Scope, Trace, ZIO}
+import zio.{Scope, Trace, ZIO}
 import zio.stacktracer.TracingImplicits.disableAutoTrace
 import zio.stm.ZSTM
 
@@ -14,41 +14,23 @@ object TestConstructor extends TestConstructorLowPriority1 {
   type WithOut[Environment, In, Out0] = TestConstructor[Environment, In] { type Out = Out0 }
 
   implicit def AssertConstructor[A <: TestResult]: TestConstructor.WithOut[Any, A, Spec[Any, Nothing]] =
-    TestConstructorInstances.AssertConstructor0
-      .asInstanceOf[TestConstructor.WithOut[Any, A, Spec[Any, Nothing]]]
+    new TestConstructor[Any, A] {
+      type Out = Spec[Any, Nothing]
+      def apply(label: String)(
+        assertion: => A
+      )(implicit sourceLocation: SourceLocation, trace: Trace): Spec[Any, Nothing] =
+        test(label)(ZIO.succeed(assertion))
+    }
 }
 
 trait TestConstructorLowPriority1 extends TestConstructorLowPriority2 {
 
   implicit def AssertZIOConstructor[R, E, A <: TestResult]: TestConstructor.WithOut[R, ZIO[R, E, A], Spec[R, E]] =
-    TestConstructorInstances.AssertZIOConstructor0
-      .asInstanceOf[TestConstructor.WithOut[R, ZIO[R, E, A], Spec[R, E]]]
-}
-
-trait TestConstructorLowPriority2 extends TestConstructorLowPriority3 {
-
-  implicit def AssertZSTMConstructor[R, E, A <: TestResult]: TestConstructor.WithOut[R, ZSTM[R, E, A], Spec[R, E]] =
-    TestConstructorInstances.AssertZSTMConstructor0
-      .asInstanceOf[TestConstructor.WithOut[R, ZSTM[R, E, A], Spec[R, E]]]
-}
-
-trait TestConstructorLowPriority3 {
-
-  implicit def AssertEitherConstructor[E, A <: TestResult]: TestConstructor.WithOut[Any, Either[E, A], Spec[Any, E]] =
-    TestConstructorInstances.AssertEitherConstructor0
-      .asInstanceOf[TestConstructor.WithOut[Any, Either[E, A], Spec[Any, E]]]
-}
-
-private[this] object TestConstructorInstances {
-
-  val AssertZIOConstructor0: TestConstructor.WithOut[Any, ZIO[Any, Any, TestResult], Spec[Any, Any]] =
-    new TestConstructor[Any, ZIO[Any, Any, TestResult]] {
-      type Out = Spec[Any, Any]
+    new TestConstructor[R, ZIO[R, E, A]] {
+      type Out = Spec[R, E]
       def apply(
         label: String
-      )(
-        assertion: => ZIO[Any, Any, TestResult]
-      )(implicit sourceLocation: SourceLocation, trace: Trace): Spec[Any, Any] = {
+      )(assertion: => ZIO[R, E, A])(implicit sourceLocation: SourceLocation, trace: Trace): Spec[R, E] = {
         val assertion0 = ZIO.suspendSucceed(assertion)
         Spec.labeled(
           label,
@@ -58,31 +40,28 @@ private[this] object TestConstructorInstances {
         )
       }
     }
+}
 
-  val AssertZSTMConstructor0: TestConstructor.WithOut[Any, ZSTM[Any, Any, TestResult], Spec[Any, Any]] =
-    new TestConstructor[Any, ZSTM[Any, Any, TestResult]] {
-      type Out = Spec[Any, Any]
+trait TestConstructorLowPriority2 extends TestConstructorLowPriority3 {
+
+  implicit def AssertZSTMConstructor[R, E, A <: TestResult]: TestConstructor.WithOut[R, ZSTM[R, E, A], Spec[R, E]] =
+    new TestConstructor[R, ZSTM[R, E, A]] {
+      type Out = Spec[R, E]
       def apply(label: String)(
-        assertion: => ZSTM[Any, Any, TestResult]
-      )(implicit sourceLocation: SourceLocation, trace: Trace): Spec[Any, Any] =
-        AssertZIOConstructor0(label)(assertion.commit)
+        assertion: => ZSTM[R, E, A]
+      )(implicit sourceLocation: SourceLocation, trace: Trace): Spec[R, E] =
+        test(label)(assertion.commit)
     }
+}
 
-  val AssertEitherConstructor0: TestConstructor.WithOut[Any, Either[Any, TestResult], Spec[Any, Any]] =
-    new TestConstructor[Any, Either[Any, TestResult]] {
-      type Out = Spec[Any, Any]
-      def apply(label: String)(
-        assertion: => Either[Any, TestResult]
-      )(implicit sourceLocation: SourceLocation, trace: Trace): Spec[Any, Any] =
-        AssertZIOConstructor0(label)(ZIO.fromEither(assertion))
-    }
+trait TestConstructorLowPriority3 {
 
-  val AssertConstructor0: TestConstructor.WithOut[Any, TestResult, Spec[Any, Any]] =
-    new TestConstructor[Any, TestResult] {
-      type Out = Spec[Any, Any]
+  implicit def AssertEitherConstructor[E, A <: TestResult]: TestConstructor.WithOut[Any, Either[E, A], Spec[Any, E]] =
+    new TestConstructor[Any, Either[E, A]] {
+      type Out = Spec[Any, E]
       def apply(label: String)(
-        assertion: => TestResult
-      )(implicit sourceLocation: SourceLocation, trace: Trace): Spec[Any, Any] =
-        AssertZIOConstructor0(label)(ZIO.succeed(assertion))
+        assertion: => Either[E, A]
+      )(implicit sourceLocation: SourceLocation, trace: Trace): Spec[Any, E] =
+        test(label)(ZIO.fromEither(assertion))
     }
 }
