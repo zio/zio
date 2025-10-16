zio/zio

commit adb1cc1c6ece30904741ff6a0b8cda67faf5362e

Optimize `race` and synchronous interruption (#9848)
* Optimize `race` and synchronous interruption

* Fix failing test

* PR comments

* Start both fibers concurrently and add test

core/shared/src/main/scala/zio/internal/FiberRuntime.scala
@@ -67,19 +67,22 @@ final class FiberRuntime[E, A](fiberId: FiberId.Runtime, fiberRefs0: FiberRefs, 
   @volatile private var _exitValue = null.asInstanceOf[Exit[E, A]]
 
   def await(implicit trace: Trace): UIO[Exit[E, A]] =
-    ZIO.suspendSucceed {
-      val exitValue = self._exitValue
-      if (exitValue ne null) Exit.succeed(exitValue)
-      else
-        ZIO.asyncInterrupt[Any, Nothing, Exit[E, A]](
-          { k =>
-            val cb = (exit: Exit[_, _]) => k(Exit.Success(exit.asInstanceOf[Exit[E, A]]))
-            unsafe.addObserver(cb)(Unsafe)
-            Left(ZIO.succeed(unsafe.removeObserver(cb)(Unsafe)))
-          },
-          id
-        )
-    }
+    ZIO.suspendSucceed(awaitUnsafe)
+
+  @inline
+  private[this] def awaitUnsafe(implicit trace: Trace): UIO[Exit[E, A]] = {
+    val exitValue = self._exitValue
+    if (exitValue ne null) Exit.succeed(exitValue)
+    else
+      ZIO.asyncInterrupt[Any, Nothing, Exit[E, A]](
+        { k =>
+          val cb = (exit: Exit[_, _]) => k(Exit.Success(exit.asInstanceOf[Exit[E, A]]))
+          unsafe.addObserver(cb)(Unsafe)
+          Left(ZIO.succeed(unsafe.removeObserver(cb)(Unsafe)))
+        },
+        id
+      )
+  }
 
   private[this] def childrenChunk(children: java.util.Set[Fiber.Runtime[?, ?]]): Chunk[Fiber.Runtime[_, _]] =
     // may be executed by a foreign fiber (under Sync), hence we're risking a race over the _children variable being set back to null by a concurrent transferChildren call
@@ -118,6 +121,26 @@ final class FiberRuntime[E, A](fiberId: FiberId.Runtime, fiberRefs0: FiberRefs, 
         ZIO.updateRuntimeFlags(patch)
       } else {
         Exit.unit
+      }
+    }
+
+  override def interruptAs(fiberId: FiberId)(implicit trace: Trace): UIO[Exit[E, A]] =
+    ZIO.suspendSucceed {
+      val exit = _exitValue
+      if (exit ne null) Exit.succeed(exit)
+      else {
+        val cause = Cause.interrupt(fiberId, StackTrace(self.fiberId, Chunk.single(trace)))
+        inbox.add(FiberMessage.InterruptSignal(cause))
+
+        // If the fiber is not running (which means it's suspended), and the current thread is in the same executor as the fiber,
+        // then execute the runloop on the current thread, avoiding context switching and suspension
+        if (running.compareAndSet(false, true)) {
+          val executor = getCurrentExecutor()
+          if (executor.isCurrentThreadInExecutor) drainQueueOnCurrentThread(0)
+          else drainQueueLaterOnExecutor(false)
+        }
+
+        awaitUnsafe(trace)
       }
     }
 
@@ -553,9 +576,9 @@ final class FiberRuntime[E, A](fiberId: FiberId.Runtime, fiberRefs0: FiberRefs, 
   }
 
   private[zio] def getCurrentExecutor(): Executor =
-    getFiberRef(FiberRef.overrideExecutor) match {
-      case None        => Runtime.defaultExecutor
+    getFiberRefOrNull(FiberRef.overrideExecutor) match {
       case Some(value) => value
+      case _           => Runtime.defaultExecutor
     }
 
   private[zio] def getFiberRef[A](fiberRef: FiberRef[A]): A =
@@ -1491,7 +1514,8 @@ final class FiberRuntime[E, A](fiberId: FiberId.Runtime, fiberRefs0: FiberRefs, 
         self.getFiberRefs()
 
       def removeObserver(observer: Exit[E, A] => Unit)(implicit unsafe: Unsafe): Unit =
-        self.tell(FiberMessage.Stateful(_.asInstanceOf[FiberRuntime[E, A]].removeObserver(observer)))
+        if (self._exitValue ne null)
+          self.tell(FiberMessage.Stateful(_.asInstanceOf[FiberRuntime[E, A]].removeObserver(observer)))
 
       def poll(implicit unsafe: Unsafe): Option[Exit[E, A]] =
         Option(self.exitValue())

benchmarks/src/main/scala/zio/EmptyRaceBenchmark.scala
@@ -11,7 +11,7 @@ import java.util.concurrent.TimeUnit
 @Warmup(iterations = 10, time = 3, timeUnit = TimeUnit.SECONDS)
 @Measurement(iterations = 10, time = 3, timeUnit = TimeUnit.SECONDS)
 @Fork(1)
-@Threads(1)
+@Threads(4)
 class EmptyRaceBenchmark {
   @Param(Array("1000"))
   var size: Int = _
@@ -28,15 +28,11 @@ class EmptyRaceBenchmark {
   }
 
   @Benchmark
-  def zioEmptyRace(): Int = zioEmptyRace(BenchmarkUtil)
-
-  private[this] def zioEmptyRace(runtime: Runtime[Any]): Int = {
+  def zioEmptyRace(): Int = {
     def loop(i: Int): UIO[Int] =
       if (i < size) ZIO.never.raceFirst(ZIO.succeed(i + 1)).flatMap(loop)
       else ZIO.succeed(i)
 
-    Unsafe.unsafe { implicit unsafe =>
-      runtime.unsafe.run(loop(0)).getOrThrowFiberFailure()
-    }
+    BenchmarkUtil.unsafeRun(loop(0))
   }
 }

core-tests/jvm-native/src/test/scala/zio/ZIOSpecJVM.scala
@@ -1,7 +1,10 @@
 package zio
 
 import zio.test.Assertion.isNull
+import zio.test.TestAspect.{blocking, nonFlaky, sequential, withLiveClock}
 import zio.test._
+
+import java.util.concurrent.atomic.AtomicBoolean
 
 object ZIOSpecJVM extends ZIOBaseSpec {
 
@@ -46,6 +49,36 @@ object ZIOSpecJVM extends ZIOBaseSpec {
           _ <- ZIO.fromAutoCloseable(loadNonExistingFile)
         } yield assert(shouldBeNull)(isNull)
       }
+    ),
+    suite("race")(
+      test("interrupts both fibers running synchronous effects") {
+        // NOTE: Using Java's concurrency classes since we want our effects to run fully synchronously
+        val latch              = new java.util.concurrent.CountDownLatch(2)
+        val p1, p2             = new java.util.concurrent.Semaphore(1)
+        val p1Called, p2Called = new AtomicBoolean()
+
+        val f1 = ZIO.succeed {
+          latch.countDown()
+          p1.acquire()
+        } *> ZIO.succeed(p1Called.set(true))
+
+        val f2 = ZIO.succeed {
+          latch.countDown()
+          p2.acquire()
+        } *> ZIO.succeed(p2Called.set(true))
+
+        p1.acquire()
+        p2.acquire()
+        for {
+          f <- f1.race(f2).fork
+          _  = latch.await()
+          _ <- f.interruptFork
+          _ <- ZIO.sleep(2.millis) // No better way to do this, unfortunately
+          _  = p1.release()
+          _  = p2.release()
+          _ <- f.await
+        } yield assertTrue(!p1Called.get(), !p2Called.get())
+      } @@ nonFlaky(100) @@ blocking @@ withLiveClock
     )
-  )
+  ) @@ sequential
 }

core-tests/shared/src/test/scala/zio/ZIOSpec.scala
@@ -2760,37 +2760,37 @@ object ZIOSpec extends ZIOBaseSpec {
         } yield assertTrue(value == 42)
       },
       test("async should not resume fiber twice after interruption") {
+        implicit val us: Unsafe = Unsafe
         for {
-          step            <- Promise.make[Nothing, Unit]
+          started         <- Promise.make[Nothing, Unit]
+          step            <- Promise.make[Nothing, ZIO[Any, Nothing, Unit] => Unit]
           unexpectedPlace <- Ref.make(List.empty[Int])
           runtime         <- ZIO.runtime[Live]
           fork <- ZIO
                     .async[Any, Nothing, Unit] { k =>
-                      Unsafe.unsafe { implicit unsafe =>
-                        runtime.unsafe.fork {
-                          step.await *> ZIO.succeed(k(unexpectedPlace.update(1 :: _)))
-                        }
+                      runtime.unsafe.fork {
+                        step.await *> ZIO.succeed(k(unexpectedPlace.update(1 :: _)))
                       }
+                      started.unsafe.succeedUnit
                       ()
                     }
-                    .ensuring(ZIO.async[Any, Nothing, Unit] { _ =>
-                      Unsafe.unsafe { implicit unsafe =>
-                        runtime.unsafe.fork {
-                          step.succeed(())
-                        }
-                      }
+                    .ensuring(ZIO.async[Any, Nothing, Unit] { k =>
+                      step.unsafe.succeed(k)
                       ()
                       // never complete
                     })
                     .ensuring(unexpectedPlace.update(2 :: _))
                     .forkDaemon
-          result     <- Live.withLive(fork.interrupt)(_.timeout(5.seconds))
+          _          <- started.await
+          result     <- fork.interrupt.timeout(100.millis)
           unexpected <- unexpectedPlace.get
+          // Invoke the callback now we collected everything otherwise the fiber is stuck in an uninterruptible region
+          _ <- step.await.map(_(Exit.unit))
         } yield {
           assert(unexpected)(isEmpty) &&
           assert(result)(isNone) // timeout happens
         }
-      } @@ zioTag(interruption) @@ flaky,
+      } @@ zioTag(interruption) @@ withLiveClock @@ nonFlaky(10),
       test("test interruption of infinite async in uninterruptible region") {
         for {
           finalized <- Ref.make(false)

core/jvm-native/src/main/scala/zio/internal/ZScheduler.scala
@@ -58,6 +58,9 @@ private final class ZScheduler(autoBlocking: Boolean) extends Executor { parent 
     supervisor.start()
   }
 
+  override private[zio] def isCurrentThreadInExecutor: Boolean =
+    Thread.currentThread().isInstanceOf[ZScheduler.Worker]
+
   def metrics(implicit unsafe: Unsafe): Option[ExecutionMetrics] = {
     val metrics = new ExecutionMetrics {
       def capacity: Int =

core/shared/src/main/scala/zio/Executor.scala
@@ -89,6 +89,14 @@ abstract class Executor extends ExecutorPlatformSpecific { self =>
 
   private[zio] def stealWork(depth: Int): Boolean =
     false
+
+  /**
+   * Used to identify whether the current thread is part of this executor.
+   * @note
+   *   This method is only ovewritten in the ZScheduler
+   */
+  private[zio] def isCurrentThreadInExecutor: Boolean =
+    false
 }
 
 object Executor extends DefaultExecutors with Serializable {

core/shared/src/main/scala/zio/Fiber.scala
@@ -166,7 +166,7 @@ sealed abstract class Fiber[+E, +A] { self =>
    *   `UIO[Exit, E, A]]`
    */
   final def interrupt(implicit trace: Trace): UIO[Exit[E, A]] =
-    ZIO.fiberIdWith(fiberId => self.interruptAs(fiberId))
+    ZIO.fiberIdWith(self.interruptAs)
 
   /**
    * Interrupts the fiber as if interrupted from the specified fiber. If the
@@ -176,7 +176,7 @@ sealed abstract class Fiber[+E, +A] { self =>
    * @return
    *   `UIO[Exit, E, A]]`
    */
-  final def interruptAs(fiberId: FiberId)(implicit trace: Trace): UIO[Exit[E, A]] =
+  def interruptAs(fiberId: FiberId)(implicit trace: Trace): UIO[Exit[E, A]] =
     self.interruptAsFork(fiberId) *> self.await
 
   /**

core/shared/src/main/scala/zio/ZIO.scala
@@ -1391,8 +1391,10 @@ sealed trait ZIO[-R, +E, +A]
    */
   final def raceFirst[R1 <: R, E1 >: E, A1 >: A](that: => ZIO[R1, E1, A1])(implicit
     trace: Trace
-  ): ZIO[R1, E1, A1] =
-    (self.exit race that.exit).unexit
+  ): ZIO[R1, E1, A1] = {
+    val f = (exit: Exit[E1, A1], loser: Fiber[E1, A1]) => loser.interrupt *> exit
+    self.raceWith(that)(f, f)
+  }
 
   @deprecated("use raceFirst", "2.0.7")
   final def raceFirstAwait[R1 <: R, E1 >: E, A1 >: A](that: => ZIO[R1, E1, A1])(implicit
@@ -1423,19 +1425,14 @@ sealed trait ZIO[-R, +E, +A]
    * with the fibers. It can be considered a low-level building block for
    * higher-level operators like `race`.
    */
-  private final def raceFibersWith[R1 <: R, ER, E2, B, C](right: ZIO[R1, ER, B])(
+  private final def raceFibersWith[R1 <: R, ER, E2, B, C](right: => ZIO[R1, ER, B])(
     leftWins: (Fiber.Runtime[E, A], Fiber.Runtime[ER, B]) => ZIO[R1, E2, C],
     rightWins: (Fiber.Runtime[ER, B], Fiber.Runtime[E, A]) => ZIO[R1, E2, C],
     leftScope: FiberScope = null,
     rightScope: FiberScope = null
   )(implicit trace: Trace): ZIO[R1, E2, C] =
     ZIO.withFiberRuntime[R1, E2, C] { (parentFiber, parentStatus) =>
-      val graft = ZIO.Grafter(parentFiber)
       import java.util.concurrent.atomic.AtomicBoolean
-
-      implicit val unsafe: Unsafe = Unsafe
-
-      val parentRuntimeFlags = parentStatus.runtimeFlags
 
       @inline def complete[E0, E1, A, B](
         winner: Fiber.Runtime[E0, A],
@@ -1443,35 +1440,37 @@ sealed trait ZIO[-R, +E, +A]
         cont: (Fiber.Runtime[E0, A], Fiber.Runtime[E1, B]) => ZIO[R1, E2, C],
         ab: AtomicBoolean,
         cb: ZIO[R1, E2, C] => Any
-      ): Any =
-        if (ab.compareAndSet(true, false)) {
+      ): Unit =
+        if (ab.compareAndSet(false, true)) {
           cb(cont(winner, loser))
         }
 
-      val raceIndicator = new AtomicBoolean(true)
+      val graft    = ZIO.Grafter(parentFiber)
+      val leftEff  = graft.applyOnExit(self)
+      val rightEff = graft.applyOnExit(right)
 
-      val leftFiber  = ZIO.unsafe.makeChildFiber(trace, self, parentFiber, parentRuntimeFlags, leftScope)
-      val rightFiber = ZIO.unsafe.makeChildFiber(trace, right, parentFiber, parentRuntimeFlags, rightScope)
+      val flags      = parentStatus.runtimeFlags
+      val leftFiber  = ZIO.unsafe.makeChildFiber(trace, leftEff, parentFiber, flags, leftScope)(Unsafe)
+      val rightFiber = ZIO.unsafe.makeChildFiber(trace, rightEff, parentFiber, flags, rightScope)(Unsafe)
 
-      val startLeftFiber  = leftFiber.startSuspended()
-      val startRightFiber = rightFiber.startSuspended()
+      ZIO.async[R1, E2, C](
+        { cb =>
+          val raceIndicator = new AtomicBoolean()
 
-      ZIO
-        .async[R1, E2, C](
-          { cb =>
-            leftFiber.addObserver { _ =>
-              complete(leftFiber, rightFiber, leftWins, raceIndicator, cb)
-            }
+          leftFiber.addObserver { _ =>
+            complete(leftFiber, rightFiber, leftWins, raceIndicator, cb)
+          }(Unsafe)
 
-            rightFiber.addObserver { _ =>
-              complete(rightFiber, leftFiber, rightWins, raceIndicator, cb)
-            }
+          rightFiber.addObserver { _ =>
+            complete(rightFiber, leftFiber, rightWins, raceIndicator, cb)
+          }(Unsafe)
 
-            startLeftFiber(graft.applyOnExit(self))
-            startRightFiber(graft.applyOnExit(right))
-          },
-          leftFiber.id <> rightFiber.id
-        )
+          leftFiber.startConcurrently(leftEff)
+          rightFiber.startConcurrently(rightEff)
+          ()
+        },
+        leftFiber.id <> rightFiber.id
+      )
     }
 
   /**
@@ -1488,14 +1487,14 @@ sealed trait ZIO[-R, +E, +A]
         winner.await.flatMap {
           case exit: Exit.Success[?] =>
             winner.inheritAll *> leftDone(exit, loser)
-          case exit: Exit.Failure[_] =>
+          case exit =>
             leftDone(exit, loser)
         },
       (winner, loser) =>
         winner.await.flatMap {
           case exit: Exit.Success[B] =>
             winner.inheritAll *> rightDone(exit, loser)
-          case exit: Exit.Failure[E1] =>
+          case exit =>
             rightDone(exit, loser)
         }
     )

commit 5cae2f0dd8521c69f6bf4b7bff4cbc606a30ca79

Fix FiberRuntime observer memory leak
/fixes #10181

This change fixes a memory leak introduced in #9848 due to flipped boolean.
The change introduced an optimization attempting to avoid removing observers when a fiber is already complete.
Unfortunately, the check was inverted and resulted in `fiber.await` leaking observers.

core/shared/src/main/scala/zio/internal/FiberRuntime.scala
@@ -1606,7 +1606,7 @@ final class FiberRuntime[E, A](fiberId: FiberId.Runtime, fiberRefs0: FiberRefs, 
         self.getFiberRefs()
 
       def removeObserver(observer: Exit[E, A] => Unit)(implicit unsafe: Unsafe): Unit =
-        if (self._exitValue ne null)
+        if (self._exitValue eq null)
           self.tell(FiberMessage.Stateful(_.asInstanceOf[FiberRuntime[E, A]].removeObserver(observer)))
 
       def poll(implicit unsafe: Unsafe): Option[Exit[E, A]] =

core-tests/shared/src/test/scala/zio/FiberRuntimeSpec.scala
@@ -66,6 +66,24 @@ object FiberRuntimeSpec extends ZIOBaseSpec {
         }
       }
     ),
+    suite("observers")(
+      test("removed on uncompleted fiber - bug #10181") {
+        val observerCalled = Ref.unsafe.make(false)
+        for {
+          p     <- Promise.make[Nothing, Unit]
+          fiber <- ZIO.never.interruptible.onInterrupt(_ => p.succeedUnit).fork.uninterruptible
+          // Add an observer, then remove it while fiber is still running
+          _ <- ZIO.succeed {
+                 val obs: Exit[Nothing, Unit] => Unit = _ => observerCalled.unsafe.set(true)
+                 fiber.unsafe.addObserver(obs)
+                 fiber.unsafe.removeObserver(obs)
+               }
+          _      <- fiber.interruptAsFork(FiberId.None)
+          _      <- p.await
+          called <- observerCalled.get
+        } yield assertTrue(!called)
+      } @@ nonFlaky(100) @@ timeout(10.seconds)
+    ),
     suite("async")(
       test("async callback after interruption is ignored") {
         ZIO.suspendSucceed {