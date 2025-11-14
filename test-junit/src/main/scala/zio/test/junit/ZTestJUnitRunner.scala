7f4a8bbff781468613dce8efe5071b4edf08bab1
kyri-petrou <67301607+kyri-petrou@users.noreply.github.com>
2025-11-14 05:34:33 +0200

    fmt

--- test-junit/src/main/scala/zio/test/junit/ZTestJUnitRunner.scala
+++ test-junit/src/main/scala/zio/test/junit/ZTestJUnitRunner.scala
@@ -97,7 +97,8 @@ class ZTestJUnitRunner(klass: Class[_]) extends Runner with Filterable {
       spec
         .runSpecAsApp(instrumented, TestArgs.empty, Console.ConsoleLive)
         .provide(
-          Scope.default >>> (liveEnvironment >>> (TestEnvironment.live <*> ZLayer.environment[Scope]) +!+ spec.bootstrap)
+          Scope.default >>> (liveEnvironment >>> (TestEnvironment.live <*> ZLayer
+            .environment[Scope]) +!+ spec.bootstrap)
         )
     }(Trace.empty, Unsafe.unsafe).getOrThrowFiberFailure()(Unsafe.unsafe)
   }
