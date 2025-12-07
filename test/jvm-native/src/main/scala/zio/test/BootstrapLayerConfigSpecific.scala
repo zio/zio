599bb9675c958dc68ecbe44fffb2b92743a763ec
Vittorio Parrella <vittorioparrella+1@gmail.com>
2025-12-07 12:40:21 +0100

    applied formatting

--- test/jvm-native/src/main/scala/zio/test/BootstrapLayerConfigSpecific.scala
+++ test/jvm-native/src/main/scala/zio/test/BootstrapLayerConfigSpecific.scala
@@ -6,42 +6,40 @@ import zio.internal.stacktracer.Tracer
 /**
  * Bootstrap layer that supports executor override via system property.
  *
- * Reads the `zio.test.executor` system property to determine which executor to use.
+ * Reads the `zio.test.executor` system property to determine which executor to
+ * use.
  */
 private[test] object BootstrapLayerConfigSpecific {
 
   def make: ZLayer[Any, Any, TestEnvironment] =
     TestExecutorConfig.fromSystemProperty match {
-      case Some(TestExecutorConfig.ExecutorType.Default) =>
-        {
-          implicit val trace: Trace = Tracer.newTrace
-          Runtime.setExecutor(Unsafe.unsafe { implicit unsafe => Executor.makeDefault(true) }) >>> testEnvironment
-        }
-      case Some(TestExecutorConfig.ExecutorType.ZScheduler) | None =>
-        {
-          implicit val trace: Trace = Tracer.newTrace
-          Runtime.setExecutor(Unsafe.unsafe { implicit unsafe => Executor.makeDefault(false) }) >>> testEnvironment
-        }
-      case Some(TestExecutorConfig.ExecutorType.NIO) =>
-        {
-          implicit val trace: Trace = Tracer.newTrace
-          val nioExecutorLayer = ZLayer.scoped {
-            zio.internal.NIOExecutor.live.build.map(_.get).flatMap { executor =>
-              ZIO.addFinalizer(ZIO.succeedBlocking(executor.shutdown())).as(executor)
-            }
+      case Some(TestExecutorConfig.ExecutorType.Default) => {
+        implicit val trace: Trace = Tracer.newTrace
+        Runtime.setExecutor(Unsafe.unsafe(implicit unsafe => Executor.makeDefault(true))) >>> testEnvironment
+      }
+      case Some(TestExecutorConfig.ExecutorType.ZScheduler) | None => {
+        implicit val trace: Trace = Tracer.newTrace
+        Runtime.setExecutor(Unsafe.unsafe(implicit unsafe => Executor.makeDefault(false))) >>> testEnvironment
+      }
+      case Some(TestExecutorConfig.ExecutorType.NIO) => {
+        implicit val trace: Trace = Tracer.newTrace
+        val nioExecutorLayer = ZLayer.scoped {
+          zio.internal.NIOExecutor.live.build.map(_.get).flatMap { executor =>
+            ZIO.addFinalizer(ZIO.succeedBlocking(executor.shutdown())).as(executor)
           }
-          val nioBootstrap = nioExecutorLayer.flatMap { env =>
-            val executor = env.get
-            val runtimeConfigLayer = Runtime.setExecutor(executor)
-            val dependencies: ZLayer[Any, Nothing, Clock with Console with System with Random] =
-              (ZLayer.succeed(executor) >>> zio.NIOClock.live) ++
-                ZLayer.succeed(Console.ConsoleLive) ++
-                ZLayer.succeed(System.SystemLive) ++
-                ZLayer.succeed(Random.RandomLive)
-            val fullProvider = runtimeConfigLayer >>> dependencies
-            fullProvider >>> TestEnvironment.live
-          }
-          nioBootstrap
         }
+        val nioBootstrap = nioExecutorLayer.flatMap { env =>
+          val executor           = env.get
+          val runtimeConfigLayer = Runtime.setExecutor(executor)
+          val dependencies: ZLayer[Any, Nothing, Clock with Console with System with Random] =
+            (ZLayer.succeed(executor) >>> zio.NIOClock.live) ++
+              ZLayer.succeed(Console.ConsoleLive) ++
+              ZLayer.succeed(System.SystemLive) ++
+              ZLayer.succeed(Random.RandomLive)
+          val fullProvider = runtimeConfigLayer >>> dependencies
+          fullProvider >>> TestEnvironment.live
+        }
+        nioBootstrap
+      }
     }
 }
--- test/jvm-native/src/main/scala/zio/test/TestExecutorConfig.scala
+++ test/jvm-native/src/main/scala/zio/test/TestExecutorConfig.scala
@@ -7,35 +7,38 @@ import zio.internal.NIOExecutor
  * Configuration utility for overriding the test executor via system property.
  *
  * Supports the following values for `zio.test.executor`:
- *  - `default` - Standard ZIO default executor
- *  - `zscheduler` - ZScheduler executor (current default)
- *  - `nio` - NIOExecutor for testing NIO functionality
+ *   - `default` - Standard ZIO default executor
+ *   - `zscheduler` - ZScheduler executor (current default)
+ *   - `nio` - NIOExecutor for testing NIO functionality
  *
  * @example
- * {{{
+ *   {{{
  * sbt "testOnly * -Dzio.test.executor=nio"
- * }}}
+ *   }}}
  */
 object TestExecutorConfig {
 
   sealed trait ExecutorType
   object ExecutorType {
-    case object Default extends ExecutorType
+    case object Default    extends ExecutorType
     case object ZScheduler extends ExecutorType
-    case object NIO extends ExecutorType
+    case object NIO        extends ExecutorType
   }
 
   /**
-   * Reads the `zio.test.executor` system property and returns the corresponding executor type.
+   * Reads the `zio.test.executor` system property and returns the corresponding
+   * executor type.
    *
-   * @return Some(ExecutorType) if property is set, None otherwise
-   * @throws IllegalArgumentException if the property value is not recognized
+   * @return
+   *   Some(ExecutorType) if property is set, None otherwise
+   * @throws IllegalArgumentException
+   *   if the property value is not recognized
    */
   def fromSystemProperty: Option[ExecutorType] =
     sys.props.get("zio.test.executor").map {
-      case "default" => ExecutorType.Default
+      case "default"    => ExecutorType.Default
       case "zscheduler" => ExecutorType.ZScheduler
-      case "nio" => ExecutorType.NIO
+      case "nio"        => ExecutorType.NIO
       case unknown =>
         throw new IllegalArgumentException(
           s"Unknown executor type: '$unknown'. Supported values: default, zscheduler, nio"
