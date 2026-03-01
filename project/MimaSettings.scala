9f72b10adfea19a49bc397bf6db14bf1b42bb788
Tianlin0725 <tele.he@gmail.com>
2026-03-02 01:19:34 +0800

    build: add MiMa exclusion for ZStream.buffer binary compatibility
    The buffer method implementation was changed to fix issue #9810,
    but the method signature remains the same. Adding MiMa exclusion
    to allow the behavioral fix while maintaining binary compatibility.

--- project/MimaSettings.scala
+++ project/MimaSettings.scala
@@ -14,6 +14,7 @@ object MimaSettings {
         exclude[Problem]("zio.stm.ZSTM#internal*"),
         exclude[Problem]("zio.stm.ZSTM$internal*"),
         exclude[Problem]("zio.stream.internal*"),
+        exclude[IncompatibleMethTypeProblem]("zio.stream.ZStream.buffer"),
         exclude[IncompatibleResultTypeProblem]("zio.stm.TRef.todo"),
         exclude[DirectMissingMethodProblem]("zio.stm.TRef.versioned_="),
         exclude[IncompatibleResultTypeProblem]("zio.stm.TRef.versioned"),
