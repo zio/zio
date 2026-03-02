659fab23fd137fa0c5e539472e3edee5a8232e71
Tianlin0725 <tele.he@gmail.com>
2026-03-02 21:27:52 +0800

    fix: restore correct ZStream buffer test files from original implementation

--- streams-tests/shared/src/test/scala/zio/stream/ZStreamBufferRegressionSpec.scala
+++ streams-tests/shared/src/test/scala/zio/stream/ZStreamBufferRegressionSpec.scala
@@ -5,8 +5,8 @@ import zio.test._
 import zio.test.Assertion._
 
 /**
- * Regression test for issue #9810 Verifies that buffer(1) only buffers 1
- * element, not 2
+ * Regression test for issue #9810
+ * Verifies that buffer(1) only buffers 1 element, not 2
  */
 object ZStreamBufferRegressionSpec extends ZIOSpecDefault {
 
@@ -15,55 +15,57 @@ object ZStreamBufferRegressionSpec extends ZIOSpecDefault {
       for {
         started <- Ref.make(Chunk.empty[Int])
         stream = ZStream
-                   .fromIterable(1 to 3)
-                   .mapZIO(i => started.update(_ :+ i).as(i))
-                   .buffer(1)
-
+          .fromIterable(1 to 3)
+          .mapZIO(i => started.update(_ :+ i).as(i))
+          .buffer(1)
+        
         // Pull first element and check how many have started
         startedAfterFirst <- ZIO.scoped {
-                               stream.toPull.flatMap { pull =>
-                                 for {
-                                   _ <- pull.map(_.head).catchAll(_ => ZIO.dieMessage("Unexpected end of stream"))
-                                   _ <- TestClock.adjust(50.millis)
-                                   s <- started.get
-                                 } yield s
-                               }
-                             }
+          stream.toPull.flatMap { pull =>
+            for {
+              _ <- pull.map(_.head).catchAll(_ => ZIO.dieMessage("Unexpected end of stream"))
+              _ <- TestClock.adjust(50.millis)
+              s <- started.get
+            } yield s
+          }
+        }
       } yield assert(startedAfterFirst.toList)(equalTo(List(1, 2)))
     } @@ TestAspect.timeout(5.seconds),
+    
     test("buffer(2) does not run more than two elements ahead") {
       for {
         started <- Ref.make(Chunk.empty[Int])
         stream = ZStream
-                   .fromIterable(1 to 4)
-                   .mapZIO(i => started.update(_ :+ i).as(i))
-                   .buffer(2)
-
+          .fromIterable(1 to 4)
+          .mapZIO(i => started.update(_ :+ i).as(i))
+          .buffer(2)
+        
         startedAfterFirst <- ZIO.scoped {
-                               stream.toPull.flatMap { pull =>
-                                 for {
-                                   _ <- pull.map(_.head).catchAll(_ => ZIO.dieMessage("Unexpected end of stream"))
-                                   _ <- TestClock.adjust(50.millis)
-                                   s <- started.get
-                                 } yield s
-                               }
-                             }
+          stream.toPull.flatMap { pull =>
+            for {
+              _ <- pull.map(_.head).catchAll(_ => ZIO.dieMessage("Unexpected end of stream"))
+              _ <- TestClock.adjust(50.millis)
+              s <- started.get
+            } yield s
+          }
+        }
       } yield assert(startedAfterFirst.toList)(equalTo(List(1, 2, 3)))
     } @@ TestAspect.timeout(5.seconds),
+    
     test("buffer(1) with slow consumer - producer should not race ahead") {
       for {
         processed <- Ref.make(0)
         fiber <- ZStream
-                   .fromIterable(1 to 100)
-                   .tap(_ => processed.update(_ + 1))
-                   .buffer(1)
-                   .runForeach(_ => ZIO.sleep(1.hour))
-                   .fork
-
+          .fromIterable(1 to 100)
+          .tap(_ => processed.update(_ + 1))
+          .buffer(1)
+          .runForeach(_ => ZIO.sleep(1.hour))
+          .fork
+        
         // Wait a bit and check how many were processed
-        _     <- TestClock.adjust(1.second)
+        _ <- TestClock.adjust(1.second)
         count <- processed.get
-        _     <- fiber.interrupt
+        _ <- fiber.interrupt
       } yield assertTrue(count <= 2) // At most 2: 1 in buffer + 1 being processed
     }
   )
