98dc84a04a7fc29e3abd3ba61622d8dc9023f921
WWEqqq870 <h59146083@gmail.com>
2026-03-19 13:10:28 +0200

    add: performance benchmark for single permit semaphore

---
+++ benchmarks/src/main/scala/zio/SinglePermitSemaphoreBenchmark.scala
@@ -0,0 +1,26 @@
+package zio
+
+import org.openjdk.jmh.annotations._
+import java.util.concurrent.TimeUnit
+
+@State(Scope.Thread)
+@BenchmarkMode(Array(Mode.Throughput))
+@OutputTimeUnit(TimeUnit.SECONDS)
+class SinglePermitSemaphoreBenchmark {
+
+  var semaphore: Semaphore = _
+
+  @Setup
+  def setup(): Unit = {
+    semaphore = Unsafe.unsafe(implicit u => 
+      Runtime.default.unsafe.run(Semaphore.make(1)).getOrThrow()
+    )
+  }
+
+  @Benchmark
+  def zioSemaphore(): Unit = {
+    Unsafe.unsafe { implicit u =>
+      Runtime.default.unsafe.run(semaphore.withPermit(ZIO.unit)).getOrThrow()
+    }
+  }
+}
