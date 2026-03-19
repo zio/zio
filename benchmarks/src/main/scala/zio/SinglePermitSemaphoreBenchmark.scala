451e1e19b5d0199424c47ca1d68d331055023392
WWEqqq870 <h59146083@gmail.com>
2026-03-19 16:01:10 +0200

    fix: use fully qualified name for JMH Scope

--- benchmarks/src/main/scala/zio/SinglePermitSemaphoreBenchmark.scala
+++ benchmarks/src/main/scala/zio/SinglePermitSemaphoreBenchmark.scala
@@ -3,20 +3,21 @@ package zio
 import org.openjdk.jmh.annotations._
 import java.util.concurrent.TimeUnit
 
-@State(Scope.Thread)
+@State(org.openjdk.jmh.annotations.Scope.Thread)
 @BenchmarkMode(Array(Mode.Throughput))
 @OutputTimeUnit(TimeUnit.SECONDS)
 class SinglePermitSemaphoreBenchmark {
-
   var semaphore: Semaphore = _
 
   @Setup
-  def setup(): Unit =
-    semaphore = Unsafe.unsafe(implicit u => Runtime.default.unsafe.run(Semaphore.make(1)).getOrThrow())
+  def setup(): Unit = {
+    implicit val unsafe: Unsafe = Unsafe.unsafe
+    semaphore = Runtime.default.unsafe.run(Semaphore.make(1L)).getOrThrow()
+  }
 
   @Benchmark
-  def zioSemaphore(): Unit =
-    Unsafe.unsafe { implicit u =>
-      Runtime.default.unsafe.run(semaphore.withPermit(ZIO.unit)).getOrThrow()
-    }
+  def zioSemaphore(): Unit = {
+    implicit val unsafe: Unsafe = Unsafe.unsafe
+    Runtime.default.unsafe.run(semaphore.withPermit(ZIO.unit)).getOrThrow()
+  }
 }
