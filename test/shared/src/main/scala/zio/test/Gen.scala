2e63b2cc520241d9d0acf26178bac39225ce816d
business123655 <thulasibalan.official@gmail.com>
2026-03-31 00:14:12 +0530

    Fix deterministic bug in Gen.fromIterable (#9101)

--- test/shared/src/main/scala/zio/test/Gen.scala
+++ test/shared/src/main/scala/zio/test/Gen.scala
@@ -442,11 +442,27 @@ object Gen extends GenZIO with FunctionVariants with TimeVariants {
    * Constructs a deterministic generator that only generates the specified
    * fixed values.
    */
-  def fromIterable[R, A](
-    as: Iterable[A],
-    shrinker: A => ZStream[R, Nothing, A] = defaultShrinker
-  )(implicit trace: Trace): Gen[R, A] =
-    Gen(ZStream.fromIterable(as).map(a => Sample.unfold(a)(a => (a, shrinker(a)))))
+    def fromIterable[R, A](as: Iterable[A])(implicit trace: Trace): Gen[R, A] =
+    Gen {
+      ZIO.serviceWithZIO[TestConfig] { config =>
+        if (as.isEmpty)
+          ZStream.empty
+        else
+          ZStream.succeed {
+            val iterator = as.iterator
+            Sample.unfold[R, Iterator[A], A](iterator) { it =>
+              if (it.hasNext) {
+                val nextValue = it.next()
+                (nextValue, it)
+              } else {
+                val newIt = as.iterator
+                (newIt.next(), newIt)
+              }
+            }
+          }
+      }
+    }
+  
 
   /**
    * Constructs a generator from a function that uses randomness. The returned
