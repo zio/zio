abe8feed51c618df9fc360197f42f4704296529a
Farid Ismailov <faridliners@gmail.com>
2026-04-19 12:46:55 +0300

    Fix checkN resampling with effectful generators

--- test/shared/src/main/scala/zio/test/package.scala
+++ test/shared/src/main/scala/zio/test/package.scala
@@ -398,7 +398,9 @@ package object test extends CompileVariants {
     sourceLocation: SourceLocation,
     trace: Trace
   ): ZIO[checkConstructor.OutEnvironment, checkConstructor.OutError, TestResult] =
-    TestConfig.samples.flatMap(n => checkStream(rv.sample.forever.take(n.toLong))(a => checkConstructor(test(a))))
+    TestConfig.samples.flatMap(n =>
+      checkStream(ZStream.repeatZIO(rv.sample.runHead).collectSome.take(n.toLong))(a => checkConstructor(test(a)))
+    )
 
   /**
    * A version of `check` that accepts two random variables.
@@ -800,7 +802,9 @@ package object test extends CompileVariants {
     trace: Trace
   ): ZIO[checkConstructor.OutEnvironment, checkConstructor.OutError, TestResult] =
     TestConfig.samples.flatMap(n =>
-      checkStreamPar(rv.sample.forever.take(n.toLong), parallelism)(a => checkConstructor(test(a)))
+      checkStreamPar(ZStream.repeatZIO(rv.sample.runHead).collectSome.take(n.toLong), parallelism)(a =>
+        checkConstructor(test(a))
+      )
     )
 
   /**
@@ -1009,7 +1013,7 @@ package object test extends CompileVariants {
         checkConstructor: CheckConstructor[R, In],
         trace: Trace
       ): ZIO[checkConstructor.OutEnvironment, checkConstructor.OutError, TestResult] =
-        checkStream(rv.sample.forever.take(n.toLong))(a => checkConstructor(test(a)))
+        checkStream(ZStream.repeatZIO(rv.sample.runHead).collectSome.take(n.toLong))(a => checkConstructor(test(a)))
       def apply[R <: ZAny, A, B, In](rv1: Gen[R, A], rv2: Gen[R, B])(
         test: (A, B) => In
       )(implicit
