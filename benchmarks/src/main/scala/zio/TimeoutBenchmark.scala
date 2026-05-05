6ebd3eb3bfcec9b2bd43f72b7f915a3efff1933f
Eyal Farago <efarago@salesforce.com>
2026-05-05 08:46:52 +0000

    feat: optimize ZIO.timeoutTo by eliminating one fiber via direct scheduler use
    Original implementation from @eyalfa PRs #9261 / #9266 (both closed by author).
    
    Squashed and rebased onto series/2.x. Test changes from eyalfa diff were dropped in favor of upstream-evolved ZIOSpec.
    
    Co-authored-by: Eyal Farago <efarago@salesforce.com>

---
+++ benchmarks/src/main/scala/zio/TimeoutBenchmark.scala
@@ -0,0 +1,48 @@
+package zio
+
+import org.openjdk.jmh.annotations.{Benchmark, BenchmarkMode, Mode, OutputTimeUnit, Param, State, Scope => JScope}
+
+import java.util.concurrent.TimeUnit
+import scala.concurrent.TimeoutException
+
+@State(JScope.Thread)
+@BenchmarkMode(Array(Mode.Throughput))
+@OutputTimeUnit(TimeUnit.SECONDS)
+class TimeoutBenchmark {
+  import BenchmarkUtil.unsafeRun
+
+  @Param(Array("0", "100", "10000"))
+  var n: Int = _
+
+  val effect = ZIO.foldLeft(0 until n)(0) { case (prev, x) =>
+    ZIO.succeed(prev + x)
+  }
+
+  @Benchmark
+  def zioBaseline = {
+    val _ = unsafeRun {
+      effect
+    }
+  }
+
+  @Benchmark
+  def zioTimeoutAlt = {
+    val _ = unsafeRun {
+      effect
+        .timeoutTo(ZIO.fail(new TimeoutException))
+        .apply(ZIO.succeed(_))(100.minutes)
+        .flatten
+    }
+  }
+
+  @Benchmark
+  def zioTimeoutOrig = {
+    val _ = unsafeRun {
+      effect
+        .timeoutTo(ZIO.fail(new TimeoutException))
+        .applyOrig(ZIO.succeed(_))(100.minutes)
+        .flatten
+    }
+  }
+
+}
