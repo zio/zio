e7a8e43281d93fcdbf9e3491d4ac0ef72f7b0b25
Indicareaper25 <kyle.barron2014@gmail.com>
2026-05-11 23:23:55 -0400

    Modify ZScheduler to include default argument for queue
    Added 'null' as the default argument for the custom ZIO queue in ZScheduler.

--- core/jvm-native/src/main/scala/zio/internal/ZScheduler.scala
+++ core/jvm-native/src/main/scala/zio/internal/ZScheduler.scala
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -125,7 +125,8 @@ private final class ZScheduler(autoBlocking: Boolean) extends Executor { parent 
         val targetWorker = workers(targetId)
         // Ensure we don't steal from ourselves
         if (targetWorker ne currentWorker) { 
-          val runnable = targetWorker.localQueue.poll()
+          // FIX: Added 'null' as the default argument for the custom ZIO queue
+          val runnable = targetWorker.localQueue.poll(null)
           if (runnable ne null) {
             currentWorker.localQueue.offer(runnable)
             stolen = true
