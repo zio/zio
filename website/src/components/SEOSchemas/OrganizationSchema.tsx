f2a98fcb4e6e185c936871557c2dc4d37f7224c3
Milad Khajavi <khajavi@gmail.com>
2026-05-29 13:53:02 +0330

    feat: add Organization and Website JSON-LD schema component

---
+++ website/src/components/SEOSchemas/OrganizationSchema.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+export default function OrganizationSchema() {
+  const organizationSchema = {
+    '@context': 'https://schema.org',
+    '@type': 'Organization',
+    name: 'ZIO',
+    url: 'https://zio.dev',
+    logo: 'https://zio.dev/img/zio.png',
+    description:
+      'Type-safe, composable asynchronous and concurrent programming for Scala',
+    sameAs: [
+      'https://github.com/zio/zio',
+      'https://twitter.com/zioscala',
+      'https://discord.gg/2ccFBr4',
+    ],
+    contactPoint: {
+      '@type': 'ContactPoint',
+      contactType: 'Community Support',
+      url: 'https://discord.gg/2ccFBr4',
+    },
+  };
+
+  const websiteSchema = {
+    '@context': 'https://schema.org',
+    '@type': 'WebSite',
+    url: 'https://zio.dev',
+    potentialAction: {
+      '@type': 'SearchAction',
+      target: {
+        '@type': 'EntryPoint',
+        urlTemplate: 'https://zio.dev/search?q={search_term_string}',
+      },
+      'query-input': 'required name=search_term_string',
+    },
+  };
+
+  return (
+    <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(organizationSchema) }}
+      />
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(websiteSchema) }}
+      />
+    </>
+  );
+}
