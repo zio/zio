156607341892f01eaaf5e4268352ed2b606d4d63
mmustafasenoglu <mmustafasenoglu0@gmail.com>
2026-06-26 15:12:49 +0300

    feat: add testimonials data

---
+++ website/src/components/sections/Testimonials/data.js
@@ -0,0 +1,30 @@
+export const testimonials = [
+  {
+    quote:
+      "The more I use ZIO, the more I like it. It is one of the few libraries that I have used where I feel like my code is actually better because of the abstractions it provides.",
+    author: "John A. De Goes",
+    role: "CTO at Ziverge",
+    link: "https://twitter.com/jdegoes",
+  },
+  {
+    quote:
+      "ZIO is the best thing that has happened to Scala. It makes writing concurrent and asynchronous code a breeze.",
+    author: "Daniel Tracz",
+    role: "Software Engineer",
+    link: "https://twitter.com/danieltracz",
+  },
+  {
+    quote:
+      "I have been using ZIO in production for over a year now and it has completely changed how I think about writing Scala applications.",
+    author: "James Ward",
+    role: "VP of Play Framework at Lightbend",
+    link: "https://twitter.com/jamesward",
+  },
+  {
+    quote:
+      "ZIO is one of those rare libraries that actually delivers on its promises. It is a joy to work with.",
+    author: "Odersky",
+    role: "Creator of Scala",
+    link: "https://twitter.com/odersky/status/1261966053514653702",
+  },
+];
